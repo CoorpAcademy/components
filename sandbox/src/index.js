2e58189d31ced585bcb61285ac225c825752be4d
godu <arthur.weber@viacesi.fr>
2016-01-07 14:41:14 +0100

    Fixes HMR

--- sandbox/src/index.js
+++ sandbox/src/index.js
@@ -1,32 +1,26 @@
+
+import diff from 'virtual-dom/diff';
+import patch from 'virtual-dom/patch';
+import createElement from 'virtual-dom/create-element';
+
 import App from './App';
-// import App from '../../example/app-using-virtual-dom';
-// import App from '../../example/app-no-deps';
 
-var diff = require('virtual-dom/diff');
-var patch = require('virtual-dom/patch');
-var createElement = require('virtual-dom/create-element');
-
-// 1: Create a function that declares what the DOM should look like
-function render(state)  {
-  return App(state);
-}
-
-// 2: Initialise the document
-var state = {
+const state = {
   question: {
     title: 'Quel réseau social se distingue par ses messages instantanés limités à 140 signes ?'
   }
 };
 
-var tree = render(state);               // We need an initial tree
-var rootNode = createElement(tree);     // Create an initial root DOM node ...
-document.body.appendChild(rootNode);    // ... and it should be in the document
+let tree = App(state);
+const rootNode = createElement(tree);
+document.getElementById('app').appendChild(rootNode);
 
 if(module.hot) {
-    module.hot.accept("./App.js", function() {
-      var newTree = render(state);
-      var patches = diff(tree, newTree);
-      rootNode = patch(rootNode, patches);
-      tree = newTree;
-    });
+  module.hot.accept("./App.js", () => {
+    const App = require('./App').default;
+    const newTree = App(state);
+    const patches = diff(tree, newTree);
+    patch(rootNode, patches);
+    tree = newTree;
+  });
 }
