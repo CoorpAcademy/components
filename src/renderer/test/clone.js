f9d45fb32d489ea7b1a949e94b48601f084c216c
chrisdugne <chris.dugne@uralys.com>
2016-03-04 15:29:42 +0100

    small enhancement on testing
    Signed-off-by: chrisdugne <chris.dugne@uralys.com>

--- src/renderer/test/clone.js
+++ src/renderer/test/clone.js
@@ -6,47 +6,55 @@ import virtualDom from '../virtual-dom';
 
 const rendererTest = ({h, clone}, name) => {
   test(`${name}: should clone element`, t => {
-    const node = h('foo', {bar: 'baz'}, ['quz']);
+    const node = h('foo', {bar: 'baz'}, ['qux']);
     const cloneNode = clone(node);
 
     t.not(cloneNode, node);
-    t.same(cloneNode, h('foo', {bar: 'baz'}, ['quz']));
+    t.same(cloneNode, h('foo', {bar: 'baz'}, ['qux']));
   });
 
   test(`${name}: should clone element and replace property`, t => {
-    const node = h('foo', {bar: 'baz'}, ['quz']);
-    const cloneNode = clone(node, {bar: 'quz'});
+    const node = h('foo', {bar: 'baz'}, ['qux']);
+    const cloneNode = clone(node, {bar: 'qux'});
 
-    t.same(cloneNode, h('foo', {bar: 'quz'}, ['quz']));
+    t.same(cloneNode, h('foo', {bar: 'qux'}, ['qux']));
+  });
+
+  test(`${name}: should clone element and replace deep property`, t => {
+    const node = h('h1', {style:{color:'grey'}}, ['qux']);
+    const pink = {style:{color:'pink'}};
+    const cloneNode = clone(node, pink);
+
+    t.same(cloneNode, h('h1', pink, ['qux']));
   });
 
   test(`${name}: should clone element and add property`, t => {
-    const node = h('foo', {bar: 'baz'}, ['quz']);
+    const node = h('foo', {bar: 'baz'}, ['qux']);
     const cloneNode = clone(node, {baz: 'baz'});
 
     t.same(cloneNode, h('foo', {
       bar: 'baz',
       baz: 'baz'
-    }, ['quz']));
+    }, ['qux']));
   });
 
   test(`${name}: should clone element and add deep property`, t => {
-    const node = h('foo', {bar: {baz: 'baz'}}, ['quz']);
-    const cloneNode = clone(node, {bar: {quz: 'quz'}});
+    const node = h('foo', {bar: {baz: 'baz'}}, ['qux']);
+    const cloneNode = clone(node, {bar: {qux: 'qux'}});
 
     t.same(cloneNode, h('foo', {
       bar: {
         baz: 'baz',
-        quz: 'quz'
+        qux: 'qux'
       }
-    }, ['quz']));
+    }, ['qux']));
   });
 
   test(`${name}: should clone element and replace children`, t => {
-    const node = h('foo', {bar: 'baz'}, ['quz']);
-    const cloneNode = clone(node, null, ['quuz']);
+    const node = h('foo', {bar: 'baz'}, ['qux']);
+    const cloneNode = clone(node, null, ['quux']);
 
-    t.same(cloneNode, h('foo', {bar: 'baz'}, ['quuz']));
+    t.same(cloneNode, h('foo', {bar: 'baz'}, ['quux']));
   });
 };
 
