8f7373e7774e8440285a9adf5a1d8ea354c29c3e
chrisdugne <chris.dugne@uralys.com>
2016-03-15 19:00:28 +0100

    #35 used data-attributes for virtual-dom + enhanced molecule.moduleBubble

--- src/design/molecule/module-bubble/index.js
+++ src/design/molecule/module-bubble/index.js
@@ -2,17 +2,20 @@ import style from './module-bubble.css'
 
 export default (renderer, skin) => (props) => {
   const {h} = renderer;
-
-  console.log('module-bubble props : ', props, props.status);
+  const iconCode = String.fromCharCode(skin.icons[props.status]);
+  const inverted = props.inverted === 'true';
+  const className = inverted ? style.inverted : style.default;
 
   return (
     <div>
-      <span className={style[props.status]}
+      <span className={className}
+            attributes={{
+              'data-icon': iconCode
+            }}
             style={{
-              background: skin && skin.module[props.status]
+              background: skin && skin.mod[props.status]
             }}
       >
-        {props.status}
       </span>
       <span class="disciplineTreeItemProgressionText">
         label-todo
