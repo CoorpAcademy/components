8fa9468e484acdac905ae5a326d0f7464fb400ac
chrisdugne <chris.dugne@uralys.com>
2016-03-25 18:10:41 +0100

    #35 cards BG

--- src/design/organism/discipline-cards/index.js
+++ src/design/organism/discipline-cards/index.js
@@ -3,6 +3,7 @@ import createDisciplineCard from '../discipline-card';
 
 export default (engine, options) => (props) => {
   const {h} = engine;
+  const {skin} = options;
 
   const DisciplineCard = createDisciplineCard(engine, options);
 
@@ -15,8 +16,17 @@ export default (engine, options) => (props) => {
     );
   });
 
+  const skinBackgroundUrl = skin && skin.images && skin.images.tree;
+  const skinBG = ( skin && skin.theme && skin.theme.background ) || '#123';
+  const bg = skinBackgroundUrl ? 'url(' + skinBackgroundUrl + ')' : skinBG;
+
   return (
-    <div className={style.default}>
+    <div
+      className={style.default}
+      style={{
+        background: bg
+      }}
+    >
       {disciplines}
     </div>
   );
