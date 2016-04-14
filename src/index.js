102559adec4a7d8df924726795150af05717bfed
esa-coorp <eric.sampaio@coorpacademy.com>
2016-04-14 09:46:34 +0200

    publish discipline-card component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import createSlide from './design/organism/slide';
 import createModuleBubble from './design/molecule/module-bubble';
 import createDisciplineCards from './design/organism/discipline-cards';
+import createDisciplineCard from './design/organism/discipline-card';
 
 export {
   createSlide,
   createModuleBubble,
-  createDisciplineCards
+  createDisciplineCards,
+  createDisciplineCard
 };
