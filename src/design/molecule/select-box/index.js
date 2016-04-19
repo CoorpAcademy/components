2d52fd72ed49b0b4a202405e2d247989b16e54de
chrisdugne <chris.dugne@uralys.com>
2016-04-19 14:15:04 +0200

    [#79] added a theme for select-box

--- src/design/molecule/select-box/index.js
+++ src/design/molecule/select-box/index.js
@@ -1,11 +1,39 @@
 import style from './select-box.css';
 
+const spanInline = (theme, skin) => {
+  let color;
+
+  switch(theme){
+  case 'plain':
+    color = skin && skin.theme.lock;
+    break;
+  default:
+    color = skin && skin.texts.inverted;
+  }
+
+  return {
+    borderColor: color
+  };
+};
+
+const selectInline = (theme, skin) => {
+  switch(theme){
+  case 'plain':
+    return {
+      color: skin && skin.texts.normal,
+      backgroundColor: skin && skin.backgrounds.input
+    };
+  default:
+    return {};
+  }
+};
+
 export default (engine, options) => {
   const {h} = engine;
   const {skin} = options;
 
   return (props) => {
-    const {list, current, onChange} = props;
+    const {list, current, onChange, theme = 'default'} = props;
     const iconCode = String.fromCharCode(skin.icons.select);
 
     const selectOptions = list.map(function(item){
@@ -21,12 +49,16 @@ export default (engine, options) => {
     });
 
     return (
-      <span className={style.default}
-            attributes={{
-              'data-icon': iconCode
-            }}>
-        <select className={style.selector}
-                onChange={onChange}
+      <span
+        className={style[theme]}
+        style={spanInline(theme, skin)}
+        attributes={{
+          'data-icon': iconCode
+        }}
+      >
+        <select
+          onChange={onChange}
+          style={selectInline(theme, skin)}
         >
           {selectOptions}
         </select>
