658d99c5ef3f69dd8cf993b8869cd78f69938f24
chrisdugne <chris.dugne@uralys.com>
2016-07-28 17:59:32 +0200

    using media queries to show/hide inline images

--- src/components/molecule/theme-image/index.js
+++ src/components/molecule/theme-image/index.js
@@ -1,4 +1,5 @@
 import get from 'lodash/fp/get';
+import getOr from 'lodash/fp/getOr';
 import { checker, createValidate } from '../../../util/validation';
 import style from './style.css';
 
@@ -14,20 +15,44 @@ export default (engine, options) => {
   const {skin} = options;
 
   const ThemeImage = props => {
-    const bg = get(`images[${props.image}]`, skin);
+    const desktop = get(`images[${props.image}]`, skin);
+    const retina = getOr(desktop, `images[${props.image}-retina]`, skin);
+    const mobile = getOr(desktop, `images[${props.image}-mobile]`, skin);
     const height = get(`sizes[${props.image}].height`, skin);
     const margin = get(`sizes[${props.image}].margin`, skin);
 
     const styleOptions = {
-      backgroundImage: `url(${bg})`,
       height,
       margin
     };
 
-    return <div
-      className={style.default}
-      style={styleOptions}
-    />;
+    return (
+      <div>
+        <div
+          className={style.desktop}
+          style={{
+            backgroundImage: `url(${desktop})`,
+            ...styleOptions
+          }}
+        />
+
+        <div
+          className={style.mobile}
+          style={{
+            backgroundImage: `url(${mobile})`,
+            ...styleOptions
+          }}
+        />
+
+        <div
+          className={style.retina}
+          style={{
+            backgroundImage: `url(${retina})`,
+            ...styleOptions
+          }}
+        />
+      </div>
+    );
   };
 
   ThemeImage.validate = createValidate(conditions);
