b5d303fb6c08965cc83eed7227488a9c464bd309
Chris <chris.dugne@uralys.com>
2016-08-05 16:15:21 +0200

    adding catalog cards component (#158)
    * adding catalog cards component
    
    * prepared catalog-card
    
    * card style
    
    * added fixtures + checkers

---
+++ src/components/molecule/catalogue-card/index.js
@@ -0,0 +1,44 @@
+import { checker, createValidate } from '../../../util/validation';
+import style from './catalogue-card.css';
+
+const conditions = checker.shape({
+  props: checker.shape({
+    product: checker.shape({
+      images: checker.array
+    })
+  }),
+  children: checker.none
+});
+
+export default (engine, options = {}) => {
+  const CatalogueCard = (props, children) => {
+    const {h} = engine;
+    const {product} = props;
+    return (
+      <li className={style['products-list-item']}>
+        <div className={style['item-img']}>
+          <a href="#">
+            <img src={product.images[0] && product.images[0].url.https} />
+          </a>
+        </div>
+        <div className={style['item-desc']}>
+          <div className={style['item-desc-title']}>Cursus Stratégie Digitale</div>
+          <div className={style['item-author']}>by Coorpacademy</div>
+          <div className={style['item-desc-misc']}>
+            <div className={style['item-popularity item-popularity-4']}>
+              <span className={style.pop1}>★</span>
+              <span className={style.pop2}>★</span>
+              <span className={style.pop3}>★</span>
+              <span className={style.pop4}>★</span>
+              <span className={style.pop5}>★</span>
+            </div>
+            <div className={style['item-desc-price']}>€24.99</div>
+          </div>
+        </div>
+      </li>
+    );
+  };
+
+  CatalogueCard.validate = createValidate(conditions);
+  return CatalogueCard;
+};
