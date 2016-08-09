27e852152adb3ee3173da2e29401837d7e685840
Gaelle MOURARET <gaellemouraret@gmail.com>
2016-08-09 14:28:35 +0200

    add header & state discipline

--- src/@coorpacademy/sandbox/src/app.js
+++ src/@coorpacademy/sandbox/src/app.js
@@ -1,15 +1,20 @@
-import createCatalogCards from '../../components/organism/catalogue-cards';
+import createDisciplineHeader from '../../components/molecule/discipline-header';
+import createDisciplineScope from '../../components/molecule/discipline-scope';
 
 import products from '../assets/products';
 import sandbox from './sandbox.css';
 
 export default treant => {
   const {h} = treant;
-  const CatalogCards = createCatalogCards(treant);
+  const DisciplineHeader = createDisciplineHeader(treant);
+  const DisciplineScope = createDisciplineScope(treant);
 
   return (props, children) => (
-    <CatalogCards
-      products={products}
-    />
+    <div>
+      <DisciplineHeader
+      />
+      <DisciplineScope
+      />
+    </div>
   );
 };
