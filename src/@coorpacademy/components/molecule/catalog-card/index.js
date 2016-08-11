2b04e4070a5392577c92e989f73aef5b15c874a9
Julien Seren-Rosso <julien@coorpacademy.com>
2016-08-11 10:47:41 +0200

    fix layout

--- src/@coorpacademy/components/molecule/catalog-card/index.js
+++ src/@coorpacademy/components/molecule/catalog-card/index.js
@@ -20,9 +20,7 @@ export default (treant, options = {}) => {
     return (
       <li className={style['catalog-list-item']}>
         <div className={style['image-wrapper']}>
-          <a href={product.href}>
             <img src={product.images[0] && product.images[0].url.https} />
-          </a>
           <div className={style.overlay}>
             <HoverFill>
               <a href={product.href}>En savoir <span>plus</span></a>
