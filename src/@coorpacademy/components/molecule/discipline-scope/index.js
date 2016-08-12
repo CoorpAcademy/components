91f57811d33c6899a4ee7dce47ce7a3d2798fa88
Gaelle MOURARET <gaellemouraret@gmail.com>
2016-08-12 17:06:26 +0200

    avancement fiche discipline

--- src/@coorpacademy/components/molecule/discipline-scope/index.js
+++ src/@coorpacademy/components/molecule/discipline-scope/index.js
@@ -1,57 +1,30 @@
 import { checker, createValidate } from '../../util/validation';
 import style from './discipline-scope.css';
 
-const conditions = checker.shape({
-  props: checker.shape({
-    product: checker.shape({
-      images: checker.array
-    })
-  }),
-  children: checker.none
-});
+import createScopeTabs from '../scope-tabs';
+import createScopeContent from '../scope-content';
 
-export default (engine, options = {}) => {
+export default (treant, options = {}) => {
+  const ScopeTabs = createScopeTabs(treant, options);
+  const ScopeContent = createScopeContent(treant, options);
+
   const DisciplineScope = (props, children) => {
-    const {h} = engine;
-    const {product} = props;
+    const {h} = treant;
+    const {product, onClick, selected} = props;
     return (
       <div className={style.scope}>
-            <ul className={style.title}>
-                <li className={style.tabscurrent}>Base</li>
-                <li className={style.tabs}>Avancé</li>
-                <li className={style.tabs}>Coach</li>
-            </ul>
-            <div className={style.desc}>
-                <div className={style.desctitle}>
-                    BASE / <span className={style.desctitlecontent}>Stratégie Digitale</span>
-                </div>
-                <div className={style.leftscope}>
-                    <div className={style.lstitle}>À L'ISSUE DE CE NIVEAU, VOUS SEREZ CAPABLE DE :</div>
-                    <ul className={style.leftscopelist}>
-                        <li>Duis mollis, est non commodo luctus</li>
-                        <li>Duis mollis, est non commodo luctus</li>
-                        <li>Duis mollis, est non commodo luctus</li>
-                    </ul>
-                </div>
-                <div className={style.rightscope}>
-                <div className={style.rstitle}>chapitres</div>
-                    <ul className={style.rightscopelist}>
-                        <li>Duis mollis</li>
-                        <li>Duis mollis</li>
-                        <li>Duis mollis</li>
-                    </ul>
-                </div>
-            </div>
+            <ScopeTabs
+                onClick={onClick}
+                selected={selected}
+            />
+                <ScopeContent
+                    selected={selected}
+                />
             <div className={style.asset}>
                 <div className={style.assettitle}>assets</div>
                 <div className={style.assetdesc}>
                     <div className={style.coursescope}>
-                      <ul>
-                        <li><span>5</span> DISCIPLINES</li>
-                        <li><span>600</span> QUESTIONS</li>
-                        <li><span>15</span> NIVEAUX À VALIDER</li>
-                        <li><span>75</span> VIDÉOS DE 5 MIN</li>
-                      </ul>
+                      {product.coursescope}
                     </div>
                 </div>
             </div>
@@ -59,6 +32,5 @@ export default (engine, options = {}) => {
     );
   };
 
-  DisciplineScope.validate = createValidate(conditions);
   return DisciplineScope;
 };
--- src/@coorpacademy/sandbox/src/app.js
+++ src/@coorpacademy/sandbox/src/app.js
@@ -1,19 +1,32 @@
 import createDisciplineHeader from '../../components/molecule/discipline-header';
 import createDisciplineScope from '../../components/molecule/discipline-scope';
+import createDisciplineRightaside from '../../components/organism/discipline-rightaside';
 
 import products from '../assets/products';
-import sandbox from './sandbox.css';
+import style from './sandbox.css';
 
 export default treant => {
   const {h} = treant;
   const DisciplineHeader = createDisciplineHeader(treant);
   const DisciplineScope = createDisciplineScope(treant);
+  const DisciplineRightaside = createDisciplineRightaside(treant);
 
   return (props, children) => (
     <div>
-      <DisciplineHeader
-      />
-      <DisciplineScope
+      <div className={style.container}>
+        <DisciplineHeader
+          product={products[2]}
+        />
+        <DisciplineScope
+          product={products[2]}
+          onClick={id => console.log(id)} // eslint-disable-line no-console
+          selected={1}
+        >
+        </DisciplineScope>
+      </div>
+
+      <DisciplineRightaside
+        product={products[2]}
       />
     </div>
   );
