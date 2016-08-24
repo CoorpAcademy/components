04e8f72d903bc14f47cc2def9b93501c476cea97
chrisdugne <chris.dugne@uralys.com>
2016-08-24 17:52:03 +0200

    linked with levels

--- src/@coorpacademy/components/molecule/scope-tabs/index.js
+++ src/@coorpacademy/components/molecule/scope-tabs/index.js
@@ -4,7 +4,8 @@ import style from './scope-tabs.css';
 const conditions = checker.shape({
   props: checker.shape({
     selected: checker.bool,
-    onClick: checker.func
+    onClick: checker.func,
+    levels: checker.arrayOf(checker.string)
   }),
   children: checker.none
 });
@@ -12,16 +13,16 @@ const conditions = checker.shape({
 export default (treant, options = {}) => {
   const ScopeTabs = props => {
     const {h} = treant;
-    const {onClick, selected} = props;
+    const {onClick, selected, levels} = props;
 
     return (
       <ul className={style.tabs}>
-        {['Base', 'Avancé', 'Coach'].map((title, index) => (
+        {levels.map((level, index) => (
           <li
             onClick={() => onClick(index)}
             className={selected === index ? style.currentTab : style.tab}
           >
-            {title}
+            {level}
           </li>
         ))}
       </ul>
