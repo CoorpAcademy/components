de1282da7a5bc29b450daca33a486ce22bd42a66
chrisdugne <chris.dugne@uralys.com>
2016-08-24 16:09:17 +0200

    remove logic from sandbox, added _props on sandbox

--- src/@coorpacademy/sandbox/src/app.js
+++ src/@coorpacademy/sandbox/src/app.js
@@ -6,7 +6,21 @@ import {navigate} from '../../redux-tools/redux-history';
 import createTranslate from '../../components/util/translate';
 import * as locales from '../../components/locales';
 
+import products from '../assets/products';
+import contents from '../assets/contents';
+
 const translate = createTranslate(locales.fr);
+
+const selected = 2;
+const _props = {
+  selected,
+  product: products[0],
+  title: contents[0].title,
+  content: contents[0].levels[selected],
+  changeLevel: level => {
+    console.log(level);
+  }
+};
 
 export default (treant, {dispatch, history}) => {
   const {h} = treant;
@@ -16,7 +30,7 @@ export default (treant, {dispatch, history}) => {
       {/* <Header
         onSelectComponent={value => dispatch(navigate(history.createLocation(value)))}
       /> */}
-      <ProductCourse {...props} />
+      <ProductCourse {..._props} />
     </div>
   );
 };
