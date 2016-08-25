efd1a490ef9e698acf07d79d68f6286240e83243
godu <arthur.weber@viacesi.fr>
2016-08-25 12:19:54 +0200

    Update modules fixtures

--- src/@coorpacademy/components/template/product-course/test/fixtures/default.js
+++ src/@coorpacademy/components/template/product-course/test/fixtures/default.js
@@ -113,6 +113,209 @@ export default {
       course_scope: null,
       parent_sku: '',
       href: '/catalog/trainings/27'
-    }
+    },
+    levels: [
+      {
+        id: '1314547402568368732',
+        order: null,
+        created_at: '2016-08-11 14:38:03',
+        updated_at: '2016-08-25 08:06:58',
+        sku: '27.A',
+        title: 'Les nouveaux business',
+        slug: '27.A',
+        category: [
+          {
+            id: '1009167484147728544',
+            order: 2,
+            created_at: '2015-06-17 06:22:40',
+            updated_at: '2016-08-18 15:15:57',
+            slug: 'category_advanced',
+            status: 'Live',
+            title: 'Advanced',
+            description: 'Module Advanced',
+            type: 'module'
+          }
+        ],
+        description: 'La révolution numérique a tout accéléré : les communications, la récupération des données, la propagation de l’information et des innovations… On peut presque tout savoir instantanément : les bonnes pratiques d’une entreprise comme les mauvaises ; les goûts et les comportements de ses clients… Pour ne pas rater le coche, il faut être au fait des tendances et des changements qui se profilent. C’est ce que propose cette discipline nourrie d’exemples concrets.',
+        brand: null,
+        tax_band: {
+          value: 'Default',
+          data: {
+            id: '982563388041199711',
+            title: 'Default',
+            description: null,
+            rate: 0,
+            created_at: null,
+            updated_at: null
+          }
+        },
+        ref: 'mod_4k4tSBG3e',
+        filter: '',
+        video_sample_embed: '',
+        price: {
+          value: '€0.00',
+          data: {
+            formatted: {
+              with_tax: '€0.00',
+              without_tax: '€0.00',
+              tax: '€0.00'
+            },
+            rounded: {
+              with_tax: 0,
+              without_tax: 0,
+              tax: 0
+            },
+            raw: {
+              with_tax: 0,
+              without_tax: 0,
+              tax: 0
+            }
+          }
+        },
+        images: {},
+        author: 'Eléphant',
+        popularity: 0,
+        related_products: '',
+        course_scope: [
+          '24 QUESTIONS',
+          '3 VIDÉOS'
+        ],
+        parent_sku: '27'
+      },
+      {
+        id: '1314547375338947155',
+        order: null,
+        created_at: '2016-08-11 14:38:00',
+        updated_at: '2016-08-25 08:06:59',
+        sku: '27.C',
+        title: 'Les nouveaux business',
+        slug: '27.C',
+        category: [
+          {
+            id: '1009167626452074657',
+            order: 3,
+            created_at: '2015-06-17 06:22:57',
+            updated_at: '2016-08-18 15:15:59',
+            slug: 'category_coach',
+            status: 'Live',
+            title: 'Coach',
+            description: 'Module Coach',
+            type: 'module'
+          }
+        ],
+        description: 'La révolution numérique a tout accéléré : les communications, la récupération des données, la propagation de l’information et des innovations… On peut presque tout savoir instantanément : les bonnes pratiques d’une entreprise comme les mauvaises ; les goûts et les comportements de ses clients… Pour ne pas rater le coche, il faut être au fait des tendances et des changements qui se profilent. C’est ce que propose cette discipline nourrie d’exemples concrets.',
+        brand: null,
+        tax_band: {
+          value: 'Default',
+          data: {
+            id: '982563388041199711',
+            title: 'Default',
+            description: null,
+            rate: 0,
+            created_at: null,
+            updated_at: null
+          }
+        },
+        ref: 'mod_EJLKBHf2g',
+        filter: ' ',
+        video_sample_embed: '',
+        price: {
+          value: '€0.00',
+          data: {
+            formatted: {
+              with_tax: '€0.00',
+              without_tax: '€0.00',
+              tax: '€0.00'
+            },
+            rounded: {
+              with_tax: 0,
+              without_tax: 0,
+              tax: 0
+            },
+            raw: {
+              with_tax: 0,
+              without_tax: 0,
+              tax: 0
+            }
+          }
+        },
+        images: {},
+        author: 'Eléphant',
+        popularity: 0,
+        related_products: '',
+        course_scope: [
+          '24 QUESTIONS',
+          '3 VIDÉOS'
+        ],
+        parent_sku: '27'
+      },
+      {
+        id: '1314547361556464206',
+        order: null,
+        created_at: '2016-08-11 14:37:58',
+        updated_at: '2016-08-25 08:06:58',
+        sku: '27.B',
+        title: 'Les nouveaux business',
+        slug: '27.B',
+        category: [
+          {
+            id: '1009166626102182047',
+            order: 1,
+            created_at: '2015-06-17 06:20:58',
+            updated_at: '2016-08-18 15:15:54',
+            slug: 'category_base',
+            status: 'Live',
+            title: 'Base',
+            description: 'Module Base',
+            type: 'module'
+          }
+        ],
+        description: 'La révolution numérique a tout accéléré : les communications, la récupération des données, la propagation de l’information et des innovations… On peut presque tout savoir instantanément : les bonnes pratiques d’une entreprise comme les mauvaises ; les goûts et les comportements de ses clients… Pour ne pas rater le coche, il faut être au fait des tendances et des changements qui se profilent. C’est ce que propose cette discipline nourrie d’exemples concrets.',
+        brand: null,
+        tax_band: {
+          value: 'Default',
+          data: {
+            id: '982563388041199711',
+            title: 'Default',
+            description: null,
+            rate: 0,
+            created_at: null,
+            updated_at: null
+          }
+        },
+        ref: 'mod_EkMFBSGhx',
+        filter: ' ',
+        video_sample_embed: '',
+        price: {
+          value: '€0.00',
+          data: {
+            formatted: {
+              with_tax: '€0.00',
+              without_tax: '€0.00',
+              tax: '€0.00'
+            },
+            rounded: {
+              with_tax: 0,
+              without_tax: 0,
+              tax: 0
+            },
+            raw: {
+              with_tax: 0,
+              without_tax: 0,
+              tax: 0
+            }
+          }
+        },
+        images: {},
+        author: 'Eléphant',
+        popularity: 0,
+        related_products: '',
+        course_scope: [
+          '24 QUESTIONS',
+          '3 VIDÉOS'
+        ],
+        parent_sku: '27'
+      }
+    ]
   }
 };
