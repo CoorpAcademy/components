408d445fff41b32a974554a9aa43cd9852944743
chrisdugne <chris.dugne@uralys.com>
2016-08-25 13:25:19 +0200

    lint

--- src/@coorpacademy/components/molecule/discipline-scope/test/fixtures/default.js
+++ src/@coorpacademy/components/molecule/discipline-scope/test/fixtures/default.js
@@ -26,7 +26,7 @@ export default {
               type: 'module'
             }
           ],
-          description: 'La révolution numérique a tout accéléré : les communications, la récupération des données, la propagation de l’information et des innovations… On peut presque tout savoir instantanément : les bonnes pratiques d’une entreprise comme les mauvaises ; les goûts et les comportements de ses clients… Pour ne pas rater le coche, il faut être au fait des tendances et des changements qui se profilent. C’est ce que propose cette discipline nourrie d’exemples concrets.',
+          description: 'La révolution numérique a tout accéléré : les communications, la récupération des données,...',
           brand: null,
           tax_band: {
             value: 'Default',
@@ -93,7 +93,7 @@ export default {
               type: 'module'
             }
           ],
-          description: 'La révolution numérique a tout accéléré : les communications, la récupération des données, la propagation de l’information et des innovations… On peut presque tout savoir instantanément : les bonnes pratiques d’une entreprise comme les mauvaises ; les goûts et les comportements de ses clients… Pour ne pas rater le coche, il faut être au fait des tendances et des changements qui se profilent. C’est ce que propose cette discipline nourrie d’exemples concrets.',
+          description: 'La révolution numérique a tout accéléré : les communications, la récupération des données...',
           brand: null,
           tax_band: {
             value: 'Default',
@@ -160,7 +160,7 @@ export default {
               type: 'module'
             }
           ],
-          description: 'La révolution numérique a tout accéléré : les communications, la récupération des données, la propagation de l’information et des innovations… On peut presque tout savoir instantanément : les bonnes pratiques d’une entreprise comme les mauvaises ; les goûts et les comportements de ses clients… Pour ne pas rater le coche, il faut être au fait des tendances et des changements qui se profilent. C’est ce que propose cette discipline nourrie d’exemples concrets.',
+          description: 'La révolution numérique a tout accéléré : les communications, la récupération des données...',
           brand: null,
           tax_band: {
             value: 'Default',
