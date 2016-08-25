a9c7ea2908cd15237067732a33005263daea625b
Arthur Weber <arthur.weber@viacesi.fr>
2016-08-25 12:21:29 +0200

    Reword link component (#187)

--- src/@coorpacademy/components/util/test/navigation.js
+++ src/@coorpacademy/components/util/test/navigation.js
@@ -1,86 +1,158 @@
 import test from 'ava';
+import forEach from 'lodash/fp/forEach';
 import noop from 'lodash/fp/noop';
 import {h} from '../../../../@treantjs/core';
-import {push} from '../navigation';
+import {createNavigationHandler} from '../navigation';
+import {NAVIGATE} from '../../../redux-tools/redux-history';
 
-const createEvent = ({onPreventDefault}) => ({
-  preventDefault: onPreventDefault || noop
+const createEvent = ({onPreventDefault, props, extra = {}}) => ({
+  target: {
+    ...props
+  },
+  preventDefault: onPreventDefault || noop,
+  button: 0,
+  ...extra
 });
 
-const createHistory = ({onPush}) => ({
-  push: onPush || noop
+const createHistory = ({onCreate}) => ({
+  createLocation: pathname => {
+    onCreate(pathname);
+    return {pathname};
+  }
 });
 
-test('should call push with href property', t => {
-  t.plan(2);
+test('should dispatch navigation event', t => {
+  t.plan(3);
 
   const props = {
     href: '/foo'
   };
   const options = {
     history: createHistory({
-      onPush: href => {
-        t.is(props.href, href);
-      }
-    })
+      onCreate: href => t.is(props.href, href)
+    }),
+    dispatch: action =>
+      t.deepEqual({
+        type: NAVIGATE,
+        payload: {
+          pathname: props.href
+        }
+      }, action)
   };
   const event = createEvent({
-    onPreventDefault: () => {
-      t.pass();
-    }
+    props,
+    onPreventDefault: () => t.pass()
   });
-  const onClick = push(options, props);
+  const onClick = createNavigationHandler(options);
 
   onClick(event);
 });
 
-test('should not call push if href isn\'nt defined', t => {
-  const props = {};
+test('shouldn\'t run/launch action if event is prevented', t => {
+  const props = {
+    href: '/foo'
+  };
   const options = {
     history: createHistory({
-      onPush: href => {
-        t.fail();
-      }
-    })
+      onCreate: href => t.fail()
+    }),
+    dispatch: () => t.fail()
   };
   const event = createEvent({
-    onPreventDefault: () => {
-      t.fail();
+    props,
+    onPreventDefault: () => t.fail(),
+    extra: {
+      defaultPrevented: true
     }
   });
-  const onClick = push(options, props);
+  const onClick = createNavigationHandler(options);
 
   onClick(event);
 });
 
-test('should not call push if history isn\'nt defined', t => {
-  const props = {
-    href: '/foo'
-  };
-  const options = {};
-  const event = createEvent({
-    onPreventDefault: () => {
-      t.fail();
-    }
-  });
-  const onClick = push(options)(props);
-
-  onClick(event);
-});
-
-test('should not call push if history.push isn\'nt defined', t => {
+test('shouldn\'t run/launch action if event is a left click event', t => {
   const props = {
     href: '/foo'
   };
   const options = {
-    history: {}
+    history: createHistory({
+      onCreate: href => t.fail()
+    }),
+    dispatch: () => t.fail()
   };
   const event = createEvent({
-    onPreventDefault: () => {
-      t.fail();
+    props,
+    onPreventDefault: () => t.fail(),
+    extra: {
+      button: 1
     }
   });
-  const onClick = push(options, props);
+  const onClick = createNavigationHandler(options);
+
+  onClick(event);
+});
+
+test('shouldn\'t run/launch action if event is a modified event', t => {
+  const props = {
+    href: '/foo'
+  };
+  const options = {
+    history: createHistory({
+      onCreate: href => t.fail()
+    }),
+    dispatch: () => t.fail()
+  };
+  const events = [
+    createEvent({
+      props,
+      onPreventDefault: () => t.fail(),
+      extra: {
+        metaKey: true
+      }
+    }),
+    createEvent({
+      props,
+      onPreventDefault: () => t.fail(),
+      extra: {
+        altKey: true
+      }
+    }),
+    createEvent({
+      props,
+      onPreventDefault: () => t.fail(),
+      extra: {
+        ctrlKey: true
+      }
+    }),
+    createEvent({
+      props,
+      onPreventDefault: () => t.fail(),
+      extra: {
+        shiftKey: true
+      }
+    })
+  ];
+  const onClick = createNavigationHandler(options);
+
+  forEach(onClick, events);
+});
+
+test('shouldn\'t run/launch action if target was defined', t => {
+  const props = {
+    href: '/foo',
+    target: 'blank'
+  };
+  const options = {
+    history: createHistory({
+      onCreate: href => t.fail()
+    }),
+    dispatch: () => t.fail()
+  };
+  const event = createEvent({
+    props,
+    onPreventDefault: () => t.fail()
+  });
+  const onClick = createNavigationHandler(options);
 
   onClick(event);
 });
