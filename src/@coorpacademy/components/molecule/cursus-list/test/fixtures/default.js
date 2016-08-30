89f06f6a7afcbc2f3f0ac1dbda32370e2cf61e10
chrisdugne <chris.dugne@uralys.com>
2016-08-30 17:37:12 +0200

    prepared cursus-list on catalog template

---
+++ src/@coorpacademy/components/molecule/cursus-list/test/fixtures/default.js
@@ -0,0 +1,35 @@
+export default {
+  props: {
+    cursuses: [
+      {
+        title: 'Développement durable',
+        href: '/plop'
+      },
+      {
+        title: 'Innovations & Technologies',
+        href: '/plop'
+      },
+      {
+        title: 'Management',
+        href: '/plop'
+      },
+      {
+        title: 'Cursus Stratégie Digitale',
+        href: '/plop',
+        selected: true
+      },
+      {
+        title: 'Cursus Réseaux Sociaux',
+        href: '/plop'
+      },
+      {
+        title: 'Cursus E-Marketing et publicité',
+        href: '/plop'
+      },
+      {
+        title: 'Cursus E-commerce',
+        href: '/plop'
+      }
+    ]
+  }
+};
