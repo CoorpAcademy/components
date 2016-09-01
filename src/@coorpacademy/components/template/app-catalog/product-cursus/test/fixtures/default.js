36e403670449191e03c4348c0cfc960b80ecc53e
chrisdugne <chris.dugne@uralys.com>
2016-09-01 19:29:25 +0200

    added back validation

--- src/@coorpacademy/components/template/app-catalog/product-cursus/test/fixtures/default.js
+++ src/@coorpacademy/components/template/app-catalog/product-cursus/test/fixtures/default.js
@@ -16,6 +16,7 @@ export default {
     image,
     badge,
     linkBuy,
+    linkTry,
     rating,
     maxRating,
     assets,
--- src/@treantjs/core/resolve.js
+++ src/@treantjs/core/resolve.js
@@ -1,11 +1,20 @@
 import isFunction from 'lodash/fp/isFunction';
 
 const resolve = vTree => {
-  if (isFunction(vTree.tagName))
+  if (isFunction(vTree.tagName)) {
+    if (process.env.NODE_ENV !== 'production' && vTree.tagName.validate) {
+      vTree.tagName.validate(
+        vTree.properties,
+        vTree.children
+      );
+    }
+
     return resolve(vTree.tagName(
       vTree.properties,
       vTree.children
     ));
+  }
+
   return vTree;
 };
 
