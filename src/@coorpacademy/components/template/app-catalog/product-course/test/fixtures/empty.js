902aee1bd01cf219835ebbc690cb39bdafb2d444
godu <arthur.weber@viacesi.fr>
2016-09-02 11:05:31 +0200

    Adds empty fixture

---
+++ src/@coorpacademy/components/template/app-catalog/product-course/test/fixtures/empty.js
@@ -0,0 +1,29 @@
+import take from 'lodash/fp/take';
+import disciplineHeader from '../../../../../molecule/discipline-header/test/fixtures/default';
+import disciplineRightAside from '../../../../../organism/discipline-rightaside/test/fixtures/default';
+import disciplineScope from '../../../../../molecule/discipline-scope/test/fixtures/default';
+import catalogCards from '../../../../../organism/catalog-cards/test/fixtures/default';
+
+const {title, description, image} = disciplineHeader.props;
+const {rating, maxRating, linkBuy, linkTry, author} = disciplineRightAside.props;
+const {content, levels} = disciplineScope.props;
+const {products} = catalogCards.props;
+
+/* eslint-disable max-len */
+
+export default {
+  props: {
+    selected: null,
+    popularity: null,
+    maxPopularity: null,
+    title: null,
+    description: null,
+    image: null,
+    linkBuy: null,
+    linkTry: null,
+    author: null,
+    relatedDisciplines: [],
+    level: null,
+    levels: null
+  }
+};
