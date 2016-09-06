63ca9bec803f0fc488b28f508578b807632ba625
chrisdugne <chris.dugne@uralys.com>
2016-09-06 15:14:23 +0200

    authorized image auto sizing for discipline-header

--- src/@coorpacademy/components/molecule/discipline-header/test/fixtures/no-video.js
+++ src/@coorpacademy/components/molecule/discipline-header/test/fixtures/no-video.js
@@ -4,6 +4,6 @@ export default {
   props: {
     title: 'Les nouveaux business',
     description: 'La révolution numérique a tout accéléré : les communications, la récupération des données, la propagation de l’information et des innovations… On peut presque tout savoir instantanément : les bonnes pratiques d’une entreprise comme les mauvaises ; les goûts et les comportements de ses clients… Pour ne pas rater le coche, il faut être au fait des tendances et des changements qui se profilent. C’est ce que propose cette discipline nourrie d’exemples concrets.',
-    image: 'https://unsplash.it/g/380'
+    image: 'https://unsplash.it/g/380/250'
   }
 };
