eb82ccc916d94338c53c6953c6a508398f676034
Gaelle MOURARET <gaellemouraret@gmail.com>
2016-09-20 09:55:30 +0200

    Tentative de faire un header

---
+++ src/@coorpacademy/components/molecule/menu-list/index.js
@@ -0,0 +1,60 @@
+import find from 'lodash/fp/find';
+import identity from 'lodash/fp/identity';
+import createLink from '../../atom/link';
+import {checker, createValidate} from '../../util/validation';
+import style from './style.css';
+
+const conditions = checker.shape({
+  props: checker.shape({
+    menuitem: checker.arrayOf(
+      checker.shape({
+        href: checker.string.optional,
+        title: checker.string.optional
+      })
+    ).optional
+  }),
+  children: checker.none
+});
+
+export default (treant, options = {}) => {
+  const {h} = treant;
+  const {translate = identity} = options;
+
+  const Link = createLink(treant, options);
+
+  const MenuList = ({menuitem = []}, children) => {
+    const MenuitemDiv = menuitem.map(cursus => {
+      const {title, href, selected} = cursus;
+
+      const linkProps = selected ? {
+        className: style.selected
+      } : {};
+
+      return (
+        <li className={style.item}>
+          <Link
+            {...linkProps}
+            href={href}
+          >
+            {title}
+          </Link>
+        </li>
+      );
+    });
+
+    const selectedCursus = find({
+      selected: true
+    }, menuitem);
+
+    return (
+      <div className={style.menuitem}>
+        <ul className={style.list}>
+          {MenuitemDiv}
+        </ul>
+      </div>
+    );
+  };
+
+  MenuList.validate = createValidate(conditions);
+  return MenuList;
+};
---
+++ src/@coorpacademy/components/molecule/menu-list/test/fixtures/default.js
@@ -0,0 +1,26 @@
+export default {
+  props: {
+    menuitem: [
+      {
+        title: 'Formations',
+        href: '/plop'
+      },
+      {
+        title: 'Nos Solutions',
+        href: '/plop'
+      },
+      {
+        title: 'Coorpacademy',
+        href: '/plop'
+      },
+      {
+        title: 'Connexion',
+        href: '/plop'
+      },
+      {
+        title: 'Inscription',
+        href: '/plop'
+      }
+    ]
+  }
+};
