2228c328c831abce92aaab3941be57be7fdbc6ec
Julien Seren-Rosso <julien@coorpacademy.com>
2016-09-20 16:08:41 +0200

    add select atom

--- packages/@coorpacademy-components/src/molecule/form-group/index.js
+++ packages/@coorpacademy-components/src/molecule/form-group/index.js
@@ -2,6 +2,7 @@ import {checker, createValidate} from '../../util/validation';
 import style from './style.css';
 
 import createInputText from '../../atom/input-text';
+import createSelect from '../../atom/select';
 
 const conditions = checker.shape({
   props: checker.none,
@@ -12,12 +13,14 @@ export default (treant, options = {}) => {
   const FormGroup = (props, children) => {
     const {h} = treant;
     const InputText = createInputText(treant, options);
+    const Select = createSelect(treant, options);
 
     const fields = [1, 2, 3, 4, 5];
 
     return (
       <fieldset className={style.set}>
         <legend>Propriétés</legend>
+        <Select />
         {fields.map((field, index) => (
           <InputText />
         ))}
