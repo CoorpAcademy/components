b6f9a1456ad880ab3d06bd43bd19b3647a9dd6b5
chrisdugne <chris.dugne@uralys.com>
2016-09-22 17:02:31 +0200

    using image-link fixture

--- src/@coorpacademy/components/organism/header/test/fixtures/default.js
+++ src/@coorpacademy/components/organism/header/test/fixtures/default.js
@@ -2,11 +2,12 @@ import MenuList from '../../../../molecule/menu-list/test/fixtures/default';
 import ImageLink from '../../../../atom/image-link/test/fixtures/simple-src';
 
 const {menuItems} = MenuList.props;
-const {imageLogo} = ImageLink.props;
+const {src, href} = ImageLink.props;
 
 export default {
   props: {
     menuItems,
-    imageLogo
+    src,
+    href
   }
 };
