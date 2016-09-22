8801fc7598845deaf480c10507cd2011c82155ed
chrisdugne <chris.dugne@uralys.com>
2016-09-22 16:43:22 +0200

    started forum-discussion

---
+++ src/@coorpacademy/components/molecule/forum-post/test/fixtures/answer.js
@@ -0,0 +1,10 @@
+export default {
+  props: {
+    type: 'answer',
+    author: 'Tommy',
+    answerTo: 'Bob',
+    date: 'environ une heure',
+    text: 'Awesome !!!',
+    avatar: 'https://avatars0.githubusercontent.com/u/310658?v=5&s=260'
+  }
+};
