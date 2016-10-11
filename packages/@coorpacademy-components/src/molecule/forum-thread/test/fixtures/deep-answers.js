af07ecf1ee75841be594ece2dde87819e762e028
chrisdugne <chris.dugne@uralys.com>
2016-10-11 15:55:02 +0200

    fixed fixtures

--- packages/@coorpacademy-components/src/molecule/forum-thread/test/fixtures/deep-answers.js
+++ packages/@coorpacademy-components/src/molecule/forum-thread/test/fixtures/deep-answers.js
@@ -4,46 +4,27 @@ import Answer from '../../../forum-post/test/fixtures/answer';
 const post = Post.props;
 const answer = Answer.props;
 
+const answer1 = {
+  ...answer,
+  answers: [answer, answer]
+};
+
+const answer2 = {
+  ...answer,
+  answers: [answer1, answer1]
+};
+
 export default {
   props: {
-    post: {
-      ...post,
-      answers: [{
-        post: {
-          ...answer,
-          answers: [{
-            post: {
-              ...answer,
-              answers: [{
-                post: {
-                  ...answer,
-                  answers: [{
-                    post: {
-                      ...answer
-                    }
-                  }]
-                }
-              }]
-            }
-          }]
-        }
-      }, {
-        post: {
-          ...answer
-        }
-      }, {
-        post: {
-          ...answer
-        }
-      }, {
-        post: {
-          ...answer
-        }
-      }, {
-        post: {
-          ...answer
-        }
-      }]
-    }
+    ...post,
+    answers: [
+      answer,
+      answer,
+      answer,
+      answer,
+      answer1,
+      answer2,
+      answer1
+    ]
   }
 };
--- packages/@coorpacademy-components/src/molecule/forum-thread/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/forum-thread/test/fixtures/default.js
@@ -1,9 +1,12 @@
 import Post from '../../../forum-post/test/fixtures/post';
+import Answer from '../../../forum-post/test/fixtures/answer';
 
 const post = Post.props;
+const answer = Answer.props;
 
 export default {
   props: {
-    post
+    ...post,
+    answers: [answer, answer]
   }
 };
