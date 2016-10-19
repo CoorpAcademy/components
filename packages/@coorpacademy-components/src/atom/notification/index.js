035a5d7a80d238fc420ca4d9b5361878372b95c0
Julien Seren-Rosso <julien@coorpacademy.com>
2016-10-19 17:09:47 +0200

    add notification component

---
+++ packages/@coorpacademy-components/src/atom/notification/index.js
@@ -0,0 +1,35 @@
+import {checker, createValidate} from '../../util/validation';
+import style from './style.css';
+
+const conditions = checker.shape({
+  props: checker.shape({
+    type: checker.string,
+    message: checker.string,
+    onClose: checker.func.optional
+  }),
+  children: checker.none
+});
+
+export default (treant, options = {}) => {
+  const {h} = treant;
+
+  const Notification = (props, children) => {
+    const {
+      type,
+      message
+    } = props;
+
+    const className = type ? style[type] : style.message;
+
+    return (
+      <div className={style.wrapper}>
+        <div className={className}>
+          <span>!</span>{message}
+        </div>
+      </div>
+    );
+  };
+
+  Notification.validate = createValidate(conditions);
+  return Notification;
+};
