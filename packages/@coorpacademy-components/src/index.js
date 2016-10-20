48e9c0550800f2e896ff8b28c8b69d12e61cb420
Julien Seren-Rosso <julien@coorpacademy.com>
2016-10-20 17:52:08 +0200

    fix

--- packages/@coorpacademy-components/src/index.js
+++ packages/@coorpacademy-components/src/index.js
@@ -40,6 +40,7 @@ export createInputColor from './atom/input-color';
 export createInputReadonly from './atom/input-readonly';
 export createInputSwitch from './atom/input-switch';
 export createInputText from './atom/input-text';
+export createInputTextarea from './atom/input-textarea';
 export createLabel from './atom/label';
 export createLabelModName from './atom/label-mod-name';
 export createLabelNormal from './atom/label-normal';
