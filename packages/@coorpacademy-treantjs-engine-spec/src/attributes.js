65fb61cc602c121f0a3be380cc53faa66e0b1d64
godu <arthur.weber@viacesi.fr>
2016-10-24 17:12:15 +0200

    Split engine spec

--- packages/@coorpacademy-treantjs-engine-spec/src/attributes.js
+++ packages/@coorpacademy-treantjs-engine-spec/src/attributes.js
@@ -43,29 +43,4 @@ export default (test, {render, transform}) => {
       assert(t, el);
     });
   });
-
-  const events = {
-    click: 'onClick'
-  };
-
-  Object.keys(events).forEach(eventName => {
-    const attribute = events[eventName];
-
-    test.cb(`should attch ${eventName} listener`, t => {
-      t.plan(1);
-      const onEvent = e => {
-        t.pass();
-        t.end();
-      };
-      const container = document.createElement('div');
-      const update = render(container);
-      const vTree = <div {...{[attribute]: onEvent}}/>;
-      const el = update(vTree);
-
-      document.body.appendChild(el);
-      const customEvent = document.createEvent('Event');
-      customEvent.initEvent(eventName, true, true);
-      el.dispatchEvent(customEvent);
-    });
-  });
 };
--- packages/@coorpacademy-treantjs-engine-spec/src/index.js
+++ packages/@coorpacademy-treantjs-engine-spec/src/index.js
@@ -1,13 +1,15 @@
 import jsdom from 'jsdom';
 import testRender from './render';
-import testProperties from './properties';
+import testAttributes from './attributes';
+import testEvents from './events';
 
 const engineSpec = (test, engine) => {
   test.before(() => {
     global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
     global.window = document.defaultView;
   });
-  testProperties(test, engine);
+  testAttributes(test, engine);
+  testEvents(test, engine);
   testRender(test, engine);
 };
 
