0f8bc80e57349060eac76a1a84edaa0d15347818
chrisdugne <chris.dugne@uralys.com>
2016-11-03 13:08:12 +0100

    generated index and added Layout fixture props

--- packages/@coorpacademy-components/src/template/back-office/layout/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/template/back-office/layout/test/fixtures/default.js
@@ -1,9 +1,11 @@
 import BrandCard from '../../../../../molecule/brand-card/test/fixtures/default';
+import Header from '../../../../../organism/setup-header/test/fixtures/default';
 
 const {props} = BrandCard;
 
 export default {
   props: {
+    header: Header.props,
     brands: [props, props, props, props, props],
     create: {
       edit: 'Create a new platform',
