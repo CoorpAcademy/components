08ca57afe7a4976ced2405e0a2a1056908088cd9
chrisdugne <chris.dugne@uralys.com>
2016-11-09 17:16:49 +0100

    roles with switches

---
+++ packages/@coorpacademy-components/src/atom/input-switch/test/fixtures/unset.js
@@ -0,0 +1,7 @@
+export default {
+  props: {
+    type: 'switch',
+    title: 'Unset',
+    onChange: value => console.log(value) // eslint-disable-line no-console
+  }
+};
--- packages/@coorpacademy-components/src/organism/brand-form/test/fixtures/user-edit.js
+++ packages/@coorpacademy-components/src/organism/brand-form/test/fixtures/user-edit.js
@@ -58,7 +58,32 @@ export default {
     },
     {
       title: 'Roles',
-      fields: []
+      fields: [{
+        type: 'switch',
+        title: 'RH',
+        value: true
+      }, {
+        type: 'switch',
+        title: 'CMS',
+        value: false
+      }, {
+        type: 'switch',
+        title: 'Admin',
+        value: true
+      }, {
+        type: 'switch',
+        title: 'Super coach'
+      }, {
+        type: 'switch',
+        title: 'Moderator'
+      }, {
+        type: 'switch',
+        title: 'System',
+        value: false
+      }, {
+        type: 'switch',
+        title: 'Godmode'
+      }]
     },
     {
       title: 'Organisation',
