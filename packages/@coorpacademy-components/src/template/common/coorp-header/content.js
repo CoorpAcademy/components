9b7bc04ea1d15771e68df7ecde4386b182618ccf
Julien Seren-Rosso <julien@coorpacademy.com>
2016-11-25 15:13:17 +0100

    fix url

--- packages/@coorpacademy-components/src/template/common/coorp-header/content.js
+++ packages/@coorpacademy-components/src/template/common/coorp-header/content.js
@@ -14,67 +14,67 @@ export default (translate, props) => {
     href: rootUrl,
     menuItems: [{
       title: translate('Les formations'),
-      href: url('/catalog/trainings'),
+      href: url('catalog/trainings'),
       subItems: [
         {
           title: translate('Tous les cursus'),
-          href: url('/home-B2C')
+          href: url('home-B2C')
         },
         {
           title: translate('Toutes les formations'),
-          href: url('/catalog/trainings')
+          href: url('catalog/trainings')
         },
         {
           title: translate('Abonnement Coorpacademy'),
-          href: url('/tarifs-abonnement')
+          href: url('tarifs-abonnement')
         }
       ]
     },
     {
       title: translate('Nos Solutions Entreprises'),
-      href: url('/solutions'),
+      href: url('solutions'),
       subItems: [
         {
           title: translate('Solutions Entreprises'),
-          href: url('/solutions')
+          href: url('solutions')
         },
         {
           title: translate('Des formations adaptées à vos enjeux'),
-          href: url('/contenu-adapte-enjeux')
+          href: url('contenu-adapte-enjeux')
         },
         {
           title: translate('Toutes les formations'),
-          href: url('/catalog/trainings/')
+          href: url('catalog/trainings/')
         },
         {
           title: translate('Tarifs entreprises'),
-          href: url('/tarifs-entreprises')
+          href: url('tarifs-entreprises')
         }
       ]
     },
     {
       title: translate('Coorpacademy'),
-      href: url('/qui-sommes-nous'),
+      href: url('qui-sommes-nous'),
       subItems: [
         {
           title: translate('Qui sommes-nous ?'),
-          href: url('/qui-sommes-nous')
+          href: url('qui-sommes-nous')
         },
         {
           title: translate('Pourquoi Coorpacademy ?'),
-          href: url('/home-B2B')
+          href: url('home-B2B')
         },
         {
           title: translate('Ils nous ont fait confiance'),
-          href: url('/clients')
+          href: url('clients')
         },
         {
           title: translate('L\'équipe'),
-          href: url('/equipe')
+          href: url('equipe')
         },
         {
           title: translate('Contactez-nous'),
-          href: url('/contact')
+          href: url('contact')
         }
       ]
     },
