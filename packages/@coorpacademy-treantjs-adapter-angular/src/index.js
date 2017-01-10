2f1f49d053dc70273cbd339e75b6f1f10d69171b
chrisdugne <chris.dugne@uralys.com>
2017-01-10 11:19:36 +0100

    updated angular-adapter for React

--- packages/@coorpacademy-treantjs-adapter-angular/src/index.js
+++ packages/@coorpacademy-treantjs-adapter-angular/src/index.js
@@ -1,6 +1,8 @@
 import mapKeys from 'lodash/fp/mapKeys';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const linkWithEngine = (React, ReactDOM) => ($i18next, $rootScope, scope, element, Component) => {
+const linkWithEngine = ($rootScope, scope, element, Provider, Component, options) => {
   const update = vTree => {
     ReactDOM.render(vTree, element[0]);
   };
@@ -9,12 +11,12 @@ const linkWithEngine = (React, ReactDOM) => ($i18next, $rootScope, scope, elemen
     if (props === undefined)
       return;
 
-    const options = {
-      skin: $rootScope.skin,
-      translate: $i18next
-    };
+    const vTree = (
+      <Provider {...options}>
+        <Component {...props}/>
+      </Provider>
+    );
 
-    const vTree = React.createElement(Component, props);
     update(vTree);
   };
 
@@ -26,14 +28,15 @@ const linkWithEngine = (React, ReactDOM) => ($i18next, $rootScope, scope, elemen
   scope.$on('$destroy', () => window.angular.element(element).remove());
 };
 
-const lowerFirstLetter = string => {
-  return string[0].toLowerCase() + string.slice(-(string.length - 1));
-};
-
-const createDirective = (app, React, ReactDOM, componentName, Component) => {
+const createDirective = (app, componentName, Provider, Component) => {
   const directive = ($rootScope, $i18next) => {
+    const options = {
+      skin: $rootScope.skin,
+      translate: $i18next
+    };
+
     const link = (scope, element, attrs) => {
-      linkWithEngine(React, ReactDOM)($i18next, $rootScope, scope, element, Component);
+      linkWithEngine($rootScope, scope, element, Provider, Component, options);
     };
 
     return {
@@ -57,11 +60,11 @@ const createDirective = (app, React, ReactDOM, componentName, Component) => {
  * <slide value="slideContent"></slide>
  * <module-bubble value="modData()"></module-bubble>
  */
-const createDirectives = (app, React, ReactDOM, components) => {
+const createDirectives = (app, Provider, components) => {
   const toDirective = key => {
-    const componentName = lowerFirstLetter(key);
+    const componentName = `coorp${key}`;
     const Component = components[key];
-    createDirective(app, React, ReactDOM, componentName, Component);
+    createDirective(app, componentName, Provider, Component);
   };
 
   return mapKeys(toDirective, components);
