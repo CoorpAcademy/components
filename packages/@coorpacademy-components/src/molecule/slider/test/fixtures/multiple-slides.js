9713d74fc75351b4083e0ac3f4fae78dccd41c37
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-01-27 11:51:47 +0100

    add fixtures

--- packages/@coorpacademy-components/src/molecule/slider/test/fixtures/multiple-slides.js
+++ packages/@coorpacademy-components/src/molecule/slider/test/fixtures/multiple-slides.js
@@ -6,8 +6,24 @@ export default {
   props: {
     slides: [
       slide,
-      {...slide, cover: 'url("http://www.lorempixel.com/200/200/nature/2")'},
-      {...slide, cover: 'url("http://www.lorempixel.com/200/200/nature/3")'}
+      {...slide,
+        cover: 'url("http://www.lorempixel.com/200/200/nature/2")',
+        title: 'Hi XXX, check your battle requests,',
+        subtitle: 'You have N battle requests!',
+        cta: {
+          href: '#',
+          title: 'Start Battling'
+        }
+      },
+      {...slide,
+        cover: 'url("http://www.lorempixel.com/200/200/nature/3")',
+        title: 'Welcome back Jane, continue your course',
+        subtitle: 'The Mobile Revolution',
+        cta: {
+          href: '#',
+          title: 'Continue Learning'
+        }
+      }
     ]
   }
 };
