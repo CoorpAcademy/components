e14642c1930f0a0b4397013cc89eb8d81c75a276
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-02-01 11:09:36 +0100

    slide jsx es6 class

--- packages/@coorpacademy-components/src/molecule/slider/index.js
+++ packages/@coorpacademy-components/src/molecule/slider/index.js
@@ -66,83 +66,67 @@ class Slider extends React.Component {
       </div>
     );
 
-  const totalSlide = slides.length;
-  const appWidth = window.innerWidth + 'px';
+  handleIncrement() {
+    this.setState(prevState => ({
+      countSlide: prevState.countSlide + 33
+    }));
+  }
 
-  const MoveSlide = React.createClass({
-    getInitialState: function() {
-      return {countSlide: 0};
-    },
-
-    reset: function() {
+  handleDecrement() {
+    this.setState(prevState => ({
+      countSlide: prevState.countSlide - 33
+    }));
+    if (this.state.countSlide <= 0) {
       this.replaceState(this.getInitialState());
-    },
-
-    handleIncrement: function() {
-      this.setState((prevState) => ({
-        countSlide: prevState.countSlide + 33
-      }));
-    },
-
-    handleDecrement: function() {
-      this.setState((prevState) => ({
-        countSlide: prevState.countSlide - 33
-      }));
-      if (this.state.countSlide <= 0) {
-        this.reset();
-      }
-    },
-
-    render: function() {
-      const myslides = slides.map((slide, index) => {
-        const isActive = this.state.countSlide === index;
-        const activeSlide = isActive ? style.activeSlide : null;
-        return (
-          <li>
-            <Slide {...slide}
-              key={index}
-            />
-          </li>
-        );
-      });
-
-      const viewCtrls = totalSlide === 1 ? null : (
-        <div className={style.controls}>
-          <span className={style.leftControl}
-            onClick={this.handleDecrement}
-          />
-          <span className={style.rightControl}
-            onClick={this.handleIncrement}
-          />
-        </div>
-      );
-
-      const nexstep = this.state.countSlide;
-      const slideWidth = appWidth;
-      return (
-        <div className={style.slidesWrapper}>
-          <ul className={style.slidecontent}
-            style={{
-              width: slideWidth,
-              animation: 'AutoSlide 15s steps(nexstep) infinite'
-            }}
-          >
-            {myslides}
-          </ul>
-          {viewCtrls}
-        </div>
-      );
     }
-  });
+  }
 
-  const viewCtrls = totalSlide === 1 ? null : (<MoveSlide />);
+  render() {
+    const {
+      slides
+    } = this.props;
 
-  return (
-    <div>
-      <MoveSlide />
-    </div>
-  );
-};
+    const totalSlide = slides.length;
+
+    const myslides = slides.map((slide, index) => {
+      const isActive = this.state.countSlide === index;
+      const activeSlide = isActive ? style.activeSlide : null;
+      return (
+        <li key={index}>
+          <Slide {...slide} />
+        </li>
+      );
+    });
+
+    const viewCtrls = totalSlide === 1 ? null : (
+      <div className={style.controls}>
+        <span className={style.leftControl}
+          onClick={this.handleDecrement}
+        />
+        <span className={style.rightControl}
+          onClick={this.handleIncrement}
+        />
+      </div>
+    );
+
+    const nexstep = this.state.countSlide;
+
+    const SlidesView = (
+      <div className={style.slidesWrapper}>
+        <ul className={style.slidecontent}>
+          {myslides}
+        </ul>
+        {viewCtrls}
+      </div>
+    );
+
+    return (
+      <div>
+        {SlidesView}
+      </div>
+    );
+  }
+}
 
 Slider.propTypes = {
   slides: PropTypes.arrayOf(PropTypes.shape({
