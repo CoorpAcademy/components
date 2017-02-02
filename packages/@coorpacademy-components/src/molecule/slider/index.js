1b667ea709ab24db25d53a496de1cbb06f95a06f
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-02-02 10:01:31 +0100

    add crtl move slide

--- packages/@coorpacademy-components/src/molecule/slider/index.js
+++ packages/@coorpacademy-components/src/molecule/slider/index.js
@@ -14,16 +14,23 @@ class Slider extends React.Component {
 
   handleIncrement() {
     this.setState(prevState => ({
-      countSlide: prevState.countSlide + 33
+      countSlide: prevState.countSlide + 1
     }));
+    if (this.state.countSlide >= 2) {
+      this.setState(prevState => ({
+        countSlide: 0
+      }));
+    }
   }
 
   handleDecrement() {
     this.setState(prevState => ({
-      countSlide: prevState.countSlide - 33
+      countSlide: prevState.countSlide - 1
     }));
     if (this.state.countSlide <= 0) {
-      this.replaceState(this.getInitialState());
+      this.setState(prevState => ({
+        countSlide: 2
+      }));
     }
   }
 
@@ -36,63 +43,11 @@ class Slider extends React.Component {
 
     const myslides = slides.map((slide, index) => {
       const isActive = this.state.countSlide === index;
-      const activeSlide = isActive ? style.activeSlide : null;
+      const activeSlide = isActive ? style.activeSlide : '';
       return (
-        <li key={index}>
-          <Slide {...slide} />
-        </li>
-      );
-    });
-
-    const viewCtrls = totalSlide === 1 ? null : (
-      <div className={style.controls}>
-        <span className={style.leftControl}
-          onClick={this.handleDecrement}
-        />
-        <span className={style.rightControl}
-          onClick={this.handleIncrement}
-        />
-      </div>
-    );
-
-    const nexstep = this.state.countSlide;
-
-    const SlidesView = (
-      <div className={style.slidesWrapper}>
-        <ul className={style.slidecontent}>
-          {myslides}
-        </ul>
-        {viewCtrls}
-      </div>
-    );
-
-  handleIncrement() {
-    this.setState(prevState => ({
-      countSlide: prevState.countSlide + 33
-    }));
-  }
-
-  handleDecrement() {
-    this.setState(prevState => ({
-      countSlide: prevState.countSlide - 33
-    }));
-    if (this.state.countSlide <= 0) {
-      this.replaceState(this.getInitialState());
-    }
-  }
-
-  render() {
-    const {
-      slides
-    } = this.props;
-
-    const totalSlide = slides.length;
-
-    const myslides = slides.map((slide, index) => {
-      const isActive = this.state.countSlide === index;
-      const activeSlide = isActive ? style.activeSlide : null;
-      return (
-        <li key={index}>
+        <li key={index}
+          className={`${activeSlide} ${style.slide}`}
+        >
           <Slide {...slide} />
         </li>
       );
