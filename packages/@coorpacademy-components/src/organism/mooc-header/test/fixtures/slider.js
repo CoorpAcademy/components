62bd52b66343352afedccd0d5ac5095f2bf2eff6
Julien Seren-Rosso <julien@coorpacademy.com>
2017-02-23 16:32:19 +0100

    header logo

--- packages/@coorpacademy-components/src/organism/mooc-header/test/fixtures/slider.js
+++ packages/@coorpacademy-components/src/organism/mooc-header/test/fixtures/slider.js
@@ -9,8 +9,8 @@ const {props} = Default;
 export default {
   props: defaultsDeep(props, {
     logo: {
-      src: 'https://adidas-fr-staging.coorpacademy.com/assets/css/skin/logos/logo.7d13bba680b.svg',
-      srcMobile: 'https://adidas-fr-staging.coorpacademy.com/assets/css/skin/logos/logo.7d13bba680b.svg',
+      src: 'https://batman-staging.coorpacademy.com/assets/css/skin/logos/logo.d2b15b6d3d2.png',
+      srcMobile: 'https://batman-staging.coorpacademy.com/assets/css/skin/logos/logo-mobile.d3051ae0aef.png',
       href: '#'
     },
     slider
