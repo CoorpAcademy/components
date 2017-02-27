2368e4258d52b6ec6ef4bf5a348580fb84b6314d
Julien Seren-Rosso <julien@coorpacademy.com>
2017-02-27 10:44:06 +0100

    logos

--- packages/@coorpacademy-components/src/organism/mooc-header/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/organism/mooc-header/test/fixtures/default.js
@@ -1,8 +1,6 @@
 export default {
   props: {
     logo: {
-      src: 'https://www.coorpacademy.com/assets/img/logo.svg',
-      srcMobile: 'https://up.coorpacademy.com/assets/css/skin/logos/logo_coorpacademy_zeitgeist_small.92e2b75dbe0.svg',
       href: '#'
     },
     themes: [
