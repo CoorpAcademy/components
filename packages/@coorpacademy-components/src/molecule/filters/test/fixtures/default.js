8b8da1d2312ee0972c47e2513dec0a2f41de407d
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-02-28 15:06:06 +0100

    fixtures & style

--- packages/@coorpacademy-components/src/molecule/filters/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/filters/test/fixtures/default.js
@@ -1,7 +1,6 @@
 import dropdowns from '../../../../atom/select/test/fixtures/nolabel';
 import selectThematique from '../../../../molecule/select-multiple/test/fixtures/checked';
 
-
 export default {
   props: {
     select: dropdowns.props,
