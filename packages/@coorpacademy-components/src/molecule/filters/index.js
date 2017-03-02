dd3985a2d2cd0f37d0448e5927f5614edd2e9425
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-03-02 10:00:25 +0100

    add responsive

--- packages/@coorpacademy-components/src/molecule/filters/index.js
+++ packages/@coorpacademy-components/src/molecule/filters/index.js
@@ -4,28 +4,70 @@ import SelectMultiple from '../../molecule/select-multiple';
 import RangeSlider from '../../molecule/range-slider';
 import style from './style.css';
 
-function Filters(props) {
-  const {
-    select,
-    timer,
-    selectMultiple
-  } = props;
+class Filters extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opened: false
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
 
-  return (
-    <div>
-      <div className={style.default}>
-        <div className={style.title}>Filters</div>
-        <div className={style.wrapperFilters}>
-          <Select {...select} />
-          <div className={style.toggle}>
-            <RangeSlider {...timer} />
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return shallowCompare(this, nextProps, nextState, nextContext);
+  }
+
+  handleOnClick() {
+    this.setState({opened: !this.state.opened});
+  }
+
+  render() {
+    const {
+      timer,
+      select,
+      selectMultiple
+    } = this.props;
+
+    const isActive = this.state.opened === true;
+    const filtersActive = this.state.opened === true;
+    const sortingActive = this.state.opened === true;
+
+    return (
+      <div className={style.search}>
+        <div className={filtersActive ? style.activeDefault : style.default}>
+          <div
+            className={style.title}
+            onClick={this.handleOnClick}
+          >
+            Filters
           </div>
-          <SelectMultiple {...selectMultiple} />
-          <Select {...select} />
         </div>
-      </div>
-      <div className={style.wrapperSortBy}>
-        <div className={style.title}>sort by</div>
+        <div
+          className={sortingActive ? style.activeWrapperSortBy : style.wrapperSortBy}
+        >
+          <div
+            className={style.title}
+            onClick={this.handleOnClick}
+          >
+            sort by
+          </div>
+        </div>
+        <div className={isActive ? style.activeWrapperFilters : style.wrapperFilters} >
+          <div className={style.wrapper}>
+            <Select {...select} />
+            <div className={style.toggle}>
+              <RangeSlider {...timer} />
+            </div>
+            <SelectMultiple {...selectMultiple} />
+            <Select {...select} />
+          </div>
+          <div
+            className={style.CTAfilter}
+            onClick={this.handleOnClick}
+          >
+            Filter
+          </div>
+        </div>
         <div className={style.sorting}>
           <div className={style.mainTitle}>Catalog</div>
           <div className={style.select}>
@@ -34,8 +76,8 @@ function Filters(props) {
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Filters.propTypes = {
