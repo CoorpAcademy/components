dcaa7a73494d7d4961e3602bba16b91df81827eb
chrisdugne <chris.dugne@uralys.com>
2017-03-03 15:49:09 +0100

    fixed webpack-1 css-loader + babel config

--- packages/@coorpacademy-components/.storybook/webpack.config.js
+++ packages/@coorpacademy-components/.storybook/webpack.config.js
@@ -1,16 +1,14 @@
 const hash = '[folder]__[local]';
 const path = require('path');
 
-module.exports = function(storybookBaseConfig, configType) {
-  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
-  console.log('plop');
-  storybookBaseConfig.module.loaders.push({
-    test: /\.css$/,
-    loader: `style-loader!css-loader?minimize&modules&importLoaders=1&localIdentName=${hash}!postcss-loader`,
-    include: path.join(__dirname, '../src')
-  });
-
-  return storybookBaseConfig;
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: `style-loader!css-loader?minimize&modules&importLoaders=1&localIdentName=${hash}!postcss-loader`,
+        include: path.join(__dirname, '../src')
+      }
+    ]
+  }
 };
