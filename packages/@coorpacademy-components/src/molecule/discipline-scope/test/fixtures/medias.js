77dbec3ae8ecabceb56a5548a7e2aadadaec1c60
godu <arthur.weber@viacesi.fr>
2017-03-09 12:08:36 +0100

    Support different kind of medias

--- packages/@coorpacademy-components/src/molecule/discipline-scope/test/fixtures/medias.js
+++ packages/@coorpacademy-components/src/molecule/discipline-scope/test/fixtures/medias.js
@@ -1,5 +1,5 @@
 import scopeTabs from '../../../scope-tabs/test/fixtures/default';
-import scopeContent from '../../../scope-content/test/fixtures/videos';
+import scopeContent from '../../../scope-content/test/fixtures/medias';
 
 const levels = scopeTabs.props.levels;
 const content = scopeContent.props.content;
