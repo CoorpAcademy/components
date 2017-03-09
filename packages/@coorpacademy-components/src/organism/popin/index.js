c7d53782c2a6dd9abc6fcd33632a518e2d8e625b
chrisdugne <chris.dugne@uralys.com>
2017-03-09 17:55:51 +0100

    enhanced popin actions

--- packages/@coorpacademy-components/src/organism/popin/index.js
+++ packages/@coorpacademy-components/src/organism/popin/index.js
@@ -3,15 +3,20 @@ import get from 'lodash/fp/get';
 import getOr from 'lodash/fp/getOr';
 import identity from 'lodash/fp/identity';
 import map from 'lodash/fp/map';
-import Cta from '../../atom/cta';
+import Button from '../../atom/button';
 import Link from '../../atom/link';
 import style from './style.css';
 
 const Popin = (props, context) => {
   const {
-    popinCTA,
+    skin
+  } = context;
+
+  const {
+    ctaLabel,
+    ctaOnClick,
     header,
-    href,
+    headerOnClick,
     content
   } = props;
 
@@ -19,11 +24,16 @@ const Popin = (props, context) => {
     return {__html: content};
   }
 
+  const primary = getOr('#00B0FF', ['common', 'primary'], skin);
+
   return (
     <div className={style.default}>
       <div className={style.popin}>
-        <div className={style.header}>
-          <Link href={href}>{header}</Link>
+        <div
+          className={style.header}
+          onClick={headerOnClick}
+        >
+          {header}
         </div>
         <div className={style.content}>
           <div dangerouslySetInnerHTML={ // eslint-disable-line react/no-danger
@@ -31,17 +41,28 @@ const Popin = (props, context) => {
             }
           />
         </div>
-        <div className={style.cta}>
-          <Cta {...popinCTA} />
-        </div>
+        <Button
+          className={style.cta}
+          onClick={ctaOnClick}
+          submitValue={ctaLabel}
+          centered
+          style={{
+            backgroundColor: primary
+          }}
+        />
       </div>
     </div>
   );
 };
 
+Popin.contextTypes = {
+  skin: React.PropTypes.object
+};
+
 Popin.propTypes = {
-  popinCTA: PropTypes.object,
-  href: PropTypes.string,
+  ctaLabel: PropTypes.string,
+  ctaOnClick: PropTypes.func,
+  headerOnClick: PropTypes.func,
   header: PropTypes.string,
   content: PropTypes.string
 };
