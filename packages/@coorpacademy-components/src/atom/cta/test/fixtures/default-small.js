da5e07d5641d7c2c0fe9dddc26bb2f3ae8114d08
Jeroen Engels <jfm.engels@gmail.com>
2017-03-31 15:51:54 +0200

    Add Flow on atom/cta

--- packages/@coorpacademy-components/src/atom/cta/test/fixtures/default-small.js
+++ packages/@coorpacademy-components/src/atom/cta/test/fixtures/default-small.js
@@ -1,10 +1,16 @@
+// @flow
+import {type Props} from '../../';
+
+const props: Props = {
+  name: 'Some small name',
+  submitValue: 'Coorpacademy',
+  href: '#',
+  target: '_self',
+  light: false,
+  small: true,
+  secondary: false
+};
+
 export default {
-  props: {
-    submitValue: 'Coorpacademy',
-    href: '#',
-    target: '_self',
-    light: false,
-    small: true,
-    secondary: false
-  }
+  props
 };
