2e82f55688457a684e3e7b7584dba495fc73ec21
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-03-31 11:31:43 +0200

    ETQDev, page authors sandbox

---
+++ packages/@coorpacademy-components/src/template/common/authors/index.js
@@ -0,0 +1,135 @@
+import React, {PropTypes} from 'react';
+import map from 'lodash/fp/map';
+import * as CustomPropTypes from '../../../util/proptypes';
+import shallowCompare from '../../../util/shallow-compare';
+import CardsGrid from '../../../organism/cards-grid';
+import Picture from '../../../atom/picture';
+import SocialLink from '../../../atom/social-link';
+import Link from '../../../atom/link';
+import style from './style.css';
+
+class Authors extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullDisplay: false
+    };
+    this.handleToggleDisplay = this.handleToggleDisplay.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return shallowCompare(this, nextProps, nextState, nextContext);
+  }
+
+  handleToggleDisplay() {
+    this.setState(prevState => ({
+      fullDisplay: !prevState.fullDisplay
+    }));
+  }
+
+  render() {
+    const {
+      image,
+      title,
+      description,
+      cards,
+      cardsTitle,
+      socialTitle,
+      sociallinks,
+      website,
+      urlwebsite,
+      urlcontent
+    } = this.props;
+
+    const {translate} = this.context;
+    const toggleLabel = this.state.fullDisplay ? translate('See less') : translate('Show more');
+
+    const socialView = map.convert({cap: false})((sociallink, i) => (
+      <div
+        key={i}
+        className={style.link}
+      >
+        <SocialLink {...sociallink} />
+      </div>
+    ), sociallinks);
+
+    const authorSocial = socialView ? (
+      <div className={style.links}>
+        <span>{socialTitle}</span>{socialView}
+      </div>
+    ) : null;
+
+    return (
+      <div className={style.wrapper}>
+        <div className={style.authorsWrapper}>
+          <div className={style.header}>
+            <div className={style.imgWrapper}>
+              <Picture src={image} />
+            </div>
+            <div className={style.descWrapper}>
+              <div className={style.title}>
+                {title}
+              </div>
+              <div className={style.linksWrapper}>
+                <span>{website}</span>
+                <Link
+                  className={style.linksite}
+                  href={urlwebsite}
+                  target={'_blank'}
+                >
+                  {urlcontent}
+                </Link>
+                <div className={style.socialWrapper}>
+                  {authorSocial}
+                </div>
+              </div>
+              <div className={this.state.fullDisplay ? style.desc : style.shortDesc}>
+                <div
+                  dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                    __html: description
+                  }}
+                />
+              </div>
+              <div className={style.toggle}
+                onClick={this.handleToggleDisplay}
+              >
+                {toggleLabel}
+              </div>
+            </div>
+          </div>
+          <div className={style.cardsTitle}>
+            {cardsTitle}
+          </div>
+          <div className={style.cardsWrapper}>
+            <CardsGrid {...cards} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Authors.contextTypes = {
+  translate: PropTypes.func.isRequired
+};
+
+Authors.propTypes = {
+  cards: PropTypes.object,
+  cardsTitle: PropTypes.string,
+  image: PropTypes.oneOfType([
+    CustomPropTypes.url,
+    PropTypes.objectOf(CustomPropTypes.url)
+  ]),
+  title: PropTypes.string,
+  website: PropTypes.string,
+  urlcontent: PropTypes.string,
+  urlwebsite: PropTypes.string,
+  description: PropTypes.string,
+  socialTitle: PropTypes.string,
+  sociallinks: PropTypes.arrayOf(PropTypes.shape({
+    type: PropTypes.string,
+    link: PropTypes.string
+  }))
+};
+
+export default Authors;
---
+++ packages/@coorpacademy-components/src/template/common/authors/test/fixtures/default.js
@@ -0,0 +1,34 @@
+import cardsGrid from '../../../../../organism/cards-grid/test/fixtures/catalog';
+import cover from '../../../../../atom/picture/test/fixtures/default';
+
+const image = cover.props;
+const cards = cardsGrid.props;
+
+/* eslint-disable max-len */
+
+export default {
+  props: {
+    socialTitle: 'Social :',
+    sociallinks: [
+      {
+        type: 'facebook',
+        link: 'https://www.facebook.com/nextstagepe/'
+      },
+      {
+        type: 'twitter',
+        link: 'https://twitter.com/7A8officiel'
+      }
+    ],
+    cards,
+    cardsTitle: 'All courses from Nexstage',
+    title: 'Nexstage AM',
+    website: 'Website :',
+    urlcontent: 'www.nextstage.com',
+    urlwebsite: 'http://www.nextstage.com/',
+    description: 'NextStage AM est société de gestion indépendante, agréée par l’AMF en 2002 et l’un des pionniers et leaders du capital développement en France. Elle investit dans un nombre limité de PME et d’ETI (61 en portefeuille au 30/06/2016), françaises et européennes, innovantes, auxquelles elle apporte une expertise entrepreneuriale d’investisseur et une expérience opérationnelle et internationale forte. Elle accompagne dans la durée ces Entreprises de Taille Moyenne (ETM), non cotées et cotées sur Alternext et Euronext B et C. NextStage AM s’est vue remettre en décembre 2015 le Gold Award pour le meilleur acteur du capital développement en Europe par Private Equity Awards (méthodologie disponible sur www.private-equity-exchange.com - Ce prix ne préjuge pas des résultats futurs des fonds ou de la société de gestion.)',
+    image: {...cover,
+      '1x': 'https://s3-eu-west-1.amazonaws.com/static.coorpacademy.com/content/CoorpAcademy/content/cockpit-up/poster/ns2x-1477319801703.png',
+      '2x': 'https://s3-eu-west-1.amazonaws.com/static.coorpacademy.com/content/CoorpAcademy/content-partnerships-women-up/cockpit-partner-women-up/poster/logo-wup-1477670054938.png'
+    }
+  }
+};
