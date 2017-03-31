15b865627e8c23a04123e84cc552c7193c798b5e
Jeroen Engels <jfm.engels@gmail.com>
2017-03-31 16:12:07 +0200

    Add Flow on atom/catalog-section

--- packages/@coorpacademy-components/src/atom/catalog-section/test/fixtures/picture.js
+++ packages/@coorpacademy-components/src/atom/catalog-section/test/fixtures/picture.js
@@ -1,13 +1,19 @@
-import React from 'react';
+// @flow
+import React, {type Children} from 'react';
 import Picture from '../../../picture';
+import {type Props} from '../../';
+
+const props: Props = {
+  title: 'Picture'
+};
+
+const children: Children = (
+  <Picture
+    src='https://unsplash.it/g/500/500?random'
+  />
+);
 
 export default {
-  props: {
-    title: 'Picture'
-  },
-  children: (
-    <Picture
-      src='https://unsplash.it/g/500/500?random'
-    />
-  )
+  props,
+  children
 };
