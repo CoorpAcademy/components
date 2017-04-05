d43ea410d3469fa2b437e479701298717895477f
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-04-05 16:23:56 +0200

    test 2

--- packages/@coorpacademy-components/src/template/app-catalog/author/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/template/app-catalog/author/test/fixtures/default.js
@@ -13,7 +13,6 @@ export default {
     description: '<p><strong>Rejoignez-nous dans 17 villes en France aux formations présentielles :</strong></p><p><a href="https://www.performances.fr/gerer-emotion-contexte-professionnel.html" target="_blank">Gérer les émotions</a></p><p><a href="https://www.performances.fr/outils-gestion-stress-situation-professionnelle.html" target="_blank">Les outils de la gestion du stress</a></p><p><a href="https://www.performances.fr/formation-gestion-des-conflits.html" target="_blank">Dominer les colères et les conflits</a></p><p>L’Institut François Bocquet organise chaque année en Europe plus de mille formations en développement des talents. Plus de 150.000 professionnels ont été formés (et parfois transformés) par l\'Institut depuis 1986.</p>',
     image,
     information: {
-      href: 'http://google.fr',
       socialLinks: [
         {
           type: 'facebook',
