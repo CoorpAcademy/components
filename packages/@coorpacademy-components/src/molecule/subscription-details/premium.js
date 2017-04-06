4644094feba0e897f3f70744f51dadcfe05f4da5
marcenacp <pierre.marcenac@centraliens.net>
2017-04-06 18:07:11 +0200

    Add premium settings page

--- packages/@coorpacademy-components/src/molecule/subscription-details/premium.js
+++ packages/@coorpacademy-components/src/molecule/subscription-details/premium.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const SubscriptionPremiumDetails = props => (
-  <div />
+  <div>
+    <img
+      src={props.checkImage}
+    />
+    <div>
+      {props.translations.subscriptionPremiumTitle}
+    </div>
+    <div>
+      {props.translations.subscriptionPremiumDescriptionPart1}
+      {props.subscriptionPremiumDescriptionDate}
+      {props.translations.subscriptionPremiumDescriptionPart2}
+      {props.subscriptionPremiumDescriptionPrice}
+      {props.translations.subscriptionPremiumDescriptionPart2End}
+    </div>
+    <div>
+      {props.translations.subscriptionPremiumDescriptionPart3}
+      <a href={`${props.subscriptionPremiumDescriptionLink}`}>
+        {props.translations.subscriptionPremiumDescriptionPart3WithLink}
+      </a>
+    </div>
+  </div>
 );
 
 export default SubscriptionPremiumDetails;
