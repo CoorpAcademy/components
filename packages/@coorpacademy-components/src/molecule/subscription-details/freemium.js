fba3e3865b6988e2441ee7b62c3dfdddd30589e3
marcenacp <pierre.marcenac@centraliens.net>
2017-04-07 17:44:36 +0200

    Add global design of /settings

--- packages/@coorpacademy-components/src/molecule/subscription-details/freemium.js
+++ packages/@coorpacademy-components/src/molecule/subscription-details/freemium.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './style.css';
+import style from './freemium.css';
 
 const SubscriptionFreemiumDetails = props => (
   <div className={style.subscriptionFreemium}>
