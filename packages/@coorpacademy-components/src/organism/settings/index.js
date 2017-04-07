9b000d2dd248d71e33e6d14e77af230e63ef0974
marcenacp <pierre.marcenac@centraliens.net>
2017-04-07 18:00:33 +0200

    Fix settings style

--- packages/@coorpacademy-components/src/organism/settings/index.js
+++ packages/@coorpacademy-components/src/organism/settings/index.js
@@ -1,10 +1,9 @@
 import React, {PropTypes} from 'react';
 import TabNavigation from '../../molecule/tab-navigation';
-import style from './style.css';
 
 const Settings = props => {
   return (
-    <div className={style.settings}>
+    <div>
       <TabNavigation {...props} />
     </div>
   );
