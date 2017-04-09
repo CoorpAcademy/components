1072e2e6b8f255c287b1f22003cc7eb186f644db
Arthur WEBER <arthur.weber@viacesi.fr>
2017-04-09 19:21:39 +0200

    Adds Windows support

--- packages/@coorpacademy-components/scripts/generate-components-index.js
+++ packages/@coorpacademy-components/scripts/generate-components-index.js
@@ -1,4 +1,5 @@
 import {relative, join} from 'path';
+import slash from 'slash';
 import _join from 'lodash/fp/join';
 import sortBy from 'lodash/fp/sortBy';
 import map from 'lodash/fp/map';
@@ -15,7 +16,7 @@ const targetDir = join(__dirname, '../src');
 const _exports = pipe(
   values,
   reduce(assign, {}),
-  mapValues(path => relative(targetDir, path)),
+  mapValues(path => slash(relative(targetDir, path))),
   toPairs,
   sortBy(0),
   map(([_name, path]) => `export ${_name} from './${path}';`),
