20b8c25301abcbb0e5c0df8ab635d301a7ad07a3
chrisdugne <chris.dugne@uralys.com>
2017-04-10 18:16:54 +0200

    babili setup cc @godu

--- packages/@coorpacademy-components-bundler/webpack.config.js
+++ packages/@coorpacademy-components-bundler/webpack.config.js
@@ -96,7 +96,8 @@ const config = cssScope => ({
         }),
         new BabiliPlugin({
           comments: false,
-          sourceMap: false
+          sourceMap: false,
+          simplify: false
         }),
         componentCSS
       );
