74f1ef9985b1ab91c8c25cff2c45391656112388
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2017-04-14 18:33:43 +0200

    Use translate in product card component

--- packages/@coorpacademy-components/src/molecule/product-card/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/product-card/test/fixtures/default.js
@@ -2,9 +2,7 @@ export default {
   props: {
     title: 'Product title',
     subtitle: 'Product subtitle',
-    descriptionItems: ['item1', 'item2', 'item3'],
-    price: '9.9€',
-    cancelText: 'Cancel at any time.',
+    price: '9.9',
     checkImage: 'http://iconshow.me/media/images/Mixed/small-n-flat-icon/png2/256/-sign-check.png'
   }
 };
