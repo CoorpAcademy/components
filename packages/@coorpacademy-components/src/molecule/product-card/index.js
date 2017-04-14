40ec8484c6d221f2fbdf9dbdbfd40095e19ade93
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2017-04-14 18:33:43 +0200

    Use translate in product card component

--- packages/@coorpacademy-components/src/molecule/product-card/index.js
+++ packages/@coorpacademy-components/src/molecule/product-card/index.js
@@ -1,17 +1,27 @@
 import React, {PropTypes} from 'react';
 import style from './style.css';
 
-const ProductCard = props => {
+const ProductCard = (props, context) => {
   const {
     title,
     subtitle,
-    descriptionItems,
     price,
     cancelText,
     checkImage
   } = props;
 
-  const items = descriptionItems.map((item, index) => {
+  const { translate } = context;
+
+  const descriptionItemTranslationKeys = [
+    'product_desc_culture',
+    'product_desc_partnership',
+    'product_desc_battle',
+    'product_desc_coach',
+    'product_desc_forum',
+    'product_desc_certificates'
+  ];
+
+  const items = descriptionItemTranslationKeys.map((itemTranslationKey, index) => {
     return (
       <div
         className={style.productDescriptionItem}
@@ -21,7 +31,7 @@ const ProductCard = props => {
           className={style.productCheckbox}
           src={checkImage}
         />
-        <div className={style.productDescriptionText}>{item}</div>
+        <div className={style.productDescriptionText}>{translate(itemTranslationKey)}</div>
       </div>
     );
   });
@@ -29,25 +39,28 @@ const ProductCard = props => {
   return (
     <div className={style.product}>
       <div className={style.productHeader}>
-        <div className={style.productHeaderTitle}>{title}</div>
-        <div className={style.productHeaderSubtitle}>{subtitle}</div>
+        <div className={style.productHeaderTitle}>{translate(title)}</div>
+        <div className={style.productHeaderSubtitle}>{translate(subtitle)}</div>
       </div>
       <div className={style.productBody}>
         <div className="product__description">{items}</div>
         <hr className={style.productSeparation} />
         <div className={style.productPrice}>
-          <div className={style.priceValue}>{price}</div>
-          <div className={style.cancelText}>{cancelText}</div>
+          <div className={style.priceValue}>{price}{translate('currency_per_month')}</div>
+          <div className={style.cancelText}>{translate('product_cancel')}</div>
         </div>
       </div>
     </div>
   );
 };
 
+ProductCard.contextTypes = {
+  translate: PropTypes.func
+};
+
 ProductCard.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
-  descriptionItems: PropTypes.array.isRequired,
   price: PropTypes.string.isRequired,
   cancelText: PropTypes.string.isRequired,
   checkImage: PropTypes.string.isRequired
