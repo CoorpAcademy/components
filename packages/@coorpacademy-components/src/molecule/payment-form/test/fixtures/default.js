d9754ae68a35b034e117983a08facc9ab01edc19
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2017-04-14 15:59:46 +0200

    Add fixtures

--- packages/@coorpacademy-components/src/molecule/payment-form/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/payment-form/test/fixtures/default.js
@@ -1,14 +1,17 @@
 export default {
   props: {
-    submitText: 'Payer',
-    checkImage: 'checkImage',
-    cardsImage: 'cards',
+    submitText: 'Pay',
+    checkImage: 'http://iconshow.me/media/images/Mixed/small-n-flat-icon/png2/256/-sign-check.png',
+    cardsImage: 'http://victoriorestaurant.com/wp-content/uploads/2014/06/credit-cards.png',
     cardOwnerEmail: 'email',
-    acceptedCardsText: 'visa',
-    securedPaymentText: 'sécurisé',
-    securedPaymentImage: 'image',
+    acceptedCardsText: 'Accepted cards',
+    securedPaymentText: 'Secured payment',
+    securedPaymentImage: 'http://simpleicon.com/wp-content/uploads/lock-2.png',
     stripeKeyPublic: 'key',
-    disclaimer: 'attention',
+    disclaimer: `This is a big disclaimer. Burgdoggen meatball tri-tip ground round.
+    Ribeye boudin flank, t-bone shoulder jowl turducken beef ribs short ribs rump.
+    Shankle filet mignon leberkas ribeye kevin. Filet mignon turkey jowl brisket leberkas strip steak
+    capicola shank landjaeger tongue.`,
     errors: 'errors',
     warning: 'warning',
     createSubscription() {}
--- packages/@coorpacademy-components/src/organism/cart/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/organism/cart/test/fixtures/default.js
@@ -1,31 +1,34 @@
 export default {
   props: {
-    title: 'On a marché sur la lune',
+    title: 'Coorpacademy header',
     productCardProps: {
-      title: 'Produit',
-      subtitle: 'P0',
-      descriptionItems: [],
-      price: '9.9',
-      cancelText: 'Annuler',
-      checkImage: 'image'
+      title: 'Product title',
+      subtitle: 'Product subtitle',
+      descriptionItems: ['item1', 'item2', 'item3'],
+      price: '9.9€',
+      cancelText: 'Cancel at any time.',
+      checkImage: 'http://iconshow.me/media/images/Mixed/small-n-flat-icon/png2/256/-sign-check.png'
     },
     promoCodeProps: {
-      promoCodePlaceholder: 'placeholder',
-      promoCodeSubmit: 'Appliquer',
-      promoCodeDescription: 'description',
-      promoCodeError: 'error',
+      promoCodePlaceholder: 'Enter a promo code',
+      promoCodeSubmit: 'Apply',
+      promoCodeDescription: 'Valid promo code',
+      promoCodeError: 'Invalid promo code',
       onValidate() {}
     },
     paymentFormProps: {
-      submitText: 'Payer',
-      checkImage: 'checkImage',
-      cardsImage: 'cards',
+      submitText: 'Pay',
+      checkImage: 'http://iconshow.me/media/images/Mixed/small-n-flat-icon/png2/256/-sign-check.png',
+      cardsImage: 'http://victoriorestaurant.com/wp-content/uploads/2014/06/credit-cards.png',
       cardOwnerEmail: 'email',
-      acceptedCardsText: 'visa',
-      securedPaymentText: 'sécurisé',
-      securedPaymentImage: 'image',
+      acceptedCardsText: 'Accepted cards',
+      securedPaymentText: 'Secured payment',
+      securedPaymentImage: 'http://simpleicon.com/wp-content/uploads/lock-2.png',
       stripeKeyPublic: 'key',
-      disclaimer: 'attention',
+      disclaimer: `This is a big disclaimer. Burgdoggen meatball tri-tip ground round.
+      Ribeye boudin flank, t-bone shoulder jowl turducken beef ribs short ribs rump.
+      Shankle filet mignon leberkas ribeye kevin. Filet mignon turkey jowl brisket leberkas strip steak
+      capicola shank landjaeger tongue.`,
       errors: 'errors',
       warning: 'warning',
       createSubscription() {}
