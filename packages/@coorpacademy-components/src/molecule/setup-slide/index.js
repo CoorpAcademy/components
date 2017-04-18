f080c0441fbe6e44159683343f1935d65ff91d0a
Julien Seren-Rosso <j.serenrosso@gmail.com>
2017-04-18 18:45:55 +0200

    Fix components forms (#584)
    * fix
    
    * fix setup sliders
    
    * fix style

--- packages/@coorpacademy-components/src/molecule/setup-slide/index.js
+++ packages/@coorpacademy-components/src/molecule/setup-slide/index.js
@@ -8,66 +8,86 @@ import InputSwitch from '../../atom/input-switch';
 import ImageUpload from '../../atom/image-upload';
 import style from './style.css';
 
-const SetupSlide = props => {
-  const {
-    title,
-    fields = []
-  } = props;
+class SetupSlide extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
 
-  const buildInput = field => {
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick() {
+    this.setState(prevState => ({
+      expanded: !prevState.expanded
+    }));
+  }
+
+  render() {
     const {
-      type
-    } = field;
-    switch (type) {
-      case 'switch':
-        return (
-          <InputSwitch {...field} />
-        );
-      case 'select':
-        return (
-          <Select {...field} />
-        );
-      case 'checkbox':
-        return (
-          <InputCheckbox {...field} />
-        );
-      case 'image':
-        return (
-          <ImageUpload {...field} />
-        );
-      default:
-        return (
-          <InputText {...field} />
-        );
-    }
-  };
+      title,
+      fields = []
+    } = this.props;
 
-  const buildField = (field, index) => {
-    const input = buildInput(field);
+    const buildInput = field => {
+      const {
+        type
+      } = field;
+      switch (type) {
+        case 'switch':
+          return (
+            <InputSwitch {...field} />
+          );
+        case 'select':
+          return (
+            <Select {...field} />
+          );
+        case 'checkbox':
+          return (
+            <InputCheckbox {...field} />
+          );
+        case 'image':
+          return (
+            <ImageUpload {...field} />
+          );
+        default:
+          return (
+            <InputText {...field} />
+          );
+      }
+    };
+
+    const buildField = (field, index) => {
+      const input = buildInput(field);
+
+      return (
+        <div
+          className={style.field}
+          key={index}
+        >
+          {input}
+        </div>
+      );
+    };
+
+    const fieldsList = fields.map(buildField);
 
     return (
-      <div
-        className={style.field}
-        key={index}
-      >
-        {input}
+      <div className={style.wrapper}>
+        <div className={style.title}
+          onClick={this.handleOnClick}
+        >
+          <h3>{title}</h3>
+          <span className={this.state.expanded ? style.up : style.down} />
+        </div>
+        <div className={this.state.expanded ? style.container : style.none}>
+          {fieldsList}
+        </div>
       </div>
     );
-  };
-
-  const fieldsList = fields.map(buildField);
-
-  return (
-    <div className={style.wrapper}>
-      <div className={style.title}>
-        <h3>{title}</h3>
-      </div>
-      <div className={style.container}>
-        {fieldsList}
-      </div>
-    </div>
-  );
-};
+  }
+}
 
 SetupSlide.propTypes = {
   title: PropTypes.string.isRequired,
