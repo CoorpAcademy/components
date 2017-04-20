a3755f33b1b3e7ca51e096bd53f297cd631c2b15
marcenacp <pierre.marcenac@centraliens.net>
2017-04-20 18:42:24 +0200

    Make payment-form and promo-code stateless

--- packages/@coorpacademy-components/src/molecule/unsubscribe/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/unsubscribe/test/fixtures/default.js
@@ -2,10 +2,6 @@ export default {
   props: {
     navigateToMooc: () => {console.log('navigates to MOOC');}, // eslint-disable-line no-console
     hasUnsubscribed: true,
-    confirmUnsubscribe: 'confirm_unsubscribe',
-    keepSubscription: 'keep_subscription',
-    onUnsubscribeButtonClick: () => {console.log('unsubscribes');}, // eslint-disable-line no-console
-    unsubscribe: 'unsubscribe',
-    unsubscribeConfirmation: 'premium_unsubscribe_confirmation'
+    onUnsubscribeButtonClick: () => {console.log('unsubscribes');} // eslint-disable-line no-console
   }
 };
--- packages/@coorpacademy-components/src/molecule/unsubscribe/test/fixtures/subscribed.js
+++ packages/@coorpacademy-components/src/molecule/unsubscribe/test/fixtures/subscribed.js
@@ -2,10 +2,6 @@ export default {
   props: {
     navigateToMooc: () => {console.log('navigates to MOOC');}, // eslint-disable-line no-console
     hasUnsubscribed: false,
-    confirmUnsubscribe: 'confirm_unsubscribe',
-    keepSubscription: 'keep_subscription',
-    onUnsubscribeButtonClick: () => {console.log('unsubscribes');}, // eslint-disable-line no-console
-    unsubscribe: 'unsubscribe',
-    unsubscribeConfirmation: 'premium_unsubscribe_confirmation'
+    onUnsubscribeButtonClick: () => {console.log('unsubscribes');} // eslint-disable-line no-console
   }
 };
