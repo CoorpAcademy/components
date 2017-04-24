829c91394d2a1a45485861ce8c40dd3926d759ab
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2017-04-24 18:55:03 +0200

    Add fixtures

---
+++ packages/@coorpacademy-components/src/organism/settings/test/fixtures/freemium.js
@@ -0,0 +1,20 @@
+export default {
+  props: {
+    tabs: [
+      {
+        title: 'my_subscription_title',
+        key: 'subscribe',
+        activeContent: 'subscribe',
+        links: [
+          {
+            title: 'my_subscription_manage_freemium_title'
+          }
+        ]
+      }
+    ],
+    onTabClick: () => {},
+    onBackToCatalogButtonClick: () => {},
+    activeContent: 'subscribe',
+    tabTitle: 'subscription_and_payment'
+  }
+};
--- packages/@coorpacademy-components/storybook/components.js
+++ packages/@coorpacademy-components/storybook/components.js
@@ -388,6 +388,8 @@ import MoocHeaderFixtureSliderMultiple from '../src/organism/mooc-header/test/fi
 import MoocHeaderFixtureSlider from '../src/organism/mooc-header/test/fixtures/slider';
 import PopinFixtureDefault from '../src/organism/popin/test/fixtures/default';
 import SettingsFixtureDefault from '../src/organism/settings/test/fixtures/default';
+import SettingsFixtureFreemium from '../src/organism/settings/test/fixtures/freemium';
+import SettingsFixturePremium from '../src/organism/settings/test/fixtures/premium';
 import SetupHeaderFixtureDefault from '../src/organism/setup-header/test/fixtures/default';
 import AuthorFixtureDefault from '../src/template/app-catalog/author/test/fixtures/default';
 import AuthorFixtureLoading from '../src/template/app-catalog/author/test/fixtures/loading';
@@ -1062,7 +1064,9 @@ export const fixtures = {
       Default: PopinFixtureDefault
     },
     Settings: {
-      Default: SettingsFixtureDefault
+      Default: SettingsFixtureDefault,
+      Freemium: SettingsFixtureFreemium,
+      Premium: SettingsFixturePremium
     },
     SetupHeader: {
       Default: SetupHeaderFixtureDefault
