d756dbf22e58060e0e35be1e25cef7866e09f9ec
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2017-04-25 11:43:56 +0200

    Display price with a template

--- packages/@coorpacademy-components/src/molecule/product-card/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/product-card/test/fixtures/default.js
@@ -2,7 +2,8 @@ export default {
   props: {
     title: 'up_good_name',
     subtitle: 'up_good_description',
-    price: '9.9',
+    price: '9,9',
+    currency: '€',
     checkImage: 'http://iconshow.me/media/images/Mixed/small-n-flat-icon/png2/256/-sign-check.png'
   }
 };
