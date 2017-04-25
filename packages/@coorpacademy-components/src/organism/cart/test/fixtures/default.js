1870ca962404c8b54cae8ab2488da97cfd90d53e
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2017-04-25 11:43:56 +0200

    Display price with a template

--- packages/@coorpacademy-components/src/organism/cart/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/organism/cart/test/fixtures/default.js
@@ -4,7 +4,8 @@ export default {
     productCardProps: {
       title: 'up_good_name',
       subtitle: 'up_good_description',
-      price: '9.9',
+      price: '9,9',
+      currency: '€',
       checkImage: 'http://iconshow.me/media/images/Mixed/small-n-flat-icon/png2/256/-sign-check.png'
     },
     promoCodeProps: {
