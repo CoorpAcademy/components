0ac6a72fafa489335ad340125e4e716284dc0fc2
Julien Seren-Rosso <julien.seren-rosso@coorpacademy.com>
2017-05-02 18:03:09 +0200

    fix footer theme image

--- packages/@coorpacademy-components/src/util/image.js
+++ packages/@coorpacademy-components/src/util/image.js
@@ -3,15 +3,12 @@ import getOr from 'lodash/fp/getOr';
 
 export default (skin, defaultImage) => {
   const defaultUrl = get(['images', defaultImage], skin);
-  const defaultProperties = get(['properties', defaultImage], skin);
 
   return image => {
     const url = getOr(defaultUrl, ['images', image], skin);
-    const properties = getOr(defaultProperties, ['properties', image], skin);
 
     const style = {
-      backgroundImage: url ? `url(${url})` : 'none',
-      ...properties
+      backgroundImage: url ? `url(${url})` : 'none'
     };
 
     return url ? style : null;
