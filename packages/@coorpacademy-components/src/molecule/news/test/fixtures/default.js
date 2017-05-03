e9da6f58da5970306e519896a02c164e1e062d18
Joan Ortega <jomaora@gmail.com>
2017-05-03 14:40:04 +0200

    Set html on news (#621)
    * Revert "Adding markup lib for news description (#620)"
    
    This reverts commit 54840dcc54eb2a2d750492c7489f0b446925441a.
    
    * Adding inner html for news description

--- packages/@coorpacademy-components/src/molecule/news/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/news/test/fixtures/default.js
@@ -3,7 +3,7 @@ export default {
     image: 'http://minecraft.fr/wp-content/uploads/2013/05/Pa8WZj1-235x300.jpg',
     title: 'A World Of Infinite Opportunities',
     date: '3 january 2018',
-    description: 'Vestibulum id **ligula** porta <code>felis</code> euismod semper. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla. ', // eslint-disable-line max-len
+    description: 'Vestibulum id <b>ligula</b> porta felis <i>euismod semper</i>. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla. ', // eslint-disable-line max-len
     authorLogo: 'https://static.coorpacademy.com/content/digital/raw/logo_coorpacademy-1487689750995.svg',
     cta: {
       submitValue: 'Read more',
