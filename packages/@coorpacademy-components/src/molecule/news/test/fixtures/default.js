b913e97e241f611ff45afee7ff144450eaee04ed
godu <arthur.weber@viacesi.fr>
2017-05-04 12:09:14 +0200

    Fixes news title

--- packages/@coorpacademy-components/src/molecule/news/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/news/test/fixtures/default.js
@@ -1,9 +1,9 @@
 export default {
   props: {
     image: 'http://minecraft.fr/wp-content/uploads/2013/05/Pa8WZj1-235x300.jpg',
-    title: 'A World Of Infinite Opportunities, A World Of Infinite Opportunities, A World Of Infinite Opportunities Donec ullamcorper nulla non metus auctor fringilla.',
+    title: 'A World Of Infinite Opportunities',
     date: '3 january 2018',
-    description: 'Vestibulum id <b>ligula</b> porta felis <i>euismod semper</i>. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. ', // eslint-disable-line max-len
+    description: 'Vestibulum id <b>ligula</b> porta felis <i>euismod semper</i>.', // eslint-disable-line max-len
     authorLogo: 'https://static.coorpacademy.com/content/digital/raw/logo_coorpacademy-1487689750995.svg',
     cta: {
       submitValue: 'Read more',
---
+++ packages/@coorpacademy-components/src/molecule/news/test/fixtures/small-desciption.js
@@ -0,0 +1,8 @@
+import defaultFixture from './default';
+
+export default {
+  props: {
+    ...defaultFixture.props,
+    description: 'Vestibulum id'
+  }
+};
