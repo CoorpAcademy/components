a4a463023eff5e45cde649b5732a730d337e3087
Julien Seren-Rosso <julien@coorpacademy.com>
2017-05-04 18:12:16 +0200

    fix arrows

--- packages/@coorpacademy-components/src/molecule/dashboard/cards-list/index.js
+++ packages/@coorpacademy-components/src/molecule/dashboard/cards-list/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import head from 'lodash/fp/head';
 import last from 'lodash/fp/last';
+import shallowCompare from '../../../util/shallow-compare';
 import Card from '../../card';
 import style from './style.css';
 
@@ -10,7 +11,19 @@ class CardsList extends React.Component {
     super(props);
     this._cards = [];
 
+    this.state = {
+      left: {
+        disabled: true,
+        hidden: true
+      },
+      right: {
+        disabled: true,
+        hidden: true
+      }
+    };
+
     this.leftBound = this.leftBound.bind(this);
+    this.maxLeftBound = this.maxLeftBound.bind(this);
     this.rightBound = this.rightBound.bind(this);
     this.wrapperWidth = this.wrapperWidth.bind(this);
     this.cardWidth = this.cardWidth.bind(this);
@@ -20,19 +33,44 @@ class CardsList extends React.Component {
     this.handleOnLeft = this.handleOnLeft.bind(this);
     this.handleOnRight = this.handleOnRight.bind(this);
     this.scrollTo = this.scrollTo.bind(this);
+    this.updateArrowsState = this.updateArrowsState.bind(this);
   }
 
   componentDidMount() {
     this._cardsWrapper.addEventListener('scroll', this.handleScroll);
     this._cardsCount = this.props.cards.length;
+
+    if (window) {
+      window.addEventListener('resize', this.updateArrowsState);
+    }
+
+    this.updateArrowsState();
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return shallowCompare(this, nextProps, nextState, nextContext);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!shallowCompare(this, prevProps, prevState)) {
+      this.updateArrowsState();
+    }
   }
 
   componentWillUnmount() {
     this._cardsWrapper.removeEventListener('scroll', this.handleScroll);
+
+    if (window) {
+      window.addEventListener('resize', this.updateArrowsState);
+    }
   }
 
   leftBound() {
     return this._cardsWrapper.scrollLeft;
+  }
+
+  maxLeftBound() {
+    return this.cardsWidth() - this.wrapperWidth();
   }
 
   rightBound() {
@@ -48,13 +86,16 @@ class CardsList extends React.Component {
   }
 
   cardsWidth() {
-    return this._cardsCount * this.cardWidth();
+    return this._cards.map(el => {
+      return el.scrollWidth;
+    }).reduce((a, b) => a + b, 0);
   }
 
   getPossiblePositions(filter) {
-    const cardWidth = this.cardWidth();
-    return this._cards.map((card, index) => {
-      return index * cardWidth;
+    return this._cards.map((card, index, arr) => {
+      return arr.slice(0, index).reduce((a, b) => {
+        return a + b.scrollWidth;
+      }, 0);
     }).filter(filter);
   }
 
@@ -84,8 +125,8 @@ class CardsList extends React.Component {
   }
 
   handleOnRight() {
-    const currentScrollPos = this.rightBound();
-    if (currentScrollPos >= this.cardsWidth()) {
+    const currentScrollPos = this.leftBound();
+    if (currentScrollPos >= this.maxLeftBound()) {
       return;
     }
 
@@ -94,8 +135,12 @@ class CardsList extends React.Component {
 
   scrollTo(currentScrollPos, toPrevious) {
     const possiblePositions = this.getPossiblePositions(el => {
-      return toPrevious ? el < currentScrollPos : el > currentScrollPos;
+      return toPrevious ? el < currentScrollPos : el > (currentScrollPos + this.wrapperWidth());
     });
+
+    if (!toPrevious) {
+      possiblePositions.push(this.cardsWidth());
+    }
 
     if (possiblePositions.length === 0) {
       return;
@@ -103,6 +148,24 @@ class CardsList extends React.Component {
 
     const actualPosition = toPrevious ? last(possiblePositions) : head(possiblePositions) - this.wrapperWidth();
     this._cardsWrapper.scrollLeft = actualPosition;
+    this.updateArrowsState();
+  }
+
+  updateArrowsState() {
+    const shouldHideArrows = this.cardsWidth() <= this.wrapperWidth();
+    const leftArrowDisabled = this.leftBound() <= 0;
+    const rightArrowDisabled = this.rightBound() >= this.cardsWidth();
+
+    this.setState({
+      left: {
+        hidden: shouldHideArrows,
+        disabled: leftArrowDisabled
+      },
+      right: {
+        hidden: shouldHideArrows,
+        disabled: rightArrowDisabled
+      }
+    });
   }
 
   render() {
@@ -124,26 +187,36 @@ class CardsList extends React.Component {
       );
     });
 
+    const leftArrowView = this.state.left.hidden ? null : (
+      <div className={this.state.left.disabled ? style.leftDisabled : style.left}
+        onClick={this.handleOnLeft}
+      />
+    );
+
+    const rightArrowView = this.state.right.hidden ? null : (
+      <div className={this.state.right.disabled ? style.rightDisabled : style.right}
+        onClick={this.handleOnRight}
+      />
+    );
+
     return (
       <div
         className={style.wrapper}
         data-name="cards-list"
       >
         <div className={style.list}>
-          <div className={style.title}>
-            {title}
+          <div className={style.listWrapper}>
+            <div className={style.title}>
+              {title}
+            </div>
+            <div className={style.cards} // eslint-disable-next-line no-return-assign
+              ref={div => this._cardsWrapper = div} // eslint-disable-line react/jsx-no-bind
+            >
+              {cardsView}
+            </div>
           </div>
-          <div className={style.cards} // eslint-disable-next-line no-return-assign
-            ref={div => this._cardsWrapper = div} // eslint-disable-line react/jsx-no-bind
-          >
-            {cardsView}
-          </div>
-          <div className={style.left}
-            onClick={this.handleOnLeft}
-          />
-          <div className={style.right}
-            onClick={this.handleOnRight}
-          />
+          {leftArrowView}
+          {rightArrowView}
         </div>
       </div>
     );
