bcdbb993928f0d66d0419549a7fe20293852e2aa
Jeroen Engels <jfm.engels@gmail.com>
2017-05-17 16:21:28 +0200

    Implement check answer for qcmDrag questions

--- packages/@coorpacademy-progression-engine/src/check-answer.js
+++ packages/@coorpacademy-progression-engine/src/check-answer.js
@@ -6,20 +6,27 @@ import sortBy from 'lodash/fp/sortBy';
 import isEqual from 'lodash/fp/isEqual';
 import identity from 'lodash/fp/identity';
 import toLower from 'lodash/fp/toLower';
-import {type Question, type QCMQuestion} from './types';
+import {type Question, type AcceptedAnswers, type Answer} from './types';
 
 type CheckAnswerOptions = {
   maxTypos?: number
 };
 
 const sort = sortBy(identity);
-const normalize = pipe(map(toLower), sort);
+const normalize = (orderMatters: boolean, answer: Answer): Answer =>
+  pipe(map(toLower), orderMatters ? identity : sort)(answer);
 
-function checkAnswerForQCM(question: QCMQuestion, givenAnswer: Array<string>): boolean {
-  const sortedGivenAnswer = normalize(givenAnswer);
-  return some((allowedAnswer: Array<string>): boolean => {
-    return isEqual(normalize(allowedAnswer), sortedGivenAnswer);
-  }, question.content.answers);
+function checkAnswerForQCM(
+  orderMatters: boolean,
+  allowedAnswers: AcceptedAnswers,
+  givenAnswer: Array<string>
+): boolean {
+  const sortedGivenAnswer = normalize(orderMatters, givenAnswer);
+  return some(
+    (allowedAnswer: Answer): boolean =>
+      isEqual(normalize(orderMatters, allowedAnswer), sortedGivenAnswer),
+    allowedAnswers
+  );
 }
 
 export default function checkAnswer(
@@ -27,9 +34,12 @@ export default function checkAnswer(
   question: Question,
   givenAnswer: Array<string>
 ): boolean {
+  const allowedAnswers = question.content.answers;
   switch (question.type) {
     case 'qcm':
-      return checkAnswerForQCM(question, givenAnswer);
+      return checkAnswerForQCM(false, allowedAnswers, givenAnswer);
+    case 'qcmDrag':
+      return checkAnswerForQCM(true, allowedAnswers, givenAnswer);
     default:
       return true;
   }
