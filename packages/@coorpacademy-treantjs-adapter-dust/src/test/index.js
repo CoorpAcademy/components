683b1a95b07b849695e76bafc55d47217ad21e34
godu <arthur.weber@viacesi.fr>
2017-05-20 16:10:36 +0200

    Extract providerProps from context

--- packages/@coorpacademy-treantjs-adapter-dust/src/test/index.js
+++ packages/@coorpacademy-treantjs-adapter-dust/src/test/index.js
@@ -4,14 +4,14 @@ import {createElement} from 'react';
 import each from 'lodash/fp/each';
 import toHelpers from '..';
 
-const macro = (t, {components, template, provider}, expected) => {
+const macro = (t, {components, template, provider, data}, expected) => {
   const helpers = toHelpers(components, provider);
   each(helper => helper(dust), helpers);
   const compiledTemplate = dust.compile(template, 'template');
   dust.loadSource(compiledTemplate);
 
   return new Promise((resolve, reject) =>
-    dust.render('template', {}, (err, out) => {
+    dust.render('template', data, (err, out) => {
       if (err) return reject(err);
       t.is(out, expected);
       resolve();
@@ -54,4 +54,20 @@ test(
     template: '{@title children="foo"/}'
   },
   '<h1>foo</h1>'
+);
+
+test(
+  'should extract provider props from context',
+  macro,
+  {
+    components: {
+      Title: () => createElement('h1')
+    },
+    provider: ({tagName}) => createElement(tagName),
+    template: '{@title context="context"/}',
+    data: {
+      context: {tagName: 'blink'}
+    }
+  },
+  '<blink></blink>'
 );
