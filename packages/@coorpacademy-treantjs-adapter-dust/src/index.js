90cef382cd5f803148ccf5b98ffe3057e7e99518
godu <arthur.weber@viacesi.fr>
2017-05-20 16:01:12 +0200

    Increases coverage adapter-dust

--- packages/@coorpacademy-treantjs-adapter-dust/src/index.js
+++ packages/@coorpacademy-treantjs-adapter-dust/src/index.js
@@ -1,21 +1,21 @@
 import map from 'lodash/fp/map';
 import toPairs from 'lodash/fp/toPairs';
 import kebabCase from 'lodash/fp/kebabCase';
-import React from 'react';
-import {renderToString} from 'react-dom/server';
+import {createElement} from 'react';
+import {renderToStaticMarkup} from 'react-dom/server';
 
 const toHelpers = (components, Provider) => {
   const toHelper = ([componentName, Component]) => {
     return (dust, options) => {
       dust.helpers[kebabCase(componentName)] = (chunk, context, bodies, props) => {
-        let vTree = React.createElement(Component, props);
+        let vTree = createElement(Component, props);
         if (Provider) {
           const providerOptions = {
             skin: context.get('skin')
           };
-          vTree = React.createElement(Provider, providerOptions, vTree);
+          vTree = createElement(Provider, providerOptions, vTree);
         }
-        const html = renderToString(vTree);
+        const html = renderToStaticMarkup(vTree);
         chunk.write(html);
       };
     };
