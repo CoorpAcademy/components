bad5f5372e65c788c60734e75f0ccc9d362c3277
Arthur Weber <arthur.weber@viacesi.fr>
2017-05-24 16:01:13 +0200

    Adds updateState (#675)

--- packages/@coorpacademy-progression-engine/src/update-progression.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import type {Progression} from './types';
-
-type Action = {};
-
-export default function updateProgression(progression: Progression, action: Action): Progression {
-  return progression;
-}
