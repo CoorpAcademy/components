29a9f9e28167eb8c6f91cf7d2cf3f5859f9e6dce
chrisdugne <chris.dugne@uralys.com>
2017-06-08 16:17:03 +0200

    started popin-end

--- packages/@coorpacademy-components/src/template/app-player/popin-end/index.js
+++ packages/@coorpacademy-components/src/template/app-player/popin-end/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from '../../../atom/link';
+import SlidesHeader from '../../../molecule/slides/slides-header';
+import Summary from '../../../organism/app-player/summary';
 import style from './style.css';
 
-const PopinEnd = props => {
-  const {success, header} = props;
-  const {cta} = header || {};
-
-  const ctaElement = cta ? <Link href={cta.href}>{cta.title}</Link> : null;
+const SlidePlayer = props => {
+  const {header, summary} = props;
 
   return (
     <div className={style.wrapper}>
-      <p>End</p>
-      <p>success: {success}</p>
-      {ctaElement}
+      <SlidesHeader {...header} />
+      <div className={style.popinWrapper}>
+        <Summary {...summary} />
+      </div>
     </div>
   );
 };
 
-PopinEnd.propTypes = {
-  success: PropTypes.bool
+SlidePlayer.propTypes = {
+  header: PropTypes.shape(SlidesHeader.propTypes),
+  summary: PropTypes.shape(Summary.propTypes)
 };
 
-export default PopinEnd;
+export default SlidePlayer;
