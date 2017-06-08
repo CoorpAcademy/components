19edbc3ac73bb5eb8a99467d24bb29e110c1a17b
David Corticchiato <david.corticchiato@gmail.com>
2017-06-08 13:41:25 +0200

    init-component script (component boilerplate)

---
+++ packages/@coorpacademy-components/scripts/init-component.js
@@ -0,0 +1,91 @@
+/*
+ * > npm run init-component path/to/component
+ *
+ * Example :
+ *
+ * > npm run init-component -- atom/beautiful-input
+ *
+ * atom
+ *   +  /beautiful-input
+ *     +  /index.js (with BeautifulInput component)
+ *     +  /style.css (with .default class)
+ *     +  /test
+ *     +    /fixtures
+ *     +      /default.js (empty props)
+*/
+
+const {pascalCase} = require('change-case');
+const {outputFile} = require('fs-extra');
+const last = require('lodash/fp/last');
+const mkdirp = require('mkdirp-promise');
+const minimist = require('minimist');
+const path = require('path');
+
+const srcDir = () => path.join(__dirname, '..', 'src');
+const baseName = baseDir => last(baseDir.split(path.sep));
+
+const createBaseDir = async relativePath => {
+  const baseDir = path.join(srcDir(), relativePath);
+
+  await mkdirp(baseDir);
+  return baseDir;
+};
+
+const writeComponentSource = async baseDir => {
+  const componentName = pascalCase(baseName(baseDir));
+  const content = [
+    `import React from 'react';`,
+    `import style from './style.css';`,
+    ``,
+    `const ${componentName} = () => <div className={style.default}>TODO</div>;`,
+    ``,
+    `export default ${componentName};`,
+    ``
+  ].join('\n');
+
+  return outputFile(path.join(baseDir, 'index.js'), content);
+};
+
+const writeStyleSource = async baseDir => {
+  const content = [
+    '.default {}',
+    ''
+  ].join('\n');
+
+  return outputFile(path.join(baseDir, 'style.css'), content);
+};
+
+const writeDefaultFixture = async baseDir => {
+  const fixturesDir = path.join(baseDir, 'test', 'fixtures');
+  const content = [
+    'export default {',
+    '  props: {}',
+    '};',
+    ''
+  ].join('\n');
+
+  await mkdirp(fixturesDir);
+
+  return outputFile(path.join(fixturesDir, 'default.js'), content);
+};
+
+const initComponent = async ({relativePath}) => {
+  const baseDir = await createBaseDir(relativePath);
+
+  return Promise.all([
+    writeComponentSource(baseDir),
+    writeStyleSource(baseDir),
+    writeDefaultFixture(baseDir)
+  ]);
+};
+
+const runScript = async () => {
+  const argv = minimist(process.argv.slice(2));
+  const [relativePath] = argv._;
+
+  await initComponent({relativePath});
+
+  console.log(`Base component has been created into ${relativePath}`);
+};
+
+runScript();
