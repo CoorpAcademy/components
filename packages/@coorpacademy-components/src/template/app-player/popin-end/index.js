47771cbcd06aa484b46ee75106505981128f9a13
chrisdugne <chris.dugne@uralys.com>
2017-06-13 20:50:49 +0200

    enhanced template

--- packages/@coorpacademy-components/src/template/app-player/popin-end/index.js
+++ packages/@coorpacademy-components/src/template/app-player/popin-end/index.js
@@ -5,19 +5,25 @@ import Loader from '../../../atom/loader';
 import Summary from '../../../organism/app-player/summary';
 import style from './style.css';
 
+const Content = props => {
+  if (props.header) {
+    return (
+      <div className={style.popinWrapper}>
+        <Summary {...props} />
+      </div>
+    );
+  } else {
+    return <Loader />;
+  }
+};
+
 const PopinEnd = props => {
   const {header, summary} = props;
-
-  const content = summary.header
-    ? <div className={style.popinWrapper}>
-        <Summary {...summary} />
-      </div>
-    : <Loader />;
 
   return (
     <div className={style.wrapper}>
       <SlidesHeader {...header} />
-      {content}
+      <Content {...summary} />
     </div>
   );
 };
