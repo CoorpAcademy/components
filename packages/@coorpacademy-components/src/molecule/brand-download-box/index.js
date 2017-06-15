378e6b7cc733e7ceb28845b75478d5ddd53f8985
David Corticchiato <david.corticchiato@gmail.com>
2017-06-15 15:46:29 +0200

    brand-download-box: icon migration from Font Awesome to Nova

--- packages/@coorpacademy-components/src/molecule/brand-download-box/index.js
+++ packages/@coorpacademy-components/src/molecule/brand-download-box/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import AlertIcon from '@coorpacademy/nova-icons/solid/interface-feedback/interface-alert-circle';
+import getOr from 'lodash/fp/getOr';
 import Button from '../../atom/button';
+import Provider from '../../atom/provider';
+
 import style from './style.css';
 
-const BrandDownloadBox = props => {
+const BrandDownloadBox = (props, context) => {
   const {title = '', warning = '', description = '', href = '', submitValue = ''} = props;
+  const {skin} = context;
+  const negativeColor = getOr('#F73F52', 'common.negative', skin);
+
 
   const type = warning
     ? <div className={style.texts}>
-        <i className={style.icon} />
+        <AlertIcon className={style.icon} color={negativeColor} />
         <span className={style.warning}>{warning}</span>
         <p className={style.description}>{description}</p>
       </div>
@@ -23,6 +30,10 @@ const BrandDownloadBox = props => {
       <Button className={style.button} type="a" href={href} submitValue={submitValue} />
     </div>
   );
+};
+
+BrandDownloadBox.contextTypes = {
+  skin: Provider.childContextTypes.skin
 };
 
 BrandDownloadBox.propTypes = {
