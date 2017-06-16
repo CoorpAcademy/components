0434f9cf22f11c24956f5b57d4e79f79a4e07f92
Joan Ortega <jomaora@gmail.com>
2017-06-16 14:35:10 +0200

    allow empty answers

--- packages/@coorpacademy-app-player/src/services/progressions.js
+++ packages/@coorpacademy-app-player/src/services/progressions.js
@@ -8,6 +8,7 @@ import uniqueId from 'lodash/fp/uniqueId';
 import update from 'lodash/fp/update';
 import pipe from 'lodash/fp/pipe';
 import sample from 'lodash/fp/sample';
+import getOr from 'lodash/fp/getOr';
 import set from 'lodash/fp/set';
 import reduce from 'lodash/fp/reduce';
 import progressionsData from './progressions.data';
@@ -35,7 +36,7 @@ const save = progression => {
 };
 
 export const createAnswer = async (progressionId, payload) => {
-  const userAnswers = payload.answers;
+  const userAnswers = getOr([''], 'answers', payload);
   const slideId = payload.content.ref;
   const slide = await SlidesService.findById(slideId);
   const progression = await findById(progressionId);
