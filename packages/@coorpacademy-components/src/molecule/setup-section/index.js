005381e517ef2d8c6dd9da62268b89ae190543b6
Joan Ortega <jomaora@gmail.com>
2017-06-20 14:57:01 +0200

    migration novaicon setup-section

--- packages/@coorpacademy-components/src/molecule/setup-section/index.js
+++ packages/@coorpacademy-components/src/molecule/setup-section/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ArrowDownIcon from '@coorpacademy/nova-icons/composition/navigation/arrow-down';
+import ArrowUpIcon from '@coorpacademy/nova-icons/composition/navigation/arrow-top';
+import get from 'lodash/fp/get';
 import Provider from '../../atom/provider';
 import InputSwitch from '../../atom/input-switch';
 import style from './style.css';
 
 const SetupSection = (props, context) => {
-  const {translate} = context;
+  const {translate, skin} = context;
+  const brand = get('common.brand', skin);
+  const medium = get('common.medium', skin);
 
   const {title, onUp, onDown, display} = props;
+  const colorUp = onDown ? brand : medium;
+  const colorDown = onUp ? brand : medium;
 
   return (
     <div className={style.wrapper}>
@@ -19,15 +26,16 @@ const SetupSection = (props, context) => {
           {translate('Show')}
         </div>
         <InputSwitch {...display} />
-        <div className={onDown ? style.down : style.disabledDown} onClick={onDown} />
-        <div className={onUp ? style.up : style.disabledUp} onClick={onUp} />
+        <ArrowDownIcon color={colorUp} className={onDown ? style.arrow : style.disabled} onClick={onDown} />
+        <ArrowUpIcon color={colorDown} className={onUp ? style.arrow : style.disabled} onClick={onUp} />
       </div>
     </div>
   );
 };
 
 SetupSection.contextTypes = {
-  translate: Provider.childContextTypes.translate
+  translate: Provider.childContextTypes.translate,
+  skin: Provider.childContextTypes.skin
 };
 
 SetupSection.propTypes = {
