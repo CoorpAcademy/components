92af0f2dd19cac7e89325581418169883da7a77f
David Corticchiato <david.corticchiato@gmail.com>
2017-06-20 17:54:59 +0200

    start-battle: icon migration from Font Awesome to Nova

--- packages/@coorpacademy-components/src/molecule/dashboard/start-battle/index.js
+++ packages/@coorpacademy-components/src/molecule/dashboard/start-battle/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import getOr from 'lodash/fp/getOr';
+import BoltIcon from '@coorpacademy/nova-icons/composition/coorpacademy/bolt';
+import Provider from '../../../atom/provider';
 import Link from '../../../atom/link';
 import style from './style.css';
 
 const StartBattle = (props, context) => {
   const {label, onClick, href} = props;
+  const {skin} = context;
+
+  const white = getOr('#fff', 'common.white', skin);
+  const battleColor = getOr('#fff', 'common.battle', skin);
 
   return (
     <div className={style.container} data-name="start-battle">
       <div className={style.wrapper}>
         <div className={style.start}>
           <Link className={style.animate} onClick={onClick} href={href}>
-            <div className={style.icon} />
-            <div className={style.bubble}>
-              <div className={style.icon} />
+            <div className={style.backBubble}>
+              <BoltIcon color={battleColor} className={style.icon} />
+            </div>
+            <div className={style.bubble} />
+            <div className={style.iconBubble}>
+              <BoltIcon color={white} className={style.icon} />
             </div>
             <div
               className={style.label}
@@ -27,6 +37,10 @@ const StartBattle = (props, context) => {
       </div>
     </div>
   );
+};
+
+StartBattle.contextTypes = {
+  skin: Provider.childContextTypes.skin
 };
 
 StartBattle.propTypes = {
