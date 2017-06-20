01bbb9a0ce6e0ecf5e2637ff7a6affd8a789babe
Adriean Khisbe <adriean.khisbe@live.fr>
2017-06-20 14:58:44 +0200

    draft de la structure du composant analytics sidebar

--- packages/@coorpacademy-components/src/organism/analytics-sidebar/index.js
+++ packages/@coorpacademy-components/src/organism/analytics-sidebar/index.js
@@ -2,10 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './style.css';
 
+const FilterItem = props => {
+  return (
+    <li>
+      {props.text}
+    </li>
+  );
+};
+
+const SelectItem = props => {
+  return (
+    <li>
+      {props.text}
+    </li>
+  );
+};
+
 const AnalyticsSidebar = props => {
   return (
     <div className={style.text}>
-      {props.text}
+      <div className={style.navigation}>
+        <ul>
+          <FilterItem text="Target" />
+          <FilterItem text="Population" />
+          <SelectItem text="Engagament" />
+          <SelectItem text="Per User" />
+          <SelectItem text="Per Module" />
+          <SelectItem text="Exports" />
+        </ul>
+      </div>
+      <div className={style.info}>
+        TO BE DONE
+      </div>
     </div>
   );
 };
