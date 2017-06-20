ffafa8f01330bea4f5183f299a764bb826cd3881
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-06-20 09:25:20 +0200

    installation csslint

--- packages/@coorpacademy-components/postcss.config.js
+++ packages/@coorpacademy-components/postcss.config.js
@@ -3,6 +3,19 @@ module.exports = {
     require('postcss-modules-values-replace')(),
     require('postcss-calc')(),
     require('postcss-color-function')(),
+    require('stylelint')(
+    {"rules": {
+        "color-no-hex": [
+          true,
+          {
+            "message": "Please use variable colors of color.css",
+            "severity": "error"
+          }
+        ],
+        "indentation": [2]
+      }
+    }
+    ),
     require('autoprefixer')({
       browsers: ['last 2 versions']
     })
