e759e963bc5779d267525a0a34e926c58a331c15
Adriean Khisbe <adriean.khisbe@live.fr>
2017-06-21 11:39:49 +0200

    styling refactors in progress

--- packages/@coorpacademy-components/src/organism/sidebar/index.js
+++ packages/@coorpacademy-components/src/organism/sidebar/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import getOr from 'lodash/fp/getOr';
 import Link from '../../atom/link/index';
+import Provider from '../../atom/provider';
 import Select from '../../atom/select/index';
 import style from './style.css';
 
@@ -20,29 +22,43 @@ const FilterItem = props => {
 
 const SelectItem = props => {
   return (
-    <li className={props.selected ? `${style.linkItem} ${style.selectLinkItem}` : style.linkItem}>
+    <li
+      className={style.linkItem}
+      style={{
+        borderLeftColor: props.selected ? props.color : 'none'
+      }}
+    >
       <Link href={props.href} onClick={props.handleOnClick}>
         {props.title}
       </Link>
     </li>
-  );
+  ); // a color to handle
 };
 
 const InfoItem = props => {
   return (
-    <ul className={style.infoItem}>
+    <ul
+      className={style.infoItem}
+    >
       <li className={style.infoItemTitle}>
         {props.title}
       </li>
-      <li className={style.infoItemContent}>
+      <li className={style.infoItemContent}
+          style={{
+            borderLeftColor: props.color
+          }}
+      >
         {props.info}
       </li>
     </ul>
   );
 };
 
-const Sidebar = props => {
+const Sidebar = (props, context) => {
   const sections = Array.isArray(props.items[0]) ? props.items : [props.items];
+  const {skin} = context;
+  const defaultColor = getOr('#00B0FF', 'common.primary', skin);
+
   return (
     <div className={style.sidebar}>
       {sections.map((sidebarSection, idx) => (
@@ -56,6 +72,7 @@ const Sidebar = props => {
                     title={item.title}
                     options={item.options}
                     handleOnChange={item.onChange}
+                    color={defaultColor}
                   />
                 );
               else if (item.type === 'link')
@@ -66,10 +83,13 @@ const Sidebar = props => {
                     handleOnClick={item.onClick}
                     href={item.href}
                     selected={item.selected || false}
+                    color={defaultColor}
                   />
                 );
               else if (item.type === 'info')
-                return <InfoItem key={index} title={item.title} info={item.value} />;
+                return (
+                  <InfoItem key={index} title={item.title} info={item.value} color={defaultColor} />
+                );
               return null;
             })}
           </ul>
@@ -108,6 +128,9 @@ Sidebar.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.oneOfType([SectionProptype, PropTypes.arrayOf(SectionProptype).isRequired])
   ).isRequired
+};
+Sidebar.contextTypes = {
+  skin: Provider.childContextTypes.skin
 };
 
 export default Sidebar;
