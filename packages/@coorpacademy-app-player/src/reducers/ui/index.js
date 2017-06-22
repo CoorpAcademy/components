207c563b6548ba48e5fe3271224bc666c2c2afda
Joan Ortega <jomaora@gmail.com>
2017-06-22 14:37:23 +0200

    adding structure for onclick accordion and toggle

--- packages/@coorpacademy-app-player/src/reducers/ui/index.js
+++ packages/@coorpacademy-app-player/src/reducers/ui/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import answers from './answers';
+import corrections from './corrections';
 import current from './current';
 
 export default combineReducers({
   answers,
-  current
+  current,
+  corrections
 });
