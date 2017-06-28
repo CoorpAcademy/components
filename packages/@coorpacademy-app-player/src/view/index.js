48f22a5a7ba2d89c391ca347b213a12ba2ab762b
godu <arthur.weber@viacesi.fr>
2017-06-28 17:31:20 +0200

    Fix view

--- packages/@coorpacademy-app-player/src/view/index.js
+++ packages/@coorpacademy-app-player/src/view/index.js
@@ -2,6 +2,7 @@ import {createElement} from 'react';
 import cond from 'lodash/fp/cond';
 import constant from 'lodash/fp/constant';
 import get from 'lodash/fp/get';
+import isNil from 'lodash/fp/isNil';
 import isEqual from 'lodash/fp/isEqual';
 import pipe from 'lodash/fp/pipe';
 import Provider from '@coorpacademy/components/es/atom/provider';
@@ -13,13 +14,15 @@ import createLoadingView from './loading';
 
 const wrapInProvider = options => vNode => createElement(Provider, options, vNode);
 
+const hasNotProgression = pipe(getCurrentProgression, isNil);
+const hasNotContent = pipe(getCurrentContent, isNil);
 const matchRoute = route => pipe(getRoute, isEqual(route));
 const matchContentType = contentType => pipe(getCurrentContent, get('type'), isEqual(contentType));
 
 const createMapStateToProps = (options, store) =>
   cond([
-    [getCurrentProgression, createLoadingView(options, store)],
-    [getCurrentContent, createLoadingView(options, store)],
+    [hasNotProgression, createLoadingView(options, store)],
+    [hasNotContent, createLoadingView(options, store)],
     [matchRoute('correction'), createPopinCorrectionView(options, store)],
     [matchContentType('slide'), createSlideView(options, store)],
     [matchContentType('success'), createPopinEndView(options, store)],
