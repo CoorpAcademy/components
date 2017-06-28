f40fc7004dbb34d0fe536fc493fef6ab9180574f
Joan Ortega <jomaora@gmail.com>
2017-06-28 17:43:32 +0200

    adding pdf and tests

---
+++ packages/@coorpacademy-app-player/src/actions/ui/test/corrections.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import set from 'lodash/fp/set';
+import macro from '../../test/helpers/macro';
+import {selectResource, UI_SELECT_RESOURCE_IN_POPIN} from '../corrections';
+
+test('should set state with selected resource', macro, {}, t => ({}), selectResource('videoId'), [
+  [
+    {
+      type: UI_SELECT_RESOURCE_IN_POPIN,
+      payload: {id: 'videoId'}
+    },
+    set('ui.current.playResource', 'videoId', {})
+  ]
+]);
