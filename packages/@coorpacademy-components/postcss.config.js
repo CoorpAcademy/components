ba3b108a291e7ebf3d1405f340ecfe2cecee8322
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-07-03 10:46:22 +0200

    stylelint + postcssReporter

--- packages/@coorpacademy-components/postcss.config.js
+++ packages/@coorpacademy-components/postcss.config.js
@@ -1,21 +1,26 @@
 module.exports = {
   plugins: [
+    require('postcss-easy-import'),
     require('postcss-modules-values-replace')(),
     require('postcss-calc')(),
     require('postcss-color-function')(),
-    require('stylelint')(
-    {"rules": {
-        "block-no-empty": null,
-        "color-no-invalid-hex": true,
-        "declaration-colon-space-after": "always",
-        "indentation": ["tab", {
-          "except": [2]
-        }],
-        "max-empty-lines": 2,
-        "color-no-hex": [true, { "severity": "error", "message": "Please use variable colors of color.css"}]
+    require("postcss-reporter")({"clearMessages": true}),
+    require('stylelint')({
+      "rules": {
+        'block-no-empty': null,
+        'color-no-invalid-hex': true,
+        'declaration-colon-space-after': 'always',
+        'indentation': ['tab', {'except': [2]}],
+        'max-empty-lines': 2,
+        'color-no-hex': [
+          true,
+          {
+            'severity': 'error',
+            'message': 'Please use variable colors of color.css'
+          }
+        ]
       }
-    }
-    ),
+    }),
     require('autoprefixer')({
       browsers: ['last 2 versions']
     })
