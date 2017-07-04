2c5c2f34a01d4b5eb964d4f5b518f7d585d7f04b
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-07-04 18:01:23 +0200

    test lint

--- packages/@coorpacademy-components/postcss.config.js
+++ packages/@coorpacademy-components/postcss.config.js
@@ -4,19 +4,19 @@ module.exports = {
     require('postcss-modules-values-replace')(),
     require('postcss-calc')(),
     require('postcss-color-function')(),
-    require("postcss-reporter")({"clearMessages": true}),
+    require('postcss-reporter')({clearMessages: true}),
     require('stylelint')({
-      "rules": {
+      rules: {
         'block-no-empty': null,
         'color-no-invalid-hex': true,
         'declaration-colon-space-after': 'always',
-        'indentation': ['tab', {'except': [2]}],
+        indentation: ['tab', {except: [2]}],
         'max-empty-lines': 2,
         'color-no-hex': [
           true,
           {
-            'severity': 'error',
-            'message': 'Please use variable colors of color.css'
+            severity: 'error',
+            message: 'Please use variable colors of color.css'
           }
         ]
       }
