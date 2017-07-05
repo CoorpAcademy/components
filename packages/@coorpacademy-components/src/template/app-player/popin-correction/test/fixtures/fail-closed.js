965b764602793924a52fdad0e92230364a568ee8
David Corticchiato <david.corticchiato@gmail.com>
2017-07-05 17:02:04 +0200

    fix wrong answer max width

--- packages/@coorpacademy-components/src/template/app-player/popin-correction/test/fixtures/fail-closed.js
+++ packages/@coorpacademy-components/src/template/app-player/popin-correction/test/fixtures/fail-closed.js
@@ -5,7 +5,7 @@ export default defaultsDeep(correctClosed, {
   props: {
     header: {
       fail: true,
-      corrections: [{answer: 'Je ne sais pas', isCorrect: false}],
+      corrections: [{answer: 'Je ne sais pas et cela me rend vraiment triste ... sniff', isCorrect: false}],
       title: 'Aïe Aïe Aïe',
       subtitle: 'Mauvaise réponse'
     }
