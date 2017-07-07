c82eabde7e3f773229a4232aff63e3c0ad900c83
godu <arthur.weber@viacesi.fr>
2017-07-07 12:12:42 +0200

    Forbids console.error & console.warn in tests

--- packages/@coorpacademy-app-player/src/view/test/utils/render.js
+++ packages/@coorpacademy-app-player/src/view/test/utils/render.js
@@ -1,20 +1,10 @@
-import sinon from 'sinon';
 import identity from 'lodash/fp/identity';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import Provider from '@coorpacademy/components/es/atom/provider';
 
 const render = vNode => {
-  sinon.stub(console, 'error').callsFake(error => {
-    throw new Error(error);
-  });
-  try {
-    ReactDOM.renderToStaticMarkup(React.createElement(Provider, {translate: identity}, vNode));
-    console.error.restore(); // eslint-disable-line no-console
-  } catch (err) {
-    console.error.restore(); // eslint-disable-line no-console
-    throw err;
-  }
+  ReactDOM.renderToStaticMarkup(React.createElement(Provider, {translate: identity}, vNode));
 };
 
 export default render;
