c67b9b6f6d87004d6b401c79f4d759f085dd2d0e
Joan Ortega <jomaora@gmail.com>
2017-07-12 16:21:40 +0200

    adding flag to skip host check to allow acces to app-player

--- packages/@coorpacademy-app-player/sandbox/webpack.config.js
+++ packages/@coorpacademy-app-player/sandbox/webpack.config.js
@@ -1,8 +1,9 @@
 const set = require('lodash/fp/set');
+const pipe = require('lodash/fp/pipe');
 const config = require('../webpack.config');
 
 const entry = {
   Player: ['babel-polyfill', './sandbox']
 };
 
-module.exports = set('entry', entry)(config);
+module.exports = pipe(set('entry', entry), set('devServer.disableHostCheck', true))(config);
