11bef8ab689e26651164133da7f136249ee25a37
godu <arthur.weber@viacesi.fr>
2017-07-17 14:14:45 +0200

    Adds stylelint script

--- packages/@coorpacademy-components/postcss.config.js
+++ packages/@coorpacademy-components/postcss.config.js
@@ -4,16 +4,6 @@ module.exports = {
     require('postcss-modules-values-replace')(),
     require('postcss-calc')(),
     require('postcss-color-function')(),
-    require('stylelint')({
-      rules: {
-        'color-no-invalid-hex': [true, {severity: 'error'}],
-        'color-no-hex': [true, {severity: 'error'}],
-        'declaration-colon-space-after': 'always',
-        'declaration-block-no-duplicate-properties': true,
-        indentation: ['tab', {except: [2]}],
-        'max-empty-lines': 2
-      }
-    }),
     require('postcss-reporter')({clearAllMessages: true}),
     require('autoprefixer')({
       browsers: ['last 2 versions']
