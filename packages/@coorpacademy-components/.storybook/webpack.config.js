1f1443500e98264569192665dbcaa0c022e98646
godu <arthur.weber@viacesi.fr>
2017-07-18 16:03:23 +0200

    Fixes storybook

--- packages/@coorpacademy-components/.storybook/webpack.config.js
+++ packages/@coorpacademy-components/.storybook/webpack.config.js
@@ -3,39 +3,67 @@ const path = require('path');
 const webpack = require('webpack');
 const lowerCase = require('lodash/fp/lowerCase');
 
-
 module.exports = (storybookBaseConfig, configType) => {
   const NODE_ENV = lowerCase(configType);
 
-  storybookBaseConfig.module.rules.push({
-    test: /\.css$/,
-    use: [
-      {
-        loader: 'style-loader'
+  storybookBaseConfig.module.rules = [
+    {
+      test: /\.(ttf|otf|eot|svg|woff)$/,
+      loader: 'file-loader'
+    },
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      options: {
+        cacheDirectory: true,
+        presets: [
+          [
+            'env',
+            {
+              targets: {
+                browsers: ['last 2 versions']
+              },
+              modules: false,
+              loose: true
+            }
+          ]
+        ],
+        plugins: ['transform-object-assign']
       },
-      {
-        loader: 'css-loader',
-        options: {
-          minimize: true,
-          modules: true,
-          importLoaders: 1,
-          localIdentName: `${hash}`
+      include: [
+        __dirname,
+        path.join(__dirname, '../storybook'),
+        path.join(__dirname, '../src')
+      ]
+    },
+    {
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'style-loader'
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            minimize: true,
+            modules: true,
+            importLoaders: 1,
+            localIdentName: `${hash}`
+          }
+        },
+        {
+          loader: 'postcss-loader'
         }
-      },
-      {
-        loader: 'postcss-loader'
-      }
-    ],
-    include: path.join(__dirname, '../src')
-  });
-
-  storybookBaseConfig.plugins = [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(NODE_ENV)
-      }
-    })
+      ],
+      include: path.join(__dirname, '../src')
+    }
   ];
+
+  storybookBaseConfig.plugins = storybookBaseConfig.plugins.filter(
+    plugin => !(plugin instanceof webpack.optimize.UglifyJsPlugin)
+  );
+
+  console.log(storybookBaseConfig.plugins);
 
   return storybookBaseConfig;
 };
