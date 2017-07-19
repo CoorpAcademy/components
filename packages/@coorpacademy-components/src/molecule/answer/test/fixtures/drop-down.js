726b2c02f10c1c3bf03a95239752978de4d48570
Joan Ortega <jomaora@gmail.com>
2017-07-19 10:03:53 +0200

    clean style on question images to add service media computed image (#826)

--- packages/@coorpacademy-components/src/molecule/answer/test/fixtures/drop-down.js
+++ packages/@coorpacademy-components/src/molecule/answer/test/fixtures/drop-down.js
@@ -7,6 +7,8 @@ export default {
     model: {
       type: 'dropDown',
       ...answerProps
-    }
+    },
+    media:
+      'https://api-staging.coorpacademy.com/api-service/medias?h=300&w=300&q=90&url=https://user-images.githubusercontent.com/7602475/28273504-db1da18a-6b0e-11e7-97cd-b2e307fc51c7.png'
   }
 };
