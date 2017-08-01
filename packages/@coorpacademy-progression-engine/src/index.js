2b7964123f7c27c62e5f1f31b26971c904405d70
T1B0 <rtfmbb@gmail.com>
2017-08-01 17:15:18 +0200

    exports getEngineConfig to lib public api

--- packages/@coorpacademy-progression-engine/src/index.js
+++ packages/@coorpacademy-progression-engine/src/index.js
@@ -4,5 +4,13 @@ import checkAnswer from './check-answer';
 import checkAnswerCorrectness from './check-answer-correctness';
 import updateState from './update-state';
 import createProgression from './create-progression';
+import getConfig from './config';
 
-export {computeNextStep, checkAnswer, checkAnswerCorrectness, updateState, createProgression};
+export {
+  computeNextStep,
+  checkAnswer,
+  checkAnswerCorrectness,
+  updateState,
+  createProgression,
+  getConfig
+};
