45725a41312bf8964985849ab0c5d8d9ab1db382
chrisdugne <chris.dugne@uralys.com>
2017-08-08 18:36:06 +0200

    enhanced iframe testing

--- packages/@coorpacademy-components/src/molecule/video-iframe/test/index.js
+++ packages/@coorpacademy-components/src/molecule/video-iframe/test/index.js
@@ -2,39 +2,43 @@ import 'jsdom-global/register';
 import test from 'ava';
 import React from 'react';
 import {mount} from 'enzyme';
-import once from 'lodash/fp/once';
+import EventEmitter from 'events'; // eslint-disable-line fp/no-events
 import VideoIframe from '..';
 
-test('should call listeners within props', t => {
-  t.plan(6);
+test('should call listeners within props, then remove them on willComponentUnmount', t => {
+  t.plan(7);
 
-  class Player {
-    // eslint-disable-next-line class-methods-use-this
-    on(eventName, listener) {
-      listener();
+  class Player extends EventEmitter {
+    constructor() {
+      super();
     }
 
-    // eslint-disable-next-line class-methods-use-this
     off(eventName, listener) {
       t.pass();
+      this.removeListener(eventName, listener);
     }
   }
 
   const props = {
     type: 'vimeo',
     src: 'foo',
-    onPause: once(() => t.pass()),
-    onPlay: once(() => t.pass()),
-    onEnded: once(() => t.pass())
+    onPause: () => t.pass(),
+    onPlay: () => t.pass(),
+    onEnded: () => t.pass()
   };
 
   const component = <VideoIframe {...props} />;
   const context = {Vimeo: {Player}};
   const video = mount(component, {context});
+  const instance = video.instance();
 
-  video.instance().handlePlay();
-  video.instance().handlePause();
-  video.instance().handleEnded();
+  instance.setRefIframe('foo');
+  t.is(instance.iframe, 'foo');
+
+  instance.player.emit('play');
+  instance.player.emit('pause');
+  instance.player.emit('ended');
+
   video.unmount();
 });
 
