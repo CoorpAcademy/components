bf2fa1a51843ca37eb50162c8eefac924be18c7d
chrisdugne <chris.dugne@uralys.com>
2017-08-31 19:32:29 +0200

    refacto actions tests for answers-validation + accordion tests

---
+++ packages/@coorpacademy-app-player/src/actions/ui/test/answers-validation/check-accordion.js
@@ -0,0 +1,92 @@
+import test from 'ava';
+import pipe from 'lodash/fp/pipe';
+import set from 'lodash/fp/set';
+import flatten from 'lodash/fp/flatten';
+import macro from '../../../test/helpers/macro';
+import {validateAnswer} from '../../answers';
+import {
+  PROGRESSION_CREATE_ANSWER_REQUEST,
+  PROGRESSION_CREATE_ANSWER_SUCCESS
+} from '../../../api/progressions';
+import {accordionIsOpenAt, fetchCorrection} from './shared';
+
+const wrongAnswer = pipe(
+  set('state.content.ref', 'baz'),
+  set('state.isCorrect', false),
+  set('content', {ref: '5.C7'})
+)({});
+
+const viewedOneLesson = set(
+  'state.viewedResources',
+  [{type: 'chapter', ref: '5.C7', resources: ['lesson_1']}],
+  wrongAnswer
+);
+
+const viewedThreeLessons = set(
+  'state.viewedResources',
+  [{type: 'chapter', ref: '5.C7', resources: ['lesson_1', 'lesson_2', 'lesson_3']}],
+  wrongAnswer
+);
+
+const stateWithSlideAndManyResources = pipe(
+  set('data.progressions.entities.foo.state.nextContent', {type: 'slide', ref: 'baz'}),
+  set('data.slides.entities.baz.lessons', ['lesson_1', 'lesson_2', 'lesson_3'])
+)({});
+
+const services = result => t => ({
+  Progressions: {
+    createAnswer: (id, payload) => {
+      t.is(id, 'foo');
+      t.deepEqual(payload, {
+        content: {type: 'slide', ref: 'baz'},
+        answers: ['bar']
+      });
+      return result;
+    },
+    findById: id => {
+      t.is(id, 'foo');
+      return 'foo';
+    }
+  },
+  Answers: {
+    findById: id => {
+      t.is(id, 'foo');
+      return ['Bonne réponse'];
+    }
+  }
+});
+
+const answer = result => [
+  [
+    {
+      type: PROGRESSION_CREATE_ANSWER_REQUEST,
+      meta: {progressionId: 'foo'}
+    },
+    set('ui.current.progressionId', 'foo', {})
+  ],
+  [
+    {
+      type: PROGRESSION_CREATE_ANSWER_SUCCESS,
+      meta: {progressionId: 'foo'},
+      payload: result
+    }
+  ]
+];
+
+test(
+  'should provide a wrong answer and see klf opened',
+  macro,
+  stateWithSlideAndManyResources,
+  services(viewedOneLesson),
+  validateAnswer('foo', {answers: ['bar']}),
+  flatten([answer(viewedOneLesson), accordionIsOpenAt(0), fetchCorrection])
+);
+
+test(
+  'should provide a wrong answer and see video opened',
+  macro,
+  stateWithSlideAndManyResources,
+  services(viewedThreeLessons),
+  validateAnswer('foo', {answers: ['bar']}),
+  flatten([answer(viewedThreeLessons), accordionIsOpenAt(1), fetchCorrection])
+);
---
+++ packages/@coorpacademy-app-player/src/actions/ui/test/answers-validation/check-success.js
@@ -0,0 +1,93 @@
+import test from 'ava';
+import pipe from 'lodash/fp/pipe';
+import set from 'lodash/fp/set';
+import flatten from 'lodash/fp/flatten';
+import macro from '../../../test/helpers/macro';
+import {validateAnswer} from '../../answers';
+import {
+  PROGRESSION_CREATE_ANSWER_REQUEST,
+  PROGRESSION_CREATE_ANSWER_SUCCESS
+} from '../../../api/progressions';
+import {ANSWER_FETCH_REQUEST, ANSWER_FETCH_SUCCESS} from '../../../api/answers';
+import {accordionIsOpenAt} from './shared';
+
+const successfullyFetchAnswers = [
+  [
+    {
+      type: ANSWER_FETCH_REQUEST,
+      meta: {
+        progressionId: 'foo',
+        slideId: 'baz'
+      }
+    },
+    set('ui.answers.0.correction', null, {})
+  ],
+  [
+    {
+      type: ANSWER_FETCH_SUCCESS,
+      meta: {
+        progressionId: 'foo',
+        slideId: 'baz'
+      },
+      payload: {
+        correctAnswer: ['Bonne réponse'],
+        corrections: [{answer: 'bar', isCorrect: false}]
+      }
+    },
+    set('ui.answers.0.correction', 'Bonne réponse', {})
+  ]
+];
+
+const createCorrectAnswer = [
+  [
+    {
+      type: PROGRESSION_CREATE_ANSWER_REQUEST,
+      meta: {progressionId: 'foo'}
+    },
+    set('ui.current.progressionId', 'foo', {})
+  ],
+  [
+    {
+      type: PROGRESSION_CREATE_ANSWER_SUCCESS,
+      meta: {progressionId: 'foo'},
+      payload: pipe(set('state.content.ref', 'baz'), set('state.isCorrect', true))({})
+    },
+    set('data.progressions.entities.foo', null, {})
+  ]
+];
+
+test(
+  'should submit answers with the current content and refresh progression state',
+  macro,
+  set('data.progressions.entities.foo.state.nextContent', {
+    type: 'slide',
+    ref: 'baz'
+  })({}),
+  t => ({
+    Progressions: {
+      createAnswer: (id, payload) => {
+        t.is(id, 'foo');
+        t.deepEqual(payload, {
+          content: {type: 'slide', ref: 'baz'},
+          answers: ['bar']
+        });
+        return pipe(set('state.content.ref', 'baz'), set('state.isCorrect', true))({});
+      },
+      findById: id => {
+        t.is(id, 'foo');
+        return 'foo';
+      }
+    },
+    Answers: {
+      findById: (id, slideId, givenAnswer) => {
+        t.is(id, 'foo');
+        return {
+          correctAnswer: ['Bonne réponse'],
+          corrections: givenAnswer.map(answer => ({answer, isCorrect: false}))
+        };
+      }
+    }
+  }),
+  validateAnswer('foo', {answers: ['bar']}),
+  flatten([createCorrectAnswer, accordionIsOpenAt(2), successfullyFetchAnswers])
+);
