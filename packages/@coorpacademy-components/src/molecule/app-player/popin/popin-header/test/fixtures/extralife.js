a4a268e977e539dc1631fedd21dd412eeab1afa9
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-09-14 14:45:45 +0200

    transformation fixtures

--- packages/@coorpacademy-components/src/molecule/app-player/popin/popin-header/test/fixtures/extralife.js
+++ packages/@coorpacademy-components/src/molecule/app-player/popin/popin-header/test/fixtures/extralife.js
@@ -1,10 +1,13 @@
 export default {
   props: {
     fail: true,
-    remainingLifeRequests: 1,
+
     lives: 0,
     title: 'Aïe Aïe Aïe',
-    remainingLifeRequestsSentence: 'Bonus ! Récupérez une vie en regardant la leçon !',
+    extraLife: {
+      active: true, // remainingLifeRequests
+      sentence: 'Bonus ! Récupérez une vie en regardant la leçon !' // remainingLifeRequestsSentence
+    },
     subtitle: 'Mauvaise réponse',
     corrections: [{answer: 'Je ne sais pas', isCorrect: false}],
     cta: {
