b47de3ca8dd99cfef2b92cc1c280246cd67d4b99
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-09-15 09:54:29 +0200

    Adding accepted action for extra-life

--- packages/@coorpacademy-app-player/src/utils/state-extract.js
+++ packages/@coorpacademy-app-player/src/utils/state-extract.js
@@ -87,8 +87,12 @@ export const getCurrentCorrection = state => {
   return getCorrection(getId(progression), getId(slide))(state);
 };
 
-export const getXtralife = (progressionId, slideId) => state => {
-  return get(['data', 'xtralife', 'entities', progressionId], state);
+export const getExtralifeRefused = (progressionId, slideId) => state => {
+  return get(['data', 'ExtralifeRefused', 'entities', progressionId], state);
+};
+
+export const getExtralifeAccepted = (progressionId, slideId) => state => {
+  return get(['data', 'ExtralifeAccepted', 'entities', progressionId], state);
 };
 
 export const getClue = (progressionId, slideId) => state => {
