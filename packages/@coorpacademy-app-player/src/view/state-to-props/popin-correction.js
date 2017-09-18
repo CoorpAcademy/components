4b84304dbbdfbdc50751c4ec1b59cc2b95be9855
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-09-18 15:06:12 +0200

    fix cta next or GameOver

--- packages/@coorpacademy-app-player/src/view/state-to-props/popin-correction.js
+++ packages/@coorpacademy-app-player/src/view/state-to-props/popin-correction.js
@@ -59,7 +59,7 @@ const popinCorrectionStateToProps = (options, store) => state => {
             sentence: translate('Bonus ! Get an extra life by viewing the lesson')
           },
           cta: {
-            title: isExtraLifeActive ? translate('Game over') : translate('Next'),
+            title: progression.state.lives === 0 ? translate('Game over') : translate('Next'),
             onClick: progression.state.lives !== 0 ? resetProgression : requestExtralifeRefused
           },
           ...header
