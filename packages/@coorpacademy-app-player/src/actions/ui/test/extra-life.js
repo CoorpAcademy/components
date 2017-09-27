084525cdf23b43aa7baae609621a490a1be75500
David Corticchiato <david.corticchiato@gmail.com>
2017-09-27 16:38:53 +0200

    manage popin cta cases

--- packages/@coorpacademy-app-player/src/actions/ui/test/extra-life.js
+++ packages/@coorpacademy-app-player/src/actions/ui/test/extra-life.js
@@ -2,11 +2,18 @@ import test from 'ava';
 import pipe from 'lodash/fp/pipe';
 import set from 'lodash/fp/set';
 import macro from '../../test/helpers/macro';
-import {UI_REVIVAL_PENDING, pending, refuseExtraLifeAndReset} from '../extra-life';
+import {
+  UI_REVIVAL_PENDING,
+  pending,
+  acceptExtraLifeAndReset,
+  refuseExtraLifeAndReset
+} from '../extra-life';
 import {UI_SELECT_PROGRESSION} from '../progressions';
 import {
   PROGRESSION_FETCH_REQUEST,
   PROGRESSION_FETCH_FAILURE,
+  PROGRESSION_EXTRALIFEACCEPTED_REQUEST,
+  PROGRESSION_EXTRALIFEACCEPTED_SUCCESS,
   PROGRESSION_EXTRALIFEREFUSED_REQUEST,
   PROGRESSION_EXTRALIFEREFUSED_SUCCESS
 } from '../../api/progressions';
@@ -45,6 +52,60 @@ test(
     },
     {
       type: PROGRESSION_EXTRALIFEREFUSED_SUCCESS,
+      payload: 'foo',
+      meta: {progressionId: 'foo'}
+    },
+    [
+      {
+        type: UI_SELECT_PROGRESSION,
+        payload: {id: 'foo'}
+      },
+      set('ui.current.progressionId', 'foo', {})
+    ],
+    [
+      {
+        type: PROGRESSION_FETCH_REQUEST,
+        meta: {id: 'foo'}
+      },
+      set('data.progressions.entities.foo', null, {})
+    ],
+    {
+      type: PROGRESSION_FETCH_FAILURE,
+      meta: {id: 'foo'},
+      error: true,
+      payload: new Error()
+    }
+  ]
+);
+
+test(
+  'should dispatch accept and reset progression',
+  macro,
+  pipe(
+    set('ui.current.progressionId', 'foo'),
+    set('data.progressions.entities.foo._id', 'foo'),
+    set('data.progressions.entities.foo.content', {type: 'node', ref: 'extraLife'})
+  )({}),
+  t => ({
+    Progressions: {
+      findById: id => {
+        t.is(id, 'foo');
+        throw new Error();
+      },
+      acceptExtraLife: id => {
+        t.is(id, 'foo');
+        return 'foo';
+      }
+    }
+  }),
+  acceptExtraLifeAndReset('foo'),
+  [
+    {
+      type: PROGRESSION_EXTRALIFEACCEPTED_REQUEST,
+      meta: {progressionId: 'foo'}
+    },
+    {
+      type: PROGRESSION_EXTRALIFEACCEPTED_SUCCESS,
       payload: 'foo',
       meta: {progressionId: 'foo'}
     },
