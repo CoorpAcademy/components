89ef62bc073805b61212495ec3edb5209da706fc
Gaelle MOURARET <gaellemouraret@gmail.com>
2017-09-27 10:51:09 +0200

    correction action name

--- packages/@coorpacademy-app-player/src/reducers/ui/extra-life.js
+++ packages/@coorpacademy-app-player/src/reducers/ui/extra-life.js
@@ -1,7 +1,7 @@
 import set from 'lodash/fp/set';
 import {UI_REVIVAL_PENDING} from '../../actions/ui/extra-life';
 
-const uiExtraLifeReducer = (state = {acceptPending: false}, action) => {
+const uiExtraLifeReducer = (state = {acceptRevivalPending: false}, action) => {
   switch (action.type) {
     case UI_REVIVAL_PENDING: {
       return set('acceptRevivalPending', true, state);
