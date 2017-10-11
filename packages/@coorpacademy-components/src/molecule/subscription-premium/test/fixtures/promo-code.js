95feeebc56ce2faea31ef7587beff008ca3a48fa
David Corticchiato <david.corticchiato@gmail.com>
2017-10-11 11:51:22 +0200

    add promo code case in store settings (#1008)
    * add promo code case in store settings
    
    * add missing propType

--- packages/@coorpacademy-components/src/molecule/subscription-premium/test/fixtures/promo-code.js
+++ packages/@coorpacademy-components/src/molecule/subscription-premium/test/fixtures/promo-code.js
@@ -1,8 +1,7 @@
 export default {
   props: {
     subscriptionPremiumDescriptionDate: '24/04/2017',
-    subscriptionPremiumDescriptionPrice: '9.90',
-    isPromoCode: false,
+    isPromoCode: true,
     checkImage: 'http://iconshow.me/media/images/Mixed/small-n-flat-icon/png2/256/-sign-check.png'
   }
 };
