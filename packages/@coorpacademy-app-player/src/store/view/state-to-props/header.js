d3f99a7307c7055ccf3d4379c6b99db330b8abaf
chrisdugne <chris.dugne@uralys.com>
2017-10-12 15:31:21 +0200

    plugged header props

--- packages/@coorpacademy-app-player/src/store/view/state-to-props/header.js
+++ packages/@coorpacademy-app-player/src/store/view/state-to-props/header.js
@@ -1,13 +1,48 @@
+import get from 'lodash/fp/get';
 import getOr from 'lodash/fp/getOr';
-import {getLives, getCurrentContent} from '../../utils/state-extract';
+import {getEngine, getLives, getCurrentContent} from '../../utils/state-extract';
+
+const headerContent = (engineRef, state) => {
+  const content = getCurrentContent(state);
+
+  switch (engineRef) {
+    case 'learner':
+      return {
+        title: 'todo get level name',
+        details: get('level')(content)
+      };
+    case 'microlearning':
+      return {
+        title: getOr('', 'name')(content)
+      };
+    default:
+      return null;
+  }
+};
+
+const headerSubcontent = (engineRef, state) => {
+  switch (engineRef) {
+    case 'learner':
+      return {
+        title: 'todo get chapter name',
+        details: 'todo i/n'
+      };
+    case 'microlearning':
+    default:
+      return null;
+  }
+};
 
 const headerProps = (options, store) => state => {
-  const content = getCurrentContent(state);
+  const engine = getEngine(state);
+  const {ref: engineRef} = engine;
   return {
-    backHref: '/',
-    primary: {
-      title: getOr('', 'name', content)
+    type: engineRef,
+    content: {
+      href: '/',
+      ...headerContent(engineRef, state)
     },
+    subcontent: headerSubcontent(engineRef, state),
     lives: {
       count: getLives(state)
     }
