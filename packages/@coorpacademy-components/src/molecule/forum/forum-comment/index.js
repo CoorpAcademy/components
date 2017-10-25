25758df82d02b6184476e6f8f28efcdd19ef32c1
esa-coorp <eric.sampaio@coorpacademy.com>
2017-10-25 16:38:37 +0200

    fix style

--- packages/@coorpacademy-components/src/molecule/forum/forum-comment/index.js
+++ packages/@coorpacademy-components/src/molecule/forum/forum-comment/index.js
@@ -12,6 +12,7 @@ const ForumComment = (props, context) => {
   const {avatar, onPost, onChange, value, textareaDisabled, postDisabled, newPost = false} = props;
 
   const primary = get('common.primary', skin);
+  const light = get('common.light', skin);
 
   const avatarView =
     avatar &&
@@ -26,6 +27,9 @@ const ForumComment = (props, context) => {
         onClick={onPost}
         disabled={postDisabled}
         submitValue={translate('Post')}
+        style={{
+          backgroundColor: postDisabled ? light : primary
+        }}
         className={postDisabled ? style.disabledButton : style.button}
       />
     </div>
