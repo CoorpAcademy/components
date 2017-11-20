5614729fd2f71264bc0aec83be8c3bd8753a83a3
David Corticchiato <david.corticchiato@gmail.com>
2017-11-20 18:38:01 +0100

    animation-loop component using animation frames

--- packages/@coorpacademy-components/src/template/app-player/popin-end/test/animator.test.js
+++ packages/@coorpacademy-components/src/template/app-player/popin-end/test/animator.test.js
@@ -1,42 +1,69 @@
 import test from 'ava';
-import React from 'react';
-import identity from 'lodash/fp/identity';
-import {render} from 'enzyme';
-import Animator from '../animator';
+import {
+  FIRST_FRAME,
+  getTargetedValue,
+  updateCounter,
+  formatCounter,
+  updateChildProps
+} from '../animator';
 import correctFixture from './fixtures/correct';
 
-const TestApp = props => {
-  const {ChildType, ...childProps} = props;
+test('should return 0 if regex does not match', t => {
+  const value = getTargetedValue({foo: '+++5'}, 'foo');
 
-  return (
-    <Animator startTimer={identity} stopTimer={identity}>
-      <ChildType {...childProps} />
-    </Animator>
-  );
-};
-
-test('should not add rank and stars if not present in original props', t => {
-  let latestProps = {};
-  const TestChild = props => {
-    latestProps = props;
-    return <div />;
-  };
-
-  render(<TestApp ChildType={TestChild} />);
-
-  t.false('stars' in latestProps.summary.header);
-  t.false('rank' in latestProps.summary.header);
+  t.is(value, 0);
 });
 
-test('should initialize rank and stars to 0', t => {
-  let latestProps = {};
-  const TestChild = props => {
-    latestProps = props;
-    return <div />;
-  };
+test('should return 0 if path does not exist', t => {
+  const value = getTargetedValue({foo: '+5'}, 'bar');
 
-  render(<TestApp ChildType={TestChild} {...correctFixture.props} />);
+  t.is(value, 0);
+});
 
-  t.is(latestProps.summary.header.stars, '+0');
-  t.is(latestProps.summary.header.rank, '+0');
+test('should return targeted value if regex match', t => {
+  const value = getTargetedValue({foo: '+5'}, 'foo');
+
+  t.is(value, 5);
+});
+
+test('should return 0 at frame 0', t => {
+  const value = updateCounter(100, 0);
+
+  t.is(value, 0);
+});
+
+test('should return target after last frame', t => {
+  const value = updateCounter(100, {index: FIRST_FRAME + 100});
+
+  t.is(value, 100);
+});
+
+test('should not format counter if not present in original props', t => {
+  const props = {bar: '+0'};
+  const newProps = formatCounter(props, 'foo', 100)(props);
+
+  t.deepEqual(newProps, props);
+});
+
+test('should format counter if present in original props', t => {
+  const props = {foo: '+0'};
+  const newProps = formatCounter(props, 'foo', 100)(props);
+
+  t.deepEqual(newProps, {foo: '+100'});
+});
+
+test('should set counters in child props to 0 at first frame', t => {
+  const frame = {index: 0};
+  const newProps = updateChildProps(correctFixture.props, {frame});
+
+  t.is(newProps.summary.header.stars, '+0');
+  t.is(newProps.summary.header.rank, '+0');
+});
+
+test('should set counters in child props to target after last frame', t => {
+  const frame = {index: FIRST_FRAME + 29};
+  const newProps = updateChildProps(correctFixture.props, {frame});
+
+  t.is(newProps.summary.header.stars, '+29');
+  t.is(newProps.summary.header.rank, '+5');
 });
