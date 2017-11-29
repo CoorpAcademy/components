5151a2814ba8fc4d2c0e2faaa39bc639de313914
David Corticchiato <david.corticchiato@gmail.com>
2017-11-29 15:26:45 +0100

    swapper enzyme test

--- packages/@coorpacademy-components/src/hoc/swapper/index.js
+++ packages/@coorpacademy-components/src/hoc/swapper/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import isEqual from 'lodash/fp/isEqual';
-import shallowCompare from '../../util/shallow-compare';
+
+export const safeCancelAnimationFrame = requestId => requestId && cancelAnimationFrame(requestId);
 
 class Swapper extends React.Component {
   state = {
@@ -11,41 +11,16 @@ class Swapper extends React.Component {
     this.requestID = requestAnimationFrame(() => this.setState(() => ({init: false})));
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (isEqual(this.props, nextProps)) return;
-
-    const {requestID} = this;
-    if (requestID) cancelAnimationFrame(requestID);
-
-    this.requestID = requestAnimationFrame(() => {
-      this.setState(
-        () => ({init: true}),
-        () => {
-          this.requestID = requestAnimationFrame(() => {
-            this.setState(() => ({init: false}));
-          });
-        }
-      );
-    });
-  }
-
-  shouldComponentUpdate(...args) {
-    return shallowCompare(this, ...args);
-  }
-
   componentWillUnmount() {
-    const {requestID} = this;
-    if (requestID) cancelAnimationFrame(requestID);
+    safeCancelAnimationFrame(this.requestID);
   }
 
   render() {
     const {init} = this.state;
     const {children, ...props} = this.props;
-
     const child = React.Children.only(children);
 
-    if (init) return React.cloneElement(child, props)
-    return child;
+    return init ? child : React.cloneElement(child, props);
   }
 }
 
---
+++ packages/@coorpacademy-components/src/hoc/swapper/test/on-mount.js
@@ -0,0 +1,63 @@
+import browserEnv from 'browser-env';
+import test from 'ava';
+import React from 'react';
+import {mount, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Swapper, {safeCancelAnimationFrame} from '..';
+
+browserEnv({pretendToBeVisual: true});
+configure({adapter: new Adapter()});
+
+const nextFrame = () => new Promise(requestAnimationFrame);
+
+const TestApp = () => (
+  <Swapper data-selected data-more={42}>
+    <div data-selected={false} data-name="test" />
+  </Swapper>
+);
+
+test('should not crash if trying to cancel animation frame without requestId', t => {
+  return t.notThrows(safeCancelAnimationFrame);
+});
+
+test('should cancel animation frame callback', async t => {
+  t.plan(1);
+
+  let requestId = requestAnimationFrame(() => t.pass());
+
+  await nextFrame();
+
+  requestId = requestAnimationFrame(() => t.pass());
+  safeCancelAnimationFrame(requestId);
+
+  await nextFrame();
+});
+
+test('should not apply new props before next frame', t => {
+  const wrapper = mount(<TestApp />);
+  const props = wrapper.find('div').props();
+
+  wrapper.unmount();
+
+  t.deepEqual(props, {
+    'data-name': 'test',
+    'data-selected': false
+  });
+});
+
+test('should apply new props after next frame', async t => {
+  const wrapper = mount(<TestApp />);
+
+  await nextFrame();
+  wrapper.update();
+
+  const props = wrapper.find('div').props();
+
+  wrapper.unmount();
+
+  return t.deepEqual(props, {
+    'data-name': 'test',
+    'data-selected': true,
+    'data-more': 42
+  });
+});
