c15dd4fd303600d0758d3f1ad8897a09b4535cb6
David Corticchiato <david.corticchiato@gmail.com>
2017-12-01 16:40:21 +0100

    selectProgression instead of fetching directly

--- packages/@coorpacademy-app-racing/src/store/actions/ui/progressions.js
+++ packages/@coorpacademy-app-racing/src/store/actions/ui/progressions.js
@@ -40,43 +40,6 @@ export const selectProgression = id => async (dispatch, getState) => {
   });
 
   const progressionId = getCurrentProgressionId(getState());
-  const response = await dispatch(fetchProgression(progressionId));
-  if (response.error) return response;
 
-  await dispatch(fetchStartRank());
-  const progressionContent = getProgressionContent(getState());
-  const engine = getEngine(getState());
-  await dispatch(fetchContent(progressionContent.type, progressionContent.ref));
-  await dispatch(fetchBestProgression(progressionContent, progressionId));
-  await dispatch(fetchEngineConfig(engine));
-  await dispatch(fetchContentInfo(progressionContent, engine));
-
-  const {ref, type} = getStepContent(getState());
-
-  switch (type) {
-    case 'slide': {
-      const chapterResult = await dispatch(fetchSlideChapter(ref));
-      const slideResult = getSlide(ref)(getState());
-
-      if (isNil(get('context.title', slideResult))) {
-        return chapterResult;
-      }
-      return dispatch(selectRoute('context'));
-    }
-    case 'node': {
-      switch (ref) {
-        case 'extraLife': {
-          const prevContent = getPrevStepContent(getState());
-          await dispatch(fetchContent(prevContent.type, prevContent.ref));
-          return dispatch(fetchAnswer(progressionId, get('ref', prevContent), []));
-        }
-      }
-    }
-    case 'success': // eslint-disable-line no-fallthrough
-    case 'failure': {
-      await dispatch(fetchEndRank(progressionId));
-      await dispatch(fetchRecommendations(progressionId));
-      return dispatch(fetchExitNode(ref));
-    }
-  }
+  return  dispatch(fetchProgression(progressionId));
 };
