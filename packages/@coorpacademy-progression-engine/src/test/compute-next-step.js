60503344e7361905f980cba512a53e6b001d47e2
David Corticchiato <david.corticchiato@gmail.com>
2017-12-12 10:51:16 +0100

    use first slide pool if no question answered

--- packages/@coorpacademy-progression-engine/src/test/compute-next-step.js
+++ packages/@coorpacademy-progression-engine/src/test/compute-next-step.js
@@ -5,6 +5,7 @@ import get from 'lodash/fp/get';
 import merge from 'lodash/fp/merge';
 import pipe from 'lodash/fp/pipe';
 import find from 'lodash/fp/find';
+import omit from 'lodash/fp/omit';
 import type {State} from '../types';
 import computeNextStep from '../compute-next-step';
 import allSlides from './fixtures/slides';
@@ -38,6 +39,24 @@ test('should return the slide with higher priority from slides', t => {
     ...state,
     content: nextStep,
     slides: [...state.slides, nextStep.ref]
+  };
+
+  const nextNextStep = computeNextStep(engine, slides, nextState);
+
+  t.is(find({_id: nextNextStep.ref}, prioritySlides).position, 0);
+});
+
+test('should return first slide pool if no slides in state', t => {
+  const state: State = omit(['slides'], stateBeforeGettingNextContent);
+
+  const nextStep = computeNextStep(engine, slides, state);
+
+  t.is(nextStep.ref, pipe(find({position: 1}), get('_id'))(prioritySlides));
+
+  const nextState = {
+    ...state,
+    content: nextStep,
+    slides: [nextStep.ref]
   };
 
   const nextNextStep = computeNextStep(engine, slides, nextState);
