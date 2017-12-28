d3fee3b3e62b2f8b56e641cb3cb0310fb61f0cc6
Joan Ortega <jomaora@gmail.com>
2017-12-28 10:32:14 +0100

    eslint

--- packages/@coorpacademy-components/src/molecule/feedback/index.js
+++ packages/@coorpacademy-components/src/molecule/feedback/index.js
@@ -1,45 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import Provider from '../../atom/provider';
-import ResourceBrowser from '../../organism/resource-browser'
+import ResourceBrowser from '../../organism/resource-browser';
 import style from './style.css';
 
 const Feedback = (props, context) => {
-  const {skin} = context;
-  const {
-    title,
-    type,
-    description,
-    resources
-  } = props;
+  const {title, description, resources} = props;
 
   return (
-    <div
-      className={style.feedbackWrapper}
-      data-name="feedback"
-    >
+    <div className={style.feedbackWrapper} data-name="feedback">
       <div className={style.title}>{title}</div>
       <div className={style.descWrapper}>
-        <div className={style.description}
+        <div
+          className={style.description}
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{__html: description}}
         />
         <div className={style.resource}>
           <ResourceBrowser resource={resources[0]} />
         </div>
-      </div>  
+      </div>
     </div>
   );
 };
 
-Feedback.contextTypes = {
-  skin: Provider.childContextTypes.skin
-};
-
 Feedback.propTypes = {
   title: PropTypes.string,
-  type: PropTypes.string,
   description: PropTypes.string,
   resources: PropTypes.checkPropTypes(ResourceBrowser.propTypes.resources)
 };
