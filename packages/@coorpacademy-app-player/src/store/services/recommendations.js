d3add8779c8126c8953312b97434e0c8e5e735be
Adriean Khisbe <adriean.khisbe@live.fr>
2018-01-03 11:52:10 +0100

    implement getNext in recommendation service

--- packages/@coorpacademy-app-player/src/store/services/recommendations.js
+++ packages/@coorpacademy-app-player/src/store/services/recommendations.js
@@ -6,28 +6,30 @@ import levelsData from './levels.data';
 const toMapByRef = reduce((map, object) => map.set(object.ref, object));
 const levels = toMapByRef(new Map(), levelsData);
 
-const getLevelRecommendation = ref => {
-  const {name, level} = levels.get(ref);
-  if (level === 'coach') {
-    return Promise.resolve({list: recommendations, nextLevel: null});
-  }
-
-  const _nextLevel = level === 'base' ? 'advanced' : 'coach';
-  const filter = {level: _nextLevel, name};
-  const nextLevel = _find(filter, levelsData);
-  return Promise.resolve({list: recommendations, nextLevel});
-};
-
 // eslint-disable-next-line import/prefer-default-export
 export const find = (type, ref) => {
   switch (type) {
     case 'chapter':
       return Promise.resolve({list: recommendations});
     case 'level':
-      return getLevelRecommendation(ref);
+      return Promise.resolve({list: recommendations});
   }
 };
 
+const getNextLevel = ref => {
+  const {name, level} = levels.get(ref);
+  if (level === 'coach') return Promise.resolve(null);
+  const _nextLevel = level === 'base' ? 'advanced' : 'coach';
+  const filter = {level: _nextLevel, name};
+  const nextLevel = _find(filter, levelsData);
+  return Promise.resolve(nextLevel);
+};
+
 export const getNext = (type, ref) => {
-  return Promise.reject(new Error());
+  switch (type) {
+    case 'chapter':
+      return Promise.resolve(null);
+    case 'level':
+      return getNextLevel(ref);
+  }
 };
