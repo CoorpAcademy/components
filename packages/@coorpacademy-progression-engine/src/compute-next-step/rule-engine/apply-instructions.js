56b62fa61b844cb0d13c7effe8b6457ad85b582e
godu <arthur.weber@viacesi.fr>
2018-01-04 14:22:50 +0100

    Reinforce typing

--- packages/@coorpacademy-progression-engine/src/compute-next-step/rule-engine/apply-instructions.js
+++ packages/@coorpacademy-progression-engine/src/compute-next-step/rule-engine/apply-instructions.js
@@ -2,24 +2,23 @@
 import reduce from 'lodash/fp/reduce';
 import set from 'lodash/fp/set';
 import update from 'lodash/fp/update';
-import type {Instruction, Variables} from './types';
+import type {State, Variables, Variable} from '../../types';
+import type {Instruction} from './types';
 
-type FlattenVariables = {[string]: string | boolean | number};
-
-const apply = (variables: FlattenVariables, instruction: Instruction): FlattenVariables => {
+const apply = <T: Variables>(variables: T, instruction: Instruction): T => {
   const {type, value, field} = instruction;
 
   switch (type) {
     case 'set':
       return set(field, value, variables);
     case 'add':
-      return update(field, v => v + value, variables);
+      return update(field, (v: Variable): Variable => v + value, variables);
     default:
       return variables;
   }
 };
 
-const updateVariables = (instructions: Array<Instruction>) => (fromState: Variables): Variables => {
+const updateVariables = (instructions: Array<Instruction>) => (fromState: State): State => {
   const {lives, stars, ...variables} = reduce(
     apply,
     {
@@ -30,7 +29,12 @@ const updateVariables = (instructions: Array<Instruction>) => (fromState: Variab
     instructions
   );
 
-  return {lives, stars, variables};
+  return {
+    ...fromState,
+    lives,
+    stars,
+    variables
+  };
 };
 
 export default updateVariables;
