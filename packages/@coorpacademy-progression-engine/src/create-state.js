f98373df9c6dfa40699a09d15e5b9a4b6a8abc15
Jeroen Engels <jfm.engels@gmail.com>
2018-01-18 14:26:09 +0100

    Add signature for createState

---
+++ packages/@coorpacademy-progression-engine/src/create-state.js
@@ -0,0 +1,10 @@
+// @flow
+import type {NewProgression, State} from './types';
+
+const createState = (progression: NewProgression): State => {
+  // TODO Implement function
+  // $FlowFixMe
+  return null;
+};
+
+export default createState;
