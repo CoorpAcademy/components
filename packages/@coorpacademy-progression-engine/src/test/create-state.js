11508ad3ee97ed4effc51e8caeb1faab5272bf9e
Jeroen Engels <jfm.engels@gmail.com>
2018-01-18 14:26:09 +0100

    Add signature for createState

---
+++ packages/@coorpacademy-progression-engine/src/test/create-state.js
@@ -0,0 +1,9 @@
+// @flow
+import test from 'ava';
+import createState from '../create-state';
+
+test('should...', t => {
+  // TODO implement
+  // $FlowFixMe
+  t.deepEqual(createState(null), {});
+});
