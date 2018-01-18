ed863926e115c0ed6eab99340401a1b63ec63e3e
Jeroen Engels <jfm.engels@gmail.com>
2018-01-18 14:26:09 +0100

    Add signature for createState

--- packages/@coorpacademy-progression-engine/src/index.js
+++ packages/@coorpacademy-progression-engine/src/index.js
@@ -2,6 +2,7 @@
 import computeNextStep, {newComputeNextStep} from './compute-next-step';
 import checkAnswer from './check-answer';
 import checkAnswerCorrectness from './check-answer-correctness';
+import createState from './create-state';
 import updateState from './update-state';
 import createProgression, {createStateFromConfig, newCreateProgression} from './create-progression';
 import getConfig from './config';
@@ -11,6 +12,7 @@ export {
   newComputeNextStep,
   checkAnswer,
   checkAnswerCorrectness,
+  createState,
   updateState,
   createProgression,
   newCreateProgression,
