1fda951c9fa99cf139be774a79cccc14c83eb5f8
Jeroen Engels <jfm.engels@gmail.com>
2018-01-23 11:17:03 +0100

    Implement new computeNextStep

--- packages/@coorpacademy-progression-engine/src/compute-next-step/test/compute-next-step.js
+++ packages/@coorpacademy-progression-engine/src/compute-next-step/test/compute-next-step.js
@@ -1,115 +1,228 @@
-// @flowk
+// @flow
 import test from 'ava';
+import omit from 'lodash/fp/omit';
+import filter from 'lodash/fp/filter';
+import assign from 'lodash/fp/assign';
+import pipe from 'lodash/fp/pipe';
+import find from 'lodash/fp/find';
+import type {Engine, EngineOptions, State, Slide} from '../../types';
+import {newComputeNextStep} from '..';
+import allSlides from './fixtures/slides';
+import {
+  firstState,
+  stateBeforeGettingNextContent,
+  oneLifeLeftState,
+  failProgressionState,
+  extraLifeAlreadyRefusedProgressionState
+} from './fixtures/states';
 
-test('compute-next-step', t => {
-  t.pass();
+const engine: Engine = {
+  ref: 'learner',
+  version: '1'
+};
+const engineOptions: EngineOptions = {};
+
+const merge = arr2 => (arr: Array<Slide>): Array<Slide> => {
+  return arr.map((v, i): Slide => {
+    return assign(v, arr2[i]);
+  });
+};
+
+const slidePools = [
+  {
+    chapterId: '1.A1',
+    slides: filter({chapter_id: '1.A1'}, allSlides)
+  }
+];
+
+const getCurrentSlide = (slides: Array<Slide>, state: State): ?Slide =>
+  slides.find(s => s._id === (state.content && state.content.ref));
+
+test('should return the slide with the highest position if any slides have a position', t => {
+  const prioritySlides: Array<Slide> = pipe(
+    filter({chapter_id: '1.A1'}),
+    merge([
+      {position: 0},
+      {position: 0},
+      {position: 0},
+      {_id: 'highest_position', position: 1},
+      {position: 0}
+    ])
+  )(allSlides);
+  const availableContent = {
+    slidePools: [
+      {
+        chapterId: '1.A1',
+        slides: prioritySlides
+      }
+    ]
+  };
+  const currentSlide: ?Slide = find({_id: '1.A1.1'}, allSlides);
+  if (!currentSlide) throw new Error('Slide `1.A1.1` not found');
+
+  const givenAnswer = {
+    currentSlide,
+    answer: [],
+    godMode: true
+  };
+
+  const result1 = newComputeNextStep(
+    engine,
+    engineOptions,
+    firstState,
+    givenAnswer,
+    availableContent
+  );
+  t.deepEqual(result1, {
+    nextContent: {
+      type: 'slide',
+      ref: 'highest_position'
+    },
+    instructions: undefined,
+    isCorrect: true
+  });
+
+  const stateWithAdditionalSlide: State = {
+    ...firstState,
+    content: firstState.nextContent,
+    nextContent: result1.nextContent,
+    slides: [...firstState.slides, result1.nextContent.ref]
+  };
+
+  const result2 = newComputeNextStep(
+    engine,
+    engineOptions,
+    stateWithAdditionalSlide,
+    givenAnswer,
+    availableContent
+  );
+  t.deepEqual(omit(['nextContent.ref'], result2), {
+    nextContent: {
+      type: 'slide'
+    },
+    instructions: undefined,
+    isCorrect: true
+  });
+  t.regex(result2.nextContent.ref, /^1\.A1\.[1-9]+$/);
 });
 
-// import filter from 'lodash/fp/filter';
-// import assign from 'lodash/fp/assign';
-// import pipe from 'lodash/fp/pipe';
-// import find from 'lodash/fp/find';
-// // // import omit from 'lodash/fp/omit';
-// import matches from 'lodash/fp/matches';
-// import type {State, Content, Slide} from '../../types';
-// import computeNextStep from '..';
-// import allSlides from './fixtures/slides';
-// import {stateBeforeGettingNextContent, firstState} from './fixtures/states';
+test('should return a new slide when user is still alive', t => {
+  const state: State = Object.freeze(stateBeforeGettingNextContent);
+  const currentSlide = getCurrentSlide(allSlides, state);
+  const availableContent = {slidePools};
+  const givenAnswer = {
+    currentSlide,
+    answer: [],
+    godMode: true
+  };
 
-// const engine = {
-//   ref: 'microlearning',
-//   version: '1'
-// };
+  const result = newComputeNextStep(engine, engineOptions, state, givenAnswer, availableContent);
+  t.deepEqual(omit(['nextContent.ref'], result), {
+    nextContent: {type: 'slide'},
+    instructions: undefined,
+    isCorrect: true
+  });
+  t.regex(result.nextContent.ref, /^1\.A1\.[2-9]+$/);
+});
 
-// const merge = arr2 => (arr: Array<Slide>): Array<Slide> => {
-//   return arr.map((v, i): Slide => {
-//     return assign(v, arr2[i]);
-//   });
-// };
+test("should return the fail endpoint when user has no more lives and can't request more lives", t => {
+  const state: State = Object.freeze(oneLifeLeftState);
+  const currentSlide = getCurrentSlide(allSlides, state);
+  const availableContent = {slidePools};
+  const givenAnswer = {
+    currentSlide,
+    answer: [],
+    godMode: false
+  };
 
-// const prioritySlides: Array<Slide> = pipe(
-//   filter({chapter_id: '1.A1'}),
-//   merge([{position: 0}, {position: 0}, {position: 0}, {position: 1}, {position: 0}])
-// )(allSlides);
+  const result = newComputeNextStep(engine, engineOptions, state, givenAnswer, availableContent);
+  t.deepEqual(result, {
+    nextContent: {ref: 'failExitNode', type: 'failure'},
+    instructions: undefined,
+    isCorrect: false
+  });
+});
 
-// const slide1A11: ?Slide = find(
-//   {
-//     _id: '1.A1.1'
-//   },
-//   allSlides
-// );
+test('should return the extraLife when user has no more lives but can request lives', t => {
+  const state: State = Object.freeze({...oneLifeLeftState, remainingLifeRequests: 1});
+  const currentSlide = getCurrentSlide(allSlides, state);
+  const availableContent = {slidePools};
+  const givenAnswer = {
+    currentSlide,
+    answer: [],
+    godMode: false
+  };
 
-// if (!slide1A11) throw new Error('Slide `1.A1.1` not found');
+  const result = newComputeNextStep(engine, engineOptions, state, givenAnswer, availableContent);
+  t.deepEqual(result, {
+    nextContent: {ref: 'extraLife', type: 'node'},
+    instructions: undefined,
+    isCorrect: false
+  });
+});
 
-// const slides = [
-//   {
-//     chapterId: '1.A1',
-//     slides: prioritySlides
-//   }
-// ];
+test('should return a new slide, when user has no more lives but lives are disabled', t => {
+  const state: State = Object.freeze(failProgressionState);
+  const currentSlide = getCurrentSlide(allSlides, state);
+  const availableContent = {slidePools};
+  const livesDisabledEngine = {ref: 'learner', version: 'livesDisabled'};
+  const givenAnswer = {
+    currentSlide,
+    answer: [],
+    godMode: false
+  };
 
-// const hasHighPriority: Slide => boolean = matches({position: 1});
-// const findHigherPrioritySlide: (Array<Slide>) => ?Slide = find(hasHighPriority);
-// const getId = (slide: ?Slide) => {
-//   if (slide) return slide._id;
-//   return;
-// };
-// const tryfindHigherPrioritySlideId = pipe(findHigherPrioritySlide, getId);
+  const resultForEngine = newComputeNextStep(
+    livesDisabledEngine,
+    engineOptions,
+    state,
+    givenAnswer,
+    availableContent
+  );
+  t.deepEqual(omit(['nextContent.ref'], resultForEngine), {
+    nextContent: {type: 'slide'},
+    instructions: undefined,
+    isCorrect: false
+  });
+  t.regex(
+    resultForEngine.nextContent.ref,
+    /^1\.A1\.[2-9]+$/,
+    'does not work when lives are disabled in engine config'
+  );
 
-// test('should return the slide with higher priority from slides', t => {
-//   const state = firstState;
+  const resultForEngineConfig = newComputeNextStep(
+    engine,
+    {livesDisabled: true},
+    state,
+    givenAnswer,
+    availableContent
+  );
+  t.deepEqual(omit(['nextContent.ref'], resultForEngineConfig), {
+    nextContent: {type: 'slide'},
+    instructions: undefined,
+    isCorrect: false
+  });
+  t.regex(
+    resultForEngineConfig.nextContent.ref,
+    /^1\.A1\.[2-9]+$/,
+    'does not work when lives are disabled in engine options'
+  );
+});
 
-//   const {nextContent} =
-//     computeNextStep(engine, state, {
-//       currentSlide: slide1A11,
-//       answer: [],
-//       godMode: true,
+test('should return the failure endpoint when progression has at least one remaining extra life and extraLife has already just been refused', t => {
+  const state: State = Object.freeze(extraLifeAlreadyRefusedProgressionState);
+  const currentSlide = getCurrentSlide(allSlides, state);
+  const availableContent = {slidePools};
+  const givenAnswer = {
+    currentSlide,
+    answer: [],
+    godMode: false
+  };
 
-//       slidePools: slides
-//     }) || {};
-
-//   t.is(nextContent.ref, tryfindHigherPrioritySlideId(prioritySlides));
-
-//   const nextState: State = {
-//     ...state,
-//     content: nextContent,
-//     slides: [...state.slides, nextContent.ref]
-//   };
-
-//   const {nextContent: nextNextContent} =
-//     computeNextStep(engine, nextState, {
-//       currentSlide: slide1A11,
-//       answer: [],
-//       godMode: true,
-
-//       slidePools: slides
-//     }) || {};
-
-//   t.is((find({_id: nextNextContent.ref}, prioritySlides) || {}).position, 0);
-// });
-
-// test('should return first slide pool if no slides in state', t => {
-//   // const state: State = omit(['slides'], stateBeforeGettingNextContent);
-//   const state = firstState;
-
-//   const {nextContent} =
-//     computeNextStep(engine, state, {
-//       currentSlide: slide1A11,
-//       answer: [],
-//       godMode: true,
-
-//       slidePools: slides
-//     }) || {};
-
-//   t.is(nextContent.ref, tryfindHigherPrioritySlideId(prioritySlides));
-
-//   const nextState = {
-//     ...state,
-//     content: nextContent,
-//     slides: [nextContent.ref]
-//   };
-
-//   const nextNextStep = computeNextStep(engine, slides, nextState);
-
-//   t.is((find({_id: nextNextStep.ref}, prioritySlides) || {}).position, 0);
-// });
+  const result = newComputeNextStep(engine, engineOptions, state, givenAnswer, availableContent);
+  t.deepEqual(result, {
+    nextContent: {ref: 'failExitNode', type: 'failure'},
+    instructions: undefined,
+    isCorrect: false
+  });
+});
