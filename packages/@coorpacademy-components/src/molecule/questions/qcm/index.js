afcb7471fa94bc6647c64bdb42df5b25d9e5e3d4
Gaelle MOURARET <gaellemouraret@gmail.com>
2018-01-24 17:18:23 +0100

    add data-selected true for qcm

--- packages/@coorpacademy-components/src/molecule/questions/qcm/index.js
+++ packages/@coorpacademy-components/src/molecule/questions/qcm/index.js
@@ -16,6 +16,7 @@ const QCM = (props, context) => {
 
     const primarySkinColor = getOr('#00B0FF', 'common.primary', skin);
     const selectedStyle = selected ? {backgroundColor: primarySkinColor} : null;
+    const dataSelected = selected ? true : null;
 
     const className = classnames(
       longestAnswer === answer ? style.longestAnswer : style.answer,
@@ -28,6 +29,7 @@ const QCM = (props, context) => {
         className={className}
         onClick={onClick}
         style={selectedStyle}
+        data-selected={dataSelected}
         key={key}
         // eslint-disable-next-line react/no-danger
         dangerouslySetInnerHTML={{
