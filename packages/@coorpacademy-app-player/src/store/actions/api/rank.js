c50b6e4867dd2b47cc730a63ccc5cc3b6a71ccf7
marin42 <kalasasta@gmail.com>
2018-01-25 17:13:59 +0100

    Corrected linter error

--- packages/@coorpacademy-app-player/src/store/actions/api/rank.js
+++ packages/@coorpacademy-app-player/src/store/actions/api/rank.js
@@ -1,5 +1,5 @@
-import {getStartRank} from '../../utils/state-extract';
 import buildTask from '@coorpacademy/redux-task';
+import {getStartRank} from '../../utils/state-extract';
 
 export const RANK_FETCH_START_REQUEST = '@@rank/FETCH_START_REQUEST';
 export const RANK_FETCH_START_SUCCESS = '@@rank/FETCH_START_SUCCESS';
