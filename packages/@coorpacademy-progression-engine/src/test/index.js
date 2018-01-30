808bbda346d6af7aa2d717a519089e1b0a1e563a
Jeroen Engels <jfm.engels@gmail.com>
2018-01-30 10:45:54 +0100

    Fix tests in test/index file

--- packages/@coorpacademy-progression-engine/src/test/index.js
+++ packages/@coorpacademy-progression-engine/src/test/index.js
@@ -3,9 +3,13 @@ import test from 'ava';
 import {
   checkAnswer,
   checkAnswerCorrectness,
-  computeNextStep,
-  updateState,
-  createProgression
+  computeInitialStep,
+  computeNextStepAfterAnswer,
+  computeNextStepOnAcceptExtraLife,
+  computeNextStepOnRefuseExtraLife,
+  createProgression,
+  createState,
+  getConfig
 } from '..';
 
 test('should export checkAnswer', t => {
@@ -16,14 +20,30 @@ test('should export checkAnswerCorrectness', t => {
   t.is(typeof checkAnswerCorrectness, 'function');
 });
 
-test('should export computeNextStep', t => {
-  t.is(typeof computeNextStep, 'function');
+test('should export computeInitialStep', t => {
+  t.is(typeof computeInitialStep, 'function');
 });
 
-test('should export updateState', t => {
-  t.is(typeof updateState, 'function');
+test('should export computeNextStepAfterAnswer', t => {
+  t.is(typeof computeNextStepAfterAnswer, 'function');
+});
+
+test('should export computeNextStepOnAcceptExtraLife', t => {
+  t.is(typeof computeNextStepOnAcceptExtraLife, 'function');
+});
+
+test('should export computeNextStepOnRefuseExtraLife', t => {
+  t.is(typeof computeNextStepOnRefuseExtraLife, 'function');
+});
+
+test('should export createState', t => {
+  t.is(typeof createState, 'function');
 });
 
 test('should export createProgression', t => {
   t.is(typeof createProgression, 'function');
+});
+
+test('should export getConfig', t => {
+  t.is(typeof getConfig, 'function');
 });
