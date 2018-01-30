a776731f8ddb5d1c5453be8a212e1de10c542336
Jeroen Engels <jfm.engels@gmail.com>
2018-01-30 18:14:51 +0100

    Add adaptive tests

--- packages/@coorpacademy-progression-engine/src/compute-next-step/test/compute-next-step-after-answer.adaptive.js
+++ packages/@coorpacademy-progression-engine/src/compute-next-step/test/compute-next-step-after-answer.adaptive.js
@@ -6,15 +6,121 @@ import type {AvailableContent, Engine, EngineOptions, State} from '../../types';
 import {computeNextStepAfterAnswer, type PartialAnswerAction} from '..';
 import allSlides from './fixtures/slides';
 import getSlide from './helpers/get-slide';
-import {stateBeforeGettingNextContent} from './fixtures/states';
 
 const engine: Engine = {ref: 'learner', version: '1'};
 const engineOptions: EngineOptions = {};
+const partialAction = (state: State): PartialAnswerAction => ({
+  type: 'answer',
+  payload: {
+    answer: [],
+    content: state.nextContent,
+    godMode: true
+  }
+});
+const rulesFor1A1 = [
+  {
+    source: {
+      type: 'slide',
+      ref: 'i do not exist'
+    },
+    destination: {
+      type: 'slide',
+      ref: '1.A1.3'
+    },
+    instructions: [{field: 'foo', type: 'set', value: 'highest_priority'}],
+    conditions: [],
+    priority: 0
+  },
+  {
+    source: {
+      type: 'slide',
+      ref: '1.A1.2'
+    },
+    destination: {
+      type: 'slide',
+      ref: '1.A1.3'
+    },
+    instructions: [{field: 'foo', type: 'set', value: 'higher_priority'}],
+    conditions: [],
+    priority: 5
+  },
+  {
+    source: {
+      type: 'slide',
+      ref: '1.A1.2'
+    },
+    destination: {
+      type: 'slide',
+      ref: '1.A1.3'
+    },
+    instructions: [{field: 'foo', type: 'set', value: 'lower_priority'}],
+    conditions: [],
+    priority: 10
+  },
+  {
+    source: {
+      type: 'slide',
+      ref: '1.A1.3'
+    },
+    destination: {
+      type: 'slide',
+      ref: '1.A1.4'
+    },
+    instructions: [],
+    conditions: [
+      {
+        target: {
+          scope: 'variable',
+          field: 'lives'
+        },
+        operator: 'EQUALS',
+        values: [2]
+      }
+    ],
+    priority: 10
+  },
+  {
+    source: {
+      type: 'slide',
+      ref: '1.A1.3'
+    },
+    destination: {
+      type: 'slide',
+      ref: '1.A1.5'
+    },
+    instructions: [],
+    conditions: [
+      {
+        target: {
+          scope: 'variable',
+          field: 'lives'
+        },
+        operator: 'EQUALS',
+        values: [1]
+      }
+    ],
+    priority: 20
+  },
+  {
+    source: {
+      type: 'slide',
+      ref: '*'
+    },
+    destination: {
+      type: 'slide',
+      ref: 'slide-matching-star-source'
+    },
+    instructions: [],
+    conditions: [],
+    priority: 100
+  }
+];
+
 const availableContent: AvailableContent = [
   {
     ref: '1.A1',
     slides: filter({chapter_id: '1.A1'}, allSlides),
-    rules: null
+    rules: rulesFor1A1
   },
   {
     ref: '2.A1',
@@ -23,17 +129,37 @@ const availableContent: AvailableContent = [
   }
 ];
 
-test('should return a slide from slide pools if chapter rules is null or empty', t => {
-  const state: State = Object.freeze(stateBeforeGettingNextContent);
+const adaptiveState: State = {
+  content: {
+    ref: '1.A1.1',
+    type: 'slide'
+  },
+  nextContent: {
+    ref: '1.A1.2',
+    type: 'slide'
+  },
+  lives: 1,
+  livesDisabled: false,
+  stars: 0,
+  slides: ['1.A1.1'],
+  requestedClues: [],
+  viewedResources: [],
+  isCorrect: true,
+  step: {
+    current: 1
+  },
+  remainingLifeRequests: 1,
+  hasViewedAResourceAtThisStep: false,
+  allAnswers: [],
+  variables: {}
+};
+
+test('should return a slide from slide pools if chapter rules is null or empty for that given chapter', t => {
+  const state: State = Object.freeze({
+    ...adaptiveState,
+    nextContent: {type: 'slide', ref: '2.A1.1'}
+  });
   const currentSlide = getSlide(allSlides, state.nextContent);
-  const partialAction: PartialAnswerAction = {
-    type: 'answer',
-    payload: {
-      answer: [],
-      content: state.nextContent,
-      godMode: true
-    }
-  };
 
   const resultAction = computeNextStepAfterAnswer(
     engine,
@@ -41,7 +167,7 @@ test('should return a slide from slide pools if chapter rules is null or empty',
     state,
     availableContent,
     currentSlide,
-    partialAction
+    partialAction(state)
   );
   t.deepEqual(omit(['payload.nextContent.ref'], resultAction), {
     type: 'answer',
@@ -54,7 +180,89 @@ test('should return a slide from slide pools if chapter rules is null or empty',
       isCorrect: true
     }
   });
-  t.regex(resultAction.payload.nextContent.ref, /^1\.A1\.[2-9]+$/);
+  t.regex(resultAction.payload.nextContent.ref, /^2\.A1\.[2-9]+$/);
 });
 
-// TODO Add more tests related to adaptive
+test('should return the slide from the chapter rules matching the source with the lowest priority value first', t => {
+  const state: State = Object.freeze(adaptiveState);
+  const currentSlide = getSlide(allSlides, state.nextContent);
+
+  const resultAction = computeNextStepAfterAnswer(
+    engine,
+    engineOptions,
+    state,
+    availableContent,
+    currentSlide,
+    partialAction(state)
+  );
+  t.deepEqual(resultAction, {
+    type: 'answer',
+    payload: {
+      answer: [],
+      content: state.nextContent,
+      godMode: true,
+      nextContent: {type: 'slide', ref: '1.A1.3'},
+      instructions: [{field: 'foo', type: 'set', value: 'higher_priority'}],
+      isCorrect: null
+    }
+  });
+});
+
+test('should match the rule with "*" source whatever state.nextContent may be', t => {
+  const state: State = Object.freeze({
+    ...adaptiveState,
+    nextContent: {
+      type: 'slide',
+      ref: '1.A1.5'
+    }
+  });
+  const currentSlide = getSlide(allSlides, state.nextContent);
+
+  const resultAction = computeNextStepAfterAnswer(
+    engine,
+    engineOptions,
+    state,
+    availableContent,
+    currentSlide,
+    partialAction(state)
+  );
+  t.deepEqual(resultAction, {
+    type: 'answer',
+    payload: {
+      answer: [],
+      content: state.nextContent,
+      godMode: true,
+      nextContent: {type: 'slide', ref: 'slide-matching-star-source'},
+      instructions: [],
+      isCorrect: null
+    }
+  });
+});
+
+test("should only select a rule if it matches the rules's conditions", t => {
+  const state: State = Object.freeze({
+    ...adaptiveState,
+    nextContent: {type: 'slide', ref: '1.A1.3'}
+  });
+  const currentSlide = getSlide(allSlides, state.nextContent);
+
+  const resultAction = computeNextStepAfterAnswer(
+    engine,
+    engineOptions,
+    state,
+    availableContent,
+    currentSlide,
+    partialAction(state)
+  );
+  t.deepEqual(resultAction, {
+    type: 'answer',
+    payload: {
+      answer: [],
+      content: state.nextContent,
+      godMode: true,
+      nextContent: {type: 'slide', ref: '1.A1.5'},
+      instructions: [],
+      isCorrect: null
+    }
+  });
+});
