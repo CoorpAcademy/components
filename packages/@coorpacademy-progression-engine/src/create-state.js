11ba0e0d03c18c281d0a369c5bfd84b3d374b336
godu <arthur.weber@viacesi.fr>
2018-02-01 15:14:51 +0100

    livesDisabled reducer returns config value

--- packages/@coorpacademy-progression-engine/src/create-state.js
+++ packages/@coorpacademy-progression-engine/src/create-state.js
@@ -7,27 +7,6 @@ const createState = (progression: Progression): State => {
   const engineConfig: Config = getConfig(progression.engine);
   const config: Config = {...engineConfig, ...progression.engineOptions};
 
-  // const initialState: State = {
-  //   nextContent: {
-  //     type: 'node',
-  //     ref: 'empty'
-  //   },
-  //   lives: config.lives,
-  //   livesDisabled: config.livesDisabled,
-  //   isCorrect: true,
-  //   slides: [],
-  //   stars: 0,
-  //   requestedClues: [],
-  //   viewedResources: [],
-  //   step: {
-  //     current: 1
-  //   },
-  //   remainingLifeRequests: config.remainingLifeRequests,
-  //   hasViewedAResourceAtThisStep: false,
-  //   allAnswers: [],
-  //   variables: {}
-  // };
-
   // $FlowFixMe: While waiting on a good solution to have a dummy initial State
   return updateState(config, {}, progression.actions);
 };
