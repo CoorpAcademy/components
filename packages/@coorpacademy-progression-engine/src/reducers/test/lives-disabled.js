8e141b20bcdd7e468546e906195b48dd80ce8e27
godu <arthur.weber@viacesi.fr>
2018-02-01 15:14:51 +0100

    livesDisabled reducer returns config value

---
+++ packages/@coorpacademy-progression-engine/src/reducers/test/lives-disabled.js
@@ -0,0 +1,47 @@
+// @flow
+import test from 'ava';
+import getConfig from '../../config';
+import livesDisabled from '../lives-disabled';
+import {moveAction} from './fixtures/actions';
+import {microlearning} from './fixtures/engines';
+
+const config = getConfig(microlearning);
+
+test('should return livesDisabled from config', t => {
+  t.true(
+    livesDisabled({
+      ...config,
+      livesDisabled: true
+    })(undefined, moveAction)
+  );
+  t.true(
+    livesDisabled({
+      ...config,
+      livesDisabled: true
+    })(false, moveAction)
+  );
+  t.true(
+    livesDisabled({
+      ...config,
+      livesDisabled: true
+    })(true, moveAction)
+  );
+  t.false(
+    livesDisabled({
+      ...config,
+      livesDisabled: false
+    })(undefined, moveAction)
+  );
+  t.false(
+    livesDisabled({
+      ...config,
+      livesDisabled: false
+    })(false, moveAction)
+  );
+  t.false(
+    livesDisabled({
+      ...config,
+      livesDisabled: false
+    })(true, moveAction)
+  );
+});
