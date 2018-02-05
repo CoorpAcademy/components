061c9de1e3ca85221740d8c993397c7fc2cc8534
Jeroen Engels <jfm.engels@gmail.com>
2018-02-05 15:48:51 +0100

    Use config instead of engine in checkAnswer functions

--- packages/@coorpacademy-progression-engine/src/check-answer.js
+++ packages/@coorpacademy-progression-engine/src/check-answer.js
@@ -1,11 +1,11 @@
 // @flow
 import checkAnswerCorrectness from './check-answer-correctness';
-import type {Question, Answer, Engine} from './types';
+import type {Question, Answer, Config} from './types';
 
 export default function checkAnswer(
-  engine: Engine,
+  config: Config,
   question: Question,
   givenAnswer: Answer
 ): boolean {
-  return checkAnswerCorrectness(engine, question, givenAnswer).isCorrect;
+  return checkAnswerCorrectness(config, question, givenAnswer).isCorrect;
 }
--- packages/@coorpacademy-progression-engine/src/test/check-answer-correctness.qcm-graphic.js
+++ packages/@coorpacademy-progression-engine/src/test/check-answer-correctness.qcm-graphic.js
@@ -1,12 +1,10 @@
 // @flow
 import test from 'ava';
-import type {QCMGraphicQuestion, AcceptedAnswers} from '../types';
+import getConfig from '../config';
+import type {AcceptedAnswers, Config, QCMGraphicQuestion} from '../types';
 import {assertCorrect, assertIncorrect} from './helpers/assert-check-answer-correctness';
 
-const engine = {
-  ref: 'microlearning',
-  version: 'latest'
-};
+const config: Config = getConfig({ref: 'microlearning', version: 'latest'});
 
 function createQuestion(answers: AcceptedAnswers): QCMGraphicQuestion {
   return {
@@ -24,15 +22,15 @@ test('should return true when the given answer is in the accepted answers', t =>
     ['answer1', 'answer4']
   ]);
 
-  assertCorrect(t, engine, question, ['answer1', 'answer3']);
-  assertCorrect(t, engine, question, ['answer2', 'answer4']);
-  assertCorrect(t, engine, question, ['answer1', 'answer4']);
+  assertCorrect(t, config, question, ['answer1', 'answer3']);
+  assertCorrect(t, config, question, ['answer2', 'answer4']);
+  assertCorrect(t, config, question, ['answer1', 'answer4']);
 });
 
 test('should return true even when the given answer does not have the same case as the accepted answers', t => {
   const question = createQuestion([['answer2']]);
 
-  assertCorrect(t, engine, question, ['ANSWER2']);
+  assertCorrect(t, config, question, ['ANSWER2']);
 });
 
 test('should return true when the given answer is in the accepted answers but values are in a different order', t => {
@@ -42,9 +40,9 @@ test('should return true when the given answer is in the accepted answers but va
     ['answer1', 'answer4']
   ]);
 
-  assertCorrect(t, engine, question, ['answer3', 'answer1']);
-  assertCorrect(t, engine, question, ['answer4', 'answer2']);
-  assertCorrect(t, engine, question, ['answer4', 'answer1']);
+  assertCorrect(t, config, question, ['answer3', 'answer1']);
+  assertCorrect(t, config, question, ['answer4', 'answer2']);
+  assertCorrect(t, config, question, ['answer4', 'answer1']);
 });
 
 test('should return false when the given answer is not in the accepted answers', t => {
@@ -54,10 +52,10 @@ test('should return false when the given answer is not in the accepted answers',
     ['answer1', 'answer4']
   ]);
 
-  assertIncorrect(t, engine, question, ['answer2', 'answer1'], [false, true]);
-  assertIncorrect(t, engine, question, ['answer1', 'answer2'], [true, false]);
-  assertIncorrect(t, engine, question, ['answer3', 'answer4'], [true, false]);
-  assertIncorrect(t, engine, question, ['answer5', 'answer4'], [false, true]);
+  assertIncorrect(t, config, question, ['answer2', 'answer1'], [false, true]);
+  assertIncorrect(t, config, question, ['answer1', 'answer2'], [true, false]);
+  assertIncorrect(t, config, question, ['answer3', 'answer4'], [true, false]);
+  assertIncorrect(t, config, question, ['answer5', 'answer4'], [false, true]);
 });
 
 test('should return false when the given answer has more elements that the accepted answers', t => {
@@ -67,8 +65,8 @@ test('should return false when the given answer has more elements that the accep
     ['answer1', 'answer4']
   ]);
 
-  assertIncorrect(t, engine, question, ['answer1', 'answer3', 'answer2'], [true, true, false]);
-  assertIncorrect(t, engine, question, ['answer1', 'answer5', 'answer3'], [true, false, true]);
+  assertIncorrect(t, config, question, ['answer1', 'answer3', 'answer2'], [true, true, false]);
+  assertIncorrect(t, config, question, ['answer1', 'answer5', 'answer3'], [true, false, true]);
 });
 
 test('should return false when the given answer has less elements that the accepted answers', t => {
@@ -78,14 +76,14 @@ test('should return false when the given answer has less elements that the accep
     ['answer1', 'answer4']
   ]);
 
-  assertIncorrect(t, engine, question, ['answer1'], [true]);
-  assertIncorrect(t, engine, question, ['answer2'], [true]);
-  assertIncorrect(t, engine, question, ['answer5'], [false]);
-  assertIncorrect(t, engine, question, [], []);
+  assertIncorrect(t, config, question, ['answer1'], [true]);
+  assertIncorrect(t, config, question, ['answer2'], [true]);
+  assertIncorrect(t, config, question, ['answer5'], [false]);
+  assertIncorrect(t, config, question, [], []);
 });
 
 test("should return false when the given answer isn't the same but resembles the accepted answers", t => {
   const question = createQuestion([['answer2']]);
 
-  assertIncorrect(t, engine, question, ['answe2r'], [false]);
+  assertIncorrect(t, config, question, ['answe2r'], [false]);
 });
