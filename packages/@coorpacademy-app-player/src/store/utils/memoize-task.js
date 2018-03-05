ad76cf4f0d73ce76e27b84aa2a91b3f9364747bb
David Corticchiato <david.corticchiato@gmail.com>
2018-03-05 18:36:48 +0100

    forward cache failures

--- packages/@coorpacademy-app-player/src/store/utils/memoize-task.js
+++ packages/@coorpacademy-app-player/src/store/utils/memoize-task.js
@@ -1,18 +1,25 @@
 import memoize from 'lodash/fp/memoize';
 import partial from 'lodash/fp/partial';
 
-const memoizeTask = task => {
-  const memoizedTask = memoize(task);
+export const _memoizeTask = _memoize => task => {
+  const memoizedTask = _memoize(task);
 
   return function(first, ...rest) {
     const callTaskWithArgs = partial(memoizedTask, [first, ...rest]);
     const promise = Promise.resolve(callTaskWithArgs());
-    const clearCache = () => memoizedTask.cache.delete(first);
 
-    promise.then(clearCache).catch(clearCache);
-
-    return promise;
+    return promise
+      .then(res => {
+        memoizedTask.cache.delete(first);
+        return res;
+      })
+      .catch(taskError => {
+        memoizedTask.cache.delete(first);
+        throw taskError;
+      });
   };
 };
+
+const memoizeTask = _memoizeTask(memoize);
 
 export default memoizeTask;
--- packages/@coorpacademy-app-player/src/store/utils/test/memoize-task.js
+++ packages/@coorpacademy-app-player/src/store/utils/test/memoize-task.js
@@ -1,13 +1,11 @@
 import test from 'ava';
-import memoizeTask from '../memoize-task';
+import memoize from 'lodash/fp/memoize';
+import memoizeTask, {_memoizeTask} from '../memoize-task';
 
 test('it should return the same promise if first argument is the same and last task did not finish yet', async t => {
   const setPlayerScore = memoizeTask((playerId, score) => Promise.resolve(score));
   const firstPromise = setPlayerScore('john', 0);
   const secondPromise = setPlayerScore('john', 1);
-
-  t.is(firstPromise, secondPromise);
-
   const firstScore = await firstPromise;
   const secondScore = await secondPromise;
 
@@ -19,9 +17,6 @@ test('it not should return the same promise if first argument is not the same an
   const setPlayerScore = memoizeTask((playerId, score) => Promise.resolve(score));
   const firstPromise = setPlayerScore('john', 0);
   const secondPromise = setPlayerScore('jane', 1);
-
-  t.not(firstPromise, secondPromise);
-
   const firstScore = await firstPromise;
   const secondScore = await secondPromise;
 
@@ -29,11 +24,47 @@ test('it not should return the same promise if first argument is not the same an
   t.is(secondScore, 1);
 });
 
-test('it not should return the same promise if first argument is the same and last task finished before second call', async t => {
+test('it not should return the same promise if first argument is the same and last task succeeded before second call', async t => {
   const setPlayerScore = memoizeTask((playerId, score) => Promise.resolve(score));
   const firstScore = await setPlayerScore('john', 0);
   const secondScore = await setPlayerScore('john', 1);
 
   t.is(firstScore, 0);
   t.is(secondScore, 1);
+});
+
+test('it should forward any task error', async t => {
+  const setPlayerScore = memoizeTask((playerId, score) =>
+    Promise.reject(new Error(`Can't set score to ${score} from ${playerId}`))
+  );
+  const firstPromise = setPlayerScore('john', 0);
+  const secondPromise = setPlayerScore('john', 1);
+
+  await t.throws(firstPromise, "Can't set score to 0 from john");
+  return t.throws(secondPromise, "Can't set score to 0 from john");
+});
+
+test('it not should return the same promise if first argument is the same and last task failed before second call', async t => {
+  const setPlayerScore = memoizeTask((playerId, score) =>
+    Promise.reject(new Error(`Can't set score to ${score} from ${playerId}`))
+  );
+
+  await t.throws(setPlayerScore('john', 0), "Can't set score to 0 from john");
+  return t.throws(setPlayerScore('john', 1), "Can't set score to 1 from john");
+});
+
+test('it should forward any error from the memoize cache', t => {
+  const memoizeTaskWithCacheFailures = _memoizeTask(task => {
+    const memoizedTask = memoize(task);
+
+    memoizedTask.cache.delete = cacheId => {
+      throw new Error(`No, I won't delete cache id ${cacheId}`, memoizedTask.cache);
+    };
+
+    return memoizedTask;
+  });
+
+  const setPlayerScore = memoizeTaskWithCacheFailures((playerId, score) => Promise.resolve(score));
+
+  return t.throws(setPlayerScore('john', 0), "No, I won't delete cache id john");
 });
