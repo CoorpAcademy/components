ecf018fef710f40f0c1dfe3fb642d551e079bccc
David Corticchiato <david.corticchiato@gmail.com>
2018-03-05 16:59:16 +0100

    use reduce instead of repeating manually memoizeService()

--- packages/@coorpacademy-app-player/src/store/middlewares/index.js
+++ packages/@coorpacademy-app-player/src/store/middlewares/index.js
@@ -1,26 +1,32 @@
 import {applyMiddleware, compose} from 'redux';
 import ReduxThunk from 'redux-thunk';
-import pipe from 'lodash/fp/pipe';
+import reduce from 'lodash/fp/reduce';
 import get from 'lodash/fp/get';
 import set from 'lodash/fp/set';
 import memoizeTask from '../utils/memoize-task';
 import ErrorLogger from './error-logger';
 
-const memoizeService = keyPath => options => {
+const memoizeService = (options, keyPath) => {
   const service = get(keyPath, options);
 
   return set(keyPath, memoizeTask(service), options);
 };
 
-const wrapOptions = pipe(
-  memoizeService('services.Progressions.acceptExtraLife'),
-  memoizeService('services.Progressions.refuseExtraLife'),
-  memoizeService('services.Progressions.postAnswer')
-);
+const memoizeMoveTypeServices = options =>
+  reduce(memoizeService, options, [
+    'services.Progressions.acceptExtraLife',
+    'services.Progressions.refuseExtraLife',
+    'services.Progressions.postAnswer'
+  ]);
 
 const middlewares = options => {
+  const optionsWithMemoizedServices = memoizeMoveTypeServices(options);
+
   return compose(
-    applyMiddleware(ReduxThunk.withExtraArgument(wrapOptions(options)), ErrorLogger(options)),
+    applyMiddleware(
+      ReduxThunk.withExtraArgument(optionsWithMemoizedServices),
+      ErrorLogger(options)
+    ),
     typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : f => f
