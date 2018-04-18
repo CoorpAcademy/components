e28908eddc08bb29638d999cd0f859a4c67c24b8
Arthur Weber <arthur.weber@coorpacademy.com>
2018-04-18 16:51:30 +0200

    Adds description to image-upload (#1258)

--- packages/@coorpacademy-components/src/atom/image-upload/test/fixtures/mobile.js
+++ packages/@coorpacademy-components/src/atom/image-upload/test/fixtures/mobile.js
@@ -6,6 +6,8 @@ export default {
   props: {
     ...props,
     title: 'Mobile (*)',
+    description:
+      'Recommended aspect ratio: 1:1 (e.g. 250px x 250px). Maximum accepted aspect ratio: 2:1 (e.g. 500px x 250px. Meaning, no more than twice as large as it is tall)',
     previewImage: 'https://static.coorpacademy.com/content/up/raw/logo_mobile-1491560713685.svg'
   }
 };
