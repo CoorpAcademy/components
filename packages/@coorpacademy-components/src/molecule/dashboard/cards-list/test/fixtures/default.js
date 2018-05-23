6f6e676290b356b503df67c7b5f8642ea74d8820
esa-coorp <eric.sampaio@coorpacademy.com>
2018-05-23 12:27:11 +0200

    fix scroll on dashboard

--- packages/@coorpacademy-components/src/molecule/dashboard/cards-list/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/dashboard/cards-list/test/fixtures/default.js
@@ -5,6 +5,9 @@ const {props} = Card;
 export default {
   props: {
     title: 'Most popular',
-    cards: [props, props, props]
+    cards: [props, props, props, {}, {}, {}, props, props, props, {}, {}, {}, props, props, props],
+    onScroll: (skip, limit) => {
+      console.log(skip, limit);
+    }
   }
 };
