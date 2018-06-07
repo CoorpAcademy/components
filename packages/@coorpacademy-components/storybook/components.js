19ac9feb1aab9ee7f5c2d773cc67c791aea62515
Silouane <silouane.galinou@efrei.net>
2018-06-07 18:02:23 +0200

    sticky footer css modification

--- packages/@coorpacademy-components/storybook/components.js
+++ packages/@coorpacademy-components/storybook/components.js
@@ -582,6 +582,7 @@ import TemplateAppPlayerPlayerFixtureQcmGraphic from '../src/template/app-player
 import TemplateAppPlayerPlayerFixtureQcmShort from '../src/template/app-player/player/test/fixtures/qcm-short';
 import TemplateAppPlayerPlayerFixtureQcm from '../src/template/app-player/player/test/fixtures/qcm';
 import TemplateAppPlayerPlayerFixtureRange from '../src/template/app-player/player/test/fixtures/range';
+import TemplateAppPlayerPlayerFixtureStickyFooter from '../src/template/app-player/player/test/fixtures/sticky-footer';
 import TemplateAppPlayerPopinCorrectionFixtureCorrectClosed from '../src/template/app-player/popin-correction/test/fixtures/correct-closed';
 import TemplateAppPlayerPopinCorrectionFixtureCorrectNextChapter from '../src/template/app-player/popin-correction/test/fixtures/correct-next-chapter';
 import TemplateAppPlayerPopinCorrectionFixtureCorrectOpenKlf from '../src/template/app-player/popin-correction/test/fixtures/correct-open-klf';
@@ -1510,7 +1511,8 @@ export const fixtures = {
       QcmGraphic: TemplateAppPlayerPlayerFixtureQcmGraphic,
       QcmShort: TemplateAppPlayerPlayerFixtureQcmShort,
       Qcm: TemplateAppPlayerPlayerFixtureQcm,
-      Range: TemplateAppPlayerPlayerFixtureRange
+      Range: TemplateAppPlayerPlayerFixtureRange,
+      StickyFooter: TemplateAppPlayerPlayerFixtureStickyFooter
     },
     TemplateAppPlayerPopinCorrection: {
       CorrectClosed: TemplateAppPlayerPopinCorrectionFixtureCorrectClosed,
