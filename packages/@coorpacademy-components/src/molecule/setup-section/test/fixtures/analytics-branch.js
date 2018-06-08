817195ca4632d7940ce3e14b18a73d7f9fa94e2d
Faustino Kialungila <faustino.kialungila@coorpacademy.com>
2018-06-08 16:10:23 +0200

    Fix ident

--- packages/@coorpacademy-components/src/molecule/setup-section/test/fixtures/analytics-branch.js
+++ packages/@coorpacademy-components/src/molecule/setup-section/test/fixtures/analytics-branch.js
@@ -1,16 +1,16 @@
 export default {
-    props: {
-      id: '98aze89138',
-      title: 'Branch',
-      onUp: e => {
-        console.log(e);
-      },
-      onDown: e => {
-        console.log(e);
-      },
-      display: {
-        value: true,
-        onChange: value => console.log(value)
-      }
+  props: {
+    id: '98aze89138',
+    title: 'Branch',
+    onUp: e => {
+      console.log(e);
+    },
+    onDown: e => {
+      console.log(e);
+    },
+    display: {
+      value: true,
+      onChange: value => console.log(value)
     }
-  };
+  }
+};
