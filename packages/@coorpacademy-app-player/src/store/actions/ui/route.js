764a6cd74f07667aeff17e9b08df03ea6c967c4a
chrisdugne <chris.dugne@uralys.com>
2018-06-12 18:30:16 +0200

    added scrollTo on selectroute action

--- packages/@coorpacademy-app-player/src/store/actions/ui/route.js
+++ packages/@coorpacademy-app-player/src/store/actions/ui/route.js
@@ -9,6 +9,10 @@ export const selectRoute = route => (dispatch, getState) => {
 
   const nextRoute = currentRoute === route ? null : route;
 
+  if (route === 'answer') {
+    window.scrollTo(0, 0);
+  }
+
   return dispatch({
     type: UI_SELECT_ROUTE,
     payload: nextRoute,
