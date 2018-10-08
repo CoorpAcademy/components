3d8a8aff959d8bcf19aa04da74149ca2c9d28cfd
BertrandD <bernaz@pm.me>
2018-10-08 18:42:54 +0200

    fix empty props

--- packages/@coorpacademy-components/src/template/team-builder/motionned-team/index.js
+++ packages/@coorpacademy-components/src/template/team-builder/motionned-team/index.js
@@ -1,24 +1,27 @@
 import {Motion, spring} from 'react-motion';
 import React from 'react';
+import isEmpty from 'lodash/fp/isEmpty';
 import Team from '../team';
 
-const MotionnedMyTeam = props => (
-  <Motion defaultStyle={{scale: 0}} style={{scale: spring(1)}}>
-    {interpolatedStyle => {
-      return (
-        <div
-          key={'MyTeam'}
-          style={{
-            minWidth: '300px',
-            minHeight: '300px',
-            transform: `scale(${interpolatedStyle.scale})`
-          }}
-        >
-          {props ? <Team {...props} /> : null}{' '}
-        </div>
-      );
-    }}
-  </Motion>
-);
+const MotionnedMyTeam = props => {
+  return (
+    <Motion defaultStyle={{scale: 0}} style={{scale: spring(1)}}>
+      {interpolatedStyle => {
+        return (
+          <div
+            key={'MyTeam'}
+            style={{
+              minWidth: '300px',
+              minHeight: '300px',
+              transform: `scale(${interpolatedStyle.scale})`
+            }}
+          >
+            {!isEmpty(props) ? <Team {...props} /> : null}{' '}
+          </div>
+        );
+      }}
+    </Motion>
+  );
+};
 
 export default MotionnedMyTeam;
