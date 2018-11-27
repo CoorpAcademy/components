242a873696c99b18915529953cfd760261349b5c
chrisdugne <chris.dugne@uralys.com>
2018-11-27 16:10:24 +0100

    extracted exitnodes services

--- packages/@coorpacademy-player-services/src/exit-nodes.js
+++ packages/@coorpacademy-player-services/src/exit-nodes.js
@@ -1,18 +1,9 @@
-import pipe from 'lodash/fp/pipe';
-import reduce from 'lodash/fp/reduce';
 import update from 'lodash/fp/update';
-import values from 'lodash/fp/values';
-import exitNodesData from './fixtures/exit-nodes';
 import {appendJWPOptions} from './slides';
 
-const exitNodeStore = pipe(
-  values,
-  reduce((exitNodeMap, exitNode) => exitNodeMap.set(exitNode.ref, exitNode), new Map())
-)(exitNodesData);
-
 // eslint-disable-next-line import/prefer-default-export,require-await
-export const findById = async id => {
-  if (!exitNodeStore.has(id)) throw new Error('ExitNode not found');
-  const exitNode = exitNodeStore.get(id);
+export const findById = fixtures => async id => {
+  const {getExitNode} = fixtures;
+  const exitNode = getExitNode(id);
   return update('media', appendJWPOptions('src.0.'), exitNode);
 };
