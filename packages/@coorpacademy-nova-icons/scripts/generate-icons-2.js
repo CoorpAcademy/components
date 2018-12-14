72844e59fd3827bd2fbe2372e15a36e715f04489
Vincent Catillon <devotion95@gmail.com>
2018-12-14 15:53:27 +0100

    Use SVGR to generate components (alpha)

---
+++ packages/@coorpacademy-nova-icons/scripts/generate-icons-2.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-console */
+
+import path from 'path';
+import fs from 'fs';
+
+import chalk from 'chalk';
+import globby from 'globby';
+import shelljs from 'shelljs';
+import svgr from '@svgr/core';
+
+const searchPath = path.resolve(`${__dirname}/../third-party`);
+const outputPath = path.resolve(`${__dirname}/../src/components`);
+shelljs.mkdir('-p', outputPath);
+
+const generateComponent = ({name, content}, native) => {
+  const options = {
+    noSemi: true,
+    plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx', '@svgr/plugin-prettier'],
+    native
+  };
+  return svgr(content, options).then(jsCode => {
+    const extension = (native && 'native') || 'web';
+    const fileName = path.join(outputPath, name.replace('.svg', `.${extension}.js`));
+    fs.writeFileSync(fileName, jsCode);
+
+    return fileName;
+  });
+};
+
+globby
+  .sync('**/*.svg', {
+    cwd: searchPath,
+    nodir: true,
+    absolute: true
+  })
+  .sort()
+  .map(file => ({
+    name: file.split(/[\\/]/).pop(),
+    content: fs.readFileSync(file)
+  }))
+  .map(file => Promise.all([generateComponent(file), generateComponent(file, true)]))
+  .map(result =>
+    result
+      .then(fileNames =>
+        console.log(
+          chalk.underline('Components generated:'),
+          ...fileNames.map(fileName => `\n - ${chalk.green(fileName)}`)
+        )
+      )
+      .catch(e => console.error(e))
+  );
