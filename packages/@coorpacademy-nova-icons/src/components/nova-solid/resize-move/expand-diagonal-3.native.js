b0fd51b239f91fe29dcdcb4a34c943c30fe9f430
Vincent CATILLON <contact@vincent-catillon.fr>
2019-01-22 13:58:21 +0100

    Add expand and shrink icons (#1444)

---
+++ packages/@coorpacademy-nova-icons/src/components/nova-solid/resize-move/expand-diagonal-3.native.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Svg, { Path, G } from "react-native-svg";
+
+const SvgComponent = props => (
+  <Svg viewBox="0 0 24 24" {...props}>
+    <Path fill="none" d="M0 0h24v24H0z" />
+    <G fill={props.color}>
+      <Path d="M16 3.999h2.586l-6.293 6.293 1.414 1.414L20 5.413v2.586h2v-6h-6zM2 15.999v6h6v-2H5.414l6.293-6.293-1.414-1.414L4 18.585v-2.586z" />
+    </G>
+  </Svg>
+);
+
+export default SvgComponent;
