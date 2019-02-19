1c04cdd3f6eeebeee2515a36656a494eab12690a
chrisdugne <chris.dugne@uralys.com>
2019-02-19 15:12:56 +0100

    added types for actions.ui.corrections

--- packages/@coorpacademy-player-store/src/actions/ui/corrections.js
+++ packages/@coorpacademy-player-store/src/actions/ui/corrections.js
@@ -1,3 +1,5 @@
+// @flow strict
+
 export const UI_TOGGLE_ACCORDION = '@@ui/TOGGLE_ACCORDION';
 export const UI_SELECT_RESOURCE_IN_POPIN = '@@ui/SELECT_RESOURCE_IN_POPIN';
 
@@ -5,14 +7,30 @@ export const ACCORDION_LESSON = 0;
 export const ACCORDION_KLF = 1;
 export const ACCORDION_TIPS = 2;
 
-export const toggleAccordion = id => ({
+type AccordionId = 0 | 1 | 2;
+
+type ToggleAccordionAction = {
+  type: '@@ui/TOGGLE_ACCORDION',
+  payload: {
+    id: AccordionId
+  }
+};
+
+type SelectResourceAction = {
+  type: '@@ui/SELECT_RESOURCE_IN_POPIN',
+  payload: {
+    id: string
+  }
+};
+
+export const toggleAccordion = (id: AccordionId): ToggleAccordionAction => ({
   type: UI_TOGGLE_ACCORDION,
   payload: {
     id
   }
 });
 
-export const selectResource = id => ({
+export const selectResource = (id: string): SelectResourceAction => ({
   type: UI_SELECT_RESOURCE_IN_POPIN,
   payload: {
     id
