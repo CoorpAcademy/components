25faf6957e0aa12fce54e95c8a65805452bb9a8b
alan_langlois <skay406@gmail.com>
2019-03-20 14:11:09 +0100

    editing spiral (#1526)

--- packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/spiral.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/spiral.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const SvgComponent = props => (
-  <svg id="prefix__Calque_1" x={0} y={0} viewBox="0 0 802 802" xmlSpace="preserve" {...props}>
-    <style>{'.prefix__st1{fill-rule:evenodd;clip-rule:evenodd}'}</style>
-    <g opacity={0.08}>
-      <g id="prefix__Group">
-        <path id="prefix__Path" className="prefix__st1" d="M432.8.4h-64.1l32.1 400.8z" />
-        <path
-          className="prefix__st1"
-          d="M584 .4h-72.6L400.8 401.2zM290.2.4h-72.7l183.3 400.8zM801.6 210.5v-85.6L400.8 401.2zM795.6.4H686L400.8 401.2zM801.6 363.1V297L400.8 401.2zM115.7.4H6l394.8 400.8zM0 439.3v66l400.8-104.1z"
-        />
-        <path className="prefix__st1" d="M217.5 802h72.7l110.6-400.8z" />
-        <path className="prefix__st1" d="M0 591.7v85.6l400.8-276.1zM686 802h109.6L400.8 401.2z" />
-        <path className="prefix__st1" d="M368.7 802h64.1l-32-400.8z" />
-        <path className="prefix__st1" d="M0 297v66.1l400.8 38.1z" />
-        <path
-          className="prefix__st1"
-          d="M6 802h109.7l285.1-400.8zM511.4 802H584L400.8 401.2zM0 124.9v85.6l400.8 190.7zM400.8 401.2l400.8 104.1v-66zM801.6 677.3v-85.6L400.8 401.2z"
-        />
-      </g>
-    </g>
+  <svg viewBox="0 0 802 802" {...props}>
+    <path d="M432.8.4h-64.1l32.1 400.8z" />
+    <path
+      fill="currentColor"
+      d="M584 .4h-72.6L400.8 401.2zM290.2.4h-72.7l183.3 400.8zM801.6 210.5v-85.6L400.8 401.2zM795.6.4H686L400.8 401.2zM801.6 363.1V297L400.8 401.2zM115.7.4H6l394.8 400.8zM0 439.3v66l400.8-104.1z"
+    />
+    <path fill="currentColor" d="M217.5 802h72.7l110.6-400.8z" />
+    <path
+      fill="currentColor"
+      d="M0 591.7v85.6l400.8-276.1zM686 802h109.6L400.8 401.2z"
+    />
+    <path fill="currentColor" d="M368.7 802h64.1l-32-400.8z" />
+    <path fill="currentColor" d="M0 297v66.1l400.8 38.1z" />
+    <path
+      fill="currentColor"
+      d="M6 802h109.7l285.1-400.8zM511.4 802H584L400.8 401.2zM0 124.9v85.6l400.8 190.7zM400.8 401.2l400.8 104.1v-66zM801.6 677.3v-85.6L400.8 401.2z"
+    />
   </svg>
 );
 
