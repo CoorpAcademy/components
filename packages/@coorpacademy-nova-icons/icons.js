c39f3f24540208f44e5e67830701600ff113f725
Faustino Kialungila <Faustino.kialungila@gmail.com>
2019-03-22 16:28:27 +0100

    Exporting new icon (#1531)
    * Exporting new icon
    
    * export close-circle either

--- packages/@coorpacademy-nova-icons/icons.js
+++ packages/@coorpacademy-nova-icons/icons.js
@@ -51,6 +51,8 @@ const icons: Array<Icon> = [
   {filePath: path.resolve('./third-party/nova-line.iconjar/icons/check-circle-1.svg')},
   {filePath: path.resolve('./third-party/nova-line.iconjar/icons/computer-screen-1.svg')},
   {filePath: path.resolve('./third-party/nova-line.iconjar/icons/qr-code.svg')},
+  {filePath: path.resolve('./third-party/nova-line.iconjar/icons/remove-circle-1.svg')},
+  {filePath: path.resolve('./third-party/nova-line.iconjar/icons/close-circle.svg')},
   {filePath: path.resolve('./third-party/nova-solid.iconjar/icons/bookmark-3.svg')},
   {filePath: path.resolve('./third-party/nova-solid.iconjar/icons/bookmark-5.svg')},
   {filePath: path.resolve('./third-party/nova-solid.iconjar/icons/audio-control-play.svg')},
