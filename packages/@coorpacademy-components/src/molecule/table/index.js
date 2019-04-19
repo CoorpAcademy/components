33ceecdb222100200461729fcea916f22841b7f9
Julien Vaillant <julienvaillant@MBP-de-Julien-V.localdomain>
2019-04-19 10:25:34 +0200

    feat(table): add more options to table component

--- packages/@coorpacademy-components/src/molecule/table/index.js
+++ packages/@coorpacademy-components/src/molecule/table/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import PropTypes from 'prop-types';
 import get from 'lodash/fp/get';
 import {
@@ -11,31 +12,30 @@ import Checkbox from '../../atom/checkbox';
 import Link from '../../atom/link';
 import style from './style.css';
 
+const createOptionsView = (_options, hasOptions) => {
+  const optionsView = _options.map((option, oIndex) => {
+    const {onChange, selected} = option;
+
+    return (
+      <div key={oIndex} className={selected ? style.selected : style.option}>
+        <button onClick={onChange}>{option.title}</button>
+      </div>
+    );
+  });
+
+  return hasOptions ? <div className={style.options}>{optionsView}</div> : null;
+};
+
 const Table = (props, context) => {
-  const {rows = [], columns = []} = props;
+  const {rows = [], columns = [], editable = true} = props;
   const {skin} = context;
 
   const mediumColor = get('common.medium', skin);
-  // const brandColor = get('common.brand', skin);
 
   const headerView = columns.map((column, cIndex) => {
     const {title, filtered, options = []} = column;
 
     const hasOptions = options.length > 0;
-
-    const createOptionsView = _options => {
-      const optionsView = _options.map((option, oIndex) => {
-        const {onChange, selected} = option;
-
-        return (
-          <div key={oIndex} className={selected ? style.selected : style.option}>
-            <button onClick={onChange}>{option.title}</button>
-          </div>
-        );
-      });
-
-      return hasOptions ? <div className={style.options}>{optionsView}</div> : null;
-    };
 
     const OptionsIcon = (hasOptions && (filtered ? FunnelIcon : ArrowDown)) || null;
 
@@ -44,7 +44,7 @@ const Table = (props, context) => {
         <div className={hasOptions ? style.toggle : style.noOptions}>
           <Checkbox id={title} name={title} checked className={style.checkbox} />
           <label htmlFor={title}>{title}</label>
-          {options.length > 0 ? createOptionsView(options) : null}
+          {options.length > 0 ? createOptionsView(options, hasOptions) : null}
           <div className={style.optionsIconWrapper}>
             {hasOptions ? (
               <OptionsIcon
@@ -60,29 +60,36 @@ const Table = (props, context) => {
     );
   });
 
-  headerView.unshift(
-    <th key="header">
-      <div className={style.noOptions} />
-    </th>
-  );
+  editable &&
+    headerView.unshift(
+      <th key="header">
+        <div className={style.noOptions} />
+      </th>
+    );
 
   const bodyView = rows.map((row, index) => {
     const {fields = [], editHref} = row;
+    const trClasses = cn({[style.highlighted]: row.highlighted});
 
     const tableRows = fields.map((field, fIndex) => {
       return <td key={fIndex}>{field}</td>;
     });
 
-    tableRows.unshift(
-      // TODO restore dropped support of hoverColor={brandColor}
-      <td key="header">
-        <Link href={editHref}>
-          <PencilIcon color={mediumColor} height={16} width={16} />
-        </Link>
-      </td>
-    );
+    editable &&
+      tableRows.unshift(
+        // TODO restore dropped support of hoverColor={brandColor}
+        <td key="header">
+          <Link href={editHref}>
+            <PencilIcon color={mediumColor} height={16} width={16} />
+          </Link>
+        </td>
+      );
 
-    return <tr key={index}>{tableRows}</tr>;
+    return (
+      <tr className={trClasses} key={index}>
+        {tableRows}
+      </tr>
+    );
   });
 
   return (
@@ -102,10 +109,11 @@ Table.contextTypes = {
 };
 
 Table.propTypes = {
+  editable: PropTypes.bool,
   rows: PropTypes.arrayOf(
     PropTypes.shape({
       fields: PropTypes.arrayOf(PropTypes.string),
-      editHref: PropTypes.string.isRequired
+      editHref: PropTypes.string
     })
   ),
   columns: PropTypes.arrayOf(
---
+++ packages/@coorpacademy-components/src/molecule/table/test/fixtures/readonly.js
@@ -0,0 +1,34 @@
+export default {
+  props: {
+    editable: false,
+    rows: [
+      {
+        fields: ['Savoir planter des choux', 'Oui', 'Non', 'Non'],
+        highlighted: true
+      },
+      {
+        fields: ['Développer pour les nuls', 'Oui', 'Oui', 'Non']
+      },
+      {
+        fields: ['La peinture flamande au 15e siècle', 'Oui', 'Non', 'Non']
+      },
+      {
+        fields: ['Arrêter de procrastiner au travail', 'Oui', 'Non', 'Non']
+      }
+    ],
+    columns: [
+      {
+        title: 'Title'
+      },
+      {
+        title: 'Fr'
+      },
+      {
+        title: 'En'
+      },
+      {
+        title: 'Ru'
+      }
+    ]
+  }
+};
