e3c7413b439746232dabbbe4af247472f3fc6755
Vincent Catillon <devotion95@gmail.com>
2019-04-30 15:49:40 +0200

    Fix typeof

--- packages/@coorpacademy-nova-icons/src/types.js
+++ packages/@coorpacademy-nova-icons/src/types.js
@@ -1,3 +1,9 @@
 // @flow
 
-export type Icon = typeof React$Component<React$ElementProps<'svg'>>;
+import * as React from 'react';
+
+type Props = React$ElementProps<'svg'>;
+
+class Component extends React.Component<Props>{};
+
+export type Icon = typeof Component;
--- packages/@coorpacademy-nova-icons/src/types.native.js
+++ packages/@coorpacademy-nova-icons/src/types.native.js
@@ -1,5 +1,10 @@
 // @flow
 
+import * as React from 'react';
 import type {SvgProps} from 'react-native-svg';
 
-export type Icon = typeof React$Component<SvgProps>;
+type Props = SvgProps;
+
+class Component extends React.Component<Props>{};
+
+export type Icon = typeof Component;
