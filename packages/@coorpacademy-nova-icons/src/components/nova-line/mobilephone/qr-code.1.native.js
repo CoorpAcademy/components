ec39fdf99bd8f147f2bc43c122c248419abca844
Vincent Catillon <devotion95@gmail.com>
2019-05-03 12:47:32 +0200

    Add QR Code and mobile icons

---
+++ packages/@coorpacademy-nova-icons/src/components/nova-line/mobilephone/qr-code.1.native.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+
+const SvgComponent = props => (
+  <Svg viewBox="0 0 24 24" {...props}>
+    <Path fill="none" d="M0-.001h24v24H0z" />
+    <Path
+      fill={props.color}
+      d="M12 9.999h-2v-4h4v2h-2zM11 14.999H6v-5h2v3h3zM12 10.999h2v4h-2zM18 13.999h-2v-2h-1v-2h3z"
+    />
+    <Path
+      d="M20 21.999H4c-1.103 0-2-.896-2-2v-16c0-1.104.897-2 2-2h16c1.103 0 2 .896 2 2v16c0 1.104-.897 2-2 2zm-16-18v16h16.001l-.001-16H4z"
+      fill={props.color}
+    />
+    <Path
+      fill={props.color}
+      d="M6 5.999h3v3H6zM15 14.999h3v3h-3zM15 5.999h3v3h-3zM9 15.999h5v2H9zM6 15.999h2v2H6z"
+    />
+  </Svg>
+);
+
+export default SvgComponent;
---
+++ packages/@coorpacademy-nova-icons/src/components/nova-line/network/network-mobile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SvgComponent = props => (
+  <svg viewBox="0 0 24 24" {...props}>
+    <path fill="none" d="M0 0h24v24H0z" />
+    <g fill="currentColor">
+      <path d="M0 12c0 5.515 4.486 10 10 10v-2a7.968 7.968 0 0 1-3-.588V16h3v-2H7v-4h3V8H7V4.59A7.95 7.95 0 0 1 10 4V2C4.486 2 0 6.486 0 12zm2.264 2A7.947 7.947 0 0 1 2 12c0-.691.098-1.359.264-2H5v4H2.264zm.818 2H5v2.235A8.04 8.04 0 0 1 3.082 16zM5 5.766V8H3.082A8.064 8.064 0 0 1 5 5.766zM21 2h-8c-1.104 0-2 .898-2 2v16a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4c0-1.102-.896-2-2-2zm0 2v2h-8V4h8zm0 4l.001 8H13V8h8zm-8 12v-2h8.002v2H13z" />
+    </g>
+  </svg>
+);
+
+export default SvgComponent;
