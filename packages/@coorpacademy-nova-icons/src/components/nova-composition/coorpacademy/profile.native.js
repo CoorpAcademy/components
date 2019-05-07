b3ae473aa98681105a1f45534b1656fcae0b3dd5
Vincent CATILLON <devotion95@gmail.com>
2019-05-07 16:24:05 +0200

    Fix SVG properties (#1576)

--- packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/profile.native.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/profile.native.js
@@ -6,7 +6,14 @@ const SvgComponent = props => (
     <G fill={props.color}>
       <Path d="M13.502 12.506c1.8 0 3.2-1.5 3.2-3.2s-1.4-3.3-3.2-3.3-3.3 1.5-3.3 3.2 1.5 3.3 3.3 3.3zM13.502 13.306c-3.8 0-6.5 2-6.5 4.9v.8h13v-.8c0-2.9-2.7-4.9-6.5-4.9z" />
     </G>
-    <Circle fill="none" stroke={props.color} cx={13.5} cy={13.5} r={12.5} />
+    <Circle
+      fill="none"
+      stroke={props.color}
+      strokeWidth={2}
+      cx={13.5}
+      cy={13.5}
+      r={12.5}
+    />
   </Svg>
 );
 
