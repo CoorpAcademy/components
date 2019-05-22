8d862f692333350011c2a1ce74bf89435f824b91
rocket-star <rudy.fernando@hotmail.fr>
2019-05-22 16:42:04 +0200

    delete import classenames

--- packages/@coorpacademy-components/src/template/battle/index.js
+++ packages/@coorpacademy-components/src/template/battle/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import Provider from '../../atom/provider';
 import Link from '../../atom/link';
 import Avatar from '../../atom/avatar';
