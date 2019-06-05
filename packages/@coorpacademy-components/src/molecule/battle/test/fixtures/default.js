634c648f484b811b59d5950136b9a86848364209
rocket-star <rudy.fernando@hotmail.fr>
2019-06-05 18:29:40 +0200

    refactor component v1

--- packages/@coorpacademy-components/src/molecule/battle/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/battle/test/fixtures/default.js
@@ -1,6 +1,6 @@
 export default {
   props: {
-    name: 'User',
+    username: 'User',
     courseName: 'courseName',
     submitReviewCourseValue: 'Review course',
     submitStartBattleValue: 'Start battle',
@@ -8,6 +8,7 @@ export default {
     onClickButtonCourse: () => console.log('test onClickButtonCourse'),
     onClickButtonBattle: () => console.log('test onClickButtonBattle'),
     urlAvatar: 'http://minecraft.fr/wp-content/uploads/2013/05/Pa8WZj1-235x300.jpg',
-    urlImg: 'https://img.bfmtv.com/c/1256/708/c32/01a671c64243d70195cbe64a18f2a.jpg'
+    urlImg: 'https://img.bfmtv.com/c/1256/708/c32/01a671c64243d70195cbe64a18f2a.jpg',
+    id: '1'
   }
 };
