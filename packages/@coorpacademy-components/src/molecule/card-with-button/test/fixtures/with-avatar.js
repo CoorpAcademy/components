28a7a4efbd14a1899dfc4a0030932fcaa9634fbe
rocket-star <rudy.fernando@hotmail.fr>
2019-06-13 15:43:22 +0200

    add new css file

---
+++ packages/@coorpacademy-components/src/molecule/card-with-button/test/fixtures/with-avatar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import style from '../../with-avatar.css';
+import Avatar from '../../../../atom/avatar';
+
+export default {
+  props: {
+    username: 'User',
+    courseName: 'courseName',
+    secondaryButtonLabel: 'Review course',
+    primaryButtonLabel: 'Start battle',
+    tagLabel: 'Coach',
+    onClickSecondaryButton: () => console.log('test onClickButtonCourse'),
+    onClickPrimaryButton: () => console.log('test onClickButtonBattle'),
+    backgroundImg: 'https://img.bfmtv.com/c/1256/708/c32/01a671c64243d70195cbe64a18f2a.jpg'
+  },
+  children: (
+    <div>
+      <div className={style.injection}>
+        <div className={style.avt}>
+          <Avatar url="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947_1280.jpg" />
+        </div>
+      </div>
+      <div className={style.wrapper}>
+        <span>
+          <strong>Avatar </strong>is challenging you
+          <p>
+            <strong>Coorpacademy</strong>
+          </p>
+        </span>
+      </div>
+    </div>
+  )
+};
---
+++ packages/@coorpacademy-components/src/template/battle-requests/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Provider from '../../atom/provider';
+import CardWithButton from '../../molecule/card-with-button';
+import Avatar from '../../atom/avatar';
+import style from './style.css';
+
+const Battles = (props, context) => {
+  const {translate} = context;
+  const battles = props.battles;
+  const trads = props.trads;
+  return (
+    <ul className={style.cards}>
+      {battles.map(battle => {
+        const challengeLabel = translate('is challenging you', {
+          username: `<strong>${battle.username}</strong>`,
+          courseName: `<p><strong>${battle.courseName}</strong></p>`
+        });
+        const battleCardProps = {
+          username: battle.username,
+          courseName: battle.courseName,
+          primaryButtonLabel: trads.battleCTA,
+          secondaryButtonLabel: trads.reviewCTA,
+          tagLabel: battle.tagLabel,
+          onClickSecondaryButton: battle.onClickSecondaryButton,
+          onClickPrimaryButton: battle.onClickPrimaryButton,
+          backgroundImg: battle.backgroundImg
+        };
+        return (
+          <li key={battle.id} className={style.cardsItem}>
+            <div className={style.card}>
+              <CardWithButton {...battleCardProps}>
+                <div className={style.injection}>
+                  <Avatar url={battle.urlAvatar} />
+                </div>
+                <div
+                  className={style.wrapper}
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: challengeLabel
+                  }}
+                />
+              </CardWithButton>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+Battles.contextTypes = {
+  translate: Provider.childContextTypes.translate
+};
+
+Battles.propTypes = {
+  battles: PropTypes.arrayOf(PropTypes.shape(CardWithButton.PropTypes)),
+  trads: PropTypes.objectOf(PropTypes.string)
+};
+
+export default Battles;
