a2caf9caf87a653dfb3da6a63f178e084b3a054e
Faustino Kialungila <Faustino.kialungila@gmail.com>
2019-08-08 11:52:27 +0200

    Handle JWP 214000 Error (#1611)
    * Handle JWP 214000 Error
    
    * Remove duplicated setState
    
    * also set url when component did update
    
    * use regex to avoid adding new props
    
    * check if has matched

--- packages/@coorpacademy-components/src/molecule/video-player/jwplayer.js
+++ packages/@coorpacademy-components/src/molecule/video-player/jwplayer.js
@@ -9,10 +9,18 @@ import style from './jwplayer.css';
 class JWPlayer extends React.Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      fileUrl: ''
+    };
     this.handlePlay = this.handlePlay.bind(this);
     this.handleResume = this.handleResume.bind(this);
     this.handlePause = this.handlePause.bind(this);
     this.handleEnded = this.handleEnded.bind(this);
+    this.handleError = this.handleError.bind(this);
+  }
+
+  componentDidMount() {
+    this.setFileUrl();
   }
 
   componentDidUpdate(prevProps) {
@@ -23,6 +31,15 @@ class JWPlayer extends React.Component {
         window.jwplayer().play();
       }
     }
+    if (prevProps.jwpOptions.file !== this.props.jwpOptions.file) {
+      this.setFileUrl();
+    }
+  }
+
+  setFileUrl() {
+    const {file} = this.props.jwpOptions;
+
+    return this.setState({fileUrl: file});
   }
 
   handlePlay(e) {
@@ -41,15 +58,37 @@ class JWPlayer extends React.Component {
     this.props.onEnded && this.props.onEnded(e);
   }
 
+  handleError(error) {
+    const {code} = error;
+
+    // This error mostly appears on IE11 on W7
+    // Since IE11 dont kinda support M3U8 sometimes,
+    // We've decided to switch from M3U8 to mp4 whenever it appears
+    if (code === 214000) {
+      const {file: videoUrl} = this.props.jwpOptions;
+      const regex = /^https:\/\/content.jwplatform\.com\/manifests\/(\w+).m3u8/;
+      const matched = videoUrl.match(regex);
+
+      if (matched) {
+        const videoId = matched[1];
+        return this.setState({
+          fileUrl: `https://content.jwplatform.com/videos/${videoId}-1080.mp4`
+        });
+      }
+    }
+  }
+
   render() {
     return (
       <ReactJWPlayer
+        {...this.props.jwpOptions}
         className={style.wrapper}
         onPlay={this.handlePlay}
         onResume={this.handleResume}
         onPause={this.handlePause}
         onOneHundredPercent={this.handleEnded}
-        {...this.props.jwpOptions}
+        onError={this.handleError}
+        file={this.state.fileUrl}
       />
     );
   }
