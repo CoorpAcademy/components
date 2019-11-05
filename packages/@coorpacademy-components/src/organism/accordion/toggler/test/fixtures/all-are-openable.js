61a4c493ecc61a450676b10708f830ad332e7573
esa-coorp <eric.sampaio@coorpacademy.com>
2019-11-05 11:13:46 +0100

    fix review

--- packages/@coorpacademy-components/src/organism/accordion/toggler/test/fixtures/all-are-openable.js
+++ packages/@coorpacademy-components/src/organism/accordion/toggler/test/fixtures/all-are-openable.js
@@ -10,7 +10,8 @@ import containerFixture from '../../../container/test/fixtures/default';
 export default {
   props: {
     tabProps: containerFixture.props.tabProps,
-    type: 'all'
+    type: 'all',
+    theme: 'setup'
   },
   children: [
     <QcmGraphic key="1" {...qcmGraphicFixture.props} />,
--- packages/@coorpacademy-components/src/organism/accordion/toggler/test/fixtures/no-childrens.js
+++ packages/@coorpacademy-components/src/organism/accordion/toggler/test/fixtures/no-childrens.js
@@ -3,6 +3,7 @@ import containerFixture from '../../../container/test/fixtures/default';
 export default {
   props: {
     tabProps: containerFixture.props.tabProps,
-    type: 'all'
+    type: 'all',
+    theme: 'setup'
   }
 };
