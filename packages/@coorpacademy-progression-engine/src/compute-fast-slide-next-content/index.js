5dfcebecf83b7b2117bc9dc24cc5e95db55911f4
Silouane G <silouane.galinou@coorpacademy.com>
2019-11-06 17:06:26 +0100

    add babel-lodash-plugin

--- packages/@coorpacademy-progression-engine/src/compute-fast-slide-next-content/index.js
+++ packages/@coorpacademy-progression-engine/src/compute-fast-slide-next-content/index.js
@@ -1,6 +1,12 @@
 // @flow
 
-import {get, map, min, multiply, pipe, size, sum} from 'lodash/fp';
+import pipe from 'lodash/fp/pipe';
+import get from 'lodash/fp/get';
+import map from 'lodash/fp/map';
+import min from 'lodash/fp/min';
+import multiply from 'lodash/fp/multiply';
+import size from 'lodash/fp/size';
+import sum from 'lodash/fp/sum';
 import type {Config, Content} from '../types';
 import type {FastSlideNextContent} from './types';
 
@@ -19,13 +25,15 @@ const getFastSlideExitNode = (
         type: 'failure',
         ref: 'failExitNode'
       };
+
+  // $FlowFixMe
   const starsMax = pipe(
     // $FlowFixMe
     map(pipe([get('slides'), size, s => min([s, slidesToComplete])])),
-    // $FlowFixMe
     sum,
     multiply(starsPerCorrectAnswer)
   )(availableContent);
+
   const instructions = godMode
     ? [
         {
