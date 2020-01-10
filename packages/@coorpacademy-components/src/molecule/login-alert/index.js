b7a1932d6735110dd00352fef4bcf4f4530fedc9
BertrandD <bernaz@pm.me>
2020-01-10 11:40:05 +0100

    few feedbacks

--- packages/@coorpacademy-components/src/molecule/login-alert/index.js
+++ packages/@coorpacademy-components/src/molecule/login-alert/index.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {NovaSolidInterfaceFeedbackInterfaceAlertTriangle as AlertIcon} from '@coorpacademy/nova-icons';
-import Provider from '../../atom/provider';
-import {container, iconContainer, content, icon} from './style.css';
+import styles from './style.css';
 
-const LoginAlert = ({body}, context) => (
-  <div className={container}>
+const LoginAlert = ({body}) => (
+  <div className={styles.container}>
     <div
-      className={content}
+      className={styles.content}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{__html: body}}
     />
-    <div className={iconContainer}>
-      <AlertIcon height={16} className={icon} />
+    <div className={styles.iconContainer}>
+      <AlertIcon className={styles.icon} />
     </div>
   </div>
 );
-
-LoginAlert.contextTypes = {
-  skin: Provider.childContextTypes.skin,
-  translate: Provider.childContextTypes.translate
-};
 
 LoginAlert.propTypes = {
   body: PropTypes.text
