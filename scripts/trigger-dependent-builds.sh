18eccf48fdd506be14dee3d6db2ce988a6856488
Adrien Becchis <adrien.becchis@gmail.com>
2020-01-24 10:21:12 +0100

    Update trigger adjustments (#1677) :trackball:
    - Fix mooc branch for dependent build (use master!) :wrench:
    - Override triggered build message to be able to detect them :label:

--- scripts/trigger-dependent-builds.sh
+++ scripts/trigger-dependent-builds.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "About to trigger dependent builds"
-for build in api-progression:master app-backoffice:master coorpacademy-cockpit:master coorpacademy:deploy-production
+for build in api-progression:master app-backoffice:master coorpacademy-cockpit:master coorpacademy:master
 do
     echo "Triggering build for Coorpacademy/${build%:*}"
      curl -s -X POST -H "Content-Type: application/json" -H "Accept:application/json" -H "Travis-API-Version: 3" \
-      -H "Authorization: token $TRAVIS_TOKEN" -d '{"request":{"branch":"'${build#*:}'"}}' \
+      -H "Authorization: token $TRAVIS_TOKEN" -d '{"request":{"branch":"'${build#*:}'","message":"Update-Node trigger :trackball:"}}' \
       https://api.travis-ci.com/repo/CoorpAcademy%2F${build%:*}/requests
 done
