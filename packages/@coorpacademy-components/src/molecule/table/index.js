c234519a89528bbf420a464ed6ef37663d2df467
Adams <adams.roy27@gmail.com>
2020-01-24 11:21:19 +0100

    use get from lodash for safety

--- packages/@coorpacademy-components/src/molecule/table/index.js
+++ packages/@coorpacademy-components/src/molecule/table/index.js
@@ -94,7 +94,7 @@ const Table = (props, context) => {
   }
 
   const renderIcon = field => {
-    switch (field.icon) {
+    switch (get('icon', field)) {
       case 'draft':
         return <DraftIcon width={25} height={25} />;
       case 'validate':
