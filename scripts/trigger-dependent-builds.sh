6fb9ba6e03c5fd379c0c176cb154e99aed21d015
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-01-24 10:01:13 +0100

    Fix mooc branch for dependent build (use master!) :wrench:

--- scripts/trigger-dependent-builds.sh
+++ scripts/trigger-dependent-builds.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "About to trigger dependent builds"
-for build in api-progression:master app-backoffice:master coorpacademy-cockpit:master coorpacademy:deploy-production
+for build in api-progression:master app-backoffice:master coorpacademy-cockpit:master coorpacademy:master
 do
     echo "Triggering build for Coorpacademy/${build%:*}"
      curl -s -X POST -H "Content-Type: application/json" -H "Accept:application/json" -H "Travis-API-Version: 3" \
