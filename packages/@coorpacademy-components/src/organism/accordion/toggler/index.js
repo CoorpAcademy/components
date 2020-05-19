04921461fc60db2270593fa87b31645d4d3b938a
godu <arthur.weber@viacesi.fr>
2020-05-19 16:59:30 +0200

    Fix props-state sync bug

--- packages/@coorpacademy-components/src/organism/accordion/toggler/index.js
+++ packages/@coorpacademy-components/src/organism/accordion/toggler/index.js
@@ -1,44 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {forEach} from 'lodash/fp';
+import {map, pipe, set, get, getOr, identity, constant} from 'lodash/fp';
+
 import Accordion from '../container';
 
+const mapi = map.convert({cap: false});
+const map2 = (iteratee, arr1, arr2) => {
+  return mapi((value1, index) => {
+    const value2 = get(index, arr2);
+    return iteratee(value1, value2);
+  }, arr1);
+};
+
 class AccordionToggler extends React.Component {
+  static getDerivedStateFromProps(props, state) {
+    if (state.isOpen) return null;
+
+    const isOpen = map(getOr(false, 'isOpen'), props.tabProps);
+
+    return {isOpen};
+  }
+
   constructor(props) {
     super(props);
-    this.state = {
-      tabProps: props.tabProps
-    };
+
+    this.state = {isOpen: null};
 
     this.handleOnClick = this.handleOnClick.bind(this);
-    this.newOpenState = this.newOpenState.bind(this);
   }
 
   handleOnClick(key) {
     this.setState(prevState => {
-      const newState = {...prevState};
-      forEach.convert({cap: false})((part, index) => {
-        part.isOpen = this.newOpenState(index === key, part.isOpen);
-      }, newState.tabProps);
-
-      return newState;
+      return {
+        isOpen: pipe(
+          this.props.oneTabOnly ? map(constant(false)) : identity,
+          set(key, !get(key, prevState.isOpen))
+        )(prevState.isOpen)
+      };
     });
   }
 
-  newOpenState(isSelectedTab, currentOpenState) {
-    const isOpenOtherTab = this.props.oneTabOnly ? false : currentOpenState;
-    return isSelectedTab ? !currentOpenState : isOpenOtherTab;
+  mergePropsAndState() {
+    return map2(
+      (props, isOpen) => ({
+        ...props,
+        isOpen
+      }),
+      this.props.tabProps,
+      this.state.isOpen
+    );
   }
 
   render() {
     const {children = [], theme} = this.props;
     return (
-      <Accordion tabProps={this.state.tabProps} onClick={this.handleOnClick} theme={theme}>
+      <Accordion tabProps={this.mergePropsAndState()} onClick={this.handleOnClick} theme={theme}>
         {children}
       </Accordion>
     );
   }
 }
+
 AccordionToggler.propTypes = {
   tabProps: PropTypes.arrayOf(PropTypes.shape(Accordion.PropTypes)),
   oneTabOnly: PropTypes.bool,
