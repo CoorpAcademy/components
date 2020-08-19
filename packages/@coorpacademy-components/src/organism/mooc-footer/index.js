8178b3fe7f8694ec577560e90e83500b60f9b8c1
stefano <stefanoacosta.92@gmail.com>
2020-08-19 12:49:40 +0200

    mooc footer WIP

--- packages/@coorpacademy-components/src/organism/mooc-footer/index.js
+++ packages/@coorpacademy-components/src/organism/mooc-footer/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-// import style from './style.css';
+// eslint-disable-next-line no-unused-vars
+import {isEmpty} from 'lodash/fp';
+import Provider from '../../atom/provider';
+import Link from '../../atom/link';
+import Title from '../../atom/title';
+import Picture from '../../atom/picture';
+// eslint-disable-next-line css-modules/no-unused-class
+import style from './style.css';
 
 const socialLinksTypes = ['facebook', 'twitter', 'linkedIn'];
 
+// eslint-disable-next-line react/prefer-stateless-function
 class MoocFooter extends React.Component {
   static propTypes = {
     headSection: PropTypes.shape({
@@ -12,27 +19,83 @@ class MoocFooter extends React.Component {
       androidLink: PropTypes.string,
       iosLink: PropTypes.string
     }),
+    // eslint-disable-next-line react/no-unused-prop-types
     socialLinks: PropTypes.arrayOf(
-        PropTypes.shape({
-          type: PropTypes.oneOf(socialLinksTypes),
-          link: PropTypes.string
-        })
+      PropTypes.shape({
+        type: PropTypes.oneOf(socialLinksTypes),
+        link: PropTypes.string
+      })
     ),
-    sections: PropTypes.arrayOf(
+    // eslint-disable-next-line react/no-unused-prop-types
+    siteMapSections: PropTypes.arrayOf(
       PropTypes.shape({
         title: PropTypes.string,
-        links: PropTypes.arrayOf(PropTypes.shape({title: PropTypes.string, link: PropTypes.string}))
+        pages: PropTypes.arrayOf(PropTypes.shape({title: PropTypes.string, link: PropTypes.string}))
       })
     )
+  };
+
+  static contextTypes = {
+    translate: Provider.childContextTypes.translate
   };
 
   constructor(props) {
     super(props);
   }
 
-  // eslint-disable-next-line class-methods-use-this
   render() {
-    return <div>Just a plain test</div>;
+    // eslint-disable-next-line react/destructuring-assignment
+    const headSection = isEmpty(this.props.headSection) ? (
+      <div data-name="headSection">
+        <div className={`${style.logoWrapper} ${style.vertLineRight}`}>
+          <div className={style.logoMobile} data-name="logo-mobile">
+            {/* TODO: ask how to get the static logourl */}
+            <Picture src="logourl" />
+          </div>
+        </div>
+        <div data-name="mobile-marketing-text">
+          <p>{this.translate('coorp_mobile_marketing_text')}</p>
+        </div>
+        <div data-name="mobile-apps-buttons-wrapper">
+          <p>{this.translate('coorp_mobile_marketing_text')}</p>
+        </div>
+      </div>
+    ) : null;
+    const sections = this.siteMapSections.map((section, index) => {
+      return (
+        <div key={index}>
+          <Title>{this.translate(section.title)}</Title>
+          <ul>
+            {/* TODO: are dynamic translations managed?? */}
+            {section.pages.map((page, pindex) => {
+              return (
+                <li key={pindex}>
+                  <Link href={page.link}>{page.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    });
+    const siteMap = (
+      <div data-name="siteMap">
+        <div data-name="sections">{sections}</div>
+        <ul>
+          {this.siteMapSections.map((section, index) => {
+            // return <li key={index}>{section}</li>;
+            return null;
+          })}
+        </ul>
+      </div>
+    );
+    // return <div>Just a plain test</div>;
+    return (
+      <div className={style.wrapper}>
+        {headSection}
+        {siteMap}
+      </div>
+    );
   }
 }
 
