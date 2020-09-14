3d4e372e6aa977392bd7d626f5cf8968695b35f5
stefano <stefanoacosta.92@gmail.com>
2020-09-14 15:00:32 +0200

    Styles: podcast background image (black filter, fixes other css props based on Trellos 1849 card)

--- packages/@coorpacademy-components/src/template/external-course/test/fixtures/podcast.js
+++ packages/@coorpacademy-components/src/template/external-course/test/fixtures/podcast.js
@@ -5,7 +5,6 @@ const {props} = Default;
 
 export default {
   props: defaultsDeep(props, {
-    backgroundImageUrl:
-      'https://api-staging.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/page_discipline/1_recherche_en_ligne.jpg&amp;h=400&amp;w=400&amp;q=80&amp;m=contain'
+    backgroundImageUrl: 'https://static.coorpacademy.com/site/podcast.jpg'
   })
 };
