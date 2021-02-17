b428ffa616a160e91cbda37cfcf7edc0308ba1ba
esa-coorp <eric.sampaio@coorpacademy.com>
2021-02-17 16:15:26 +0100

    Fix svg color

--- packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/logout.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/logout.js
@@ -1,22 +1,10 @@
 import * as React from "react";
 
 const SvgComponent = (props) => (
-  <svg
-    id="prefix__Calque_1"
-    x={0}
-    y={0}
-    viewBox="0 0 18.1 18"
-    xmlSpace="preserve"
-    {...props}
-  >
-    <style>
-      {
-        ".prefix__st0{fill:none;stroke:#f73f52;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10}"
-      }
-    </style>
-    <path className="prefix__st0" d="M1 9h10.9M9 6.1L11.9 9 9 11.9" />
+  <svg viewBox="0 0 18.1 18" {...props}>
+    <path stroke="currentColor" d="M1 9h10.9M9 6.1L11.9 9 9 11.9" />
     <path
-      className="prefix__st0"
+      stroke="currentColor"
       d="M1.7 5.7C3 2.9 5.8 1 9 1c4.4 0 8 3.6 8 8s-3.6 8-8 8c-3.3 0-6.1-1.9-7.3-4.7"
     />
   </svg>
--- packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/logout.native.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/logout.native.js
@@ -1,19 +1,11 @@
 import * as React from "react";
 import Svg, { Path } from "react-native-svg";
-/* SVGR has dropped some elements not supported by react-native-svg: style */
 
 const SvgComponent = (props) => (
-  <Svg
-    id="prefix__Calque_1"
-    x={0}
-    y={0}
-    viewBox="0 0 18.1 18"
-    xmlSpace="preserve"
-    {...props}
-  >
-    <Path className="prefix__st0" d="M1 9h10.9M9 6.1L11.9 9 9 11.9" />
+  <Svg viewBox="0 0 18.1 18" {...props}>
+    <Path stroke={props.color} d="M1 9h10.9M9 6.1L11.9 9 9 11.9" />
     <Path
-      className="prefix__st0"
+      stroke={props.color}
       d="M1.7 5.7C3 2.9 5.8 1 9 1c4.4 0 8 3.6 8 8s-3.6 8-8 8c-3.3 0-6.1-1.9-7.3-4.7"
     />
   </Svg>
