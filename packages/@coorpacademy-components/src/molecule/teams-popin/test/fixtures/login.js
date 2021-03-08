077fe3c632a830a772c761d16c639c2a67fc2497
stefano <stefanoacosta.92@gmail.com>
2021-03-08 15:42:47 +0100

    adds about info label

--- packages/@coorpacademy-components/src/molecule/teams-popin/test/fixtures/login.js
+++ packages/@coorpacademy-components/src/molecule/teams-popin/test/fixtures/login.js
@@ -1,10 +1,11 @@
 export default {
   props: {
-    header: 'Welcome to Faurecia academy on Microsoft Teams',
-    content:
-      'To connect your account with Microsoft Teams, please go to your learning platform and log in',
+    header: 'Welcome to Coorpacademy App on Microsoft Teams',
+    content: 'To discover your dashboard please Log in.',
     type: 'login',
     buttonLabel: 'Login to my learning platform',
+    aboutLabel:
+      'If you want to know more about Coorpacademy, please contact us at contact@coorpacademy.com',
     onButtonClick: () => {}
   }
 };
