381b51e0560e39dab94da1bd1ef1011e3e8a2f01
Joan Ortega <jomaora@gmail.com>
2021-04-06 16:12:29 +0200

    wip - adding navigation arrows

--- packages/@coorpacademy-components/src/template/activity/engine-list.js
+++ packages/@coorpacademy-components/src/template/activity/engine-list.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {getOr} from 'lodash/fp';
+import {
+  NovaCompositionNavigationArrowLeft as ArrowLeft,
+  NovaCompositionNavigationArrowRight as ArrowRight
+} from '@coorpacademy/nova-icons';
 import Provider from '../../atom/provider';
 import EngineStars from './engine-stars';
 import style from './engine-list.css';
@@ -20,12 +25,27 @@ class EngineList extends React.Component {
 
   render() {
     const {engines = []} = this.props;
+    const {skin} = this.context;
+    const dark = getOr('#90A4AE', 'common.dark', skin);
 
     const engineTabs = engines.map(engine => <EngineStars {...engine} key={engine.type} />);
 
+    const leftArrowView = (
+      <div className={style.circle} onClick={this.handleOnLeft} data-name="card-list-left-arrow">
+        <ArrowLeft color={dark} className={style.left} width={10} height={10} />
+      </div>
+    );
+    const rightArrowView = (
+      <div className={style.circle} onClick={this.handleOnRight} data-name="card-list-right-arrow">
+        <ArrowRight color={dark} className={style.right} width={10} height={10} />
+      </div>
+    );
+
     return (
       <div className={style.allStars} data-name="engineList">
+        <div className={style.navigationLeft}>{leftArrowView}</div>
         {engineTabs}
+        <div className={style.navigationRight}>{rightArrowView}</div>
       </div>
     );
   }
