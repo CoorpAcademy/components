985d728ededa07a58617012611591e480da4c594
Faustino Kialungila <Faustino.kialungila@gmail.com>
2021-06-14 16:05:49 +0200

    patch: export audio control icons (#1899)

---
+++ packages/@coorpacademy-nova-icons/src/components/nova-line/audio/audio-control-fast-forward.native.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Svg, { Path, G } from "react-native-svg";
+
+const SvgComponent = (props) => (
+  <Svg viewBox="0 0 24 24" {...props}>
+    <Path fill="none" d="M0 0h24v24H0z" />
+    <G fill={props.color}>
+      <Path d="M12 2C6.486 2 2 6.486 2 12c0 5.515 4.486 10 10 10s10-4.485 10-10c0-5.514-4.486-10-10-10zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z" />
+      <Path d="M12 8v2.667L8 8v8l4-2.667V16l6-4z" />
+    </G>
+  </Svg>
+);
+
+export default SvgComponent;
