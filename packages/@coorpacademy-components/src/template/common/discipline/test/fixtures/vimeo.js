b7904d384247de83e9dbeae63b2f42be8f89f5da
godu <arthur.weber@viacesi.fr>
2021-06-18 10:31:38 +0200

    Set size during the loading of vimeo player

--- packages/@coorpacademy-components/src/template/common/discipline/test/fixtures/vimeo.js
+++ packages/@coorpacademy-components/src/template/common/discipline/test/fixtures/vimeo.js
@@ -1,14 +1,20 @@
 import {defaultsDeep} from 'lodash/fp';
 import disciplineHeader from '../../../../../molecule/discipline-header/test/fixtures/vimeo';
+import disciplineScope from '../../../../../molecule/discipline-scope/test/fixtures/vimeo';
 import Default from './default';
 
 const {props} = Default;
 const {title, description, video} = disciplineHeader.props;
+const {content: level, levels, selected, onClick} = disciplineScope.props;
 
 export default {
   props: defaultsDeep(props, {
     title,
     description,
-    video
+    video,
+    level,
+    levels,
+    selected,
+    onClick
   })
 };
