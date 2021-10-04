e124574e8aa8bacf5d370d7b8a3d15027f43739e
Joan Ortega <joan.ortega@coorpacademy.com>
2021-10-04 11:33:14 +0200

    Update forms for coorpmanager (#1957)
    * update font style for titles and subtitle of groups in form and clear line separator
    
    * Modify title component for coorpamanager
    
    * update style for new title component
    
    * restore medium color value

--- packages/@coorpacademy-components/src/atom/title/index.js
+++ packages/@coorpacademy-components/src/atom/title/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './style.css';
 
-const Title = ({children}) => {
-  return <h1>{children}</h1>;
+const Title = props => {
+  const {title, subtitle} = props;
+
+  const subtitleSection = subtitle ? <div className={style.subtitle}>{subtitle}</div> : null;
+
+  return (
+    <div>
+      <div className={style.title}>{title}</div>
+      {subtitleSection}
+    </div>
+  );
 };
 
 Title.propTypes = {
-  children: PropTypes.node
+  title: PropTypes.string,
+  subtitle: PropTypes.string
 };
 export default Title;
