3b4d44d51a6c219ac7cd8f7f19399b0e01d38c9b
esa-coorp <eric.sampaio@coorpacademy.com>
2021-10-13 17:25:59 +0200

    Fix tests

--- packages/@coorpacademy-components/src/template/app-player/popin-end/test/fixtures/next-level-adaptive-video.js
+++ packages/@coorpacademy-components/src/template/app-player/popin-end/test/fixtures/next-level-adaptive-video.js
@@ -9,6 +9,7 @@ export default defaultsDeep(correct, {
       feedback: {
         type: 'success',
         title: 'VOTRE ATOUT SÉDUCTION: LA VIVACITÉ D’ESPRIT (MAJORITÉ DE C).',
+        mediaDescription: 'media title',
         description:
           'Au premier abord, on vous prend pour une Madame Bluff. Qui tente, à coups de blablabla pêché dans la collection « Pour les Nuls », d’impressionner la galerie. Tout faux. Car, lorsqu’on vous sert des arguments un peu pointus sur un sujet, vous dégainez sans problème de vraies connaissances. Inflexible. Cultivée. Apte à discourir, en une bouteille de bourgueil, de la Cour des comptes, de la crise malienne ou de l’expo Dalienne. Vous parlez bien, comme un dico. Et vous avez l’esprit qui fuse, capable d’anticiper une réaction, de réconforter avec les bons mots, d’analyser une émotion. Vient le temps des conseils : soyez vigilante, par votre agilité, à ne pas écraser l’autre, plus lent. Veillez aussi à savoir vous taire pour mieux écouter. Un garçon aime bien briller, éblouir. Respectez son temps de parole, apprenez de lui, que le dating devienne un échange et pas une conférence.',
         media: {
