6bd27fa95959f216fbfc88931279561b002b7ff3
Fausto95 <faustino.kialungila@gmail.com>
2021-10-15 16:29:32 +0200

    lint fix

--- packages/@coorpacademy-components/src/molecule/answer/test/fixtures/qcm-arabic.js
+++ packages/@coorpacademy-components/src/molecule/answer/test/fixtures/qcm-arabic.js
@@ -10,8 +10,7 @@ export default {
     },
     media: {
       type: 'img',
-      url:
-        'https://user-images.githubusercontent.com/7602475/28273504-db1da18a-6b0e-11e7-97cd-b2e307fc51c7.png'
+      url: 'https://user-images.githubusercontent.com/7602475/28273504-db1da18a-6b0e-11e7-97cd-b2e307fc51c7.png'
     }
   }
 };
