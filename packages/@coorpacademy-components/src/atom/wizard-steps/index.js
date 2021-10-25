c1d2914c76bf373c4654ff87bf32b1e7228fd04d
Joan Ortega <jomaora@gmail.com>
2021-10-25 15:43:30 +0200

    adding skin to wizard without color nor done steps

--- packages/@coorpacademy-components/src/atom/wizard-steps/index.js
+++ packages/@coorpacademy-components/src/atom/wizard-steps/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './style.css';
 
 const WizardSteps = (props, context) => {
   const {steps} = props;
 
   const wizard = steps.map((step, index) => {
-    return <div key={`step-${index}`}>{step.title}</div>;
+    return (
+      <div className={style.step} key={`step-${index}`} data-step="{index}">
+        <label className={style.label}>{step.title}</label>
+        <div className={style.dot}></div>
+    </div>
+    );
   });
 
-  return <div>{wizard}</div>;
+  return <div className={style.container}>{wizard}</div>;
 };
 
 WizardSteps.propTypes = {
