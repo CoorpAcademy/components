6da672ae62c6b0ff2393cbba806c750cd71ebabf
Joan Ortega <jomaora@gmail.com>
2021-10-27 14:09:51 +0200

    Adding icon and hover for selected mode

--- packages/@coorpacademy-components/src/atom/search-result-item/index.js
+++ packages/@coorpacademy-components/src/atom/search-result-item/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import {
+  NovaCompositionNavigationMore as AddIcon,
+  NovaCompositionCoorpacademyCheck as SelectedIcon
+} from '@coorpacademy/nova-icons';
 import style from './style.css';
 
 const SearchResultItem = props => {
@@ -10,6 +14,9 @@ const SearchResultItem = props => {
     <div className={classnames(style.container, selected ? style.selected : style.unselected)}>
       <div className={style.text}>{text}</div>
       <div className={style.information}>{information}</div>
+      <div className={selected ? style.selectedIconWrapper : style.unselectedIconWrapper}>
+        {selected ? <SelectedIcon className={style.icon} /> : <AddIcon className={style.icon} />}
+      </div>
     </div>
   );
 };
