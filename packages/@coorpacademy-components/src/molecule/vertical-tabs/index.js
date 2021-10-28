e1636b00a5848ab3197ef9c3938a5f13bf155f43
DjamelSoualmi <djamel.soualmi@coorpacademy.com>
2021-10-28 10:44:59 +0200

    add vertical tab molecule

---
+++ packages/@coorpacademy-components/src/molecule/vertical-tabs/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {NovaSolidStatusCheckCircle2 as CheckIcon} from '@coorpacademy/nova-icons';
+import {snakeCase} from 'lodash/fp';
+import Link from '../../atom/link';
+import style from './style.css';
+
+const LeftIcons = {
+  BlueValidatedCircle: CheckIcon
+};
+
+const buildTab = (tab, index) => {
+  const {title, href, selected, onClick, leftIcon, rightIcon} = tab;
+  const className = selected ? style.selectedTab : style.notSelectedTab;
+
+  const LeftIcon = LeftIcons[leftIcon];
+  return (
+    <div
+      data-name={`vertical_tab_${snakeCase(title)}`}
+      className={className}
+      key={index}
+      onClick={onClick}
+    >
+      <div>
+        {rightIcon}
+        <Link href={href} className={style.title}>
+          {title}
+        </Link>
+      </div>
+      {LeftIcon ? <LeftIcon className={style.leftIcon} /> : null}
+    </div>
+  );
+};
+
+const VerticalTabs = props => {
+  const {tabs} = props;
+  const tabsList = tabs.map(buildTab);
+
+  return <div className={style.wrapper}>{tabsList}</div>;
+};
+
+VerticalTabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: Link.propTypes.children,
+      href: Link.propTypes.href,
+      selected: PropTypes.bool,
+      leftIcon: PropTypes.string,
+      rightIcon: PropTypes.string,
+      onClick: PropTypes.func
+    })
+  )
+};
+export default VerticalTabs;
---
+++ packages/@coorpacademy-components/src/molecule/vertical-tabs/test/fixtures/default.js
@@ -0,0 +1,55 @@
+export default {
+  props: {
+    tabs: [
+      {
+        title: 'Franche',
+        leftIcon: 'BlueValidatedCircle',
+        rightIcon: '🇫🇷',
+        onClick: () => console.log,
+        selected: false
+      },
+      {
+        title: 'Spanish',
+        leftIcon: 'BlueValidatedCircle',
+        rightIcon: '🇪🇸',
+        onClick: () => console.log,
+        selected: false
+      },
+      {
+        title: 'Italian',
+        leftIcon: 'BlueValidatedCircle',
+        rightIcon: '🇮🇹',
+        onClick: () => console.log,
+        selected: false
+      },
+      {
+        title: 'German',
+        leftIcon: '',
+        rightIcon: '🇩🇪',
+        onClick: () => console.log,
+        selected: false
+      },
+      {
+        title: 'Russian',
+        leftIcon: '',
+        rightIcon: '🇷🇺',
+        onClick: () => console.log,
+        selected: false
+      },
+      {
+        title: 'Polish',
+        leftIcon: '',
+        rightIcon: '🇵🇱',
+        onClick: () => console.log,
+        selected: false
+      },
+      {
+        title: 'Turkish',
+        leftIcon: '',
+        rightIcon: '🇹🇷',
+        onClick: () => console.log,
+        selected: false
+      }
+    ]
+  }
+};
