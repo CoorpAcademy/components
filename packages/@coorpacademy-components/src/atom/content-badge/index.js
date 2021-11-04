6e63b2112af9446f8b200ce197cbb8e0df3d20ac
Faustino Kialungila <Faustino.kialungila@gmail.com>
2021-11-04 17:46:06 +0100

    fix: proptypes (#1997)

--- packages/@coorpacademy-components/src/atom/content-badge/index.js
+++ packages/@coorpacademy-components/src/atom/content-badge/index.js
@@ -24,7 +24,16 @@ const ContentBadge = props => {
 };
 
 ContentBadge.propTypes = {
-  category: PropTypes.string,
+  category: PropTypes.oneOf([
+    'base',
+    'advanced',
+    'coach',
+    'chapter',
+    'scorm',
+    'video',
+    'article',
+    'podcast'
+  ]),
   label: PropTypes.string
 };
 
