489f12a939428cfff6fd1dc5a1961bf57646ecd1
esa-coorp <eric.sampaio@coorpacademy.com>
2021-11-10 09:24:52 +0100

    Fix input/textarea animations

--- packages/@coorpacademy-components/src/molecule/brand-form-group/test/fixtures/cm-default.js
+++ packages/@coorpacademy-components/src/molecule/brand-form-group/test/fixtures/cm-default.js
@@ -5,14 +5,20 @@ export default {
       {
         title: 'Domain name',
         value: 'samsung.coorpacademy.com',
+        hint: 'Hint <span style="color:red;">10<span>/40',
+        placeholder: 'This is an input',
         theme: 'coorpmanager',
+        description: 'This is the tooltip text',
         type: 'text',
         disabled: true
       },
       {
         title: 'Platform name',
         value: 'Samsung',
+        hint: 'Hint <span style="color:red;">10<span>/40',
+        placeholder: 'This is an input',
         theme: 'coorpmanager',
+        description: 'This is the tooltip text',
         type: 'text',
         disabled: true
       },
@@ -25,14 +31,19 @@ export default {
       {
         title: 'TextArea',
         value: '',
+        hint: 'Hint <span style="color:red;">10<span>/40',
+        placeholder: 'This is an input',
         theme: 'coorpmanager',
-        type: 'textarea',
-        hint: ''
+        description: 'This is the tooltip text',
+        type: 'textarea'
       },
       {
         title: 'Default field',
         value: '',
+        hint: 'Hint <span style="color:red;">10<span>/40',
+        placeholder: 'This is an input',
         theme: 'coorpmanager',
+        description: 'This is the tooltip text',
         type: 'unknow'
       },
       {
