1ec0b5c1410001e7737c9638537adda47f43ffdd
stefano <stefanoacosta.92@gmail.com>
2021-11-10 15:09:16 +0100

    fix

--- packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/bullet-point.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/coorpacademy/bullet-point.js
@@ -4,7 +4,7 @@ const SvgComponent = (props) => (
   <svg viewBox="0 0 14 14" fill="none" {...props}>
     <path
       d="M1.556 8.361a1.556 1.556 0 100-3.111 1.556 1.556 0 000 3.111zm5.444 0A1.556 1.556 0 107 5.25a1.556 1.556 0 000 3.111zm5.444 0a1.556 1.556 0 100-3.111 1.556 1.556 0 000 3.111z"
-      fill="#1D1D2B"
+      fill="currentColor"
     />
   </svg>
 );
