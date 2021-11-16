7aa9ae05ce122514a2f9a930a28d7f433d176c85
DjamelSoualmi <djamel.soualmi@coorpacademy.com>
2021-11-16 14:42:43 +0100

    use new button in cm banner

--- packages/@coorpacademy-components/src/molecule/banner/index.js
+++ packages/@coorpacademy-components/src/molecule/banner/index.js
@@ -2,27 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {NovaSolidInterfaceFeedbackInterfaceAlertDiamond as QuestionIcon} from '@coorpacademy/nova-icons';
-import Link from '../../atom/link';
+import Link from '../../atom/button-link';
 import style from './style.css';
 
 const Banner = props => {
   const {type, message, firstCTA, firstCTALabel, secondCTALabel, secondCTA} = props;
   return (
-    <div className={classnames(style.banner, type === 'error' && style.errorBanner)}>
+    <div
+      className={classnames(style.banner, type === 'error' && style.errorBanner)}
+      title={message}
+    >
       <div data-name={`${type}-banner-message`} className={style.message}>
         <QuestionIcon className={style.icon} />
         {message}
       </div>
       {firstCTALabel ? (
-        <Link data-name="first-banner-cta" className={style.button} onClick={firstCTA}>
-          {firstCTALabel}
-        </Link>
+        <div className={style.button}>
+          <Link
+            data-name="first-banner-cta"
+            aria-label={firstCTALabel}
+            label={firstCTALabel}
+            onClick={firstCTA}
+            type="text"
+          />
+        </div>
       ) : null}
       {firstCTALabel && secondCTALabel ? <div className={style.buttonsBar} /> : null}
       {secondCTALabel ? (
-        <Link data-name="second-banner-cta" className={style.button} onClick={secondCTA}>
-          {secondCTALabel}
-        </Link>
+        <div className={style.button}>
+          <Link
+            data-name="second-banner-cta"
+            onClick={secondCTA}
+            aria-label={secondCTALabel}
+            label={secondCTALabel}
+            type="text"
+          />
+        </div>
       ) : null}
     </div>
   );
