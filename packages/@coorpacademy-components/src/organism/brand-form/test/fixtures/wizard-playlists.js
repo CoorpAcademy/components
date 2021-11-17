fc79be17008aff1333f3bfce42189da914aa7474
Joan Ortega <joan.ortega@coorpacademy.com>
2021-11-17 17:33:03 +0100

    Organism general settings (#2021)
    * Adding fixture for wizard playlist
    
    * set size for inputs for coorpmanager
    
    * adding bottom margin for coorpmanager inputs
    
    * adding style for chips on organism
    
    * review

---
+++ packages/@coorpacademy-components/src/organism/brand-form/test/fixtures/wizard-playlists.js
@@ -0,0 +1,31 @@
+export default {
+  props: {
+    groups: [
+      {
+        fields: [
+          {
+            type: 'text',
+            title: 'Playlist name',
+            placeholder: 'Playlist name',
+            description: 'This is the tooltip text',
+            theme: 'coorpmanager',
+            hint: '15 / 30 characters',
+            value: 'Marketing expert',
+            size: 'default'
+          },
+          {
+            type: 'textarea',
+            title: 'Description',
+            placeholder: 'Description',
+            description: 'This is the tooltip text',
+            theme: 'coorpmanager',
+            hint: '80 / 200 characters',
+            size: 'medium',
+            value:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fermentum sem ac fermentum luctus. Integer eu dui magna. Donec ut tristique dui. Vestibulum non accumsan tellus. Donec luctus erat vitae aliquet viverra. Mauris malesuada tortor quis viverra vestibulum. Nullam laoreet porta massa vitae porta.'
+          }
+        ]
+      }
+    ]
+  }
+};
