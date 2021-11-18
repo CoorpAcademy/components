122cc5dc575942471559fc5c02a9acf6c9cb25fe
GhaziCoorpacademy <ghazi.ayari@coorpacademy.com>
2021-11-18 16:43:09 +0100

    ...

--- packages/@coorpacademy-components/src/molecule/custom-playlist/index.js
+++ packages/@coorpacademy-components/src/molecule/custom-playlist/index.js
@@ -1,54 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import Tag from "../../atom/tag";
+import ButtonLink from "../../atom/button-link";
+import BulletPointMenuButton from "../../molecule/bullet-point-menu-button";
+import style from "./style.css";
 
-import Tag from '../../atom/tag';
-import ButtonLink from '../../atom/button-link';
-import BulletPointMenuButton from '../../molecule/bullet-point-menu-button';
-
-
-import style from './style.css';
-
-
-const CustomPlaylist = props => {
-
-
-  const {title} = props;
+const CustomPlaylist = (props) => {
+  const { title } = props;
   return (
-    <div className={style.wrapper}>
+    <div className={style.wrapper} style={{ marginTop: "150px" }}>
       <div className={style.title}>{title}</div>
       <div className={style.settings}>
         <div>
-      <Tag label="Published" type="published" />
-      </div>
-      <div className={style.edit}>
-      <ButtonLink type="secondary" 
-                    label="Edit"
-                    ariaLabel="aria button"
-                    dataName="default-button"
-                    icon={{
-                            position: "left",
-                            type: "edit"
-                          }}
-      />
-
-      </div> 
-      <BulletPointMenuButton buttonAriaLabel="aria button" 
-                    menuAriaLabel="aria menu"
-                    buttons={[
-                      {
-                        "data-name": "menu-archive-button",
-                        "label": "Archive",
-                        "type": "default"
-                      },
-                      
-                      {
-                        "data-name": "menu-delete-button",
-                        "label": "Delete",
-                        "type": "dangerous"
-                      }
-                    ]}
-                   
-      />
+          <Tag label="Published" type="published" />
+        </div>
+        <div className={style.edit}>
+          <ButtonLink
+            type="secondary"
+            label="Edit"
+            ariaLabel="aria button"
+            dataName="default-button"
+            icon={{
+              position: "left",
+              type: "edit",
+            }}
+          />
+        </div>
+        <BulletPointMenuButton
+          buttonAriaLabel="aria button"
+          menuAriaLabel="aria menu"
+          buttons={[
+            {
+              "data-name": "menu-archive-button",
+              label: "Archive",
+              type: "default",
+            },
+            {
+              "data-name": "menu-delete-button",
+              label: "Delete",
+              type: "dangerous",
+            },
+          ]}
+        />
       </div>
     </div>
   );
