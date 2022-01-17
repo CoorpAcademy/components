bfeea411deb2e586a03916820d15105017df565f
Fausto95 <faustino.kialungila@gmail.com>
2022-01-17 14:20:00 +0100

    theming coorpmanager loader

--- packages/@coorpacademy-components/src/atom/loader/index.js
+++ packages/@coorpacademy-components/src/atom/loader/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {NovaCompositionCoorpacademyCmLoader as CMLoader} from '@coorpacademy/nova-icons';
 import classnames from 'classnames';
 import style from './style.css';
 
-const Loader = ({className}) => {
-  return (
+const Loader = ({className, theme = 'default'}) => {
+  return theme !== 'default' ? (
+    <div data-name="loader" className={className}>
+      <CMLoader className={style.cmLoaderGrey} />
+      <CMLoader className={style.cmLoaderLightGrey} />
+      <CMLoader className={style.cmLoaderBlue} />
+    </div>
+  ) : (
     <div data-name="loader" className={classnames(style.container, className)}>
       <div className={style.dots}>
         <div className={style.red} />
@@ -17,7 +24,8 @@ const Loader = ({className}) => {
 };
 
 Loader.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  theme: PropTypes.oneOf(['default', 'coorpmanager'])
 };
 
 export default Loader;
