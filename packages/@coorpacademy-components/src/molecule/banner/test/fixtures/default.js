45b766676f5d75c0763a7d45d93d846088a1a394
GhaziCoorpacademy <94372828+GhaziCoorpacademy@users.noreply.github.com>
2022-01-24 10:42:28 +0100

    banner button - one line text (#2100)

--- packages/@coorpacademy-components/src/molecule/banner/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/banner/test/fixtures/default.js
@@ -2,7 +2,7 @@ export default {
   props: {
     type: 'warning',
     message: 'Changes saved but not published. Do you want to update this custom playlist now? ',
-    firstCTALabel: 'Undo',
+    firstCTALabel: 'Delete all changes',
     firstCTA: () => console.log('first cta'),
     secondCTALabel: 'Update',
     secondCTA: () => console.log('second cta')
