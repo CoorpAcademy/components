6638927509f2112373fc5278efde8cb093900e10
GhaziCoorpacademy <ghazi.ayari@coorpacademy.com>
2022-01-28 17:34:15 +0100

    cleaned fixtures

--- packages/@coorpacademy-components/src/atom/card-image-preview/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/atom/card-image-preview/test/fixtures/default.js
@@ -1,7 +1,6 @@
 export default {
   props: {
     image:
-      'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/partner-wedemain/fr/medias/img/cover/shutterstock_248741149-1470302136299.jpg&h=500&w=500&q=90',
-    type: 'course'
+      'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/partner-wedemain/fr/medias/img/cover/shutterstock_248741149-1470302136299.jpg&h=500&w=500&q=90'
   }
 };
