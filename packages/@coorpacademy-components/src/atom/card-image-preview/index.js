c00f646dba6e2b389dc05163b0dc203ffee7f376
GhaziCoorpacademy <ghazi.ayari@coorpacademy.com>
2022-01-28 17:29:57 +0100

    fix lint

--- packages/@coorpacademy-components/src/atom/card-image-preview/index.js
+++ packages/@coorpacademy-components/src/atom/card-image-preview/index.js
@@ -1,4 +1,4 @@
-import React, {memo} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {get} from 'lodash/fp';
 import {NovaCompositionCoorpacademyPictures as PicturesIcon} from '@coorpacademy/nova-icons';
