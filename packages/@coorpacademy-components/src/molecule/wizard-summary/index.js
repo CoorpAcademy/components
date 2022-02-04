77bb97bffae7049fb38bf90f988dc0489ffd6be0
GhaziCoorpacademy <ghazi.ayari@coorpacademy.com>
2022-02-04 10:47:41 +0100

    fix props

--- packages/@coorpacademy-components/src/molecule/wizard-summary/index.js
+++ packages/@coorpacademy-components/src/molecule/wizard-summary/index.js
@@ -84,8 +84,8 @@ const buildSections = sections => {
   });
 };
 
-const BuildAction = ({action}) => {
-  const {onClick, text, disabled = false, icon, 'aria-label': ariaLabel, side} = action;
+const BuildAction = ({action,side}) => {
+  const {onClick, text, disabled = false, icon, 'aria-label': ariaLabel} = action;
   const handleClick = useMemo(() => () => onClick(), [onClick]);
 
   if (!action) return null;
