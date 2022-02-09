416c240553c44ab32309a05bc92b5be43c587fdd
Stefano Acosta <33550261+StefanoA1@users.noreply.github.com>
2022-02-09 11:09:26 +0100

    Revert "Redesign molecule Qcm && add coverage &&  add getShadowBoxColorFromPrimary util function (#2121)" (#2129)
    This reverts commit d8b46ad04bd3eb54312cd9737ab003f996ea7912.

--- packages/@coorpacademy-components/src/molecule/questions/qcm/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/questions/qcm/test/fixtures/default.js
@@ -3,50 +3,42 @@ export default {
     answers: [
       {
         title: 'There is no need for a password for this, you only need your ID to connect',
-        'aria-label': 'aria Answer',
         onClick: () => {},
         selected: false
       },
       {
         title: 'Lorem ipsum',
-        'aria-label': 'aria Answer',
         onClick: () => {},
         selected: false
       },
       {
         title: 'Lorem',
-        'aria-label': 'aria Answer',
         onClick: () => {},
         selected: true
       },
       {
         title: 'You need to have a password',
-        'aria-label': 'aria Answer',
         onClick: () => {},
         selected: false
       },
       {
         title: 'Pouet',
-        'aria-label': 'aria Answer',
         onClick: () => {},
         selected: true
       },
       {
         title:
           'Alios autem dicere aiunt multo etiam inhumanius (quem locum breviter paulo ante perstrinxi) praesidii adiumentique causa, non benevolentiae neque caritatis, amicitias esse expetendas; itaque, ut quisque minimum firmitatis haberet minimumque virium',
-        'aria-label': 'aria Answer',
         onClick: () => {},
         selected: false
       },
       {
         title: 'Lorem ipsum dolor',
-        'aria-label': 'aria Answer',
         onClick: () => {},
         selected: false
       },
       {
         title: 'all your base are belong to us',
-        'aria-label': 'aria Answer',
         onClick: () => {},
         selected: false
       }
