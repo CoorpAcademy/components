20e778d642a9692ffc67f7b429c9cd4f69b8d706
Stefano Acosta <33550261+StefanoA1@users.noreply.github.com>
2022-02-18 16:36:29 +0100

    Fill in the blank redesign (#2134)
    * Free text refactor
    
    * select multiline
    
    * select tests
    
    * ie11 fixes: free-text border-width
    
    * ie placeholder fix
    
    * header fix
    
    * analytics select fix
    
    * catalog select fix

--- packages/@coorpacademy-components/src/molecule/questions/template/index.js
+++ packages/@coorpacademy-components/src/molecule/questions/template/index.js
@@ -22,11 +22,12 @@ const Template = ({template, answers}) => {
     if (type === 'answerField') {
       const field = find({name: part.value}, answers);
       if (!field) return null;
+      const {type: fieldType, ...fieldProps} = field;
       const fieldView =
-        field.type === 'text' ? (
-          <FreeText {...field} className={style.text} />
+        fieldType === 'text' ? (
+          <FreeText {...fieldProps} className={style.text} />
         ) : (
-          <DropDown {...field} theme="template" />
+          <DropDown {...fieldProps} theme="player" />
         );
 
       return (
@@ -46,20 +47,13 @@ const Template = ({template, answers}) => {
 
 const TextPropTypes = {
   ...DropDown.propTypes,
-  type: PropTypes.string,
-  name: PropTypes.string
-};
-
-const DropDownPropTypes = {
-  ...DropDown.propTypes,
-  type: PropTypes.string,
-  name: PropTypes.string
+  type: PropTypes.string
 };
 
 Template.propTypes = {
   template: PropTypes.string,
   answers: PropTypes.arrayOf(
-    PropTypes.oneOfType([PropTypes.shape(DropDownPropTypes), PropTypes.shape(TextPropTypes)])
+    PropTypes.oneOfType([PropTypes.shape(DropDown.propTypes), PropTypes.shape(TextPropTypes)])
   )
 };
 
