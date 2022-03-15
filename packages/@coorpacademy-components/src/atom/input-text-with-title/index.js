593d6f2d060e39f5b941158d429d1fac1c03623b
Djamel Soualmi <58167920+DjamelSoualmi@users.noreply.github.com>
2022-03-15 11:48:03 +0100

    fix mandatory courses input width (#2161)
    * fix mandatory courses input width

--- packages/@coorpacademy-components/src/atom/input-text-with-title/index.js
+++ packages/@coorpacademy-components/src/atom/input-text-with-title/index.js
@@ -5,11 +5,12 @@ import style from './style.css';
 
 const InputTextWithTitle = props => {
   const {inputText, title, 'data-name': dataName, 'aria-label': ariaLabel} = props;
+  const inputTextClass = inputText.size === 'small' ? style.smallInputText : style.defaultInputText;
 
   return (
     <div className={style.container} data-name={dataName} aria-label={ariaLabel}>
       <span className={style.title}>{title}</span>
-      <div className={style.inputText}>
+      <div className={inputTextClass}>
         <InputText {...inputText} />
       </div>
     </div>
@@ -17,7 +18,11 @@ const InputTextWithTitle = props => {
 };
 
 InputTextWithTitle.propTypes = {
-  inputText: PropTypes.shape({...InputText.propTypes, theme: PropTypes.string}),
+  inputText: PropTypes.shape({
+    ...InputText.propTypes,
+    theme: PropTypes.string,
+    size: PropTypes.string
+  }),
   title: PropTypes.string,
   'aria-label': PropTypes.string,
   'data-name': PropTypes.string
--- packages/@coorpacademy-components/src/atom/input-text-with-title/test/fixtures/error.js
+++ packages/@coorpacademy-components/src/atom/input-text-with-title/test/fixtures/error.js
@@ -3,6 +3,6 @@ import inputText from '../../../input-text/test/fixtures/cm-default';
 export default {
   props: {
     title: 'Mandatory courses to complete certification',
-    inputText: {...inputText.props, error: 'this is error', hint: ''}
+    inputText: {...inputText.props, error: 'this is error', hint: '', size: 'small'}
   }
 };
