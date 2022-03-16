6d05980c11d4f0eddeee5278b02f64e4346751b6
DjamelSoualmi <djamel.soualmi@coorpacademy.com>
2022-03-16 15:27:19 +0100

    fix margin and width

--- packages/@coorpacademy-components/src/atom/input-text-with-title/test/fixtures/error.js
+++ packages/@coorpacademy-components/src/atom/input-text-with-title/test/fixtures/error.js
@@ -3,6 +3,12 @@ import inputText from '../../../input-text/test/fixtures/cm-default';
 export default {
   props: {
     title: 'Mandatory courses to complete certification',
-    inputText: {...inputText.props, error: 'this is error', hint: '', size: 'small'}
+    inputText: {
+      ...inputText.props,
+      title: 'Amount (6 items max)',
+      error: 'this is error',
+      hint: '',
+      size: 'small'
+    }
   }
 };
