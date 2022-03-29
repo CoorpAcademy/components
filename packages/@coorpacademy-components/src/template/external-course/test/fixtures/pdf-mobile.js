8a3c5c30f7e8f5eb2dc53a5d6d60961103638d79
esa-coorp <eric.sampaio@coorpacademy.com>
2022-03-29 18:22:57 +0200

    Fix pdf article loading

---
+++ packages/@coorpacademy-components/src/template/external-course/test/fixtures/pdf-mobile.js
@@ -0,0 +1,9 @@
+export default {
+  props: {
+    type: 'article',
+    url: 'https://api.coorpacademy.com/external/3673d4b6-bf5a-4b24-9665-ad97dc9b89da.pdf',
+    mode: 'mobile',
+    contentType: 'application/pdf',
+    loading: false
+  }
+};
