4b7cf3bc4c11017f016f221cb721e063b200f5c5
EmelineLeduc <leduc.emeline44@gmail.com>
2022-05-10 18:01:07 +0200

    add confetti animation (wip)

--- packages/@coorpacademy-components/src/organism/review-congrats/index.js
+++ packages/@coorpacademy-components/src/organism/review-congrats/index.js
@@ -2,7 +2,6 @@ import React, {useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 import AtomLottieWrapper from '../../atom/lottie-wrapper';
 import ButtonLink from '../../atom/button-link';
-
 import MoleculeReviewCardCongrats from '../../molecule/review-card-congrats';
 import style from './style.css';
 
@@ -33,32 +32,36 @@ const ReviewCongrats = props => {
   }, []);
 
   return (
-    <div className={style.containerCongrats} aria-label={ariaLabel} data-name={dataName}>
-      <div>
-        <AtomLottieWrapper
-          {...animationLottie}
-          loop={false}
-          autoplay={false}
-          data-name="lottie-wrapper"
-        />
+    <div className={style.mainContainer} aria-label={ariaLabel} data-name={dataName}>
+      <div className={style.containerCongrats}>
+        <div className={style.title}>{title}</div>
+        <div ref={container} className={style.containerCards}>
+          <MoleculeReviewCardCongrats {...cardCongratsStar} className={style.cardStar} />
+          <MoleculeReviewCardCongrats
+            {...cardCongratsRank}
+            timerAnimation={1800}
+            className={style.cardRank}
+          />
+        </div>
+        <div className={style.buttonContainer}>
+          <ButtonLink
+            {...buttonRevising}
+            className={style.buttonRevise}
+            data-name="revise-skill-link"
+          />
+          <ButtonLink {...buttonRevisingSkill} className={style.buttonRevise} />
+        </div>
       </div>
-      <div className={style.title}>{title}</div>
-      <div ref={container} className={style.containerCards}>
-        <MoleculeReviewCardCongrats {...cardCongratsStar} className={style.cardStar} />
-        <MoleculeReviewCardCongrats
-          {...cardCongratsRank}
-          timerAnimation={1800}
-          className={style.cardRank}
-        />
-      </div>
-      <div className={style.buttonContainer}>
-        <ButtonLink
-          {...buttonRevising}
-          className={style.buttonRevise}
-          data-name="revise-skill-link"
-        />
-        <ButtonLink {...buttonRevisingSkill} className={style.buttonRevise} />
-      </div>
+      <AtomLottieWrapper
+        {...animationLottie}
+        loop={false}
+        animationControl={'play'}
+        autoplay
+        data-name="lottie-wrapper"
+        className={style.lottie}
+        backupImageClassName={style.ie11Backup}
+        rendererSettings={{animationClassName: style.test}}
+      />
     </div>
   );
 };
