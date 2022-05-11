31727929eb3228368da53dda5450df829e8d7941
chrisdugne <chris.dugne@uralys.com>
2022-05-11 18:59:14 +0200

    adding podcast icons

---
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/podcast/pauserounded.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+const SvgComponent = (props) => (
+  <svg
+    id="prefix__Calque_1"
+    x={0}
+    y={0}
+    viewBox="0 0 16 16"
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style>{".prefix__st0{fill:#515161}"}</style>
+    <path
+      className="prefix__st0"
+      d="M2 .7h4c.4 0 .7.3.7.7v13.3c0 .4-.3.7-.7.7H2c-.4 0-.7-.3-.7-.7V1.3c0-.4.3-.6.7-.6zM10 .7h4c.4 0 .7.3.7.7v13.3c0 .4-.3.7-.7.7h-4c-.4 0-.7-.3-.7-.7V1.3c0-.4.3-.6.7-.6z"
+    />
+  </svg>
+);
+
+export default SvgComponent;
