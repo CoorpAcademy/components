0a3ae830eef44fd5376f1bca9ee1cfb42d52f156
EmelineLeduc <79636283+EmelineLeduc@users.noreply.github.com>
2022-05-12 11:24:36 +0200

    Organism Review Congrats with Lottie confetti (#2204)
    * add lottie animation confetti

--- packages/@coorpacademy-components/src/organism/review-congrats/index.js
+++ packages/@coorpacademy-components/src/organism/review-congrats/index.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
+import AtomLottieWrapper from '../../atom/lottie-wrapper';
 import ButtonLink from '../../atom/button-link';
 import MoleculeReviewCardCongrats from '../../molecule/review-card-congrats';
 import style from './style.css';
@@ -15,6 +16,7 @@ const ReviewCongrats = props => {
   const {
     'aria-label': ariaLabel,
     'data-name': dataName,
+    animationLottie,
     title,
     cardCongratsStar,
     cardCongratsRank,
@@ -30,23 +32,34 @@ const ReviewCongrats = props => {
   }, []);
 
   return (
-    <div className={style.containerCongrats} aria-label={ariaLabel} data-name={dataName}>
-      <div className={style.title}>{title}</div>
-      <div ref={container} className={style.containerCards}>
-        <MoleculeReviewCardCongrats {...cardCongratsStar} className={style.cardStar} />
-        <MoleculeReviewCardCongrats
-          {...cardCongratsRank}
-          timerAnimation={1800}
-          className={style.cardRank}
-        />
-      </div>
-      <div className={style.buttonContainer}>
-        <ButtonLink
-          {...buttonRevising}
-          className={style.buttonRevise}
-          data-name="revise-skill-link"
-        />
-        <ButtonLink {...buttonRevisingSkill} className={style.buttonRevise} />
+    <div className={style.mainContainer} aria-label={ariaLabel} data-name={dataName}>
+      <AtomLottieWrapper
+        {...animationLottie}
+        loop={false}
+        animationControl={'play'}
+        autoplay
+        data-name="lottie-wrapper"
+        className={style.lottie}
+        backupImageClassName={style.ie11Backup}
+      />
+      <div className={style.containerCongrats}>
+        <div className={style.title}>{title}</div>
+        <div ref={container} className={style.containerCards}>
+          <MoleculeReviewCardCongrats {...cardCongratsStar} className={style.cardStar} />
+          <MoleculeReviewCardCongrats
+            {...cardCongratsRank}
+            timerAnimation={1800}
+            className={style.cardRank}
+          />
+        </div>
+        <div className={style.buttonContainer}>
+          <ButtonLink
+            {...buttonRevising}
+            className={style.buttonRevise}
+            data-name="revise-skill-link"
+          />
+          <ButtonLink {...buttonRevisingSkill} className={style.buttonRevise} />
+        </div>
       </div>
     </div>
   );
@@ -55,6 +68,7 @@ const ReviewCongrats = props => {
 ReviewCongrats.propTypes = {
   'aria-label': PropTypes.string,
   'data-name': PropTypes.string,
+  animationLottie: PropTypes.shape(AtomLottieWrapper.propTypes),
   title: PropTypes.string,
   cardCongratsStar: PropTypes.shape(MoleculeReviewCardCongrats.propTypes),
   cardCongratsRank: PropTypes.shape(MoleculeReviewCardCongrats.propTypes),
