efbeefb2972ab65c905b0ff938df1f5d1c8ecc1d
chrisdugne <chris.dugne@uralys.com>
2022-05-17 15:17:53 +0200

    removed generated files

--- packages/@coorpacademy-app-review/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var redux_1 = require("redux");
-var navigation_1 = require("./navigation");
-var rootReducer = (0, redux_1.combineReducers)({
-    navigation: navigation_1["default"]
-});
-exports["default"] = rootReducer;
