7ea3e2c391907b4805b0f5c24e262367a4f1959a
chrisdugne <chris.dugne@uralys.com>
2022-05-18 17:05:46 +0200

    added static webapp generation

---
+++ packages/@coorpacademy-app-review/scripts/start.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+import liveServer from 'live-server';
+import historyApiFallback from 'connect-history-api-fallback';
+import build from './build.js';
+
+const PUBLIC = 'public';
+
+build().then(() => {
+  liveServer.start({
+    root: PUBLIC,
+    open: false,
+    middleware: [historyApiFallback()]
+  });
+
+  console.log('☢️  server running:');
+  console.log(chalk.bold.blue(`  - http://localhost:8080`));
+  console.log(' ');
+});
+
