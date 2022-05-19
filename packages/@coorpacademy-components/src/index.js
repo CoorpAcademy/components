31409d74f2267b826967b46a0f219bc77c9a3124
chrisdugne <chris.dugne@uralys.com>
2022-05-19 17:59:12 +0200

    moved view to @coorpacademy/components

--- packages/@coorpacademy-components/src/index.js
+++ packages/@coorpacademy-components/src/index.js
@@ -3,5 +3,6 @@ import PopinEnd from './template/app-player/popin-end';
 import PopinCorrection from './template/app-player/popin-correction';
 import Player from './template/app-player/player';
 import Loading from './template/app-player/loading';
+import AppReviewRootView from './template/app-review/root-view';
 
-export {Provider, PopinEnd, PopinCorrection, Loading, Player};
+export {Provider, PopinEnd, PopinCorrection, Loading, Player, AppReviewRootView};
