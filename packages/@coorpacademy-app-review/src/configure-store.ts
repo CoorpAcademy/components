e4b7593acb3cf3da04bcdd4c09dc171b22afc5b9
chrisdugne <chris.dugne@uralys.com>
2022-05-19 11:23:33 +0200

    fixed tsc config

--- packages/@coorpacademy-app-review/src/configure-store.ts
+++ packages/@coorpacademy-app-review/src/configure-store.ts
@@ -2,23 +2,22 @@ import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
-import * as actionCreators from './actions';
 import rootReducer from './reducers';
 
 const preloadedState = undefined;
 
 export default function configureStore(services) {
-  const _compose = process.env.NODE_ENV !== 'production' ? composeWithDevTools : compose;
+  const _compose =
+    process.env.NODE_ENV !== 'production'
+      ? composeWithDevTools({
+          name: 'app-review',
+          trace: true,
+          traceLimit: 25
+        })
+      : compose;
 
-  const composeEnhancers = _compose({
-    actionCreators,
-    name: 'app-review',
-    trace: true,
-    traceLimit: 25
-  });
-
-  const composedEnhancer = composeEnhancers(applyMiddleware(thunk));
-  const store = createStore(rootReducer, preloadedState, composedEnhancer);
+  const enhancer = _compose(applyMiddleware(thunk));
+  const store = createStore(rootReducer, preloadedState, enhancer);
 
   return store;
 }
