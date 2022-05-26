a4c9c8b6d52ab7cbe74aedfe5894d4e4b0a64a0b
chrisdugne <chris.dugne@uralys.com>
2022-05-26 11:42:17 +0200

    moved commands to cjs

--- packages/@coorpacademy-app-review/ava.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  files: ['src/**/*.test*'],
-  extensions: ['ts'],
-  require: ['ts-node/register'],
-  nodeArguments: ['--loader=ts-node/esm', '--experimental-specifier-resolution=node']
-};
