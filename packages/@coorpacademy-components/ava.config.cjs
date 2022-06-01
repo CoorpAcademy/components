3c457101b208f8f1864ef69386d85137c220e650
chrisdugne <chris.dugne@uralys.com>
2022-06-01 21:49:25 +0200

    added timeout option for /components/ava

--- packages/@coorpacademy-components/ava.config.cjs
+++ packages/@coorpacademy-components/ava.config.cjs
@@ -1,5 +1,6 @@
 const config = require('../../ava.config');
 
 module.exports = Object.assign({}, config, {
-  require: [...config.require, '../../test/helpers/throw-warning']
+  require: [...config.require, '../../test/helpers/throw-warning'],
+  timeout: '45s'
 });
