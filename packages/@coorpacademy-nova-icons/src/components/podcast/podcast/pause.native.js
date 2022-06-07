388a79f943a1162d7bdb53fb6f84e20792a59bf7
chrisdugne <chris.dugne@uralys.com>
2022-06-07 15:53:14 +0200

    updated pause icon

--- packages/@coorpacademy-nova-icons/src/components/podcast/podcast/pause.native.js
+++ packages/@coorpacademy-nova-icons/src/components/podcast/podcast/pause.native.js
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import Svg, {Path} from 'react-native-svg';
-/* SVGR has dropped some elements not supported by react-native-svg: style */
 
 const SvgComponent = props => (
-  <Svg id="prefix__Calque_1" x={0} y={0} viewBox="0 0 16 16" xmlSpace="preserve" {...props}>
+  <Svg viewBox="0 0 24 24" {...props}>
     <Path
-      className="prefix__st0"
-      d="M1.3 1.3c0-.3.3-.6.7-.6h4c.4 0 .7.3.7.7v13.3c0 .4-.3.7-.7.7H2c-.4 0-.7-.3-.7-.7V1.3zM9.3 1.3c0-.3.3-.6.7-.6h4c.4 0 .7.3.7.7v13.3c0 .4-.3.7-.7.7h-4c-.4 0-.7-.3-.7-.7V1.3z"
+      d="M2 2a1 1 0 011-1h6a1 1 0 011 1v20a1 1 0 01-1 1H3a1 1 0 01-1-1V2zM14 2a1 1 0 011-1h6a1 1 0 011 1v20a1 1 0 01-1 1h-6a1 1 0 01-1-1V2z"
+      fill={props.color}
     />
   </Svg>
 );
