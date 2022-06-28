572ec9ee6510b156407c92d00e0293d96f0b052d
chrisdugne <chris.dugne@uralys.com>
2022-06-28 09:48:56 +0200

    improved redux dev tools setup

--- packages/@coorpacademy-app-review/src/configure-store.ts
+++ packages/@coorpacademy-app-review/src/configure-store.ts
@@ -11,26 +11,16 @@ const preloadedState: unknown = undefined;
 
 // -----------------------------------------------------------------------------
 
-export default async function configureStore(): Promise<
-  Store<EmptyObject & StoreState, AnyAction> & {
-    dispatch: {};
-  }
-> {
-  const isExtensionEnabled = typeof window !== 'undefined' && !!window.__REDUX_DEVTOOLS_EXTENSION__;
-  const isProd = process.env.NODE_ENV === 'production';
-
-  const {composeWithDevTools} = isProd
-    ? {composeWithDevTools: null}
-    : // eslint-disable-next-line node/no-unsupported-features/es-syntax, import/dynamic-import-chunkname
-      await import('redux-devtools-extension');
-  const _compose =
-    !isProd && isExtensionEnabled
-      ? composeWithDevTools({
-          name: 'app-review',
-          trace: true,
-          traceLimit: 25
-        })
-      : compose;
+export default function configureStore(): Store<EmptyObject & StoreState, AnyAction> & {
+  dispatch: {};
+} {
+  const _compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        name: 'app-review',
+        trace: true,
+        traceLimit: 25
+      })
+    : compose;
 
   const enhancer = _compose(applyMiddleware(thunk, onStartApp));
   const store = createStore(rootReducer, preloadedState, enhancer);
