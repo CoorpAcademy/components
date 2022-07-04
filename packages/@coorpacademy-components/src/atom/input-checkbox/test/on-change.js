d2ff5fbf37b0ee7d32625bd5dc44c2f415e5f72a
stefano <stefanoacosta.92@gmail.com>
2022-07-04 10:02:54 +0200

    revert

--- packages/@coorpacademy-components/src/atom/input-checkbox/test/on-change.js
+++ packages/@coorpacademy-components/src/atom/input-checkbox/test/on-change.js
@@ -1,27 +1,22 @@
 import test from 'ava';
 import browserEnv from 'browser-env';
 import React from 'react';
-// import {shallow, configure} from 'enzyme';
-import {render, fireEvent} from '@testing-library/react';
+import {shallow, configure} from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import InputCheckbox from '..';
 import defaultFixture from './fixtures/default';
 
 browserEnv();
+configure({adapter: new Adapter()});
 
 test('should call the onChange function with the checked value of the target', t => {
-  t.plan(5);
+  t.plan(2);
   const expectedOnChangeValues = [false, true];
   const onChange = value => {
-    // t.is(value, expectedOnChangeValues.shift());
-    t.pass();
+    t.is(value, expectedOnChangeValues.shift());
   };
-  const {container} = render(<InputCheckbox {...defaultFixture.props} onChange={onChange} />);
+  const wrapper = shallow(<InputCheckbox {...defaultFixture.props} onChange={onChange} />);
 
-  const checkboxInput = container.querySelector('[data-name="checkbox-input-label"] input');
-  t.truthy(checkboxInput);
-  t.true(checkboxInput.checked);
-  fireEvent.click(checkboxInput);
-  t.false(checkboxInput.checked);
-  fireEvent.click(checkboxInput);
-  t.true(checkboxInput.checked);
+  wrapper.find('input').simulate('change', {target: {checked: false}});
+  wrapper.find('input').simulate('change', {target: {checked: true}});
 });
