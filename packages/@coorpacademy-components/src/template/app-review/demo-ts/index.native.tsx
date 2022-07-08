27dbcbc557294374be35b24cb1f580b3660aae61
chrisdugne <chris.dugne@uralys.com>
2022-07-08 16:58:14 +0200

    trying to test with ava

---
+++ packages/@coorpacademy-components/src/template/app-review/demo-ts/index.native.tsx
@@ -0,0 +1,10 @@
+// import React, {FunctionComponent} from 'react';
+import React from 'react';
+import {View} from 'react-native';
+
+export type Props = {plop: string};
+
+// const DemoTs = ({plop}: Props) => console.log({plop});
+const DemoTs = ({plop}: Props) => <View>{plop}</View>;
+
+export default DemoTs;
