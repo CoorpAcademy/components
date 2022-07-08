5d469eb54e7db772eb80f9287d509c4b6a2bc9ce
chrisdugne <chris.dugne@uralys.com>
2022-07-08 16:58:14 +0200

    trying to test with ava

--- packages/@coorpacademy-components/ava.config.cjs
+++ packages/@coorpacademy-components/ava.config.cjs
@@ -2,11 +2,9 @@ const config = require('../../ava.config');
 
 // https://github.com/avajs/ava/blob/main/docs/recipes/typescript.md#for-packages-without-type-module
 module.exports = Object.assign({}, config, {
-  require: [
-    ...config.require,
-    '../../test/helpers/throw-warning',
-    'ts-node/register/transpile-only'
-  ],
+  require: [...config.require, 'ts-node/register/transpile-only'],
+  files: [...config.files, 'src/**/test/**/*.ts', 'src/**/test/**/*.tsx'],
+  extensions: ['js', 'ts', 'tsx'],
   timeout: '45s',
   concurrency: 8
 });
---
+++ packages/@coorpacademy-components/src/template/app-review/demo-ts/test/fixtures.native.tsx
@@ -0,0 +1,18 @@
+import test from 'ava';
+import React from 'react';
+import {render} from '@testing-library/react-native';
+// import TemplateAppReviewDemoTs from '../index';
+import TemplateAppReviewDemoTs from '../index.native.tsx';
+import fixtureDefaultTs from './fixtures/default.ts';
+
+test('should test .native', t => {
+  const {props} = fixtureDefaultTs;
+
+  t.is(true, true);
+  // render(<TemplateAppReviewDemoTs {...props} />);
+  // const p = render(<p {...props} />);
+  // const p = render(React.createElement(TemplateAppReviewDemoTs, props));
+  const p = React.createElement(TemplateAppReviewDemoTs, props);
+  // const p = <TemplateAppReviewDemoTs />;
+  console.log({p});
+});
