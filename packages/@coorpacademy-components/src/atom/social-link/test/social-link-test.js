94354278274af2b496932e04b9420d9ee67a369c
EmelineLeduc <leduc.emeline44@gmail.com>
2022-07-08 17:37:37 +0200

    migrate test atom social-link

--- packages/@coorpacademy-components/src/atom/social-link/test/social-link-test.js
+++ packages/@coorpacademy-components/src/atom/social-link/test/social-link-test.js
@@ -1,25 +1,25 @@
 import test from 'ava';
 import browserEnv from 'browser-env';
 import React from 'react';
-import {mount, configure} from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import {render} from '@testing-library/react';
 import SocialLink from '..';
 import fixtureFacebook from './fixtures/facebook';
 import fixtureFacebookFooter from './fixtures/facebook-footer';
 
 browserEnv();
-configure({adapter: new Adapter()});
 
 test('when no mode is provided it should render with the default link styling class', t => {
-  const wrapper = mount(<SocialLink {...fixtureFacebook.props} />);
-  const link = wrapper.find('a.social-link__link');
-  t.assert(link.exists());
+  t.plan(2);
+  const {container} = render(<SocialLink {...fixtureFacebook.props} />);
+  const link = container.querySelector('a.social-link__link');
+  t.truthy(link);
   t.pass();
 });
 
 test('when footer mode is provided it should render with the linkFooter class', t => {
-  const wrapper = mount(<SocialLink {...fixtureFacebookFooter.props} />);
-  const link = wrapper.find('a.social-link__linkFooter');
-  t.assert(link.exists());
+  t.plan(2);
+  const {container} = render(<SocialLink {...fixtureFacebookFooter.props} />);
+  const link = container.querySelector('a.social-link__linkFooter');
+  t.truthy(link);
   t.pass();
 });
