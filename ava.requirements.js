1163ba2f4635f650177edbfa853752117cfad0de
godu <arthur.weber@viacesi.fr>
2022-07-13 11:05:40 +0200

    WIP

--- ava.requirements.js
+++ ava.requirements.js
@@ -1,12 +1,37 @@
-const path = require('path');
-const metroBabelRegister = require('metro-babel-register');
+const path = require("path");
+const metroBabelRegister = require("metro-babel-register");
 
-metroBabelRegister(
-  [path.join(require.resolve('react-native'), '..')],
-  {lazy: true}
-);
-require('@babel/register')({
-  extensions: ['.es6', '.es', '.jsx', '.js', '.mjs', '.ts', '.tsx']
+// global.__DEV__ = true;
+
+// const onlyList = [
+//   path.join(require.resolve("react-native"), ".."),
+//   path.join(require.resolve("react-native-mock"), "../.."),
+// ];
+// console.log({ onlyList });
+
+// metroBabelRegister(onlyList, { lazy: true });
+// require("@babel/register")({
+//   only: [],
+//   babelrc: false,
+//   configFile: false,
+// });
+// console.log(metroBabelRegister.config(onlyList))
+// require("@babel/register")({
+//   ...metroBabelRegister.config(onlyList),
+//   extensions: [
+//     ".ts",
+//     ".tsx", // Babel's default extensions
+//     ".es6",
+//     ".es",
+//     ".jsx",
+//     ".js",
+//     ".mjs",
+//   ],
+// });
+
+require('react-native-mock-render/mock');
+
+require("@babel/register")({
+  extensions: [".es6", ".es", ".jsx", ".js", ".mjs", ".ts", ".tsx"],
 });
 
-global.__DEV__ = true;
--- packages/@coorpacademy-components/src/template/app-review/demo-ts/test/fixtures.native.tsx
+++ packages/@coorpacademy-components/src/template/app-review/demo-ts/test/fixtures.native.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 import {render} from '@testing-library/react-native';
 // import TemplateAppReviewDemoTs from '../index';
 import TemplateAppReviewDemoTs from '../index.native';
-import fixtureDefaultTs from './fixtures/default.ts';
+import fixtureDefaultTs from './fixtures/default';
 
 test('should test .native', t => {
   const {props} = fixtureDefaultTs;
 
   t.is(true, true);
-  // render(<TemplateAppReviewDemoTs {...props} />);
+  render(<TemplateAppReviewDemoTs {...props} />);
   // const p = render(<p {...props} />);
   // const p = render(React.createElement(TemplateAppReviewDemoTs, props));
-  const p = React.createElement(TemplateAppReviewDemoTs, props);
+  // const p = React.createElement(TemplateAppReviewDemoTs, props);
   // const p = <TemplateAppReviewDemoTs />;
   // console.log({p});
 });
