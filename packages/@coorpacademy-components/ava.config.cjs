b30e3b71fb3dba8e14a78bc90a0a067fe50b34c2
chrisdugne <chris.dugne@uralys.com>
2022-07-18 15:28:54 +0200

    put back throw-warning for ava

--- packages/@coorpacademy-components/ava.config.cjs
+++ packages/@coorpacademy-components/ava.config.cjs
@@ -3,7 +3,7 @@ const config = require('../../ava.config');
 // https://github.com/avajs/ava/blob/main/docs/recipes/typescript.md#for-packages-without-type-module
 module.exports = Object.assign({}, config, {
   files: [...config.files, 'src/**/test/**/*.ts', 'src/**/test/**/*.tsx'],
-  require: [`${__dirname}/ava.requirements.js`],
+  require: [`${__dirname}/ava.requirements.js`, '../../test/helpers/throw-warning'],
   extensions: ['js', 'ts', 'tsx'],
   timeout: '45s',
   workerThreads: false
