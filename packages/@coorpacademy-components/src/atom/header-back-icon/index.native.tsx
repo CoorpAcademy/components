13772c79efda1fd5468a4ebb2fb4c0e606c8853c
Alan Langlois <alan@etaminstudio.com>
2022-07-20 22:43:37 +0200

    migrating header back icon

---
+++ packages/@coorpacademy-components/src/atom/header-back-icon/index.native.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, ViewStyle} from 'react-native';
+import {NovaCompositionNavigationArrowLeft as BackIcon} from '@coorpacademy/nova-icons';
+import {useTemplateContext} from '../../template/app-review/template-context';
+
+interface Props {
+  color?: string;
+  height?: number;
+  width?: number;
+  style?: ViewStyle;
+}
+
+const HeaderBackIcon = (props: Props) => {
+  const templateContext = useTemplateContext();
+  const {theme} = templateContext;
+
+  const {color = theme.colors.gray.dark, height = 16, width = 16, style} = props;
+
+  return (
+    <View style={style}>
+      <BackIcon color={color} height={height} width={width} />
+    </View>
+  );
+};
+
+export default HeaderBackIcon;
