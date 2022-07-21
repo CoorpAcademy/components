5c230c50824141d6349328efe99b739d3f92f183
esa-coorp <eric.sampaio@coorpacademy.com>
2022-07-21 11:59:35 +0200

    Fix podcast in mobile mode

---
+++ packages/@coorpacademy-components/src/molecule/external-content-viewer/test/fixtures/podcast-mobile.js
@@ -0,0 +1,11 @@
+export default {
+  props: {
+    type: 'podcast',
+    url: 'https://api.coorpacademy.com/external/fb357594-c08b-41dc-a61e-edf897630562.mp3',
+    mode: 'mobile',
+    backgroundImageUrl:
+      'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/CoorpAcademy/content-catalogue/cockpit-catalog/default/logo_coorpcast_black-1650455522441.png&h=400&w=400&q=80&m=contain',
+    contentType: 'audio/mpeg',
+    loading: false
+  }
+};
