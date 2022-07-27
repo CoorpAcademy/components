e88aa824dc419fe6869709942cf25f8fe20a036c
Bilel <bilel@MBPdeDjllet2022.localdomain>
2022-07-27 09:48:41 +0200

    rplacing /n par des br

--- packages/@coorpacademy-components/src/molecule/cm-popin/test/fixtures/no-access.js
+++ packages/@coorpacademy-components/src/molecule/cm-popin/test/fixtures/no-access.js
@@ -3,6 +3,6 @@ export default {
     content: 'You cannot access this content',
     icon: 'AlertDiamond',
     backgroundImageUrl: 'https://static.coorpacademy.com/site/player-popin-background.svg',
-    descriptionText: `You do not have access to this page or your license has \n expired. \n Please contact your administrator for more information.`
+    descriptionText: `You do not have access to this page or your license has <br/> expired. <br/> Please contact your administrator for more information.`
   }
 };
