343f8f6b372b70d5231bc8c55ad36622028ef122
Bilel <bilel@MBPdeDjllet2022.localdomain>
2022-07-27 10:39:03 +0200

    tu

--- packages/@coorpacademy-player-web/src/map-state-to-props/test/player.js
+++ packages/@coorpacademy-player-web/src/map-state-to-props/test/player.js
@@ -2,6 +2,19 @@ import test from 'ava';
 import {map, get, isEmpty, set, pipe, fromPairs, isFunction, identity, omit} from 'lodash/fp';
 
 import {mockTranslate} from '@coorpacademy/translate';
+import {
+  Analytics as AnalyticsService,
+  Answers as AnswersService,
+  Clues as CluesService,
+  Coach as CoachService,
+  Comments as CommentsService,
+  Content as ContentService,
+  ExitNodes as ExitNodesService,
+  LeaderBoard as LeaderBoardService,
+  Location as LocationService,
+  Progressions as ProgressionsService,
+  Recommendations as RecommendationsService
+} from '@coorpacademy/player-services';
 import {UI_SELECT_ROUTE} from '@coorpacademy/player-store';
 import createPlayer from '../player';
 import createHeader from '../header';
@@ -13,8 +26,24 @@ import templateSlide from './fixtures/slides/template';
 import qcmSlide from './fixtures/slides/qcm';
 import qcmDragSlide from './fixtures/slides/qcm-drag';
 import qcmGraphicSlide from './fixtures/slides/qcm-graphic';
+import * as fixtures from '../../../sandbox/fixtures';
 
-export const options = {translate: mockTranslate};
+const services = {
+  Analytics: AnalyticsService,
+  Answers: AnswersService(fixtures),
+  Clues: CluesService(fixtures),
+  Coach: CoachService,
+  Comments: CommentsService,
+  Content: ContentService(fixtures),
+  ExitNodes: ExitNodesService(fixtures),
+  LeaderBoard: LeaderBoardService,
+  Location: LocationService,
+  Logger: console,
+  Progressions: ProgressionsService(fixtures),
+  Recommendations: RecommendationsService(fixtures)
+};
+
+export const options = {translate: mockTranslate, services: services};
 export const store = {dispatch: identity};
 const createPlayerProps = createPlayer(options, store);
 const createHeaderProps = createHeader(options, store);
