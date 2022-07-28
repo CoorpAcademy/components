096acfe00e8e196d1c8fdb8252499f85bfdca92a
Bilel <bilel@MBPdeDjllet2022.localdomain>
2022-07-28 16:58:11 +0200

    removing extra popin

--- packages/@coorpacademy-components/src/template/app-player/player/index.js
+++ packages/@coorpacademy-components/src/template/app-player/player/index.js
@@ -8,13 +8,13 @@ const SlidePlayer = props => {
   const {player, popinError} = props;
   const backgroundImage = player.backgroundUrl ? `url(${player.backgroundUrl})` : null;
   return (
-      <div data-name="slidePlayer" className={style.wrapper}>
-        <div className={style.playerWrapper}>
-          <div className={style.playerBackground} style={{backgroundImage}} />
-          <SlidesPlayer {...player} />
-          {popinError ? <CMPopin {...popinError} /> : null}
-        </div>
+    <div data-name="slidePlayer" className={style.wrapper}>
+      <div className={style.playerWrapper}>
+        <div className={style.playerBackground} style={{backgroundImage}} />
+        <SlidesPlayer {...player} />
+        {popinError ? <CMPopin {...popinError} /> : null}
       </div>
+    </div>
   );
 };
 
