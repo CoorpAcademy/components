ae53f8a4e4a80f786f56c45637b194e01163f6ac
Alan Langlois <alan@etaminstudio.com>
2022-08-03 18:09:12 +0200

    adding fixtures

---
+++ packages/@coorpacademy-components/src/atom/select/test/fixtures/colored.tsx
@@ -0,0 +1,19 @@
+import {Props} from '../../index.native';
+import {createSelectChoice} from '../../../../hoc/modal/select/test/fixtures/default';
+
+type Fixture = {props: Props};
+
+const select = createSelectChoice({name: 'sel456'});
+const items = select.items || [];
+
+const fixture: Fixture = {
+  props: {
+    questionType: 'template',
+    placeholder: 'Feel free to edit',
+    analyticsID: 'foo',
+    values: items,
+    color: '#ff00ff'
+  }
+};
+
+export default fixture;
---
+++ packages/@coorpacademy-components/src/atom/select/test/fixtures/with-place-holder.tsx
@@ -0,0 +1,18 @@
+import {Props} from '../../index.native';
+import {createSelectChoice} from '../../../../hoc/modal/select/test/fixtures/default';
+
+type Fixture = {props: Props};
+
+const select = createSelectChoice({name: 'sel456'});
+const items = select.items || [];
+
+const fixture: Fixture = {
+  props: {
+    questionType: 'template',
+    placeholder: 'Feel free to edit',
+    analyticsID: 'foo',
+    values: items
+  }
+};
+
+export default fixture;
