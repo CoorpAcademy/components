7e6f385fc158ca3f6a390f65fa4d29d2c9ebdcfd
Bilel <bilel@MBPdeDjllet2022.localdomain>
2022-08-08 11:20:28 +0200

    tu

--- packages/@coorpacademy-components/src/molecule/cm-popin/test/fixtures/cookie.js
+++ packages/@coorpacademy-components/src/molecule/cm-popin/test/fixtures/cookie.js
@@ -1,7 +1,7 @@
 export default {
   props: {
     content:
-      '<span>To provide the best experience we use technologies like cookies to help the website to function and also to track how you interact with our website. But for us to provide the best user experience, enable the specific cookies from Settings. Read our <a href="https://example.com"> cookies policy</a> for more information.</span>',
+      '<span>To provide the best experience we use technologies like cookies to help the website to function and also to track how you interact with our website. But for us to provide the best user experience, enable the specific cookies from Settings. Read our <a href="https://www.privacy.coorpacademy.com/up"> cookies policy</a> for more information.</span>',
     mode: 'cookie',
     cookieTitle: 'We use cookies to optimize your user experience',
     firstButton: {
