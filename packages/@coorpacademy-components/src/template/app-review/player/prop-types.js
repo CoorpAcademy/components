39f1259b07ac0bb1d621bc8a6f99b7873e003419
Joan Ortega <jomaora@gmail.com>
2022-08-11 15:49:38 +0200

    update native player review file to avoid proptype errors

--- packages/@coorpacademy-components/src/template/app-review/player/prop-types.js
+++ packages/@coorpacademy-components/src/template/app-review/player/prop-types.js
@@ -5,7 +5,7 @@ import ReviewBackgroundPropTypes from '../../../atom/review-background/prop-type
 import ReviewHeaderPropTypes from '../../../organism/review-header/prop-types';
 import StackedSlidesPropTypes from '../../../organism/review-stacked-slides/prop-types';
 
-export const SlidesReviewPropTypes = {
+export const PlayerReviewPropTypes = {
   header: PropTypes.shape(ReviewHeaderPropTypes),
   stack: PropTypes.shape(StackedSlidesPropTypes),
   reviewBackgroundAriaLabel: ReviewBackgroundPropTypes['aria-label'],
