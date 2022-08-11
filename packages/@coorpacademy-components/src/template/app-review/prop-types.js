bf701e578f4b0162f8903489596986bbb9d13200
Joan Ortega <jomaora@gmail.com>
2022-08-11 18:08:27 +0200

    Redux rendering updated with new props

--- packages/@coorpacademy-components/src/template/app-review/prop-types.js
+++ packages/@coorpacademy-components/src/template/app-review/prop-types.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import keys from 'lodash/fp/keys';
 import Onboarding from './onboarding';
 import Skills from './skills';
-import Slides from './player';
+import Player from './player';
 
 export const ViewNames = {
   skills: 'skills',
@@ -15,5 +15,5 @@ export default {
   viewName: PropTypes.oneOf(keys(ViewNames)),
   onboarding: PropTypes.shape({...Onboarding.propTypes}),
   skills: PropTypes.shape({...Skills.propTypes}),
-  slides: PropTypes.shape({...Slides.propTypes})
+  slides: PropTypes.shape({...Player.propTypes})
 };
