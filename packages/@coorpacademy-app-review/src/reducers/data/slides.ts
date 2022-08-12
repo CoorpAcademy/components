2c648ad7677b3fff1e639484ae31bbc0a503fd14
Joan Ortega <jomaora@gmail.com>
2022-08-12 10:32:16 +0200

    refactor data.slides state and fetch-slide action - wip

--- packages/@coorpacademy-app-review/src/reducers/data/slides.ts
+++ packages/@coorpacademy-app-review/src/reducers/data/slides.ts
@@ -1,50 +1,23 @@
-import includes from 'lodash/fp/includes';
+import has from 'lodash/fp/has';
+import set from 'lodash/fp/set';
 import {SlidesAction} from '../../actions/data/slides';
 import {SLIDE_FETCH_SUCCESS} from '../../actions/api/fetch-slide';
 import {SlideFromAPI} from '../../types/common';
 
-export type SlidesState = {
-  slideRefs: string[];
-  values: Record<string, SlideFromAPI>;
-};
-
-const getInitialState = (): SlidesState => {
-  const state: SlidesState = {
-    slideRefs: [],
-    values: {}
-  };
-  return state;
-};
-
-export const initialState: SlidesState = getInitialState();
-
-// -----------------------------------------------------------------------------
-
-const setSlide = (state: SlidesState, slide: SlideFromAPI): SlidesState => {
-  const slideRefs = state.slideRefs;
-  if (!includes(slide.universalRef, slideRefs)) slideRefs.push(slide.universalRef);
-  const _state: SlidesState = {
-    slideRefs,
-    values: {
-      ...state.values,
-      [slide.universalRef]: slide
-    }
-  };
-
-  return _state;
-};
+export type SlidesState = Record<string, SlideFromAPI>;
+export const initialState: SlidesState = {};
 
 // eslint-disable-next-line default-param-last
 const reducer = (state: SlidesState = initialState, action: SlidesAction): SlidesState => {
   switch (action.type) {
     case SLIDE_FETCH_SUCCESS: {
-      return setSlide(state, action.payload);
+      const slide = action.payload;
+      if (has(slide.id, state)) return state;
+      return set(slide.id, slide, state);
     }
     default:
       return state;
   }
 };
-
-// -----------------------------------------------------------------------------
 
 export default reducer;
