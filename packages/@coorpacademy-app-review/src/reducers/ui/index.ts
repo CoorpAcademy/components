81c7fa74333f52be5145aeae16c3a2d03b69bcd4
Joan Ortega <jomaora@gmail.com>
2022-08-12 16:59:09 +0200

    Adding reducer for currentSlideRef

--- packages/@coorpacademy-app-review/src/reducers/ui/index.ts
+++ packages/@coorpacademy-app-review/src/reducers/ui/index.ts
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 
+import currentSlideRef, {CurrentSlideRefState} from './current-slide-ref';
 import finishedSlides, {FinishedSlidesState} from './finished-slides';
 import navigation, {NavigationState} from './navigation';
 import reviewStatus, {ReviewStatusState} from './review-status';
 import slides, {UISlidesState} from './slides';
 
 export type UIState = {
+  currentSlideRef: CurrentSlideRefState;
   navigation: NavigationState;
   finishedSlides: FinishedSlidesState;
   reviewStatus: ReviewStatusState;
@@ -13,6 +15,7 @@ export type UIState = {
 };
 
 export default combineReducers({
+  currentSlideRef,
   finishedSlides,
   navigation,
   reviewStatus,
