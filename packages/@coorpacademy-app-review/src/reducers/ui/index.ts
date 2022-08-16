cdb8965ba7cdaaa2df0db630c36685b6ae709b13
Joan Ortega <jomaora@gmail.com>
2022-08-16 18:02:46 +0200

    delete non necessary state property finished-slides

--- packages/@coorpacademy-app-review/src/reducers/ui/index.ts
+++ packages/@coorpacademy-app-review/src/reducers/ui/index.ts
@@ -1,7 +1,6 @@
 import {combineReducers} from 'redux';
 
 import currentSlideRef, {CurrentSlideRefState} from './current-slide-ref';
-import finishedSlides, {FinishedSlidesState} from './finished-slides';
 import navigation, {NavigationState} from './navigation';
 import reviewStatus, {ReviewStatusState} from './review-status';
 import slides, {UISlidesState} from './slides';
@@ -9,14 +8,12 @@ import slides, {UISlidesState} from './slides';
 export type UIState = {
   currentSlideRef: CurrentSlideRefState;
   navigation: NavigationState;
-  finishedSlides: FinishedSlidesState;
   reviewStatus: ReviewStatusState;
   slides: UISlidesState;
 };
 
 export default combineReducers({
   currentSlideRef,
-  finishedSlides,
   navigation,
   reviewStatus,
   slides
