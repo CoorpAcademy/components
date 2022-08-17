412500aabb891e0b729ead8a80e5446742b31555
Joan Ortega <jomaora@gmail.com>
2022-08-17 16:52:25 +0200

    Update qcm graphic slide fixture

--- packages/@coorpacademy-app-review/src/helpers/test/fixtures/qcm-graphic.ts
+++ packages/@coorpacademy-app-review/src/helpers/test/fixtures/qcm-graphic.ts
@@ -2,74 +2,85 @@ import noop from 'lodash/fp/noop';
 import {SlideFromAPI, UISlide} from '../../../types/common';
 
 export const qcmGraphicSlide: SlideFromAPI = {
-  id: 'sli_41~RSVclH',
-  universalRef: 'sli_41~RSVclH',
   question: {
-    type: 'qcmGraphic',
-    header: 'sli_41~RSVclH.header',
-    explanation: 'sli_41~RSVclH.explanation',
     content: {
+      media: {
+        src: [],
+        posters: [],
+        subtitles: []
+      },
       choices: [
         {
           media: {
-            type: 'img',
             src: [
               {
-                _id: '5fdce726359bf4003ee66997',
-                mimeType: 'image/png',
-                url: 'med_4JVRrE5gH.mediaUrl_VkBtUV9gB'
+                _id: '624efd915948a7013083e986',
+                mimeType: 'image/jpeg',
+                url: '//static.coorpacademy.com/content/CoorpAcademy/content-partnerships/cockpit-partner-au-feminin/default/true-1512554153232.jpg',
+                id: '624efd915948a7013083e986'
               }
             ],
             posters: [],
-            subtitles: []
+            subtitles: [],
+            type: 'img'
           },
-          _id: '5fdce726359bf4003ee66996',
-          label: 'sli_41~RSVclH.choice_V1gN0r4qgS',
-          value: 'sli_41~RSVclH.choice_V1gN0r4qgS',
-          items: []
+          items: [],
+          _id: '624efd915948a7013083e985',
+          label: 'Vrai',
+          value: 'sli_VkSQroQnx.choice_NJesyUo7nx',
+          id: '624efd915948a7013083e985'
         },
         {
           media: {
-            type: 'img',
             src: [
               {
-                _id: '5fdce726359bf4003ee66999',
-                mimeType: 'image/png',
-                url: 'med_Ek8gIEqxB.mediaUrl_EkcFUEclH'
+                _id: '624efd915948a7013083e988',
+                mimeType: 'image/jpeg',
+                url: '//static.coorpacademy.com/content/CoorpAcademy/content-partnerships/cockpit-partner-au-feminin/default/false-1512554160848.jpg',
+                id: '624efd915948a7013083e988'
               }
             ],
             posters: [],
-            subtitles: []
+            subtitles: [],
+            type: 'img'
           },
-          _id: '5fdce726359bf4003ee66998',
-          label: 'sli_41~RSVclH.choice_4JlLeLE5lS',
-          value: 'sli_41~RSVclH.choice_4JlLeLE5lS',
-          items: []
+          items: [],
+          _id: '624efd915948a7013083e987',
+          label: 'Faux',
+          value: 'sli_VkSQroQnx.choice_VkgayLjX3e',
+          id: '624efd915948a7013083e987'
         }
       ]
-    }
+    },
+    type: 'qcmGraphic',
+    header: 'Pour mener une bonne négociation, il ne faut ressentir aucune émotion. Vrai ou faux ?',
+    explanation: 'Sélectionnez la bonne réponse.'
   },
-  clue: 'sli_41~RSVclH.clue',
-  klf: 'sli_41~RSVclH.klf',
-  tips: 'sli_41~RSVclH.tips'
+  klf: "Il ne faut pas prendre la négociation comme une affaire personnelle, afin de rester centré sur son sujet. Mais cela ne signifie pas être dépourvu de toute émotion, car celles-ci peuvent être de vrais atouts. À condition de réussir à les exprimer sans accuser l'autre d'en être à l'origine.",
+  tips: "Pour gérer ses émotions, il faut d'abord les identifier, notamment en analysant ses ressentis physiques et en trouvant l'élément déclencheur.",
+  universalRef: 'sli_VkSQroQnx',
+  id: 'sli_VkSQroQnx'
 };
 
 export const qcmGraphicUISlide: Partial<UISlide> = {
-  questionText: 'sli_41~RSVclH.header',
+  questionText:
+    'Pour mener une bonne négociation, il ne faut ressentir aucune émotion. Vrai ou faux ?',
   answerUI: {
-    help: 'sli_41~RSVclH.explanation',
+    help: 'Sélectionnez la bonne réponse.',
     model: {
       type: 'qcmGraphic',
       answers: [
         {
-          title: 'sli_41~RSVclH.choice_V1gN0r4qgS',
-          image: 'med_4JVRrE5gH.mediaUrl_VkBtUV9gB',
+          title: 'Vrai',
+          image:
+            '//static.coorpacademy.com/content/CoorpAcademy/content-partnerships/cockpit-partner-au-feminin/default/true-1512554153232.jpg',
           selected: false,
           onClick: noop
         },
         {
-          title: 'sli_41~RSVclH.choice_4JlLeLE5lS',
-          image: 'med_Ek8gIEqxB.mediaUrl_EkcFUEclH',
+          title: 'Faux',
+          image:
+            '//static.coorpacademy.com/content/CoorpAcademy/content-partnerships/cockpit-partner-au-feminin/default/false-1512554160848.jpg',
           selected: false,
           onClick: noop
         }
