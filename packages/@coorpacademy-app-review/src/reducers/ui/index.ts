e5a1d5207b471b2cb62733940c2b3e11228ba57d
EmelineLeduc <leduc.emeline44@gmail.com>
2022-08-18 18:02:45 +0200

    add reducer answer

--- packages/@coorpacademy-app-review/src/reducers/ui/index.ts
+++ packages/@coorpacademy-app-review/src/reducers/ui/index.ts
@@ -2,13 +2,16 @@ import {combineReducers} from 'redux';
 
 import currentSlideRef, {CurrentSlideRefState} from './current-slide-ref';
 import navigation, {NavigationState} from './navigation';
+import answer, {AnswerState} from './answer';
 
 export type UIState = {
   currentSlideRef: CurrentSlideRefState;
   navigation: NavigationState;
+  answer: AnswerState;
 };
 
 export default combineReducers({
   currentSlideRef,
-  navigation
+  navigation,
+  answer
 });
