89067f52e364403b4ab841431453e0056878c4ca
Joan Ortega <jomaora@gmail.com>
2022-08-22 16:50:15 +0200

    separate createTestStore to be used by other tests

---
+++ packages/@coorpacademy-app-review/src/actions/api/test/create-test-store.ts
@@ -0,0 +1,25 @@
+import type {ExecutionContext} from 'ava';
+import constant from 'lodash/fp/constant';
+import {AnyAction, applyMiddleware, compose, createStore, Dispatch, Middleware, Store} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer, {StoreState} from '../../../reducers';
+import {Services} from '../../../types/common';
+
+const assertActionsMiddleware = (t: ExecutionContext, ACTIONS: AnyAction[]): Middleware =>
+  constant((next: Dispatch) => (action: AnyAction): unknown => {
+    const expectedAction = ACTIONS.shift();
+    if (!expectedAction) throw new Error(`Missing {type: "${action.type}"} action`);
+    t.deepEqual(action, expectedAction);
+    return next(action);
+  });
+
+export const createTestStore = (
+  t: ExecutionContext,
+  initialState: StoreState,
+  services: Services,
+  actions: AnyAction[]
+): Store => {
+  const thunkMiddleware = thunk.withExtraArgument({services});
+  const enhancer = compose(applyMiddleware(thunkMiddleware, assertActionsMiddleware(t, actions)));
+  return createStore(rootReducer, initialState, enhancer);
+};
--- packages/@coorpacademy-app-review/src/actions/api/test/fetch-slide.test.ts
+++ packages/@coorpacademy-app-review/src/actions/api/test/fetch-slide.test.ts
@@ -1,62 +1,58 @@
 import test, {ExecutionContext} from 'ava';
-import constant from 'lodash/fp/constant';
-import {AnyAction, applyMiddleware, combineReducers, compose, createStore} from 'redux';
-import thunk from 'redux-thunk';
-import data from '../../../reducers/data';
-import ui from '../../../reducers/ui';
-import rootReducer, {StoreState} from '../../../reducers';
-import {Services} from '../../../types/common';
+import {SlideFromAPI} from '../../../types/common';
 import {fetchSlide} from '../fetch-slide';
+import {freeTextSlide} from '../../../views/slides/test/fixtures/free-text';
+import {services as mockedServices} from '../../../test/util/services.mock';
+import {StoreState} from '../../../reducers';
+import {createTestStore} from './create-test-store';
 
-const assertActionsMiddleware = (t, ACTIONS) =>
-  constant(next => action => {
-    const expectedAction = ACTIONS.shift();
-    if (!expectedAction) throw new Error(`Missing {type: "${action.type}"} action`);
-    t.deepEqual(action, expectedAction);
-    return next(action);
-  });
-
-const createTestStore = (t, initialState, createServices, actions) => {
-  const thunkMiddleware = thunk.withExtraArgument({services: createServices(t)});
-  const enhancer = compose(applyMiddleware(thunkMiddleware, assertActionsMiddleware(t, actions)));
-  return createStore(rootReducer, initialState, enhancer);
+const initialState: StoreState = {
+  data: {
+    progression: null,
+    slides: {},
+    skills: [],
+    token: '1234'
+  },
+  ui: {
+    currentSlideRef: '',
+    navigation: [],
+    answers: []
+  }
 };
 
-test('should dispatch FETCH_SUCCESS and SET_CURRENT_SLIDE actions when fetchSlide return a slide', async t => {
-  const initilaState = {data: {token: '1234'}, ui: {currentSlideRef: ''}};
-
-  const createServices = t => {
-    return {
-      fetchSlide: (slideRef: string, token: string) => {
-        t.is(token, '1234');
-        t.is(slideRef, 'slide_ref');
-        return Promise.resolve({id: 'slide_ref'});
-      }
-    };
+test('should dispatch FETCH_SUCCESS and SET_CURRENT_SLIDE actions when fetchSlide return a slide', async (t: ExecutionContext) => {
+  const services = {
+    ...mockedServices,
+    fetchSlide: (slideRef: string, token: string): Promise<SlideFromAPI> => {
+      t.is(token, '1234');
+      t.is(slideRef, 'sli_VJYjJnJhg');
+      return Promise.resolve(freeTextSlide);
+    }
   };
 
   const expectedActions = [
-    {type: '@@slides/FETCH_REQUEST', meta: {slideRef: 'slide_ref'}},
-    {type: '@@slides/FETCH_SUCCESS', meta: {slideRef: 'slide_ref'}, payload: {id: 'slide_ref'}},
-    {type: '@@slide/SET_CURRENT_SLIDE', payload: {id: 'slide_ref'}}
+    {type: '@@slides/FETCH_REQUEST', meta: {slideRef: 'sli_VJYjJnJhg'}},
+    {
+      type: '@@slides/FETCH_SUCCESS',
+      meta: {slideRef: 'sli_VJYjJnJhg'},
+      payload: freeTextSlide
+    },
+    {type: '@@slide/SET_CURRENT_SLIDE', payload: freeTextSlide}
   ];
 
-  const {dispatch} = createTestStore(t, initilaState, createServices, expectedActions);
+  const {dispatch} = createTestStore(t, initialState, services, expectedActions);
 
-  await dispatch(fetchSlide('slide_ref', '1234'));
+  await dispatch(fetchSlide('sli_VJYjJnJhg', '1234'));
 });
 
 test('should dispatch SLIDE_FETCH_FAILURE action when fetchSlide fails', async t => {
-  const initilaState = {data: {token: '1234'}, ui: {currentSlideRef: ''}};
-
-  const createServices = t => {
-    return {
-      fetchSlide: (slideRef: string, token: string) => {
-        t.is(token, '1234');
-        t.is(slideRef, 'slide_ref');
-        return Promise.reject(new Error('error'));
-      }
-    };
+  const services = {
+    ...mockedServices,
+    fetchSlide: (slideRef: string, token: string): Promise<SlideFromAPI> => {
+      t.is(token, '1234');
+      t.is(slideRef, 'slide_ref');
+      return Promise.reject(new Error('error'));
+    }
   };
 
   const expectedActions = [
@@ -69,7 +65,7 @@ test('should dispatch SLIDE_FETCH_FAILURE action when fetchSlide fails', async t
     }
   ];
 
-  const {dispatch} = createTestStore(t, initilaState, createServices, expectedActions);
+  const {dispatch} = createTestStore(t, initialState, services, expectedActions);
 
   await dispatch(fetchSlide('slide_ref', '1234'));
 });
