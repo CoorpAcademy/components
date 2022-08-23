d54e35d9beebb5e93385d34501da14a0bb703b95
Joan Ortega <jomaora@gmail.com>
2022-08-23 17:48:51 +0200

    rename test

--- packages/@coorpacademy-app-review/src/actions/data/test/token.test.ts
+++ packages/@coorpacademy-app-review/src/actions/data/test/token.test.ts
@@ -18,7 +18,7 @@ const initialState: StoreState = {
   }
 };
 
-test('should dispatch SET_CURRENT_SLIDE action when setCurrentSlide is called', async t => {
+test('should dispatch STORE_TOKEN action when storeToken is called', async t => {
   const expectedActions = [{type: STORE_TOKEN, payload: 'JWT.token'}];
 
   const {dispatch} = createTestStore(t, initialState, services, expectedActions);
