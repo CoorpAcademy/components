39a29049e0c5258c105a62851b80c6c31d8e24e0
EmelineLeduc <leduc.emeline44@gmail.com>
2022-08-23 10:39:18 +0200

    modify test reducer ui Edit_Basic action and add test reducer for Edit_QCM action

--- packages/@coorpacademy-app-review/src/reducers/ui/test/answers.test.ts
+++ packages/@coorpacademy-app-review/src/reducers/ui/test/answers.test.ts
@@ -1,13 +1,18 @@
 import test from 'ava';
 import reducer from '../answers';
-import {EditBasicAction, EDIT_BASIC} from '../../../actions/ui/answers';
+import {EditAction, ANSWER_EDIT} from '../../../actions/ui/answers';
 
 test('should have inital value', t => {
-  const state = reducer(undefined, {} as EditBasicAction);
+  const state = reducer(undefined, {} as EditAction);
   t.deepEqual(state, []);
 });
 
 test('should set the value of EDIT_BASIC action', t => {
-  const state = reducer([], {type: EDIT_BASIC, payload: ['value']});
+  const state = reducer([], {type: ANSWER_EDIT.basic, payload: ['value']});
   t.deepEqual(state, ['value']);
+});
+
+test('should set the value of EDIT_QCM action', t => {
+  const state = reducer([], {type: ANSWER_EDIT.qcm, payload: ['value1', 'value2']});
+  t.deepEqual(state, ['value1', 'value2']);
 });
