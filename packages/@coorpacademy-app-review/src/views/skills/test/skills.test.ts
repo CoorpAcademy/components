9c1dac80128a3a51f831a0942d2eb1418d273192
Joan Ortega <jomaora@gmail.com>
2022-08-23 14:41:16 +0200

    Adding test for view skills

---
+++ packages/@coorpacademy-app-review/src/views/skills/test/skills.test.ts
@@ -0,0 +1,95 @@
+import test from 'ava';
+import map from 'lodash/fp/map';
+import omit from 'lodash/fp/omit';
+import {StoreState} from '../../../reducers';
+import {mapStateToSkillsProps} from '..';
+
+test('should create initial props when no skills on the state', t => {
+  const state: StoreState = {
+    data: {
+      progression: null,
+      skills: [],
+      slides: {},
+      token: '1234'
+    },
+    ui: {
+      currentSlideRef: '',
+      navigation: ['loader', 'skills'],
+      answers: []
+    }
+  };
+
+  const props = mapStateToSkillsProps(state);
+
+  t.deepEqual(props, {
+    title: '@todo title',
+    titleNoSkills: '@todo titleNoSkills',
+    textNoSkills: '@todo textNoSkills',
+    iconSkillAriaLabel: '@todo iconSkillAriaLabel',
+    isLoading: false,
+    isLoadingAriaLabel: '@todo loading',
+    listSkills: []
+  });
+});
+
+test('should create initial props when skills on the state', t => {
+  const state: StoreState = {
+    data: {
+      progression: null,
+      skills: [
+        {
+          skillRef: '_skill-ref',
+          slidesToReview: 2,
+          name: 'skill-test',
+          custom: false
+        },
+        {
+          skillRef: '_skill-ref-2',
+          slidesToReview: 5,
+          name: 'skill-test-2',
+          custom: true
+        }
+      ],
+      slides: {},
+      token: '1234'
+    },
+    ui: {
+      currentSlideRef: '',
+      navigation: ['loader', 'skills'],
+      answers: []
+    }
+  };
+
+  const props = mapStateToSkillsProps(state);
+
+  t.deepEqual(omit('listSkills', props), {
+    title: '@todo title',
+    titleNoSkills: '@todo titleNoSkills',
+    textNoSkills: '@todo textNoSkills',
+    iconSkillAriaLabel: '@todo iconSkillAriaLabel',
+    isLoading: false,
+    isLoadingAriaLabel: '@todo loading'
+  });
+  t.deepEqual(map(omit('onClick'), props.listSkills), [
+    {
+      'aria-label': '',
+      isCustom: false,
+      skillTitle: 'skill-test',
+      skillAriaLabel: '@todo skill aria label',
+      buttonLabel: '@todo button',
+      buttonAriaLabel: '@todo button aria label',
+      reviseLabel: '@todo revise',
+      reviseAriaLabel: '@todo revise aria label'
+    },
+    {
+      'aria-label': '',
+      isCustom: true,
+      skillTitle: 'skill-test-2',
+      skillAriaLabel: '@todo skill aria label',
+      buttonLabel: '@todo button',
+      buttonAriaLabel: '@todo button aria label',
+      reviseLabel: '@todo revise',
+      reviseAriaLabel: '@todo revise aria label'
+    }
+  ]);
+});
