9888c2228b210efdce7cce465d74f57b2123172c
Joan Ortega <jomaora@gmail.com>
2022-08-23 10:47:32 +0200

    Adding test unit for ui action SET_CURRENT_SLIDE

---
+++ packages/@coorpacademy-app-review/src/actions/ui/test/slides.test.ts
@@ -0,0 +1,27 @@
+import test from 'ava';
+import {createTestStore} from '../../test/create-test-store';
+import {services} from '../../../test/util/services.mock';
+import {freeTextSlide} from '../../../views/slides/test/fixtures/free-text';
+import {StoreState} from '../../../reducers';
+import {setCurrentSlide, SET_CURRENT_SLIDE} from '../slides';
+
+const initialState: StoreState = {
+  data: {
+    progression: null,
+    slides: {},
+    skills: [],
+    token: '1234'
+  },
+  ui: {
+    currentSlideRef: '',
+    navigation: [],
+    answers: []
+  }
+};
+
+test('should dispatch SET_CURRENT_SLIDE action when setCurrentSlide is called', async t => {
+  const expectedActions = [{type: SET_CURRENT_SLIDE, payload: freeTextSlide}];
+
+  const {dispatch} = createTestStore(t, initialState, services, expectedActions);
+  await dispatch(setCurrentSlide(freeTextSlide));
+});
