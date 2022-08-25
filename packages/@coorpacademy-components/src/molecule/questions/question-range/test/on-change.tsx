0e793079d56ee6e881d8b558352de07abb408d3b
Alan Langlois <alan@etaminstudio.com>
2022-08-25 10:33:46 +0200

    fixing test, wip

--- packages/@coorpacademy-components/src/molecule/questions/question-range/test/on-change.tsx
+++ packages/@coorpacademy-components/src/molecule/questions/question-range/test/on-change.tsx
@@ -2,20 +2,19 @@ import test from 'ava';
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react-native';
 import QuestionSlider, {OnChangeFunction} from '../index.native';
-import {fixture} from './fixtures/default.ts';
+
+const onSlidingComplete = () => null;
 
 test('should handle on change', t => {
   const onChange: OnChangeFunction = value => {
     t.is(value, 5);
   };
 
-  const {value, max, min, onSlidingComplete} = fixture.props;
-
   const component = (
     <QuestionSlider
-      value={value}
-      min={min}
-      max={max}
+      value={0}
+      min={100}
+      max={10}
       onChange={onChange}
       onSlidingComplete={onSlidingComplete}
       testID="question-slider"
