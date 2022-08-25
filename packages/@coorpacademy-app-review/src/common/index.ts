e35a1d495f8bdb726d47b80ea0f40ea0e6a3b233
stefano <stefanoacosta.92@gmail.com>
2022-08-25 16:00:36 +0200

    removes unused code

--- packages/@coorpacademy-app-review/src/common/index.ts
+++ packages/@coorpacademy-app-review/src/common/index.ts
@@ -1,8 +1,3 @@
-import _toString from 'lodash/fp/toString';
-
-export const TOTAL_SLIDES_STACK = 5;
-export const HIGHEST_INDEX = TOTAL_SLIDES_STACK - 1;
-
 export const VIEWS: {
   readonly skills: 'skills';
   readonly onboarding: 'onboarding';
@@ -16,5 +11,3 @@ export const VIEWS: {
 export const slideIndexes = ['0', '1', '2', '3', '4'] as const;
 
 export type SlideIndexes = typeof slideIndexes[number];
-
-export const indexToString = (index: number): SlideIndexes => _toString(index) as SlideIndexes;
