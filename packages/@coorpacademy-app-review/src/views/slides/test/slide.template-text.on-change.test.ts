a4156a3cb92756e1531db35d81f2c58a714d9b27
Joan Ortega <jomaora@gmail.com>
2022-08-26 11:55:50 +0200

    Add onChange test for template text slide

---
+++ packages/@coorpacademy-app-review/src/views/slides/test/slide.template-text.on-change.test.ts
@@ -0,0 +1,72 @@
+import test from 'ava';
+import omit from 'lodash/fp/omit';
+import get from 'lodash/fp/get';
+import {mapStateToSlidesProps} from '..';
+import {ProgressionFromAPI} from '../../../types/common';
+import {services} from '../../../test/util/services.mock';
+import {createTestStore} from '../../../actions/test/create-test-store';
+import {StoreState} from '../../../reducers';
+import {EDIT_TEMPLATE} from '../../../actions/ui/answers';
+import {Template} from '../../../types/slides';
+import {textTemplateSlide} from './fixtures/text-template';
+
+const progression: ProgressionFromAPI = {
+  _id: '123456789123',
+  content: {type: 'skill', ref: '_skill-ref'},
+  engine: {
+    ref: 'review'
+  },
+  state: {
+    allAnswers: [],
+    isCorrect: true,
+    nextContent: {
+      ref: textTemplateSlide.id,
+      type: 'slide'
+    },
+    pendingSlides: [],
+    slides: [],
+    step: {
+      current: 1
+    },
+    stars: 0
+  }
+};
+
+test('should dispatch EDIT_TEMPLATE action via the property sonChange of a Template Text slide', t => {
+  t.plan(2);
+
+  const initialState: StoreState = {
+    data: {
+      progression,
+      slides: {
+        [textTemplateSlide.id]: textTemplateSlide
+      },
+      skills: [],
+      token: '1234'
+    },
+    ui: {
+      currentSlideRef: textTemplateSlide.id,
+      navigation: ['skills', 'slides'],
+      answers: [],
+      slide: {
+        validateButton: false
+      }
+    }
+  };
+
+  const expectedActions = [{type: EDIT_TEMPLATE, payload: ['text']}];
+  const {dispatch, getState} = createTestStore(t, initialState, services, expectedActions);
+
+  const props = mapStateToSlidesProps(getState(), dispatch);
+  t.deepEqual(omit('answerUI', props.stack.slides['0']), {
+    hidden: false,
+    position: 0,
+    loading: false,
+    parentContentTitle: 'From "Using redux" chapter',
+    questionText: 'Comment le protégé doit-il percevoir son mentor ?'
+  });
+
+  const SlideProps = props.stack.slides['0'].answerUI?.model as Template;
+  const onChange = get(['0', 'onChange'], SlideProps.answers);
+  onChange('text');
+});
