e46c6a3de98b20e974f32c336e50a7902d42d4a0
Joan <jomaora@gmail.com>
2022-08-29 18:37:51 +0200

    refactor

--- packages/@coorpacademy-app-review/src/reducers/data/test/progression.test.ts
+++ packages/@coorpacademy-app-review/src/reducers/data/test/progression.test.ts
@@ -3,6 +3,7 @@ import reducer from '../progression';
 import {POST_PROGRESSION_SUCCESS, ReceivedProgression} from '../../../actions/api/post-progression';
 import {POST_ANSWER_SUCCESS} from '../../../actions/api/post-answer';
 import {postAnswerResponses, postProgressionResponse} from '../../../test/util/services.mock';
+import {freeTextSlide} from '../../../views/slides/test/fixtures/free-text';
 
 test('should have initial value', t => {
   const state = reducer(undefined, {} as ReceivedProgression);
@@ -20,7 +21,7 @@ test('should set the value of POST_PROGRESSION_SUCCESS action', t => {
 test('should set the value of POST_ANSWER_SUCCESS action', t => {
   const state = reducer(null, {
     type: POST_ANSWER_SUCCESS,
-    payload: postAnswerResponses.sli_VJYjJnJhg
+    payload: postAnswerResponses[freeTextSlide.universalRef]
   });
-  t.deepEqual(state, postAnswerResponses.sli_VJYjJnJhg);
+  t.deepEqual(state, postAnswerResponses[freeTextSlide.universalRef]);
 });
