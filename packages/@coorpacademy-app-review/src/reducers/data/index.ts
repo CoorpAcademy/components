5e7fba9fc249cd5fc7447bdb937420de283f9109
stefano <stefanoacosta.92@gmail.com>
2022-09-02 14:27:40 +0200

    reverse

--- packages/@coorpacademy-app-review/src/reducers/data/index.ts
+++ packages/@coorpacademy-app-review/src/reducers/data/index.ts
@@ -1,13 +1,11 @@
 import {combineReducers} from 'redux';
 
-import answersCorrection, {AnswersCorrectionState} from './answers-correction';
 import progression, {ProgressionState} from './progression';
 import skills, {SkillsState} from './skills';
 import slides, {SlidesState} from './slides';
 import token, {TokenState} from './token';
 
 export type DataState = {
-  answersCorrection: AnswersCorrectionState;
   isFetching?: boolean;
   progression: ProgressionState;
   skills: SkillsState;
@@ -19,6 +17,5 @@ export default combineReducers({
   progression,
   skills,
   slides,
-  token,
-  answersCorrection
+  token
 });
