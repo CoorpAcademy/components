da6d8e51f70b34cf42cedc6fe89fe827e3fafb26
esa-coorp <eric.sampaio@coorpacademy.com>
2022-09-06 11:07:24 +0200

    News without cat

---
+++ packages/@coorpacademy-components/src/molecule/news/test/fixtures/without-cta.js
@@ -0,0 +1,10 @@
+export default {
+  props: {
+    image: 'http://minecraft.fr/wp-content/uploads/2013/05/Pa8WZj1-235x300.jpg',
+    title: 'A World Of Infinite Opportunities',
+    date: '3 january 2018',
+    description: 'Vestibulum id <b>ligula</b> porta felis <i>euismod semper</i>.',
+    authorLogo:
+      'https://static.coorpacademy.com/content/digital/raw/logo_coorpacademy-1487689750995.svg'
+  }
+};
