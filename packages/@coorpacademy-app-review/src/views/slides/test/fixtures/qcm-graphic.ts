52e502127feb575015c48c8352e4fd03e08012e2
Joan <jomaora@gmail.com>
2022-09-07 18:14:46 +0200

    change fixture for qcmGraphic, 8 choices, problem with height for annswer choices section

--- packages/@coorpacademy-app-review/src/views/slides/test/fixtures/qcm-graphic.ts
+++ packages/@coorpacademy-app-review/src/views/slides/test/fixtures/qcm-graphic.ts
@@ -188,28 +188,75 @@ export const qcmGraphicSlide: SlideFromAPI = {
 };
 
 export const qcmGraphicUISlide: Partial<UISlide> = {
-  questionText:
-    'Pour mener une bonne négociation, il ne faut ressentir aucune émotion. Vrai ou faux ?',
+  questionText: 'Quels sont les 4 piliers de l’apprentissage ?',
   answerUI: {
-    help: 'Sélectionnez la bonne réponse.',
+    help: 'Sélectionnez les 4 bonnes réponses.',
     model: {
       type: 'qcmGraphic',
       answers: [
         {
-          title: 'Vrai',
+          title: 'La consolidation des acquis',
           image:
-            '//static.coorpacademy.com/content/CoorpAcademy/content-partnerships/cockpit-partner-au-feminin/default/true-1512554153232.jpg',
+            '//static.coorpacademy.com/content/CoorpAcademy/content-science-et-vie/cockpit-science-et-vie/default/shutterstock_180530003-1618999931321.jpg',
           selected: false,
-          onClick: noop,
-          ariaLabel: 'Vrai'
+          ariaLabel: 'La consolidation des acquis',
+          onClick: noop
         },
         {
-          title: 'Faux',
+          title: 'Le retour d’information',
           image:
-            '//static.coorpacademy.com/content/CoorpAcademy/content-partnerships/cockpit-partner-au-feminin/default/false-1512554160848.jpg',
+            '//static.coorpacademy.com/content/CoorpAcademy/content-science-et-vie/cockpit-science-et-vie/default/shutterstock_1170951868-1618999931510.jpg',
           selected: false,
-          onClick: noop,
-          ariaLabel: 'Faux'
+          ariaLabel: 'Le retour d’information',
+          onClick: noop
+        },
+        {
+          title: 'L’engagement actif',
+          image:
+            '//static.coorpacademy.com/content/CoorpAcademy/content-science-et-vie/cockpit-science-et-vie/default/shutterstock_439207960-1618999931642.jpg',
+          selected: false,
+          ariaLabel: 'L’engagement actif',
+          onClick: noop
+        },
+        {
+          title: 'La motivation',
+          image:
+            '//static.coorpacademy.com/content/CoorpAcademy/content-science-et-vie/cockpit-science-et-vie/default/shutterstock_538654849-1618999935091.jpg',
+          selected: false,
+          ariaLabel: 'La motivation',
+          onClick: noop
+        },
+        {
+          title: 'L’attention',
+          image:
+            '//static.coorpacademy.com/content/CoorpAcademy/content-science-et-vie/cockpit-science-et-vie/default/shutterstock_485162803-1618999941454.jpg',
+          selected: false,
+          ariaLabel: 'L’attention',
+          onClick: noop
+        },
+        {
+          title: 'La confiance en soi',
+          image:
+            '//static.coorpacademy.com/content/CoorpAcademy/content-science-et-vie/cockpit-science-et-vie/default/shutterstock_374136655-1619000050478.jpg',
+          selected: false,
+          ariaLabel: 'La confiance en soi',
+          onClick: noop
+        },
+        {
+          title: 'La capacité d’abstraction',
+          image:
+            '//static.coorpacademy.com/content/CoorpAcademy/content-science-et-vie/cockpit-science-et-vie/default/shutterstock_142530529-1618999997389.jpg',
+          selected: false,
+          ariaLabel: 'La capacité d’abstraction',
+          onClick: noop
+        },
+        {
+          title: 'L’abnégation',
+          image:
+            '//static.coorpacademy.com/content/CoorpAcademy/content-science-et-vie/cockpit-science-et-vie/default/shutterstock_5948041-1618999993222.jpg',
+          selected: false,
+          ariaLabel: 'L’abnégation',
+          onClick: noop
         }
       ]
     }
