66dbb456183dd3136bce55ae268c8c9d97025723
chrisdugne <chris.dugne@uralys.com>
2022-09-08 09:45:53 +0200

    using #757575 to get props.color for nav-bar

--- packages/@coorpacademy-nova-icons/src/components/nova-composition/navigation/nav-bar.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/navigation/nav-bar.js
@@ -6,14 +6,14 @@ const SvgComponent = props => (
       <radialGradient
         id="prefix__a"
         cx={0.5}
-        cy={1.196}
+        cy={1}
         r={1}
         gradientTransform="matrix(0 -1 .5 0 0 1.5)"
         gradientUnits="objectBoundingBox"
       >
-        <stop offset={0} stopColor="#fff" stopOpacity={0.365} />
-        <stop offset={0.752} stopColor="#fff" stopOpacity={0} />
-        <stop offset={1} stopColor="#fff" stopOpacity={0} />
+        <stop offset={0} stopColor="#757575" stopOpacity={0.365} />
+        <stop offset={0.752} stopColor="#757575" stopOpacity={0} />
+        <stop offset={1} stopColor="#757575" stopOpacity={0} />
       </radialGradient>
     </defs>
     <path data-name="Rectangle 57" fill="url(#prefix__a)" d="M0 0h94v43H0z" />
