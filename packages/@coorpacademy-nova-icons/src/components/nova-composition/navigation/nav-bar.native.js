6936aaffebbd36566ee3b92638aa1ea2ba674186
Chris <chris.dugne@uralys.com>
2022-09-08 10:20:01 +0200

    Updated nova icons (#2363)
    * using #757575 to get props.color for nav-bar
    
    * updated scripts to allow custom color on any attribute

--- packages/@coorpacademy-nova-icons/src/components/nova-composition/navigation/nav-bar.native.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/navigation/nav-bar.native.js
@@ -7,14 +7,14 @@ const SvgComponent = props => (
       <RadialGradient
         id="prefix__a"
         cx={0.5}
-        cy={1.196}
+        cy={1}
         r={1}
         gradientTransform="matrix(0 -1 .5 0 0 1.5)"
         gradientUnits="objectBoundingBox"
       >
-        <Stop offset={0} stopColor="#fff" stopOpacity={0.365} />
-        <Stop offset={0.752} stopColor="#fff" stopOpacity={0} />
-        <Stop offset={1} stopColor="#fff" stopOpacity={0} />
+        <Stop offset={0} stopColor={props.color} stopOpacity={0.365} />
+        <Stop offset={0.752} stopColor={props.color} stopOpacity={0} />
+        <Stop offset={1} stopColor={props.color} stopOpacity={0} />
       </RadialGradient>
     </Defs>
     <Path data-name="Rectangle 57" fill="url(#prefix__a)" d="M0 0h94v43H0z" />
