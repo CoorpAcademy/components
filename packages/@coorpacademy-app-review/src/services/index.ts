6ceb0fb212dd74c303b089606bcebc14d234d08f
EmelineLeduc <79636283+EmelineLeduc@users.noreply.github.com>
2022-09-12 10:09:37 +0200

    add mocked fetch rank (#2364)
    Co-authored-by: EmelineLeduc <‘leduc.emeline44@gmail.com’>

--- packages/@coorpacademy-app-review/src/services/index.ts
+++ packages/@coorpacademy-app-review/src/services/index.ts
@@ -1,5 +1,6 @@
 import {Services} from '../types/common';
 import {fetchCorrection} from './fetch-correction';
+import {fetchRank} from './fetch-rank';
 import {fetchSkills} from './fetch-skills';
 import {fetchSlide} from './fetch-slide';
 import {postAnswer} from './post-answer';
@@ -7,6 +8,7 @@ import {postProgression} from './post-progression';
 
 export const getServices = (): Services => ({
   fetchCorrection,
+  fetchRank,
   fetchSkills,
   fetchSlide,
   postAnswer,
