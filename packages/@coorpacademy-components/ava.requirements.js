0de0dcd8c554e9eecda60140a0ee6d4896fc4d73
Alan Langlois <alan@etaminstudio.com>
2022-09-12 21:56:27 +0200

    adding blur effect using sv

--- packages/@coorpacademy-components/ava.requirements.js
+++ packages/@coorpacademy-components/ava.requirements.js
@@ -6,7 +6,7 @@ require('@coorpacademy/react-native-mock-render/mock')({
     {name: 'react-native-render-html'},
     {name: 'react-native-linear-gradient'},
     {name: '@coorpacademy/react-native-slider'},
-    {name: '@react-native-community/blur'}
+    {name: '@react-native-community/blur', exports: ['BlurView']}
   ]
 });
 
