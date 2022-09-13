bf8ea96b728420c2b9390a7a4c6a44fa236d1545
Alan Langlois <alan@etaminstudio.com>
2022-09-13 09:49:18 +0200

    adding default fixture

---
+++ packages/@coorpacademy-components/src/organism/mobile-navigation/test/fixtures/default.ts
@@ -0,0 +1,14 @@
+import {
+  NovaCompositionCoorpacademyClockTime as ClockIcon,
+  NovaSolidPlacesPlacesHome2 as HomeIcon,
+  NovaCompositionCoorpacademySearch as SearchIcon
+} from '@coorpacademy/nova-icons';
+import {Props} from '../../index.native';
+
+type Fixture = {props: Props};
+
+const fixture: Fixture = {
+  props: {}
+};
+
+export default fixture;
