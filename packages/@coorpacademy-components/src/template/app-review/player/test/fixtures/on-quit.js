5f3b73195e4cdc45fa4c4fa481012f25a95cc22a
Antonia Balluais <antonia.balluais@coorpacademy.com>
2022-09-27 16:47:01 +0200

    add popin props and fixtures

---
+++ packages/@coorpacademy-components/src/template/app-review/player/test/fixtures/on-quit.js
@@ -0,0 +1,12 @@
+import headerProps from '../../../../../organism/review-header/test/fixtures/no-answered-question';
+import DefaultStackedSlides from '../../../../../organism/review-stacked-slides/test/fixtures/initial-state';
+import onReviewQuitPopin from '../../../../../molecule/cm-popin/test/fixtures/on-review-quit';
+
+export default {
+  props: {
+    header: headerProps.props,
+    stack: DefaultStackedSlides.props,
+    reviewBackgroundAriaLabel: 'review BG Aria',
+    popin: onReviewQuitPopin.props
+  }
+};
