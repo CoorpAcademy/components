9b096ed7df198d07c3f61f2bc2a05329beebabe4
godu <arthur.weber@viacesi.fr>
2022-09-30 10:38:33 +0200

    Support IE11

--- packages/@coorpacademy-components/.storybook/main.js
+++ packages/@coorpacademy-components/.storybook/main.js
@@ -10,13 +10,13 @@ module.exports = {
         }
       }
     },
+    ...(process.env.IE ? 
+    ['@storybook/addon-ie11']:
+    [
     '@storybook/addon-controls',
-    '@storybook/addon-actions'
+    '@storybook/addon-actions'])
   ],
   stories: ['../src/**/*.stories.@(js|tsx)'],
-  features: {
-    storyStoreV7: true
-  },
   core: {
     builder: {
       name: 'webpack5',
@@ -26,16 +26,25 @@ module.exports = {
     }
   },
   webpackFinal: (config, {configType}) => {
+    if (process.env.IE)
+      config.entry.unshift(
+        "core-js/features/object",
+        'core-js/features/url-search-params',
+        'core-js/features/symbol',
+        'core-js/features/iterator',
+        "eventsource-polyfill"
+      );
     config.module.rules?.forEach(rule =>
       rule.use?.forEach(u => {
         // eslint-disable-next-line node/no-extraneous-require
         if (u.loader === require.resolve('css-loader'))
-          u.options = {
-            modules: {localIdentName: `${hash}`},
-            importLoaders: 1
-          };
+        u.options = {
+          modules: {localIdentName: `${hash}`},
+          importLoaders: 1
+        };
       })
     );
+
     return config;
   }
 };
