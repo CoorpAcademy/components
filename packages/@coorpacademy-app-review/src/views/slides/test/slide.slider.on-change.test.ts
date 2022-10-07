9b8424d32a3db2693dedae145ce704752c00795b
Joan <jomaora@gmail.com>
2022-10-07 15:25:45 +0200

    force rank to have initial values

--- packages/@coorpacademy-app-review/src/views/slides/test/slide.slider.on-change.test.ts
+++ packages/@coorpacademy-app-review/src/views/slides/test/slide.slider.on-change.test.ts
@@ -42,7 +42,7 @@ const initialState: StoreState = {
     skills: [],
     token: '1234',
     corrections: {},
-    rank: {}
+    rank: {start: 10, end: Number.NaN}
   },
   ui: {
     showCongrats: false,
--- packages/@coorpacademy-app-review/src/views/slides/test/slide.template.on-change.test.ts
+++ packages/@coorpacademy-app-review/src/views/slides/test/slide.template.on-change.test.ts
@@ -43,7 +43,7 @@ const initialState: StoreState = {
     skills: [],
     token: '1234',
     corrections: {},
-    rank: {}
+    rank: {start: 10, end: Number.NaN}
   },
   ui: {
     showCongrats: false,
