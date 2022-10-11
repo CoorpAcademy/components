e27cee3751a7545de46bd9dbcb1f251c3d91bf31
EmelineLeduc <leduc.emeline44@gmail.com>
2022-10-11 11:13:52 +0200

    add location.reload in onQuitClick function (in appOptions of sandbox)

--- packages/@coorpacademy-app-review/sandbox/index.tsx
+++ packages/@coorpacademy-app-review/sandbox/index.tsx
@@ -36,8 +36,7 @@ const createSandbox = (options: SandboxOptions): void => {
       skillRef: '123',
       services,
       onQuitClick: () => {
-        // eslint-disable-next-line no-console
-        console.log('onQuitClick');
+        location.reload();
       },
       url: process.env.LAMBDA_API_REVIEW_GET_SLIDES_URL || 'http://localhost:7006'
     };
