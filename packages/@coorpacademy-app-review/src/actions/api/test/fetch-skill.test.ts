06bc86bfbde4b7409eeb28c6c79f98e754a03892
Antonia Balluais <antonia.balluais@coorpacademy.com>
2022-10-26 18:01:18 +0200

    add action test for fetchSkill

--- packages/@coorpacademy-app-review/src/actions/api/test/fetch-skill.test.ts
+++ packages/@coorpacademy-app-review/src/actions/api/test/fetch-skill.test.ts
@@ -1,6 +1,53 @@
-import {Skill} from '../../../types/common';
+import test from 'ava';
+import {createTestStore} from '../../test/create-test-store';
+import {Services} from '../../../types/common';
+import {services as mockedServices} from '../../../test/util/services.mock';
+import type {StoreState} from '../../../reducers';
+import {
+  fetchSkill,
+  SKILL_FETCH_FAILURE,
+  SKILL_FETCH_REQUEST,
+  SKILL_FETCH_SUCCESS
+} from '../fetch-skill';
 
-const initialState: Skill = {
-  ref: 'skill_NyxtYFYir',
-  name: 'Digital Awareness'
+const initialState: StoreState = {
+  data: {
+    progression: null,
+    slides: {},
+    skills: [],
+    token: '1234',
+    corrections: {},
+    rank: {start: Number.NaN, end: Number.NaN}
+  },
+  ui: {
+    showCongrats: false,
+    positions: [0, 1, 2, 3, 4],
+    currentSlideRef: '',
+    navigation: [],
+    answers: {},
+    slide: {},
+    showQuitPopin: false,
+    showButtonRevising: false
+  }
 };
+
+test('should dispatch FETCH_SKILL_SUCCESS when fetch skill is call with the correct skill', async t => {
+  t.plan(4);
+  const services: Services = {
+    ...mockedServices,
+    fetchSkill: (skillRef, token) => {
+      t.is(token, '1234');
+      t.is(skillRef, 'skill_NyxtYFYir');
+      return Promise.resolve({ref: 'skill_NyxtYFYir', name: 'Digital Awareness'});
+    }
+  };
+
+  const expectedActions = [
+    {type: SKILL_FETCH_REQUEST},
+    {type: SKILL_FETCH_SUCCESS, payload: {ref: 'skill_NyxtYFYir', name: 'Digital Awareness'}},
+    {type: SKILL_FETCH_FAILURE, payload: new Error('Fetch skill action failed')}
+  ];
+  const {dispatch} = createTestStore(t, initialState, {services}, expectedActions);
+
+  await dispatch(fetchSkill('skill_NyxtYFYir'));
+});
