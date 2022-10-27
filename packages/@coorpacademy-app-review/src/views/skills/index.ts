048d5da8e05abcbe9ddf8f44385b709a9c1b2392
EmelineLeduc <leduc.emeline44@gmail.com>
2022-10-27 15:21:32 +0200

    update mapStateToSkillsProps and unit test

--- packages/@coorpacademy-app-review/src/views/skills/index.ts
+++ packages/@coorpacademy-app-review/src/views/skills/index.ts
@@ -1,26 +1,31 @@
 import {ReviewSkillsProps} from '@coorpacademy/components/es/template/app-review/skills/prop-types';
 import {StoreState} from '../../reducers';
+import type {ConnectedOptions} from '../../types/common';
 
-export const mapStateToSkillsProps = (state: StoreState): ReviewSkillsProps => {
+export const mapStateToSkillsProps = (
+  state: StoreState,
+  options: ConnectedOptions
+): ReviewSkillsProps => {
+  const {translate} = options;
   return {
     'aria-label': '@todo aria-label',
     title: '@todo title',
     isLoading: false,
-    isLoadingAriaLabel: '@todo loading',
+    isLoadingAriaLabel: 'Review skills container is loading',
     listSkills: state.data.skills.map(skill => ({
-      'aria-label': '@todo aria-label skill',
+      'aria-label': translate('Skill Card'),
       isCustom: skill.custom,
       skillTitle: skill.name,
-      skillAriaLabel: '@todo skill aria label',
-      buttonLabel: '@todo button',
-      buttonAriaLabel: '@todo button aria label',
-      reviseLabel: '@todo revise',
-      reviseAriaLabel: '@todo revise aria label',
+      skillAriaLabel: skill.name,
+      buttonLabel: translate('Review this skill'),
+      buttonAriaLabel: translate('Review this skill'),
+      reviseLabel: translate('{{count}} questions to review', {count: skill.slidesToReview}),
+      reviseAriaLabel: translate('{{count}} questions to review', {count: skill.slidesToReview}),
       // eslint-disable-next-line no-console
       onClick: () => console.log('@todo plug dispatcher select skill')
     })),
-    titleNoSkills: '@todo title no skills',
-    textNoSkills: '@todo text no skills',
-    iconSkillAriaLabel: '@todo iconSkillAriaLabel'
+    titleNoSkills: translate('No skill to revise'),
+    textNoSkills: translate('Complete courses before revising'),
+    iconSkillAriaLabel: 'Image without information'
   };
 };
