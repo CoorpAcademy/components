d66a894201ca7020d15cfe8f62fe53dc133d07a4
EmelineLeduc <leduc.emeline44@gmail.com>
2022-10-27 14:14:03 +0200

    fix merge coflicts

--- packages/@coorpacademy-components/src/molecule/skill-card/prop-types.ts
+++ packages/@coorpacademy-components/src/molecule/skill-card/prop-types.ts
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-export default {
+const propTypes = {
   'aria-label': PropTypes.string,
   skillTitle: PropTypes.string,
   skillAriaLabel: PropTypes.string,
@@ -10,4 +10,18 @@ export default {
   reviseAriaLabel: PropTypes.string,
   isCustom: PropTypes.bool,
   onClick: PropTypes.func
+};
+
+export default propTypes;
+
+export type SkillCardProps = {
+  'aria-label': string;
+  skillTitle: string;
+  skillAriaLabel: string;
+  buttonLabel: string;
+  buttonAriaLabel: string;
+  reviseLabel: string;
+  reviseAriaLabel: string;
+  isCustom: boolean;
+  onClick: () => void;
 };
