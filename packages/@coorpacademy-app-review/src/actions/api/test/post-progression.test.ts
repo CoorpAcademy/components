64488a2cdba223a8eb1f19057c8b869312b482c7
Antonia Balluais <antonia.balluais@coorpacademy.com>
2022-10-31 12:41:45 +0100

    updat post progression and view/slides unit tests

--- packages/@coorpacademy-app-review/src/actions/api/test/post-progression.test.ts
+++ packages/@coorpacademy-app-review/src/actions/api/test/post-progression.test.ts
@@ -12,6 +12,7 @@ import type {StoreState} from '../../../reducers';
 import {SLIDE_FETCH_REQUEST, SLIDE_FETCH_SUCCESS} from '../fetch-slide';
 import {SET_CURRENT_SLIDE} from '../../ui/slides';
 import {createTestStore} from '../../test/create-test-store';
+import {SKILL_FETCH_FAILURE, SKILL_FETCH_REQUEST, SKILL_FETCH_SUCCESS} from '../fetch-skill';
 
 const initialState: StoreState = {
   data: {
@@ -35,7 +36,7 @@ const initialState: StoreState = {
 };
 
 test('should dispatch POST_PROGRESSION_SUCCESS and SLIDE_FETCH_REQUEST actions when postProgression returns a progression', async t => {
-  t.plan(9);
+  t.plan(11);
   const progression: ProgressionFromAPI = {
     _id: '62b1d1087aa12f00253f40ee',
     content: {
@@ -65,7 +66,7 @@ test('should dispatch POST_PROGRESSION_SUCCESS and SLIDE_FETCH_REQUEST actions w
     ...mockedServices,
     postProgression: (skillRef, token) => {
       t.is(token, '1234');
-      t.is(skillRef, 'skill_12345');
+      t.is(skillRef, 'skill_NyxtYFYir');
       return Promise.resolve(progression);
     },
     fetchSlide: (slideRef, token) => {
@@ -94,12 +95,14 @@ test('should dispatch POST_PROGRESSION_SUCCESS and SLIDE_FETCH_REQUEST actions w
       },
       payload: freeTextSlide
     },
-    {type: SET_CURRENT_SLIDE, payload: freeTextSlide}
+    {type: SET_CURRENT_SLIDE, payload: freeTextSlide},
+    {type: SKILL_FETCH_REQUEST},
+    {type: SKILL_FETCH_SUCCESS, payload: {ref: 'skill_NyxtYFYir', name: 'Digital Awareness'}}
   ];
 
   const {dispatch} = createTestStore(t, initialState, {services}, expectedActions);
 
-  await dispatch(postProgression('skill_12345'));
+  await dispatch(postProgression('skill_NyxtYFYir'));
 });
 
 test('should dispatch POST_PROGRESSION_FAILURE action when postProgression fails', async t => {
@@ -119,7 +122,9 @@ test('should dispatch POST_PROGRESSION_FAILURE action when postProgression fails
       type: POST_PROGRESSION_FAILURE,
       payload: new Error('error'),
       error: true
-    }
+    },
+    {type: SKILL_FETCH_REQUEST},
+    {type: SKILL_FETCH_FAILURE, payload: new Error('Fetch skill action failed'), error: true}
   ];
 
   const {dispatch} = createTestStore(t, initialState, {services}, expectedActions);
