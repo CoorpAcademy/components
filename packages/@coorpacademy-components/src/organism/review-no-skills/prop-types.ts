ed6fd0696d33380cc26acbb17d83424c6992da9d
EmelineLeduc <leduc.emeline44@gmail.com>
2022-10-31 16:12:23 +0100

    create organism ReviewNoSkills (for web)

--- packages/@coorpacademy-components/src/organism/review-no-skills/prop-types.ts
+++ packages/@coorpacademy-components/src/organism/review-no-skills/prop-types.ts
@@ -1,3 +1,13 @@
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  titleNoSkills: PropTypes.string,
+  textNoSkills: PropTypes.string,
+  iconSkillAriaLabel: PropTypes.string
+};
+
+export default propTypes;
+
 export type NoSkillsProps = {
   titleNoSkills?: string;
   textNoSkills?: string;
