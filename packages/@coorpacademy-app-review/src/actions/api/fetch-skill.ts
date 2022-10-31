6a8aaafe47855b74dcb3b1fbff3f89c24abfbc1c
Antonia Balluais <antonia.balluais@coorpacademy.com>
2022-10-31 12:53:23 +0100

    update fetchSkill action name

--- packages/@coorpacademy-app-review/src/actions/api/fetch-skill.ts
+++ packages/@coorpacademy-app-review/src/actions/api/fetch-skill.ts
@@ -8,14 +8,14 @@ export const SKILL_FETCH_REQUEST = '@@skill/FETCH_REQUEST' as const;
 export const SKILL_FETCH_SUCCESS = '@@skill/FETCH_SUCCESS' as const;
 export const SKILL_FETCH_FAILURE = '@@skill/FETCH_FAILURE' as const;
 
-export type fetchSkillAction = {
+export type ReceivedSkill = {
   type: typeof SKILL_FETCH_SUCCESS;
   payload: Skill;
 };
 
 export const fetchSkill =
   (skillRef: string) =>
-  (dispatch: Dispatch, getState: () => StoreState, {services}: ThunkOptions): fetchSkillAction => {
+  (dispatch: Dispatch, getState: () => StoreState, {services}: ThunkOptions): ReceivedSkill => {
     const action = buildTask({
       types: [SKILL_FETCH_REQUEST, SKILL_FETCH_SUCCESS, SKILL_FETCH_FAILURE],
       task: () => {
