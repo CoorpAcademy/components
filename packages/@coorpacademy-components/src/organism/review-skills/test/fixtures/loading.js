deb731185653e4a7b437ec4455b5fa0c0a124d8a
EmelineLeduc <leduc.emeline44@gmail.com>
2022-10-31 16:30:15 +0100

    delete loading and no-skills fixtures in organism ReviewSkills

--- packages/@coorpacademy-components/src/organism/review-skills/test/fixtures/loading.js
+++
@@ -1,8 +0,0 @@
-export default {
-  props: {
-    'aria-label': 'Review Skills Container',
-    title: 'Skills you can revise',
-    isLoading: true,
-    isLoadingAriaLabel: 'Review skills container is loading'
-  }
-};
