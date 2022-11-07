07e17a1bc7095d1ae61a94d245ddaa85337528c0
Antonia Balluais <antonia.balluais@coorpacademy.com>
2022-11-07 11:12:07 +0100

    update fetchSkill service

--- packages/@coorpacademy-app-review/src/services/fetch-skill.ts
+++ packages/@coorpacademy-app-review/src/services/fetch-skill.ts
@@ -3,10 +3,11 @@ import decode from 'jwt-decode';
 import {JWT, Skill} from '../types/common';
 import {toJSON} from './tools/fetch-responses';
 
-export const fetchSkill = async (skillRef: string, token: string): Promise<Skill> => {
+export const fetchSkill = async (skillRef: string, token: string): Promise<Skill | void> => {
   const {host}: JWT = decode(token);
   const response = await crossFetch(`${host}/api/v2/skills?conditions={"ref":"${skillRef}"}`, {
     headers: {authorization: token}
   });
-  return toJSON<Skill>(response);
+  const skills = await toJSON<Skill[]>(response);
+  return skills[0];
 };
