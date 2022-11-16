4c8063df9c7387deb2abc0381ccba8abcc4aa023
william.scalabre <william.scalabre@gmail.com>
2022-11-16 17:19:25 +0100

    fix linting issue

--- packages/@coorpacademy-app-review/src/types/globals.d.ts
+++ packages/@coorpacademy-app-review/src/types/globals.d.ts
@@ -1,6 +1,6 @@
-declare module "*.css";
-declare module "browser-env";
-declare module "@coorpacademy/components/es/*";
-declare module "@coorpacademy/redux-task";
-declare module "@coorpacademy/translate";
-declare module "@coorpacademy/progression-engine";
+declare module '*.css';
+declare module 'browser-env';
+declare module '@coorpacademy/components/es/*';
+declare module '@coorpacademy/redux-task';
+declare module '@coorpacademy/translate';
+declare module '@coorpacademy/progression-engine';
