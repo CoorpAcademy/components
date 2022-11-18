80d82ad1ebedf54a9a2234001fa010a0e4ea3690
Anto Ballu <113359769+antonia2929@users.noreply.github.com>
2022-11-18 14:18:47 +0100

    Change CTA next question color in CorrectionPopinProps (#2495)
    * replace cm_grey_800 by cm_grey_600 in colors palette
    
    * apply new color in cta button
    
    * use color in native podcast theme

--- packages/@coorpacademy-components/src/variables/colors.ts
+++ packages/@coorpacademy-components/src/variables/colors.ts
@@ -2,10 +2,12 @@ type Colors = {
   gray: string;
   negative: string;
   positive: string;
+  cm_grey_800: string;
 };
 
 export const COLORS: Colors = {
   gray: '#EAEAEB',
   negative: '#ed3436',
-  positive: '#35CC7F'
+  positive: '#35CC7F',
+  cm_grey_800: '#171721'
 };
