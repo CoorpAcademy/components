867f4f455e111f06a104908456484fbfdf83ef08
EmelineLeduc <leduc.emeline44@gmail.com>
2022-11-23 17:51:22 +0100

    add klf button

--- packages/@coorpacademy-components/src/molecule/review-correction-popin/index.native.tsx
+++ packages/@coorpacademy-components/src/molecule/review-correction-popin/index.native.tsx
@@ -1,17 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {TextStyle, StyleSheet, View, ViewStyle} from 'react-native';
 import RenderHTML from 'react-native-render-html';
-import {NovaCompositionCoorpacademyCheck as RightIcon} from '@coorpacademy/nova-icons';
-import {COLORS} from '../../variables/colors';
+import {
+  NovaCompositionCoorpacademyCheck as RightIcon,
+  NovaSolidStatusClose as WrongIcon,
+  NovaLineLoginKey1 as KlfIcon
+} from '@coorpacademy/nova-icons';
 import {Theme} from '../../variables/theme.native';
 import Text from '../../atom/text/index.native';
 import Touchable from '../../hoc/touchable/index.native';
 import {useTemplateContext} from '../../template/app-review/template-context';
 import {ReviewCorrectionPopinProps} from './prop-types';
 
-type StyleSheetType = {
+interface StyleSheetType {
   wrapper: ViewStyle;
-  popinRight: ViewStyle;
+  popin: ViewStyle;
   correctionSection: ViewStyle;
   iconCircle: ViewStyle;
   icon: ViewStyle;
@@ -21,23 +24,31 @@ type StyleSheetType = {
   label: TextStyle;
   button: ViewStyle;
   buttonText: TextStyle;
-};
-const createStyleSheet = (theme: Theme): StyleSheetType =>
+  buttonKlf: ViewStyle;
+  buttonKlfText: TextStyle;
+  iconKey: ViewStyle;
+  containerTooltip: ViewStyle;
+  textTooltip: TextStyle;
+  containerButtonKlf: ViewStyle;
+  triangleTooltip: ViewStyle;
+}
+
+const createStyleSheet = (theme: Theme, type: string): StyleSheetType =>
   StyleSheet.create({
     wrapper: {
       height: 'auto',
       color: theme.colors.white,
       display: 'flex'
     },
-    popinRight: {
+    popin: {
       padding: theme.spacing.medium,
-      borderRadius: 16, // TODO
+      borderRadius: 16,
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'flex-start',
-      backgroundColor: theme.colors.positive,
+      backgroundColor: type === 'wrong' ? theme.colors.negative : theme.colors.positive,
       shadowRadius: 28,
-      shadowColor: theme.colors.positive,
+      shadowColor: type === 'wrong' ? theme.colors.negative : theme.colors.positive,
       shadowOpacity: 0.4
     },
     correctionSection: {
@@ -97,8 +108,102 @@ const createStyleSheet = (theme: Theme): StyleSheetType =>
       fontWeight: theme.fontWeight.bold,
       lineHeight: 20,
       textAlign: 'center'
+    },
+    containerButtonKlf: {
+      width: '100%'
+    },
+    buttonKlf: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'rgba(255, 255, 255, 0.1)',
+      borderRadius: 7,
+      paddingHorizontal: theme.spacing.base,
+      paddingVertical: theme.spacing.small,
+      marginBottom: theme.spacing.tiny
+    },
+    buttonKlfText: {
+      color: 'white',
+      fontSize: 14,
+      fontWeight: theme.fontWeight.bold,
+      lineHeight: 20,
+      textAlign: 'center'
+    },
+    iconKey: {
+      width: 12,
+      height: 12,
+      marginRight: theme.spacing.tiny
+    },
+    containerTooltip: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundColor: theme.colors.white,
+      borderRadius: 15,
+      padding: theme.spacing.small,
+      width: '112%',
+      position: 'absolute',
+      bottom: 4,
+      right: -15
+    },
+    textTooltip: {
+      color: theme.colors.text.primary,
+      fontSize: 16,
+      fontWeight: '600',
+      lineHeight: 22,
+      textAlign: 'center'
+    },
+    triangleTooltip: {
+      width: 0,
+      height: 0,
+      backgroundColor: 'transparent',
+      borderStyle: 'solid',
+      borderLeftWidth: 20,
+      borderRightWidth: 20,
+      borderBottomWidth: 20,
+      borderLeftColor: 'transparent',
+      borderRightColor: 'transparent',
+      borderBottomColor: theme.colors.white,
+      transform: [{rotate: '180deg'}],
+      position: 'absolute',
+      left: 105,
+      top: -12
     }
   });
+
+const KlfButton = (klf: ReviewCorrectionPopinProps['klf'], styleSheet: StyleSheetType) => {
+  const {
+    buttonKlf,
+    buttonKlfText,
+    containerButtonKlf,
+    containerTooltip,
+    iconKey,
+    textTooltip,
+    triangleTooltip
+  } = styleSheet;
+  const {label} = klf;
+
+  return (
+    <View style={containerButtonKlf}>
+      <View>
+        <Touchable
+          style={containerTooltip}
+          accessibilityLabel={`aria-label-tooltip`}
+          testID="tooltip"
+        >
+          <Text style={textTooltip}>
+            17 frustrated software engineers grappling with the complexities of software
+            development.
+          </Text>
+        </Touchable>
+        <View style={triangleTooltip} />
+      </View>
+      <Touchable style={buttonKlf} accessibilityLabel={`aria-label-${label}`} testID={label}>
+        <KlfIcon style={iconKey} color="white" />
+        <Text style={buttonKlfText}>{label}</Text>
+      </Touchable>
+    </View>
+  );
+};
 
 const ReviewCorrectionPopin = ({
   information,
@@ -114,22 +219,31 @@ const ReviewCorrectionPopin = ({
   const source = {
     html: `<p aria-label='${information.message}' style='color:white; font-size:16; font-weight:600; line-height:19;'>${information.message}</p>`
   };
+  const ICONS = {
+    right: RightIcon,
+    wrong: WrongIcon
+  };
+  const Icon = ICONS[type];
 
   useEffect(() => {
-    const _stylesheet = createStyleSheet(theme);
+    const _stylesheet = createStyleSheet(theme, type);
     setStylesheet(_stylesheet);
   }, [theme]);
 
   if (!styleSheet) {
     return null;
   }
+  const cta = type === 'wrong' ? KlfButton(klf, styleSheet) : null;
 
   return (
     <View style={styleSheet.wrapper}>
-      <View style={styleSheet.popinRight}>
+      <View style={styleSheet.popin}>
         <View style={styleSheet.correctionSection}>
           <View style={styleSheet.iconCircle}>
-            <RightIcon style={styleSheet.icon} color={COLORS.positive} />
+            <Icon
+              style={styleSheet.icon}
+              color={type === 'wrong' ? theme.colors.negative : theme.colors.positive}
+            />
           </View>
           <Text style={styleSheet.resultLabel}>{resultLabel}</Text>
         </View>
@@ -141,6 +255,7 @@ const ReviewCorrectionPopin = ({
           </View>
           <RenderHTML source={source} systemFonts={['Gilroy']} />
         </View>
+        {cta}
         <Touchable
           style={styleSheet.button}
           onPress={handlePressNext}
