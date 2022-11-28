ac2698a92cbb78c02335248f52525db1ad6d3cce
EmelineLeduc <leduc.emeline44@gmail.com>
2022-11-28 12:10:24 +0100

    test onPress event on button "Next Question"

---
+++ packages/@coorpacademy-components/src/molecule/review-correction-popin/test/on-press.test.tsx
@@ -0,0 +1,33 @@
+import test from 'ava';
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import {GestureResponderEvent} from 'react-native';
+import mockedGestureEvent from '../../../test/helpers/mocked-gesture-event';
+import ReviewCorrectionPopin from '../index.native';
+import fixturesRight from './fixtures/right';
+
+test('should handle onPress on next button', t => {
+  t.plan(1);
+
+  const onClick = (event: GestureResponderEvent) => {
+    t.is(event, mockedGestureEvent);
+  };
+
+  const props = fixturesRight.props;
+  const next = {...props.next, onClick};
+
+  const component = (
+    <ReviewCorrectionPopin
+      type="right"
+      information={props.information}
+      next={next}
+      resultLabel={props.resultLabel}
+      klf={{}}
+    />
+  );
+
+  const {getByTestId} = render(component);
+  const buttonNext = getByTestId('next-question-button');
+
+  fireEvent.press(buttonNext, mockedGestureEvent);
+});
