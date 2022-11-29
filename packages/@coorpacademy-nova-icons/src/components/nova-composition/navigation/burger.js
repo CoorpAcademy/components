080e527a47bdcd43f46d06def94518761299015e
Joan Ortega <jomaora@gmail.com>
2022-11-29 08:51:07 -0500

    fix color on new header icon (#2514)

--- packages/@coorpacademy-nova-icons/src/components/nova-composition/navigation/burger.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/navigation/burger.js
@@ -1,18 +1,9 @@
 import * as React from 'react';
 
 const SvgComponent = props => (
-  <svg
-    id="prefix__Calque_1"
-    xmlns="http://www.w3.org/2000/svg"
-    x={0}
-    y={0}
-    viewBox="0 0 16 12"
-    xmlSpace="preserve"
-    {...props}
-  >
-    <style>{'.prefix__st0{fill:#515161}'}</style>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 12" {...props}>
     <path
-      className="prefix__st0"
+      fill="currentColor"
       d="M15 10H1c-.6 0-1 .4-1 1s.4 1 1 1h14c.6 0 1-.4 1-1s-.4-1-1-1zM15 5H1c-.6 0-1 .4-1 1s.4 1 1 1h14c.6 0 1-.4 1-1s-.4-1-1-1zM15 0H1C.4 0 0 .4 0 1s.4 1 1 1h14c.6 0 1-.4 1-1s-.4-1-1-1z"
     />
   </svg>
