bceb637d75c29aa1d9d759224b9636d4093f934f
Joan <jomaora@gmail.com>
2022-11-29 08:30:50 -0500

    fix color on new header icon

--- packages/@coorpacademy-nova-icons/src/components/nova-composition/navigation/burger.native.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-composition/navigation/burger.native.js
@@ -1,19 +1,10 @@
 import * as React from 'react';
 import Svg, {Path} from 'react-native-svg';
-/* SVGR has dropped some elements not supported by react-native-svg: style */
 
 const SvgComponent = props => (
-  <Svg
-    id="prefix__Calque_1"
-    xmlns="http://www.w3.org/2000/svg"
-    x={0}
-    y={0}
-    viewBox="0 0 16 12"
-    xmlSpace="preserve"
-    {...props}
-  >
+  <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 12" {...props}>
     <Path
-      className="prefix__st0"
+      fill={props.color}
       d="M15 10H1c-.6 0-1 .4-1 1s.4 1 1 1h14c.6 0 1-.4 1-1s-.4-1-1-1zM15 5H1c-.6 0-1 .4-1 1s.4 1 1 1h14c.6 0 1-.4 1-1s-.4-1-1-1zM15 0H1C.4 0 0 .4 0 1s.4 1 1 1h14c.6 0 1-.4 1-1s-.4-1-1-1z"
     />
   </Svg>
