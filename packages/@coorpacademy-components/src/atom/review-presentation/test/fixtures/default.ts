1c6fc0ce6ff29e9978a5f27db1344104ad1906a2
Antonia Balluais <antonia.balluais@coorpacademy.com>
2022-12-28 16:20:01 +0100

    add information details area label in default fixtures

--- packages/@coorpacademy-components/src/atom/review-presentation/test/fixtures/default.ts
+++ packages/@coorpacademy-components/src/atom/review-presentation/test/fixtures/default.ts
@@ -4,13 +4,26 @@ export const defaultProps = {
   reviewText:
     'Egestas elementum duis bibendum velit tempus nulla arcu eget volutpat.<br/><br/>Diam tincidunt lacus, cras ullamcorper diam, sit tincidunt fames. Maecenas eros, in mollis gravida ut neque, integer a. Vel et tellus dui in integer.',
   labelsList: {
-    skills: {text: 'Choose 1 Skill', tooltipText: 'This is the tooltip text'},
-    questions: {text: 'Answer 5 Questions', tooltipText: 'This is the tooltip text'},
+    skills: {
+      text: 'Choose 1 Skill',
+      tooltipText: 'This is the tooltip text',
+      moreDetailsAriaLabel: 'More details'
+    },
+    questions: {
+      text: 'Answer 5 Questions',
+      tooltipText: 'This is the tooltip text',
+      moreDetailsAriaLabel: 'More details'
+    },
     lifes: {
       text: 'You have Infinite Lifes',
-      tooltipText: 'This is the tooltip text, a tooltip text'
+      tooltipText: 'This is the tooltip text, a tooltip text',
+      moreDetailsAriaLabel: 'More details'
     },
-    allright: {text: 'Get it all right', tooltipText: 'Egestas elementum duis bibendum'}
+    allright: {
+      text: 'Get it all right',
+      tooltipText: 'Egestas elementum duis bibendum',
+      moreDetailsAriaLabel: 'More details'
+    }
   }
 };
 
