5487626b943b3e6653230f248146d83c9afa10f4
EmelineLeduc <leduc.emeline44@gmail.com>
2023-01-11 14:42:10 +0100

    delete tooltip next to the title in dashboard

--- packages/@coorpacademy-components/src/template/app-review/skills/index.js
+++ packages/@coorpacademy-components/src/template/app-review/skills/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import isEmpty from 'lodash/fp/isEmpty';
-import {NovaCompositionCoorpacademyInformationIcon as InformationIcon} from '@coorpacademy/nova-icons';
 import Loader from '../../../atom/loader';
 import ReviewListSkills from '../../../organism/review-skills';
 import ReviewNoSkills from '../../../organism/review-no-skills';
@@ -21,10 +20,7 @@ const ReviewSkills = props => {
 
   return (
     <div className={style.containerReviewSkill} aria-label={ariaLabel}>
-      <div className={style.title}>
-        {title}
-        <InformationIcon className={style.informationIcon} width={12} height={12} />
-      </div>
+      <div className={style.title}>{title}</div>
       {isLoading ? (
         <div className={style.loaderContainer}>
           <Loader className={style.loader} theme="default" aria-label={isLoadingAriaLabel} />
