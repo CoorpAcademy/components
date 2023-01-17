3a6cddba95de937919065bff90d41d2ee6ee4921
Antonia Balluais <antonia.balluais@coorpacademy.com>
2023-01-17 23:45:09 +0100

    add contextTypes to review header step item types

--- packages/@coorpacademy-components/src/atom/review-header-step-item/types.ts
+++ packages/@coorpacademy-components/src/atom/review-header-step-item/types.ts
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
+import Provider from '../provider';
 
 const propTypes = {
   current: PropTypes.bool,
   icon: PropTypes.oneOf(['no-answer', 'right', 'wrong']),
   value: PropTypes.string
+};
+
+export const contextTypes = {
+  translate: PropTypes.func
 };
 
 export default propTypes;
@@ -13,6 +18,8 @@ export type HeaderStepItemProps = {
   icon: 'no-answer' | 'right' | 'wrong';
   value: string;
 };
+
+export type HeaderStepItemContextTypes = {translate: typeof Provider.childContextTypes.translate};
 
 export type Fixture = {
   props: HeaderStepItemProps;
