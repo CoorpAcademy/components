e24c27c05b4a8fe6740cc0fc095174bbe0cd4526
Stefano Acosta <33550261+StefanoA1@users.noreply.github.com>
2023-01-25 10:09:43 +0100

    [RGAA] Activity (tooltip): Progression && StarsSummary from class to functional components (#2598)
    * Refactors Activity: Progression && StarsSummary
    
    * Stars Summary Refactor

--- packages/@coorpacademy-components/src/template/activity/stars-summary.js
+++ packages/@coorpacademy-components/src/template/activity/stars-summary.js
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import PropTypes from 'prop-types';
 import {get, getOr, isEmpty, map} from 'lodash/fp';
 import {
@@ -6,26 +6,34 @@ import {
   NovaCompositionNavigationArrowRight as ArrowRight,
   NovaCompositionCoorpacademyStar as StarIcon
 } from '@coorpacademy/nova-icons';
-import Provider from '../../atom/provider';
+import Provider, {GetSkinFromContext} from '../../atom/provider';
 import EngineStars from './engine-stars';
 import style from './stars-summary.css';
 
-const EngineTab = ({engine, engineIndex, firstItem}) => {
+const EngineTab = ({engine, engineIndex, firstItemIndex}) => {
   const {type} = engine;
-  const state = engineIndex < firstItem ? 'hidden' : 'active';
+  const state = useMemo(
+    () => (engineIndex < firstItemIndex ? 'hidden' : 'active'),
+    [engineIndex, firstItemIndex]
+  );
+  const dataName = useMemo(() => `${type}_total_${state}`, [state, type]);
+
   return (
-    <li className={style[state]} key={type} data-name={`${type}_total_${state}`}>
-      <EngineStars {...engine} className={engineIndex < firstItem ? style.hidden : style.active} />
+    <li className={style[state]} key={type} data-name={dataName}>
+      <EngineStars
+        {...engine}
+        className={engineIndex < firstItemIndex ? style.hidden : style.active}
+      />
     </li>
   );
 };
 EngineTab.propTypes = {
   engine: PropTypes.shape(EngineStars.propTypes),
   engineIndex: PropTypes.number,
-  firstItem: PropTypes.number
+  firstItemIndex: PropTypes.number
 };
 
-const EngineTabs = ({engines, firstItem}) => {
+const EngineTabs = ({engines, firstItemIndex}) => {
   return useMemo(
     () =>
       map.convert({cap: false})(
@@ -33,114 +41,114 @@ const EngineTabs = ({engines, firstItem}) => {
           <EngineTab
             engine={engine}
             key={`engineTab_${index}`}
-            firstItem={firstItem}
+            firstItemIndex={firstItemIndex}
             engineIndex={index}
           />
         ),
         engines
       ),
-    [engines, firstItem]
+    [engines, firstItemIndex]
   );
 };
 
 EngineTabs.propTypes = {
   engines: PropTypes.arrayOf(PropTypes.shape(EngineStars.propTypes)),
-  firstItem: PropTypes.number
+  firstItemIndex: PropTypes.number
 };
 
-class StarsSummary extends React.Component {
-  static propTypes = {
-    total: PropTypes.shape({
-      label: PropTypes.string.isRequired,
-      stars: PropTypes.number.isRequired
-    }).isRequired,
-    engines: PropTypes.arrayOf(PropTypes.shape(EngineStars.propTypes))
-  };
+const StarsSummary = (props, legacyContext) => {
+  const {total, engines} = props;
+  const skin = GetSkinFromContext(legacyContext);
 
-  static contextTypes = {
-    skin: Provider.childContextTypes.skin
-  };
+  const [firstItemIndex, setFirstItemIndex] = useState(0);
+  const [totalItems] = useState(engines.length);
 
-  constructor(props) {
-    super(props);
+  const dark = getOr('#90A4AE', 'common.dark', skin);
+  const primary = get('common.primary', skin);
 
-    const {engines = []} = props;
-    this.state = {
-      firstItem: 0,
-      totalItems: engines.length
-    };
+  const scrollTo = useCallback(page => setFirstItemIndex(page), []);
 
-    this.handleOnLeft = this.handleOnLeft.bind(this);
-    this.handleOnRight = this.handleOnRight.bind(this);
-    this.scrollTo = this.scrollTo.bind(this);
+  const handleOnLeft = useCallback(() => {
+    scrollTo(firstItemIndex - 1);
+  }, [firstItemIndex, scrollTo]);
+
+  const handleOnRight = useCallback(() => {
+    scrollTo(firstItemIndex + 1);
+  }, [firstItemIndex, scrollTo]);
+
+  if (isEmpty(engines)) {
+    return null;
   }
 
-  handleOnLeft() {
-    const {firstItem} = this.state;
-    this.scrollTo(firstItem - 1);
-  }
+  const leftArrowView =
+    totalItems > 6 && firstItemIndex > 0 ? (
+      <div
+        className={style.circle}
+        onClick={handleOnLeft}
+        data-name="left-arrow"
+        data-testid="stars-summary-left-arrow"
+      >
+        <ArrowLeft color={dark} className={style.left} width={10} height={10} />
+      </div>
+    ) : null;
 
-  handleOnRight() {
-    const {firstItem} = this.state;
-    this.scrollTo(firstItem + 1);
-  }
+  const rightArrowView =
+    totalItems > 6 && firstItemIndex < totalItems - 6 ? (
+      <div
+        className={style.circle}
+        onClick={handleOnRight}
+        data-name="right-arrow"
+        data-testid="stars-summary-right-arrow"
+      >
+        <ArrowRight color={dark} className={style.right} width={10} height={10} />
+      </div>
+    ) : null;
 
-  scrollTo(page) {
-    this.setState({
-      firstItem: page
-    });
-  }
-
-  render() {
-    const {total, engines = []} = this.props;
-    const {skin} = this.context;
-    const {firstItem, totalItems} = this.state;
-    const dark = getOr('#90A4AE', 'common.dark', skin);
-    const primary = get('common.primary', skin);
-
-    if (isEmpty(engines)) {
-      return null;
-    }
-    const leftArrowView =
-      totalItems > 6 && firstItem > 0 ? (
-        <div className={style.circle} onClick={this.handleOnLeft} data-name="left-arrow">
-          <ArrowLeft color={dark} className={style.left} width={10} height={10} />
-        </div>
-      ) : null;
-
-    const rightArrowView =
-      totalItems > 6 && firstItem < totalItems - 6 ? (
-        <div className={style.circle} onClick={this.handleOnRight} data-name="right-arrow">
-          <ArrowRight color={dark} className={style.right} width={10} height={10} />
-        </div>
-      ) : null;
-
-    return (
-      <div data-name="myStars" className={style.myStars}>
-        <div data-name="myStars-wrapper" className={style.myStarsWrapper}>
-          <ul className={style.allStars} data-name="engineList">
-            <EngineTabs engines={engines} firstItem={firstItem} />
-          </ul>
-          <div
-            className={style.footerSummaryStars}
-            style={{
-              backgroundColor: primary
-            }}
-          >
-            <div className={style.totalStars}>
-              <span>{total.label}</span>
-              <p data-name="star-counter">{total.stars}</p>
-              <div className={style.iconBubble}>
-                <StarIcon className={style.iconHeader} color={primary} />
-              </div>
+  return (
+    <div data-name="myStars" className={style.myStars}>
+      <div
+        data-name="myStars-wrapper"
+        className={style.myStarsWrapper}
+        data-testid={`stars-summary-engine-index-${firstItemIndex}`}
+      >
+        <ul
+          className={style.allStars}
+          data-name="engineList"
+          data-testid="stars-summary-engine-tabs"
+        >
+          <EngineTabs engines={engines} firstItemIndex={firstItemIndex} />
+        </ul>
+        <div
+          className={style.footerSummaryStars}
+          style={{
+            backgroundColor: primary
+          }}
+        >
+          <div className={style.totalStars}>
+            <span>{total.label}</span>
+            <p data-name="star-counter">{total.stars}</p>
+            <div className={style.iconBubble}>
+              <StarIcon className={style.iconHeader} color={primary} />
             </div>
-            <div className={style.navigationLeft}>{leftArrowView}</div>
-            <div className={style.navigationRight}>{rightArrowView}</div>
           </div>
+          <div className={style.navigationLeft}>{leftArrowView}</div>
+          <div className={style.navigationRight}>{rightArrowView}</div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+StarsSummary.propTypes = {
+  total: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    stars: PropTypes.number.isRequired
+  }).isRequired,
+  engines: PropTypes.arrayOf(PropTypes.shape(EngineStars.propTypes)).isRequired
+};
+
+StarsSummary.contextTypes = {
+  skin: Provider.childContextTypes.skin
+};
 
 export default StarsSummary;
