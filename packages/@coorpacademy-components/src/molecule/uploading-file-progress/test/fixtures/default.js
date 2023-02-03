c5e9030655da5d1cad7326a02feb3793904766ae
youssefezzahi96 <youssef.ezzahi@coorpacademy.com>
2023-02-03 11:59:06 +0100

    add uploading file component

---
+++ packages/@coorpacademy-components/src/molecule/uploading-file-progress/test/fixtures/default.js
@@ -0,0 +1,23 @@
+export default {
+  props: {
+    mainText: 'Upload in Progress ...',
+    subText:
+      'continue working (or go for a coffee) while your files are being uploading, you will be notified when it is over. ',
+    // imageUrl: 'https://static.coorpacademy.com/coorpmanager/bulk-empty-home.svg',
+    progressionValue: 3,
+    leftButtonLink: {
+      type: 'secondary',
+      label: 'Create New Import',
+      ariaLabel: 'Create New Import',
+      dataName: 'default-button',
+      onClick: () => console.log('click')
+    },
+    rightButtonLink: {
+      type: 'primary',
+      label: 'Go to Dashboard',
+      ariaLabel: 'Go to Dashboard',
+      dataName: 'default-button',
+      onClick: () => console.log('click')
+    }
+  }
+};
