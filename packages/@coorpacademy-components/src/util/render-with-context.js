ba9796cf514ecb2b07abf248c281f90ebbe78cb0
stefano <stefanoacosta.92@gmail.com>
2023-02-08 16:03:39 +0100

    update render with context

--- packages/@coorpacademy-components/src/util/render-with-context.js
+++ packages/@coorpacademy-components/src/util/render-with-context.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {render} from '@testing-library/react';
+import has from 'lodash/fp/has';
 import {mockTranslate} from '@coorpacademy/translate';
-import {WebContext} from '../atom/provider';
+import Provider, {WebContext} from '../atom/provider';
 
 export const renderWithContext = (component, options = {}) => {
-  const {context = {}, ...renderOptions} = options;
+  const {context = {}, useLegacyProvider = false, ...renderOptions} = options;
   // uses mockTranslate as a default translate function
   const {translate = mockTranslate, ...rest} = context;
 
-  const wrapper = ({children}) => (
-    <WebContext {...rest} translate={translate}>
-      {children}
-    </WebContext>
-  );
+  const wrapper = ({children}) => {
+    const hasContextTypes = useLegacyProvider || has(['type', 'contextTypes'], children);
+    return hasContextTypes ? (
+      <Provider {...rest} translate={translate}>
+        {children}
+      </Provider>
+    ) : (
+      <WebContext {...rest} translate={translate}>
+        {children}
+      </WebContext>
+    );
+  };
 
   wrapper.propTypes = {
     children: PropTypes.node
