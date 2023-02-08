a18ce22aaed7e1237947632411ab3f2bd13a5ed6
stefano <stefanoacosta.92@gmail.com>
2023-02-08 18:32:32 +0100

    undo

--- packages/@coorpacademy-components/src/atom/link/index.js
+++ packages/@coorpacademy-components/src/atom/link/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {identity, getOr, noop} from 'lodash/fp';
-import Provider, {GetSkinFromContext} from '../provider';
+import Provider from '../provider';
 import pushToHistory from '../../util/navigation';
 
 class Link extends React.Component {
@@ -65,8 +65,7 @@ class Link extends React.Component {
   };
 
   render() {
-    const {history: {createHref = identity} = {}} = this.context;
-    const skin = GetSkinFromContext(this.context);
+    const {skin, history: {createHref = identity} = {}} = this.context;
     const {
       skinHover,
       hoverColor = getOr('#00B0FF', 'common.primary', skin),
