3a8a6f045a96f6e299a4ca5fe77cbe456a89acd5
godu <arthur.weber@viacesi.fr>
2023-03-06 12:30:25 +0100

    Fix hoc animation

--- packages/@coorpacademy-components/src/hoc/transition/index.js
+++ packages/@coorpacademy-components/src/hoc/transition/index.js
@@ -3,31 +3,37 @@ import {noop} from 'lodash/fp';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+const Child = ({child, onAnimationEnd, className, name}) => {
+  const {
+    props: {className: propClassName}
+  } = child;
+
+  const handlerAnimationEnd = useMemo(
+    () => () => {
+      return onAnimationEnd(name);
+    },
+    [onAnimationEnd, name]
+  );
+
+  return React.cloneElement(child, {
+    className: classnames(propClassName, className),
+    onTransitionEnd: handlerAnimationEnd,
+    onAnimationEnd: handlerAnimationEnd
+  });
+};
+
 const Transition = props => {
   const {name, children, className, animated, onAnimationEnd = noop} = props;
 
-  if (!animated) return children;
+  const animatedChildren = useMemo(
+    () =>
+      React.Children.map(children, child => (
+        <Child child={child} onAnimationEnd={onAnimationEnd} className={className} name={name} />
+      )),
+    [children, className, name, onAnimationEnd]
+  );
 
-  const Child = ({child}) => {
-    const {
-      props: {className: propClassName}
-    } = child;
-
-    const handlerAnimationEnd = useMemo(
-      () => () => {
-        return onAnimationEnd(name);
-      },
-      []
-    );
-
-    return React.cloneElement(child, {
-      className: classnames(propClassName, className),
-      onTransitionEnd: handlerAnimationEnd,
-      onAnimationEnd: handlerAnimationEnd
-    });
-  };
-
-  return React.Children.map(children, child => <Child child={child} />);
+  return animated ? animatedChildren : children;
 };
 
 Transition.propTypes = {
