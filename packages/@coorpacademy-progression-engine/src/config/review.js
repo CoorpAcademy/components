3b349571c2ed026183ea127d9a9e609d0a77c282
AdrieanKhisbe <adriean.khisbe@live.fr>
2023-03-07 15:31:20 +0100

    Create a Boosted revision player :orange_book:

--- packages/@coorpacademy-progression-engine/src/config/review.js
+++ packages/@coorpacademy-progression-engine/src/config/review.js
@@ -1,23 +1,30 @@
 // @flow
+import {assign} from 'lodash/fp';
 import type {Config} from '../types';
 
+const CURRENT_BASE_CONFIGURATION = {
+  version: '1',
+  lives: 0,
+  livesDisabled: true,
+  maxTypos: 2,
+  slidesToComplete: 5,
+  shuffleChoices: true,
+  answerBoundaryLimit: 5,
+  starsPerAskingClue: 0,
+  starsPerCorrectAnswer: 8,
+  starsPerResourceViewed: 0,
+  remainingLifeRequests: 0
+};
+
 const configurations: Array<Config> = [
-  {
-    version: '1',
-    lives: 0,
-    livesDisabled: true,
-    maxTypos: 2,
-    slidesToComplete: 5,
-    shuffleChoices: true,
-    answerBoundaryLimit: 5,
-    starsPerAskingClue: 0,
-    starsPerCorrectAnswer: 8,
-    starsPerResourceViewed: 0,
-    remainingLifeRequests: 0
-  }
+  CURRENT_BASE_CONFIGURATION,
+  assign(CURRENT_BASE_CONFIGURATION, {
+    version: '2',
+    starsPerCorrectAnswer: 12
+  })
 ];
 
 export default {
   configurations,
-  defaultConfiguration: configurations[0]
+  defaultConfiguration: CURRENT_BASE_CONFIGURATION
 };
