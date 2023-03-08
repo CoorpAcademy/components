cf0323637cba3f1acf5fdb7bb65da50988708d55
Stefano Acosta <33550261+StefanoA1@users.noreply.github.com>
2023-03-08 15:47:58 +0100

    Favorite (From Card) refactor to function component + coverage (#2679)
    * Favorite (From Card) refactor to function component + coverage
    
    * useMemo

---
+++ packages/@coorpacademy-components/src/molecule/card/test/fixtures/eligible-for-favorite-no-tooltip-message.js
@@ -0,0 +1,21 @@
+import {defaultsDeep} from 'lodash/fp';
+import Default from './default';
+
+const {props} = Default;
+
+export default {
+  props: defaultsDeep(props, {
+    disabled: false,
+    type: 'chapter',
+    image:
+      'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/CoorpAcademy/content-marmiton/cockpit-marmiton/cover/shutterstock_520965634-1491986518210.jpg&h=500&w=500&q=90',
+    progress: 0.1,
+    notification: {
+      message: 'removed from your list',
+      icon: 'removeFavorite'
+    },
+    favorite: true,
+    removeFavoriteToolTip: '',
+    addFavoriteToolTip: ''
+  })
+};
