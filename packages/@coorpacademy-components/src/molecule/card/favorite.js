b29d23c104a41869f7234f1c637cd9c3107c8329
stefano <stefanoacosta.92@gmail.com>
2023-03-09 21:20:21 +0100

    adds ReactTooltip for Favorite (for Catalog's Cards)

--- packages/@coorpacademy-components/src/molecule/card/favorite.js
+++ packages/@coorpacademy-components/src/molecule/card/favorite.js
@@ -1,12 +1,14 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import {get, isEmpty, noop} from 'lodash/fp';
+import {v5 as uuidV5} from 'uuid';
+import {get, has, isEmpty, noop} from 'lodash/fp';
 import {
   NovaCompositionNavigationMore as MoreIcon,
   NovaCompositionCoorpacademyCheck as CheckIcon
 } from '@coorpacademy/nova-icons';
-import Provider, {GetSkinFromContext} from '../../atom/provider';
+import Provider, {GetSkinFromContext, GetTranslateFromContext} from '../../atom/provider';
+import ToolTip, {toggleStateOnKeyPress} from '../../atom/tooltip';
 import style from './favorite.css';
 
 const Favorite = (
@@ -22,6 +24,7 @@ const Favorite = (
   legacyContext
 ) => {
   const skin = GetSkinFromContext(legacyContext);
+  const translate = GetTranslateFromContext(legacyContext);
   const handleFavoriteClick = useCallback(
     e => {
       e.stopPropagation();
@@ -35,46 +38,109 @@ const Favorite = (
   const darkColor = get('common.dark', skin);
   const brandColor = get('common.brand', skin);
 
-  const toolTipView = useMemo(
+  const buttonRef = useRef();
+
+  const allowToolTip = useMemo(
+    () => !isEmpty(removeFavoriteToolTip) && !isEmpty(addFavoriteToolTip),
+    [addFavoriteToolTip, removeFavoriteToolTip]
+  );
+
+  // to be replaced by useId when React17 is bumped to React18
+  const [favoriteButtonAnchorId] = useState(
+    allowToolTip ? uuidV5('engaged-battle-', uuidV5.URL) : undefined
+  );
+
+  const toolTipContent = useMemo(
+    // eslint-disable-next-line no-nested-ternary
+    () => (allowToolTip ? (favorite ? removeFavoriteToolTip : addFavoriteToolTip) : null),
+    [addFavoriteToolTip, favorite, removeFavoriteToolTip, allowToolTip]
+  );
+
+  const [mouseLeaveTimer, setMouseLeaveTimer] = useState(undefined);
+
+  const [toolTipIsVisible, setToolTipIsVisible] = useState(false);
+
+  const handleMouseOver = useCallback(() => {
+    mouseLeaveTimer && clearTimeout(mouseLeaveTimer);
+    /* istanbul ignore next */ // has(['current', 'focus'], buttonRef) && buttonRef.current.focus();
+    setToolTipIsVisible(true);
+  }, [mouseLeaveTimer]);
+
+  const handleMouseLeave = useCallback(() => {
+    setMouseLeaveTimer(
+      setTimeout(() => {
+        setToolTipIsVisible(false);
+        // @ts-expect-error (error: blur does not exists on type never)
+        /* istanbul ignore next */ has(['current', 'blur'], buttonRef) && buttonRef.current.blur();
+      }, 500)
+    );
+  }, []);
+
+  const handleKeyPress = useCallback(
+    event => {
+      toggleStateOnKeyPress(toolTipIsVisible, setToolTipIsVisible, buttonRef)(event);
+    },
+    [toolTipIsVisible]
+  );
+
+  const iconWrapperClassName = useMemo(
+    () => classnames(style.favorite, className, favorite && style.selected),
+    [className, favorite]
+  );
+  const icon = useMemo(
     () =>
-      !isEmpty(removeFavoriteToolTip) && !isEmpty(addFavoriteToolTip) ? (
-        <div className={style.showToolTip}>
-          <span>{favorite ? removeFavoriteToolTip : addFavoriteToolTip}</span>
-        </div>
-      ) : null,
-    [addFavoriteToolTip, favorite, removeFavoriteToolTip]
+      favorite ? (
+        <CheckIcon
+          className={style.checkIcon}
+          style={{color: brandColor}}
+          width={13}
+          height={13}
+          aria-label={ariaLabel.removeFromFavorite}
+        />
+      ) : (
+        <MoreIcon
+          className={style.moreIcon}
+          style={{color: darkColor}}
+          width={13}
+          height={13}
+          aria-label={ariaLabel.addToFavorite}
+        />
+      ),
+    [ariaLabel.addToFavorite, ariaLabel.removeFromFavorite, brandColor, darkColor, favorite]
   );
 
   return (
-    <div className={style.blocFavorite}>
-      {toolTipView}
+    <div className={style.favoriteWrapper}>
+      {/* {toolTipView} */}
       <div
+        ref={buttonRef}
+        data-for={favoriteButtonAnchorId}
+        data-tooltip-place="left"
+        data-tip={allowToolTip}
         data-name="favorite"
         aria-label={ariaLabel.favorite}
-        className={classnames(style.favorite, className, favorite && style.selected)}
+        className={iconWrapperClassName}
         onClick={handleFavoriteClick}
         style={{
           color: primaryColor
         }}
+        onKeyDown={handleKeyPress}
+        onMouseLeave={handleMouseLeave}
+        onMouseOver={handleMouseOver}
+        tabIndex={0}
       >
-        {favorite ? (
-          <CheckIcon
-            className={style.checkIcon}
-            style={{color: brandColor}}
-            width={13}
-            height={13}
-            aria-label={ariaLabel.removeFromFavorite}
-          />
-        ) : (
-          <MoreIcon
-            className={style.moreIcon}
-            style={{color: darkColor}}
-            width={13}
-            height={13}
-            aria-label={ariaLabel.addToFavorite}
-          />
-        )}
+        {icon}
       </div>
+      <ToolTip
+        fontSize={12}
+        anchorId={favoriteButtonAnchorId}
+        toolTipIsVisible={toolTipIsVisible}
+        TooltipContent={toolTipContent}
+        closeToolTipInformationTextAriaLabel={translate(
+          'Press the escape key to close the information text'
+        )}
+        stickyOnKeyboardNavigation
+      />
     </div>
   );
 };
@@ -94,7 +160,8 @@ Favorite.propTypes = {
 };
 
 Favorite.contextTypes = {
-  skin: Provider.childContextTypes.skin
+  skin: Provider.childContextTypes.skin,
+  translate: Provider.childContextTypes.translate
 };
 
 export default Favorite;
