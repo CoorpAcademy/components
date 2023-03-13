b98c24f538b59d7cdfa4f8f379ad35107a1dc52c
Joan <jomaora@gmail.com>
2023-03-13 11:08:21 +0100

    Adding locale to querystring on post answer function

--- packages/@coorpacademy-review-services/src/post-answer.ts
+++ packages/@coorpacademy-review-services/src/post-answer.ts
@@ -3,26 +3,30 @@ import decode from 'jwt-decode';
 
 import {JWT, ProgressionFromAPI} from './types/services-types';
 import {toJSON} from './tools/fetch-responses';
+import {buildURL} from './tools';
 
-export const postAnswer = async (
-  progression: ProgressionFromAPI,
-  token: string,
-  answer: string[]
-): Promise<ProgressionFromAPI> => {
-  const progressionId = progression._id;
-  const slideRef = progression.state.nextContent.ref;
-  const {host}: JWT = decode(token);
-  const response = await crossFetch(`${host}/api/v2/progressions/${progressionId}/answers`, {
-    method: 'post',
-    headers: {authorization: token, 'content-type': 'application/json'},
-    body: JSON.stringify({
-      content: {
-        ref: slideRef,
-        type: 'slide'
-      },
-      answer
-    })
-  });
+export const postAnswer =
+  (locale: string | void) =>
+  async (
+    progression: ProgressionFromAPI,
+    token: string,
+    answer: string[]
+  ): Promise<ProgressionFromAPI> => {
+    const progressionId = progression._id;
+    const slideRef = progression.state.nextContent.ref;
+    const {host}: JWT = decode(token);
+    const url = buildURL(`${host}/api/v2/progressions/${progressionId}/answers`, locale);
+    const response = await crossFetch(url, {
+      method: 'post',
+      headers: {authorization: token, 'content-type': 'application/json'},
+      body: JSON.stringify({
+        content: {
+          ref: slideRef,
+          type: 'slide'
+        },
+        answer
+      })
+    });
 
-  return toJSON<ProgressionFromAPI>(response);
-};
+    return toJSON<ProgressionFromAPI>(response);
+  };
