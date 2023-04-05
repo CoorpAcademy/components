291c6872e981ce0936ad119efcbb162cc9702266
william.scalabre <william.scalabre@gmail.com>
2023-04-05 11:30:33 +0200

    add missing part in fixture mediaum description

--- packages/@coorpacademy-components/src/molecule/discipline-header/test/fixtures/medium-description.js
+++ packages/@coorpacademy-components/src/molecule/discipline-header/test/fixtures/medium-description.js
@@ -2,7 +2,7 @@ export default {
   props: {
     title: 'Optimisez vos connaissances : apprendre ou à laisser !',
     description:
-      'Pop Quiz, c’est l’interro surprise pour tester vos connaissances en 5 minutes chrono !Connaissez-vous les différentes méthodes d’apprentissage ? Répondez correctement à 15 questions, remportez 60 étoiles et découvrez (ou redécouvrez) les bonnes pratiques à adopter pour optimiser votre apprentissage. Savez-vous comment stimuler votre mémoire ? Renforcer votre attention ? Apprendre de vos pairs ?',
+      'Pop Quiz, c’est l’interro surprise pour tester vos connaissances en 5 minutes chrono !Connaissez-vous les différentes méthodes d’apprentissage ? Répondez correctement à 15 questions, remportez 60 étoiles et découvrez (ou redécouvrez) les bonnes pratiques à adopter pour optimiser votre apprentissage. Savez-vous comment stimuler votre mémoire ? Renforcer votre attention ? Apprendre de vos pairs ?  C’est le moment de le prouver !',
     image:
       'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/CoorpAcademy/content-digital-core/cockpit-digital-2016/default/shutterstock_777835606-1616609267419.jpg&h=500&w=500&q=90&m=contain',
     lastUpdated: 'Last update on 03/2023'
