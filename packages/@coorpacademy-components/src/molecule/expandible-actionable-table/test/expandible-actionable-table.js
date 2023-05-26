c8438d85e4d63ec6b36ce712d06cfbd7eba0bea6
Chris <chris.dugne@uralys.com>
2023-05-26 15:58:56 +0200

    migrations to testing-library

--- packages/@coorpacademy-components/src/molecule/expandible-actionable-table/test/expandible-actionable-table.js
+++ packages/@coorpacademy-components/src/molecule/expandible-actionable-table/test/expandible-actionable-table.js
@@ -1,41 +1,57 @@
 import test from 'ava';
 import browserEnv from 'browser-env';
 import React from 'react';
-import {mount, configure} from 'enzyme';
 import {identity} from 'lodash/fp';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import {fireEvent} from '@testing-library/react';
 import ActionableExpandableErrorsTable from '..';
+import {renderWithContext} from '../../../util/render-with-context';
 import defaultFixture from './fixtures/dashboard-saved';
 
 browserEnv();
-configure({adapter: new Adapter()});
 const translate = identity;
 
 test('should launch onClick and toggle dropDown icon style and nested table', t => {
-  const wrapper = mount(<ActionableExpandableErrorsTable {...defaultFixture.props} />, {
-    context: {translate}
-  });
-  const clickEvent = {preventDefault: () => t.pass(), stopPropagation: () => t.pass()};
+  const {getByTestId, queryAllByTestId, queryByTestId} = renderWithContext(
+    <ActionableExpandableErrorsTable {...defaultFixture.props} />,
+    {
+      context: {translate}
+    }
+  );
 
-  t.true(wrapper.find('[data-name="arrowUp-button-0"].bulkArrowDown').exists());
-  t.false(wrapper.find('[data-name="arrowUp-button-0"].bulkArrowUp').exists());
-  t.false(wrapper.find('table').at(1).exists());
+  let bulkArrowDown = getByTestId('button-arrowUp-button-0-bulkArrowDown');
+  let bulkArrowUp = queryByTestId('button-arrowUp-button-0-bulkArrowUp');
+  let tables = queryAllByTestId('expandible-actionable-table');
+  t.is(tables.length, 1);
+  t.is(bulkArrowUp, null);
 
-  wrapper.find('[data-name="arrowUp-button-0"].bulkArrowDown').simulate('click', clickEvent);
-  t.false(wrapper.find('[data-name="arrowUp-button-0"].bulkArrowDown').exists());
-  t.true(wrapper.find('[data-name="arrowUp-button-0"].bulkArrowUp').exists());
-  t.true(wrapper.find('table').at(1).exists());
+  fireEvent.click(bulkArrowDown);
 
-  wrapper.find('[data-name="arrowUp-button-0"].bulkArrowUp').simulate('click', clickEvent);
-  t.true(wrapper.find('[data-name="arrowUp-button-0"].bulkArrowDown').exists());
-  t.false(wrapper.find('[data-name="arrowUp-button-0"].bulkArrowUp').exists());
-  t.false(wrapper.find('table').at(1).exists());
+  tables = queryAllByTestId('expandible-actionable-table');
+  bulkArrowUp = getByTestId('button-arrowUp-button-0-bulkArrowUp');
+  bulkArrowDown = queryByTestId('button-arrowUp-button-0-bulkArrowDown');
 
-  wrapper.find('[data-name="arrowUp-button-1"].bulkArrowDown').simulate('click', clickEvent);
-  t.true(wrapper.find('table').at(1).exists());
-  t.true(wrapper.find('[data-name="arrowUp-button-1"].bulkArrowDown').exists());
+  t.is(tables.length, 2);
+  t.is(bulkArrowDown, null);
 
-  wrapper.find('[data-name="arrowUp-button-1"].bulkArrowDown').simulate('click', clickEvent);
-  t.false(wrapper.find('[data-name="arrowUp-button-1"].bulkArrowDown').exists());
-  t.true(wrapper.find('[data-name="arrowUp-button-1"].bulkArrowUp').at(1).exists());
+  fireEvent.click(bulkArrowUp);
+
+  bulkArrowDown = getByTestId('button-arrowUp-button-0-bulkArrowDown');
+  bulkArrowUp = queryByTestId('button-arrowUp-button-0-bulkArrowUp');
+  tables = queryAllByTestId('expandible-actionable-table');
+  t.is(tables.length, 1);
+  t.is(bulkArrowUp, null);
+
+  let nextBulkArrowDown = getByTestId('button-arrowUp-button-1-bulkArrowDown');
+  fireEvent.click(nextBulkArrowDown);
+  tables = queryAllByTestId('expandible-actionable-table');
+  t.is(tables.length, 2);
+
+  nextBulkArrowDown = getByTestId('button-arrowUp-button-1-bulkArrowDown');
+  t.truthy(nextBulkArrowDown);
+
+  fireEvent.click(nextBulkArrowDown);
+  const nextBulkArrowUp = queryAllByTestId('button-arrowUp-button-1-bulkArrowUp');
+  t.is(nextBulkArrowUp.length, 2);
+  nextBulkArrowDown = queryByTestId('button-arrowUp-button-1-bulkArrowDown');
+  t.is(nextBulkArrowDown, null);
 });
