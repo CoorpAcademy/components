eff0421c33a55bbe5bcfae66d75c79a8d2020ea7
DjamelSoualmi <djamel.soualmi@coorpacademy.com>
2023-06-07 18:36:56 +0200

    fix icons  colors

--- packages/@coorpacademy-nova-icons/src/components/nova-line/phone/mobile-phone.native.js
+++ packages/@coorpacademy-nova-icons/src/components/nova-line/phone/mobile-phone.native.js
@@ -6,12 +6,12 @@ const SvgComponent = props => (
     <G clipPath="url(#prefix__clip0_258_3084)">
       <Path
         d="M15.999 1.999h-8c-1.103 0-2 .896-2 2v16c0 1.104.897 2 2 2h8c1.103 0 2-.896 2-2v-16c0-1.103-.897-2-2-2zm0 2v2h-8v-2h8zm.001 4v8H7.999v-8H16zm-8.001 12v-2H16v2H7.999z"
-        fill="#000"
+        fill={props.color}
       />
     </G>
     <Defs>
       <ClipPath id="prefix__clip0_258_3084">
-        <Path fill="#fff" d="M0 0h24v24H0z" />
+        <Path fill={props.color} d="M0 0h24v24H0z" />
       </ClipPath>
     </Defs>
   </Svg>
