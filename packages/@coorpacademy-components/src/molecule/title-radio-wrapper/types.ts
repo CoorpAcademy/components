065ab46fae334d5335da90500a9add34a68a9eae
Antonia Balluais <antonia.balluais@coorpacademy.com>
2023-06-28 16:38:08 +0200

    fix type

--- packages/@coorpacademy-components/src/molecule/title-radio-wrapper/types.ts
+++ packages/@coorpacademy-components/src/molecule/title-radio-wrapper/types.ts
@@ -3,10 +3,12 @@ import DragAndDrop from '../../atom/drag-and-drop';
 import RadioWithTitle from '../../atom/radio-with-title';
 import SelectMultiple from '../select-multiple';
 
-export const TitleRadioWrapperProps = {
-  title: PropTypes.shape(RadioWithTitle.propTypes),
-  child: {
-    item: PropTypes.oneOf([SelectMultiple.propTypes, DragAndDrop.propTypes]),
-    childType: PropTypes.oneOf(['drag-and-drop', 'select-multipe'])
-  }
+export type TitleRadioWrapperProps = {
+  radioWithTitle: PropTypes.InferProps<typeof RadioWithTitle.propTypes>;
+  fields: {
+    item:
+      | PropTypes.InferProps<typeof DragAndDrop.propTypes>
+      | PropTypes.InferProps<typeof SelectMultiple.propTypes>;
+    childType: 'select-multiple' | 'drag-and-drop';
+  };
 };
