97eb7c1bb65a4016c50a1fa95bc22dd15e61f64d
Antonia Balluais <antonia.balluais@coorpacademy.com>
2023-06-29 15:04:48 +0200

    Revert "update title and radio wrappere type and fixture"
    This reverts commit 015fb140cdc9b2c0620bb4156f36f335c2d21c3e.

---
+++ packages/@coorpacademy-components/src/molecule/title-radio-wrapper/test/fixtures/select-cohortes.tsx
@@ -0,0 +1,46 @@
+export default {
+  radioWithTitle: {
+    title: {
+      type: 'form-group',
+      title: 'Select one or several cohortes',
+      subtitle: 'in the drop-down list'
+    }
+  },
+  field: {
+    item: {
+      theme: 'coorpmanager',
+      placeholder: 'Select cohorte(s)',
+      title: 'Cohortes',
+      description: 'You should select a cohorte',
+      multiple: true,
+      disabled: false,
+      error: '',
+      hint: '',
+      options: [
+        {
+          name: 'ABRERA FCM',
+          value: 'abrera-fcm',
+          selected: true
+        },
+        {
+          name: 'ABRERA FIS',
+          value: 'abrera-fis',
+          selected: false
+        },
+        {
+          name: 'ALLARIZ',
+          value: 'allariz',
+          selected: false
+        },
+        {
+          name: 'ALMUSSAFESS FCM',
+          value: 'almussafes-fcm',
+          selected: false
+        }
+      ],
+      onChange: (value: any) => console.log('onChange', value),
+      onError: () => true
+    },
+    childType: 'selectMultiple'
+  }
+};
--- packages/@coorpacademy-components/src/molecule/title-radio-wrapper/types.ts
+++ packages/@coorpacademy-components/src/molecule/title-radio-wrapper/types.ts
@@ -1,21 +1,10 @@
 import PropTypes from 'prop-types';
 import DragAndDrop from '../../atom/drag-and-drop';
 import RadioWithTitle from '../../atom/radio-with-title';
-import {RadioWithTitleProps} from '../../atom/radio-with-title/types';
 import SelectMultiple from '../select-multiple';
 
-export const propTypes = {
-  radioWithTitle: PropTypes.shape(RadioWithTitle.propTypes).isRequired,
-  field: {
-    item: PropTypes.oneOfType([
-      PropTypes.shape(SelectMultiple.propTypes),
-      PropTypes.shape(DragAndDrop.propTypes)
-    ])
-  }
-};
-
 export type TitleRadioWrapperProps = {
-  radioWithTitle: RadioWithTitleProps;
+  radioWithTitle: PropTypes.InferProps<typeof RadioWithTitle.propTypes>;
   field: {
     item:
       | PropTypes.InferProps<typeof DragAndDrop.propTypes>
