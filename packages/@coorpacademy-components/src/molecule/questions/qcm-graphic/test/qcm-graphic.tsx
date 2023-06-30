e846ccc6e25bfe3c96edbdd74e8809a2f93fe620
EmelineLeduc <leduc.emeline44@gmail.com>
2023-06-30 17:55:13 +0200

    migrate qcm-graphic molecule to testing-library

--- packages/@coorpacademy-components/src/molecule/questions/qcm-graphic/test/qcm-graphic.tsx
+++ packages/@coorpacademy-components/src/molecule/questions/qcm-graphic/test/qcm-graphic.tsx
@@ -9,7 +9,7 @@ import defaultFixture from './fixtures/default';
 browserEnv();
 
 test('onClick should be reachable, should match given aria-label', t => {
-  t.plan(2);
+  t.plan(5);
   let answerWasClicked = false;
   defaultFixture.props.answers[1] = {
     ...defaultFixture.props.answers[1],
@@ -19,22 +19,20 @@ test('onClick should be reachable, should match given aria-label', t => {
   };
 
   const {container} = renderWithContext(<QCMImage {...defaultFixture.props} />);
+  const secondAnswer = container.querySelector(
+    '[data-name="answerGraphic"]:nth-child(2) :nth-child(2) :nth-child(1)'
+  ) as Element;
+  t.truthy(secondAnswer);
+  t.is(
+    secondAnswer.getAttribute('aria-label'),
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sut labore et dolore magna aliqua.'
+  );
 
-  const answersImages = container.querySelector('[data-name="answerImage"]');
-  t.truthy(answersImages);
-  // t.is(
-  //   answersImages.getAttribute('aria-label'),
-  //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sut labore et dolore magna aliqua.'
-  // );
-  // t.true(answersImages.at(1).exists());
-  // t.is(
-  //   answersImages.at(1).props()['aria-label'],
-  //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sut labore et dolore magna aliqua.'
-  // );
-
-  const answer = container.querySelector('[data-name="answerGraphic"]:nth-child(1)');
+  const answer = container.querySelector(
+    '[data-name="answerGraphic"]:nth-child(2) :nth-child(2) :nth-child(2) :nth-child(1) :nth-child(1) :nth-child(1)'
+  ) as Element;
   t.truthy(answer);
-  fireEvent(answer);
+  fireEvent.click(answer);
   t.true(answerWasClicked);
   t.pass();
 });
@@ -42,12 +40,14 @@ test('onClick should be reachable, should match given aria-label', t => {
 test("should set: selected's background to Primary, unselected's no background", t => {
   t.plan(9);
   const {container} = renderWithContext(<QCMImage {...defaultFixture.props} />);
-  const firstAnswer = container.querySelector('[data-name="answerGraphic"]:nth-child(1)');
+  const firstAnswer = container.querySelector(
+    '[data-name="answerGraphic"]:nth-child(1)'
+  ) as Element;
   t.truthy(firstAnswer);
   t.is(firstAnswer.getAttribute('style'), null);
   const unselectedBackgroundAnswer = container.querySelector(
     '[data-name="answerGraphic"]:nth-child(1) :nth-child(1)'
-  );
+  ) as Element;
   t.truthy(unselectedBackgroundAnswer);
   t.is(unselectedBackgroundAnswer.getAttribute('style'), 'background-color: rgb(244, 244, 245);');
 
@@ -56,13 +56,15 @@ test("should set: selected's background to Primary, unselected's no background",
   );
   t.truthy(firstAnswerText);
 
-  const thirdAnswer = container.querySelector('[data-name="answerGraphic"]:nth-child(3)');
+  const thirdAnswer = container.querySelector(
+    '[data-name="answerGraphic"]:nth-child(3)'
+  ) as Element;
   t.truthy(thirdAnswer);
   t.is(thirdAnswer.getAttribute('style'), 'box-shadow: 0 4px 16px rgba(0, 122, 179, 0.25);');
 
   const selectedBackgroundAnswer = container.querySelector(
     '[data-name="answerGraphic"]:nth-child(3) :nth-child(1)'
-  );
+  ) as Element;
   t.truthy(selectedBackgroundAnswer);
   t.is(selectedBackgroundAnswer.getAttribute('style'), 'background-color: rgb(0, 176, 255);');
 });
