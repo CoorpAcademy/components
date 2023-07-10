97e82ebb6f06d7f85bb367bdd0a2943e07fbcf4c
EmelineLeduc <leduc.emeline44@gmail.com>
2023-07-10 15:57:45 +0200

    modify atom title to add different size

--- packages/@coorpacademy-components/src/atom/title/index.js
+++ packages/@coorpacademy-components/src/atom/title/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './style.css';
 
-const getTitleStype = type => {
+const getTitleStype = (type, size) => {
   switch (type) {
     case 'page':
       return style.titlePage;
     case 'form-group':
-      return style.titleFormGroup;
+      switch (size) {
+        case 'standard':
+          return style.titleFormGroup;
+        case 'small':
+          return style.smallTitleFormGroup;
+      }
   }
 };
 
@@ -21,13 +26,22 @@ const getSubtitleStype = (type, size) => {
           return style.subtitleFormGroup;
         case 'small':
           return style.smallSubtitleFormGroup;
+        case 'extra-small':
+          return style.extraSmallSubtitleFormGroup;
       }
   }
 };
 
 const Title = props => {
-  const {title, subtitle, type, 'data-name': dataName, subtitleSize = 'standard'} = props;
-  const titleStyle = getTitleStype(type);
+  const {
+    title,
+    subtitle,
+    type,
+    'data-name': dataName,
+    titleSize = 'standard',
+    subtitleSize = 'standard'
+  } = props;
+  const titleStyle = getTitleStype(type, titleSize);
   const subtitleStyle = getSubtitleStype(type, subtitleSize);
 
   const subtitleSection = subtitle ? <div className={subtitleStyle}>{subtitle}</div> : null;
@@ -47,6 +61,7 @@ Title.propTypes = {
   subtitle: PropTypes.string,
   type: PropTypes.oneOf(['page', 'form-group']),
   'data-name': PropTypes.string,
+  titleSize: PropTypes.string,
   subtitleSize: PropTypes.string
 };
 export default Title;
