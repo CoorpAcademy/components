18cdbba3a97f8279d3063329e20f3aebaac986df
EmelineLeduc <leduc.emeline44@gmail.com>
2023-07-11 09:50:25 +0200

    modify select-opponents component to use map.convert method

--- packages/@coorpacademy-components/src/organism/select-opponents/index.tsx
+++ packages/@coorpacademy-components/src/organism/select-opponents/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import map from 'lodash/fp/map';
 import TitleRadioWrapper from '../../molecule/title-radio-wrapper';
 import {TitleRadioWrapperProps} from '../../molecule/title-radio-wrapper/types';
 import propTypes, {SelectOpponentsProps} from './types';
 import style from './style.css';
 
+// @ts-expect-error (need to get the index)
+const mapWithIndex = map.convert({cap: false});
+
 const SelectOpponents = (props: SelectOpponentsProps) => {
   const {items} = props;
   return (
     <ul className={style.container}>
-      {items.map((item: TitleRadioWrapperProps, key: number) => {
+      {mapWithIndex((item: TitleRadioWrapperProps, key: number) => {
         return (
           <li key={key} className={style.item}>
             <TitleRadioWrapper {...item} />
           </li>
         );
-      })}
+      }, items)}
     </ul>
   );
 };
