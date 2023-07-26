63236a45d17e4d9f62a17db2de24a3659981b7c0
Antonia Balluais <antonia.balluais@coorpacademy.com>
2023-07-26 15:23:23 +0200

    Update hovered style of card
    update style icon
    
    card style on hover card

--- packages/@coorpacademy-nova-icons/icons.js
+++ packages/@coorpacademy-nova-icons/icons.js
@@ -265,13 +265,8 @@ const icons: Array<Icon> = [
   {filePath: path.resolve('./third-party/nova-solid.iconjar/icons/file-block-2.svg')},
   {filePath: path.resolve('./third-party/nova-line.iconjar/icons/cookie.svg')},
   {filePath: path.resolve('./third-party/nova-composition.iconjar/icons/revision.svg')},
-  {
-    filePath: path.resolve('./third-party/nova-composition.iconjar/icons/padlock.svg'),
-    replaceColors: false
-  },
-  {
-    filePath: path.resolve('./third-party/nova-solid.iconjar/icons/synchronize-3.svg')
-  }
+  {filePath: path.resolve('./third-party/nova-composition.iconjar/icons/padlock.svg')},
+  {filePath: path.resolve('./third-party/nova-solid.iconjar/icons/synchronize-3.svg')}
 ];
 
 export default icons;
