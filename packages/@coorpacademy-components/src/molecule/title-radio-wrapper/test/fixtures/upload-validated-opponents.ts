f1170fd5f6ad9f3a079f743b64b8c1e2d0ed9200
EmelineLeduc <leduc.emeline44@gmail.com>
2023-08-08 17:16:23 +0200

    update fixtures

--- packages/@coorpacademy-components/src/molecule/title-radio-wrapper/test/fixtures/upload-validated-opponents.ts
+++ packages/@coorpacademy-components/src/molecule/title-radio-wrapper/test/fixtures/upload-validated-opponents.ts
@@ -1,3 +1,5 @@
+import fixtureDragAndDropWithXlsx from '../../../../atom/drag-and-drop/test/fixtures/with-xlsx';
+
 export default {
   props: {
     radioWithTitle: {
@@ -12,17 +14,11 @@ export default {
       }
     },
     field: {
-      description: 'JPG or PNG, smaller than 5mb',
-      uploadLabel: 'Browse',
-      previewLabel: 'File Preview',
+      ...fixtureDragAndDropWithXlsx.props,
+      title: '',
       labelButtonLink: 'here',
       labelLink: 'Need the template? Download it',
       hrefLink: 'https://setup.coorpacademy.com/assets/templates/import-users-template.xlsx',
-      previewContent: {
-        type: 'xlsx',
-        src: 'https://static.coorpacademy.com/content/digital/raw/meta_inc-1677774948417._logo-1-1677774948417.pdf',
-        label: 'opponents-error-report.xlsx'
-      },
       loading: false
     },
     childType: 'massive-upload'
