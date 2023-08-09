c5a31534bd672029c0dd1ff18175a1a01bea28a1
EmelineLeduc <leduc.emeline44@gmail.com>
2023-08-09 12:38:24 +0200

    add fixture

--- packages/@coorpacademy-components/src/molecule/title-radio-wrapper/index.tsx
+++ packages/@coorpacademy-components/src/molecule/title-radio-wrapper/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import isEmpty from 'lodash/fp/isEmpty';
 import RadioWithTitle from '../../atom/radio-with-title';
 import SelectMultiple from '../select-multiple';
 import ImageUpload from '../../atom/image-upload';
@@ -6,14 +7,18 @@ import propTypes, {TitleRadioWrapperProps} from './types';
 import style from './style.css';
 
 const getContainerStyle = (field: Record<string, unknown>, childType: string): string => {
-  return childType === 'massive-upload' && field.loading ? style.containerUpload : style.container;
+  return childType === 'massive-upload' && (field.loading || isEmpty(field.previewContent))
+    ? style.containerUpload
+    : style.container;
 };
 
 const buildInput = (childType: string, field: any) => {
   return childType === 'select-multiple' ? (
     <SelectMultiple {...field} />
   ) : (
-    <div className={field.loading ? style.containerImageUpload : ''}>
+    <div
+      className={field.loading || isEmpty(field.previewContent) ? style.containerImageUpload : ''}
+    >
       <ImageUpload {...field} />
     </div>
   );
