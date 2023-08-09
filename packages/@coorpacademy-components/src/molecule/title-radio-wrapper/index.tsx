63a536569e6a135c1046e28b4f6847e2feffe4ab
EmelineLeduc <leduc.emeline44@gmail.com>
2023-08-09 12:09:51 +0200

    modify title-radio-wrapper for loading state

--- packages/@coorpacademy-components/src/molecule/title-radio-wrapper/index.tsx
+++ packages/@coorpacademy-components/src/molecule/title-radio-wrapper/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import isEmpty from 'lodash/fp/isEmpty';
 import RadioWithTitle from '../../atom/radio-with-title';
 import SelectMultiple from '../select-multiple';
 import ImageUpload from '../../atom/image-upload';
 import propTypes, {TitleRadioWrapperProps} from './types';
 import style from './style.css';
 
+const getContainerStyle = (field: Record<string, unknown>, childType: string): string => {
+  return childType === 'massive-upload' && field.loading ? style.containerUpload : style.container;
+};
+
 const buildInput = (childType: string, field: any) => {
   return childType === 'select-multiple' ? (
     <SelectMultiple {...field} />
   ) : (
-    <div className={field.loading || isEmpty(field.previewContent) ? style.containerUpload : ''}>
+    <div className={field.loading ? style.containerImageUpload : ''}>
       <ImageUpload {...field} />
     </div>
   );
@@ -19,7 +22,7 @@ const buildInput = (childType: string, field: any) => {
 const TitleRadioWrapper = (props: TitleRadioWrapperProps) => {
   const {radioWithTitle, field, childType} = props;
   return (
-    <div className={style.container}>
+    <div className={getContainerStyle(field, childType)}>
       {radioWithTitle ? <RadioWithTitle {...radioWithTitle} /> : null}
       <div className={style.field}>{buildInput(childType, field)}</div>
     </div>
