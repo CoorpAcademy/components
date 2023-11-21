94a61027fbdea5b16554341f06a6707181e02b0c
EmelineLeduc <leduc.emeline44@gmail.com>
2023-11-21 10:49:29 +0100

    modify fixtures

--- packages/@coorpacademy-components/src/molecule/cm-popin/test/fixtures/popin-with-link.js
+++ packages/@coorpacademy-components/src/molecule/cm-popin/test/fixtures/popin-with-link.js
@@ -1,29 +1,13 @@
+import defaultFixture from './default';
+
 export default {
   props: {
+    ...defaultFixture.props,
     content: 'Some users were not imported',
     descriptionText:
       'The massive battle will only be send to 265 users. 35 out of 300 users were not imported.',
-    onClose: () => {
-      console.log('close');
-    },
-    firstButton: {
-      label: 'Cancel',
-      type: 'secondary',
-      handleOnclick: () => {
-        console.log('cancel');
-      },
-      'aria-label': 'Cancel this operation'
-    },
-    secondButton: {
-      label: 'Undo',
-      type: 'primary',
-      handleOnclick: () => {
-        console.log('undo');
-      },
-      'aria-label': 'Undo this operation'
-    },
+    icon: 'AlertDiamond',
     link: {
-      href: '#',
       children: ['Download report'],
       onClick: () => {
         console.log('hello');
