b34adb88609430c70a704abd4790dafa49e0dc5f
AdrieanKhisbe <adriean.khisbe@live.fr>
2024-04-02 11:44:08 +0200

    Tweak config for CI fontawesome token loading to working :wrench:

--- packages/@coorpacademy-components/scripts/load-font-awesome-token.sh
+++ packages/@coorpacademy-components/scripts/load-font-awesome-token.sh
@@ -4,10 +4,22 @@ set -e
 token_file="tmp/font-awesome-token"
 
 if [ -f "$token_file" ]; then
-  cat $token_file
+  token=$(cat $token_file)
 else
   mkdir -p tmp
-  aws ssm get-parameter --name "/credentials/global/font-awesome/token" --output text --query Parameter.Value \
+
+  token=$(aws ssm get-parameter --name "/credentials/global/font-awesome/token" --output text --query Parameter.Value \
   | tr -d '\n' \
   | tee $token_file
+  )
 fi
+
+if [[ $ZSH_EVAL_CONTEXT == "toplevel:file" ]]; then
+    # if file is sourced, load the var also and print a message about it
+    export FONTAWESOME_NPM_AUTH_TOKEN=$(cat $token_file)
+    echo "Fontawesome token loaded in FONTAWESOME_NPM_AUTH_TOKEN var"
+else
+    echo -n "$token"
+fi
+
+set +e
---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+# so far simple shortcut
+source ./packages/@coorpacademy-components/scripts/load-font-awesome-token.sh
