28b9bee836b6331635c623f536435e5f4fd7b021
AdrieanKhisbe <adriean.khisbe@live.fr>
2024-04-09 16:14:17 +0200

    Quote the path in case :shield:

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TOP_LEVEL_DIR=$(dirname $(dirname $(readlink -f -- "$0")))
+TOP_LEVEL_DIR="$(dirname "$(dirname "$(readlink -f -- "$0")")")"
 
 # so far simple shortcut
-source $TOP_LEVEL_DIR/packages/@coorpacademy-components/scripts/load-font-awesome-token.sh
+source "$TOP_LEVEL_DIR/packages/@coorpacademy-components/scripts/load-font-awesome-token.sh"
