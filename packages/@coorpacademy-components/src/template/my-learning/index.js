2d57ef572eefec05e905ead33bbf7da8040c5731
Huy Nguyen <huy.nguyen@anh.tranhuy.nguyen-9GXQ>
2024-05-21 11:16:29 +0200

    add fixture for no learner feature

--- packages/@coorpacademy-components/src/template/my-learning/index.js
+++ packages/@coorpacademy-components/src/template/my-learning/index.js
@@ -108,10 +108,11 @@ FilterButton.propTypes = {
 const MyLearning = (props, context) => {
   const {
     skills,
-    selectedSkills,
+    selectedSkills = [],
     skillsStats,
     skillsLocales,
     skillsFilters,
+    learnerFeature = true,
     isLoading,
     onSkillFocusConfirm,
     onReviewSkill,
@@ -250,84 +251,86 @@ const MyLearning = (props, context) => {
           onConfirm={handleConfirmSkillPicker}
           onClose={handleCloseSkillPicker}
         />
-        <div className={style.skillFocusContainer}>
-          <header className={style.skillFocusHeader}>
-            <div className={style.skillFocusHeaderWrapper}>
-              <div className={style.skillFocusHeaderIcon}>
-                <Icon
-                  iconName="bullseye-arrow"
-                  backgroundColor="#DDD1FF"
-                  borderRadius="12px"
-                  size={{faSize: 20, wrapperSize: 48}}
-                />
-              </div>
-              <div className={style.skillFocusHeaderContent}>
-                <div className={style.skillFocusHeaderTitle}>{translate('skill_focus')}</div>
-                <div className={style.skillFocusHeaderDescription}>
-                  {translate('skills_focus_description')}
+        {learnerFeature ? (
+          <div className={style.skillFocusContainer}>
+            <header className={style.skillFocusHeader}>
+              <div className={style.skillFocusHeaderWrapper}>
+                <div className={style.skillFocusHeaderIcon}>
+                  <Icon
+                    iconName="bullseye-arrow"
+                    backgroundColor="#DDD1FF"
+                    borderRadius="12px"
+                    size={{faSize: 20, wrapperSize: 48}}
+                  />
+                </div>
+                <div className={style.skillFocusHeaderContent}>
+                  <div className={style.skillFocusHeaderTitle}>{translate('skill_focus')}</div>
+                  <div className={style.skillFocusHeaderDescription}>
+                    {translate('skills_focus_description')}
+                  </div>
                 </div>
               </div>
-            </div>
+              {selectedSkillsList.length > 0 ? (
+                <ChangeSkillFocusButton onClick={handleOpenSkillPicker} />
+              ) : null}
+            </header>
             {selectedSkillsList.length > 0 ? (
-              <ChangeSkillFocusButton onClick={handleOpenSkillPicker} />
-            ) : null}
-          </header>
-          {selectedSkillsList.length > 0 ? (
-            <div className={style.skillFocusContent}>
-              <div className={style.radarContainer}>
-                <ResponsiveLearningProfileRadarChart
-                  totalDataset={1}
-                  height={424}
-                  width={680}
-                  data={graphDatas}
-                  legend={graphLegends}
-                  onClick={handleOnDotClick}
-                  colors={[
-                    {
-                      gradient: {
-                        fill: ['#0062ffff', '#8000ff85'],
-                        stroke: ['#0062ffff', '#8000FF']
-                      },
-                      percentage: {
-                        color: '#0061FF',
-                        background:
-                          'linear-gradient(180deg, rgba(0, 97, 255, 0.10) 0%, rgba(147, 107, 255, 0.10) 100%)'
-                      },
-                      label: {
-                        color: '#020202ff'
+              <div className={style.skillFocusContent}>
+                <div className={style.radarContainer}>
+                  <ResponsiveLearningProfileRadarChart
+                    totalDataset={1}
+                    height={424}
+                    width={680}
+                    data={graphDatas}
+                    legend={graphLegends}
+                    onClick={handleOnDotClick}
+                    colors={[
+                      {
+                        gradient: {
+                          fill: ['#0062ffff', '#8000ff85'],
+                          stroke: ['#0062ffff', '#8000FF']
+                        },
+                        percentage: {
+                          color: '#0061FF',
+                          background:
+                            'linear-gradient(180deg, rgba(0, 97, 255, 0.10) 0%, rgba(147, 107, 255, 0.10) 100%)'
+                        },
+                        label: {
+                          color: '#020202ff'
+                        }
                       }
-                    }
-                  ]}
+                    ]}
+                  />
+                </div>
+                <SkillsChartSideInformationPanel sidePanelItems={skillChartPanelProps} />
+              </div>
+            ) : (
+              <div className={style.skillFocusEmpty}>
+                <Picture
+                  className={style.img}
+                  src={
+                    'https://t4.ftcdn.net/jpg/01/43/23/83/360_F_143238306_lh0ap42wgot36y44WybfQpvsJB5A1CHc.jpg'
+                  }
+                  alt={'demo'}
+                />
+                <div className={style.skillFocusEmptyTitle}>
+                  {translate('skill_focus_empty_title')}
+                </div>
+                <div className={style.skillFocusEmptyDescription}>
+                  {translate('skills_focus_empty_description')}
+                </div>
+                <ButtonLink
+                  customStyle={{
+                    width: '168px'
+                  }}
+                  type={'primary'}
+                  onClick={handleOpenSkillPicker}
+                  label={translate('choose_your_focus')}
                 />
               </div>
-              <SkillsChartSideInformationPanel sidePanelItems={skillChartPanelProps} />
-            </div>
-          ) : (
-            <div className={style.skillFocusEmpty}>
-              <Picture
-                className={style.img}
-                src={
-                  'https://t4.ftcdn.net/jpg/01/43/23/83/360_F_143238306_lh0ap42wgot36y44WybfQpvsJB5A1CHc.jpg'
-                }
-                alt={'demo'}
-              />
-              <div className={style.skillFocusEmptyTitle}>
-                {translate('skill_focus_empty_title')}
-              </div>
-              <div className={style.skillFocusEmptyDescription}>
-                {translate('skills_focus_empty_description')}
-              </div>
-              <ButtonLink
-                customStyle={{
-                  width: '168px'
-                }}
-                type={'primary'}
-                onClick={handleOpenSkillPicker}
-                label={translate('choose_your_focus')}
-              />
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        ) : null}
         <header className={style.skillListHeader}>
           <div className={style.skillListHeaderIcon}>
             <Icon
@@ -424,6 +427,7 @@ MyLearning.propTypes = {
     })
   ),
   skillsLocales: PropTypes.objectOf(PropTypes.string),
+  learnerFeature: PropTypes.bool,
   isLoading: PropTypes.bool,
   onSkillFocusConfirm: PropTypes.func,
   onReviewSkill: PropTypes.func,
