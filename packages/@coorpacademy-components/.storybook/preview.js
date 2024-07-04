9b14f7c726ece47a966b7d601da354927f0c205f
Huy Nguyen <huy.nguyen@anh.tranhuy.nguyen-9GXQ>
2024-07-04 16:28:52 +0700

    CLXP-56 [Technical] Add the ability to change language in the storybook

--- packages/@coorpacademy-components/.storybook/preview.js
+++ packages/@coorpacademy-components/.storybook/preview.js
@@ -1,23 +1,89 @@
 const React = require('react');
-const {default: createTranslate} = require('@coorpacademy/translate');
+const { useState } = require('react');
+const { default: createTranslate } = require('@coorpacademy/translate');
 const en = require('../locales/en/global');
-const {default: Provider} = require('../src/atom/provider');
+const fr = require('../locales/fr/global');
+const ja = require('../locales/ja/global');
+const ko = require('../locales/ko/global');
+const es = require('../locales/es/global');
+const vi = require('../locales/vi/global');
+const { default: Provider } = require('../src/atom/provider');
+const { default: Select } = require('../src/atom/select');
+
 const skin = require('./skin');
 
-const translate = createTranslate(en);
+const LanguageSelector = ({ setLocale }) => {
+  const options = [
+    {
+      name: 'en',
+      value: 'en',
+      selected: true
+    },
+    {
+      name: 'fr',
+      value: 'fr',
+      selected: false
+    },
+    {
+      name: 'ja',
+      value: 'ja',
+      selected: false
+    },
+    {
+      name: 'ko',
+      value: 'ko',
+      selected: false
+    },
+    {
+      name: 'es',
+      value: 'es',
+      selected: false
+    },
+    {
+      name: 'vi',
+      value: 'vi',
+      selected: false
+    },
+  ]
+  return (
+    <div style={{marginBottom: '32px'}}>
+      <Select options={options} theme={'coorpmanager'} onChange={(value) => setLocale(value)}/>
+    </div>
+  );
+};
 
-const context = {
-  skin,
-  translate,
-  Vimeo: window.Vimeo
+const withProvider = Story => {
+  const [locale, setLocale] = useState('en');
+
+  const locales = {
+    en,
+    fr,
+    ja,
+    ko,
+    es,
+    vi
+  };
+
+  const context = {
+    skin,
+    translate: createTranslate(locales[locale]),
+    Vimeo: window.Vimeo,
+  };
+
+  return (
+    <Provider {...context}>
+      <>
+      <LanguageSelector setLocale={setLocale} />
+      <Story />
+      </>
+    </Provider>
+  );
 };
 
 module.exports = {
   decorators: [
-    Story => (
-      <Provider {...context}>
-        <Story />
-      </Provider>
-    )
+    withProvider
   ]
 };
+
+
