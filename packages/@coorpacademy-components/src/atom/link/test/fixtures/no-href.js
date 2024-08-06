6dc72a06c2c7dc1d1c7e3c9dde7c865a4954f7af
adamska27 <adams.roy27@gmail.com>
2024-08-06 17:56:55 +0200

    add fixture + test

---
+++ packages/@coorpacademy-components/src/atom/link/test/fixtures/no-href.js
@@ -0,0 +1,11 @@
+export default {
+  props: {
+    skinHover: true,
+    style: {
+      color: '#2d64c9'
+    },
+    useButtonTag: false,
+    onClick: () => console.log('clicked'),
+    children: 'Link'
+  }
+};
--- packages/@coorpacademy-components/src/atom/link/test/index.js
+++ packages/@coorpacademy-components/src/atom/link/test/index.js
@@ -26,3 +26,48 @@ test('should call listeners within props', t => {
   fireEvent.mouseLeave(link);
   t.truthy(link);
 });
+
+test('should not get href attribute if no href props is passed', t => {
+  const props = {
+    onClick: once(() => t.pass()),
+    onMouseEnter: once(() => t.pass()),
+    onMouseLeave: once(() => t.pass())
+  };
+
+  const {container} = render(<Link {...props} />);
+  const link = container.querySelector('[data-name="link"]');
+
+  const hrefValue = link.getAttribute('href');
+  t.is(hrefValue, null);
+});
+
+test('should get "#" as href attribute if disabled props is passed', t => {
+  const props = {
+    disabled: true,
+    href: 'href',
+    onClick: once(() => t.pass()),
+    onMouseEnter: once(() => t.pass()),
+    onMouseLeave: once(() => t.pass())
+  };
+
+  const {container} = render(<Link {...props} />);
+  const link = container.querySelector('[data-name="link"]');
+
+  const hrefValue = link.getAttribute('href');
+  t.is(hrefValue, '#');
+});
+
+test('should get href attribute corresponding to href props', t => {
+  const props = {
+    href: 'href',
+    onClick: once(() => t.pass()),
+    onMouseEnter: once(() => t.pass()),
+    onMouseLeave: once(() => t.pass())
+  };
+
+  const {container} = render(<Link {...props} />);
+  const link = container.querySelector('[data-name="link"]');
+
+  const hrefValue = link.getAttribute('href');
+  t.is(hrefValue, 'href');
+});
