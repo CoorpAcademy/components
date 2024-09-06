541138198da8654824521778a4b77df5e331faa4
Huy Nguyen <huy.nguyen@anh.tranhuy.nguyen-9GXQ>
2024-09-06 23:49:33 +0700

    loading UI for BrandLearningPriorities

--- packages/@coorpacademy-components/src/organism/brand-learning-priorities/index.js
+++ packages/@coorpacademy-components/src/organism/brand-learning-priorities/index.js
@@ -1,23 +1,30 @@
-import React, {useState, useCallback, useMemo} from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import {convert} from 'css-color-function';
-import {get} from 'lodash/fp';
+import { convert } from 'css-color-function';
+import { get } from 'lodash/fp';
 import Provider from '../../atom/provider';
-import {SelectOptionPropTypes} from '../../atom/select';
+import Loader from '../../atom/loader';
+import { SelectOptionPropTypes } from '../../atom/select';
 import DraggableList from '../../molecule/draggable-list';
 import ButtonLink from '../../atom/button-link';
 import LearningPriorityModal from '../../molecule/learning-priority-modal';
 import style from './style.css';
 
 const BrandLearningPriorities = (props, context) => {
-  const {content, priorities, filters, isLoading, onAdd} = props;
+  const { content, priorities, filters, isLoading, onAdd } = props;
   const [openModal, setOpenModal] = useState(false);
-  const {skin, translate} = context;
+  const { skin, translate } = context;
   const primarySkinColor = get('common.primary', skin);
 
   const selectedPriorities = useMemo(
     () => content.items.map(priority => priority.priorityRef),
     [content.items]
+  );
+
+  const Loading = () => (
+    <div className={style.loading}>
+      <Loader />
+    </div>
   );
 
   const handleOpenModal = useCallback(() => setOpenModal(true), [setOpenModal]);
@@ -38,32 +45,38 @@ const BrandLearningPriorities = (props, context) => {
       <div className={style.LPdescription}>
         {translate('learning_priorities_brand_description')}
       </div>
-      <div className={style.ctaWrapper}>
-        <div className={style.priorityCount}>{`${content.items.length} ${translate('items')}`}</div>
-        <ButtonLink
-          customStyle={{
-            backgroundColor: primarySkinColor,
-            color: '#FFFFFF',
-            transition: 'background-color 0.15s ease-in-out, color 0.15s ease-in-out',
-            width: 'fit-content'
-          }}
-          hoverBackgroundColor={convert(`color(${primarySkinColor} a(0.07))`)}
-          hoverColor={primarySkinColor}
-          onClick={handleOpenModal}
-          label={translate('add_learning_priority')}
-          data-name="add-learning-priority"
-          icon={{
-            position: 'left',
-            faIcon: {
-              name: 'plus',
-              backgroundColor: primarySkinColor,
-              color: '#FFFFFF',
-              size: 16
-            }
-          }}
-        />
-      </div>
-      <DraggableList {...content} />
+
+      {isLoading ?
+        <Loading />
+        :
+        <>
+          <div className={style.ctaWrapper}>
+            <div className={style.priorityCount}>{`${content.items.length} ${translate('items')}`}</div>
+            <ButtonLink
+              customStyle={{
+                backgroundColor: primarySkinColor,
+                color: '#FFFFFF',
+                transition: 'background-color 0.15s ease-in-out, color 0.15s ease-in-out',
+                width: 'fit-content'
+              }}
+              hoverBackgroundColor={convert(`color(${primarySkinColor} a(0.07))`)}
+              hoverColor={primarySkinColor}
+              onClick={handleOpenModal}
+              label={translate('add_learning_priority')}
+              data-name="add-learning-priority"
+              icon={{
+                position: 'left',
+                faIcon: {
+                  name: 'plus',
+                  backgroundColor: primarySkinColor,
+                  color: '#FFFFFF',
+                  size: 16
+                }
+              }}
+            />
+          </div>
+          <DraggableList {...content} />
+        </>}
     </div>
   );
 };
