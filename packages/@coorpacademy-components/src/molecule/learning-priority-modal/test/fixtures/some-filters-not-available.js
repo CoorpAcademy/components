0081338a22a0fe5a6e8ba376c2a5756d17955302
Huy Nguyen <huy.nguyen@anh.tranhuy.nguyen-9GXQ>
2024-09-11 15:04:37 +0700

    update fixtures props

--- packages/@coorpacademy-components/src/molecule/learning-priority-modal/test/fixtures/some-filters-not-available.js
+++ packages/@coorpacademy-components/src/molecule/learning-priority-modal/test/fixtures/some-filters-not-available.js
@@ -15,35 +15,32 @@ export default {
       }
     ],
     preselected: ['ref-1'],
-    filters: {
-      options: [
-        {
-          name: 'All',
-          value: 'ALL',
-          selected: false,
-          count: 8
-        },
-        {
-          name: 'Skill',
-          value: 'skill',
-          selected: true,
-          count: 3
-        },
-        {
-          name: 'Certification',
-          value: 'certification',
-          selected: false,
-          count: 0
-        },
-        {
-          name: 'Playlist',
-          value: 'playlist',
-          selected: false,
-          count: 0
-        }
-      ],
-      onChange: value => console.log(value)
-    },
+    filters: [
+      {
+        name: 'All',
+        value: 'all',
+        selected: false,
+        count: 8
+      },
+      {
+        name: 'Skill',
+        value: 'skill',
+        selected: true,
+        count: 3
+      },
+      {
+        name: 'Certification',
+        value: 'certificate',
+        selected: false,
+        count: 0
+      },
+      {
+        name: 'Playlist',
+        value: 'playlist',
+        selected: false,
+        count: 0
+      }
+    ],
     isOpen: true,
     isLoading: false,
     onCancel: () => {},
