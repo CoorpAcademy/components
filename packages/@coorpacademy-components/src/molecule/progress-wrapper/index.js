864d6b4dc9b04243d81d67d7ec32a961799e3c54
adamska27 <adams.roy27@gmail.com>
2024-10-22 16:12:40 +0200

    create progress-wrapper component

---
+++ packages/@coorpacademy-components/src/molecule/progress-wrapper/index.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {isEmpty, map} from 'lodash/fp';
+import Title from '../../atom/title';
+import ProgressBar from '../progress-bar';
+import {COLORS} from '../../variables/colors';
+import Tag from '../../atom/tag';
+import ButtonLink from '../../atom/button-link';
+import Icon from '../../atom/icon';
+import Provider from '../../atom/provider';
+import style from './style.css';
+
+const DetailSection = ({type, progression, badgeUrl, onDownload, stars}) => {
+  const isLocked = progression !== 100;
+  const isStars = type === 'stars';
+
+  const DownloadButton = (
+    <ButtonLink
+      label="Download"
+      onClick={onDownload}
+      data-name="download-button"
+      aria-label="download button"
+      customStyle={{backgroundColor: '#F1F6FE'}}
+      icon={{
+        position: 'left',
+        faIcon: {
+          name: 'download',
+          color: '#0061FF',
+          size: 14,
+          customStyle: {padding: 0}
+        }
+      }}
+      disabled={isLocked}
+    />
+  );
+
+  const LockedTag = (
+    <Tag
+      label="Locked"
+      size="S"
+      icon={{
+        iconName: 'lock',
+        iconColor: '#515161',
+        preset: 's',
+        customStyle: {padding: 0}
+      }}
+    />
+  );
+
+  return isStars ? (
+    <div className={style.detailsSectionStar}>
+      <div className={style.detailsInfo}>
+        <div className={style.detailsInfoText}>
+          <span className={style.detailsTitle}>Bonus stars</span>
+        </div>
+        <div className={style.stars}>
+          <Icon iconName="star" iconColor="#FFCE0A" backgroundColor="#FFF9D1" preset="xl" />
+          <span>{stars}</span>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className={style.detailsSection}>
+      <img
+        className={style.img}
+        src={
+          badgeUrl ||
+          'https://s3.eu-west-1.amazonaws.com/static.coorpacademy.com/assets/images/diploma.svg'
+        }
+      />
+      <div className={style.detailsInfo}>
+        <div className={style.detailsInfoText}>
+          <span className={style.detailsTitle}>{type}</span>
+          {isLocked ? LockedTag : null}
+        </div>
+
+        <div className={style.buttonContainer}>{DownloadButton}</div>
+      </div>
+    </div>
+  );
+};
+
+const ProgressWrapper = (
+  {completedCourses, completedModules, title, subtitle, progression, sections},
+  context
+) => {
+  const {translate} = context;
+
+  return (
+    <div className={style.container}>
+      <div className={style.titleContainer}>
+        <Title type="form-group" titleSize="medium" title={title} subtitle={subtitle} />
+      </div>
+      <div className={style.statscontainer}>
+        <div className={style.stats}>
+          <div>
+            <span className={style.statsNumber}>{completedCourses}</span>
+            {translate('courses_completed')}
+          </div>
+          <div className={style.divider} />
+          <div>
+            <span className={style.statsNumber}>{completedModules}</span>
+            {translate('modules_completed')}
+          </div>
+        </div>
+        <div className={style.progression}>
+          <span className={style.statsNumber}>{progression}%</span>
+        </div>
+      </div>
+      <ProgressBar
+        className={style.customProgressBar}
+        style={{backgroundColor: COLORS.positive}}
+        displayInfo={false}
+        value={progression}
+        max={100}
+      />
+
+      {isEmpty(sections) ? null : (
+        <div className={style.details}>
+          {map(
+            ({type, stars, badgeUrl, onDownload}) => (
+              <DetailSection {...{type, progression, badgeUrl, onDownload, stars}} key={type} />
+            ),
+            sections
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+DetailSection.propTypes = {
+  type: PropTypes.oneOf(['diploma', 'badge', 'stars']),
+  progression: PropTypes.number,
+  badgeUrl: PropTypes.string,
+  onDownload: PropTypes.func,
+  stars: PropTypes.number
+};
+
+ProgressWrapper.contextTypes = {
+  translate: Provider.childContextTypes.translate
+};
+
+ProgressWrapper.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  completedCourses: PropTypes.number,
+  completedModules: PropTypes.number,
+  progression: PropTypes.number,
+  sections: PropTypes.arrayOf(PropTypes.shape(DetailSection.propTypes))
+};
+
+export default ProgressWrapper;
