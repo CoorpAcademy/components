1cb3153cd12e43450bd4a1a277ced2f21d9eae9c
Huy Nguyen <huy.nguyen@anh.tranhuy.nguyen-9GXQ>
2024-11-12 18:41:56 +0700

    update banner image link

--- packages/@coorpacademy-components/src/molecule/coorpmanager-base-banner/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/molecule/coorpmanager-base-banner/test/fixtures/default.js
@@ -6,6 +6,6 @@ export default {
     buttonLabel: 'Try Learning priorities',
     onClick: () => console.log('click'),
     image:
-      'https://s3-alpha-sig.figma.com/img/4c6f/87c0/99896b9146631494f8dea4f523e3cdff?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HvkcSCRSjz7RvLzB8D0HWNhuk7NUXFZSwLjBKmXxf5NPt-UCVf3wY9VUq~-bESu59PMJJVOcsdE-2h60AY65uswm-yk1yq2fd2dGvkeSXopK0YpqH-XLzdWo8FqHXVwSvB5RQlmA1KDHj1ERZjZJNijo2qXTE3eF71ksXxDm1g3c3XZgdD9TFfy8p9lLqKSBRpvway8bv7QNEcvuN-Q63-OR3EHHiftrZ-pOn0m2GFbuqIv3el-k3Zo60ZNINVeMjYkwN4~BG6PifpZdPWG9D3UenP12XNhPVCA3UJ7H1~ZTzu7q8jRG~h6TywD8S7sgowbtoQYoL5DE21bOxg0Ztg__'
+      'https://s3.eu-west-1.amazonaws.com/static.coorpacademy.com/assets/images/coorp-manager-learning-priorities-highlight-banner.png'
   }
 };
