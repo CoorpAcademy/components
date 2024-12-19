f70681264e711c9c73055ade7651e8d6134a9c87
youssefezzahi96 <youssef.ezzahi@coorpacademy.com>
2024-12-19 11:52:39 +0100

    feedbacks

--- packages/@coorpacademy-components/src/atom/select-tile/test/fixtures/default.js
+++
@@ -1,9 +0,0 @@
-export default {
-  props: {
-    size: 'responsive',
-    'data-name': 'default-button',
-    'aria-label': 'aria button',
-    faIcon: 'bullseye-pointer',
-    onClick: () => console.log('click')
-  }
-};
