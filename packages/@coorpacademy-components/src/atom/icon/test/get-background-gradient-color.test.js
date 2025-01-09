fbf7eaf35d555d1154f269dbf81ffec240c64207
silou <silouane.galinou@go1.com>
2025-01-09 15:03:52 +0100

    fix icon lightness

--- packages/@coorpacademy-components/src/atom/icon/test/get-background-gradient-color.test.js
+++ packages/@coorpacademy-components/src/atom/icon/test/get-background-gradient-color.test.js
@@ -6,17 +6,17 @@ browserEnv();
 
 test('createGradientBackground with a valid RGB color', t => {
   const result = createGradientBackground('rgba(255, 220, 209, 1)');
-  t.regex(result, /^linear-gradient\(180deg, rgb\(255, 95, 46\) 0%, rgb\(250, 58, 0\) 100%\)$/);
+  t.is(result, 'linear-gradient(180deg, rgb(255, 220, 209) 0%, rgb(255, 181, 158) 100%)');
 });
 
 test('createGradientBackground with a valid HEX color', t => {
   const result = createGradientBackground('#D9F4F7');
-  t.regex(result, /^linear-gradient\(180deg, rgb\(82, 205, 218\) 0%, rgb\(44, 190, 206\) 100%\)$/);
+  t.is(result, 'linear-gradient(180deg, rgb(217, 244, 247) 0%, rgb(175, 232, 238) 100%)');
 });
 
 test('createGradientBackground with a valid HSL color', t => {
   const result = createGradientBackground('hsla(52, 100%, 91%, 1)');
-  t.regex(result, /^linear-gradient\(180deg, rgb\(255, 227, 46\) 0%, rgb\(250, 217, 0\) 100%\)$/);
+  t.is(result, 'linear-gradient(180deg, rgb(255, 249, 209) 0%, rgb(255, 242, 158) 100%)');
 });
 
 test('createGradientBackground with invalid color format', t => {
