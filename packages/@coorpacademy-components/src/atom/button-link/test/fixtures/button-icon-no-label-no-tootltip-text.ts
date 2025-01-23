efd042f994be448e386b75f5fd0dd1a4d18ee50f
youssefezzahi96 <youssef.ezzahi@coorpacademy.com>
2025-01-23 06:38:55 +0100

    fix coverage

---
+++ packages/@coorpacademy-components/src/atom/button-link/test/fixtures/button-icon-no-label-no-tootltip-text.ts
@@ -0,0 +1,23 @@
+import {Fixture} from '../../types';
+
+const fixture: Fixture = {
+  props: {
+    'aria-label': 'Edit',
+    type: 'primary',
+    customStyle: {
+      width: 'fit-content',
+      backgroundColor: 'transparent'
+    },
+    hoverBackgroundColor: '#EAEAEB',
+    icon: {
+      position: 'left',
+      faIcon: {
+        name: 'edit',
+        color: '#515161',
+        size: 16
+      }
+    },
+    onClick: () => console.log('click')
+  }
+};
+export default fixture;
