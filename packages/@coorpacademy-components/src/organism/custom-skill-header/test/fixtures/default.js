a4fdaf8071936521c569aabb56344320e318df05
EmelineLeduc <leduc.emeline44@gmail.com>
2025-02-26 14:42:33 +0100

    add save and publish buttons in the header

--- packages/@coorpacademy-components/src/organism/custom-skill-header/test/fixtures/default.js
+++ packages/@coorpacademy-components/src/organism/custom-skill-header/test/fixtures/default.js
@@ -1,26 +1,74 @@
-const fixture = {
+const defaultFixture = {
   props: {
     onQuitClick: () => {
       console.log('onQuitClick');
     },
-    title: 'Create new skill',
+    title: 'Large Language Models',
     tag: {
-      label: 'Draft',
+      label: 'Ongoing changes',
       type: 'progress',
       size: 'S'
     },
+    saveStatus: 'Saved',
     bulletPointMenuButton: {
       buttons: [
         {
+          'data-name': 'discard-button',
+          label: 'Discard changes',
+          linkType: 'tertiary',
+          icon: {
+            position: 'left',
+            faIcon: {name: 'circle-xmark', color: '#515161', size: 14}
+          },
+          onClick: () => console.log('click Download Report'),
+          customStyle: {
+            color: '#1D1D2B',
+            hoverBackgroundColor: '#EAEAEB'
+          },
+          isCustomSkillMenu: true
+        },
+        {
+          'data-name': 'archive-button',
+          label: 'Archive',
+          linkType: 'tertiary',
+          icon: {
+            position: 'left',
+            faIcon: {name: 'folder-open', color: '#515161', size: 14}
+          },
+          onClick: () => console.log('click Download Report'),
+          customStyle: {
+            color: '#1D1D2B',
+            hoverBackgroundColor: '#EAEAEB'
+          },
+          isCustomSkillMenu: true
+        },
+        {
           'data-name': 'delete-button',
           label: 'Delete',
-          type: 'default',
-          faIcon: 'trash',
-          onClick: () => console.log('click Download Report')
+          linkType: 'tertiary',
+          icon: {
+            position: 'left',
+            faIcon: {name: 'trash', color: '#B81400', size: 14}
+          },
+          onClick: () => console.log('click Download Report'),
+          customStyle: {
+            color: '#991100',
+            hoverBackgroundColor: '#FCDADA'
+          },
+          isCustomSkillMenu: true
         }
       ]
+    },
+    saveButton: {
+      label: 'Save changes',
+      onClick: () => console.log('click Save changes'),
+      disabled: true
+    },
+    publishButton: {
+      label: 'Publish changes',
+      onClick: () => console.log('click Publish changes')
     }
   }
 };
 
-export default fixture;
+export default defaultFixture;
