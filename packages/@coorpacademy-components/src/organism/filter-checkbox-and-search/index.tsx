2dc817f940992762fd54691dc4aad9c0c10dc694
antonia.balluais <antonia.balluais@coorpacademy.com>
2025-03-07 17:40:58 +0100

    CLXP-252 component folder

--- packages/@coorpacademy-components/src/organism/filter-checkbox-and-search/index.tsx
+++ packages/@coorpacademy-components/src/organism/filter-checkbox-and-search/index.tsx
@@ -1,23 +1,36 @@
 import React, {useCallback, useState} from 'react';
 import {filter, pipe, size, toString} from 'lodash/fp';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import Title from '../../atom/title';
 import Tag from '../../atom/tag';
 import ButtonLink from '../../atom/button-link';
 import SearchForm from '../../molecule/search-form';
 import CmCheckboxWithText from '../../molecule/cm-checkbox-with-text';
+import {GetTranslateFromContext} from '../../atom/provider';
+import {WebContextValues} from '../../atom/provider/web-context';
 import style from './style.css';
 import propTypes, {FilterCheckboxAndSearchProps} from './props-types';
 
 const buttonStyle = {fontWeight: 'normal', position: 'absolute', right: 0};
-
-const FilterCkeckboxAndSearch = (props: FilterCheckboxAndSearchProps) => {
+// const INITIAL_VISIBLE_OPTIONS = 5;
+// const toggleLabel = NoOptions.length > 5 ? 'Show more' : 'Show less';
+const FilterCkeckboxAndSearch = (
+  props: FilterCheckboxAndSearchProps,
+  context: WebContextValues
+) => {
   const {title, placeholder, withSearch, onClearFilters, options} = props;
   const [searchValue, setSearchValue] = useState('');
   const onSearchChange = useCallback(value => {
     setSearchValue(value);
   }, []);
+  const [showMore, setShowMore] = useState(false);
   const selectedFiltersCount = pipe(filter({selected: true}), size)(options);
   const hasSelectedFilters = selectedFiltersCount > 0;
+  const translate = GetTranslateFromContext(context);
+  const handleShowMore = useCallback(() => {
+    setShowMore(!showMore);
+  }, [showMore]);
+
   return (
     <div className={style.container} data-testid="filter-checkbox-and-search-container">
       <div className={style.header} data-testid="header">
@@ -64,6 +77,12 @@ const FilterCkeckboxAndSearch = (props: FilterCheckboxAndSearchProps) => {
             <p className={style.optionCount}>{option.count}</p>
           </div>
         ))}
+      </div>
+      <div>
+        <div onClick={handleShowMore}>
+          {translate(showMore ? 'Show less' : 'Show more')}
+          <FontAwesomeIcon icon={showMore ? 'chevron-up' : 'chevron-down'} />
+        </div>
       </div>
     </div>
   );
