90b3771c0383eb702b75002b045baf168bfe62f9
youssefezzahi96 <youssef.ezzahi@coorpacademy.com>
2025-03-07 19:08:41 +0100

    create custom skill content listitem

---
+++ packages/@coorpacademy-components/src/organism/list-item/test/fixtures/custom-skill-content.js
@@ -0,0 +1,43 @@
+export default {
+  props: {
+    id: 'default',
+    title: 'Ensuring team cohesion and well-being',
+    subtitle: 'Coorpacademy',
+    image:
+      'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/partner-wedemain/fr/medias/img/cover/shutterstock_248741149-1470302136299.jpg&h=500&w=500&q=90',
+    tags: [
+      {
+        label: 'course',
+        type: 'default',
+        icon: {
+          iconName: 'book-open',
+          preset: 's',
+          customStyle: {
+            padding: 0
+          },
+          position: 'left'
+        }
+      }
+    ],
+    buttonLink: {
+      'aria-label': 'Delete',
+      type: 'primary',
+      customStyle: {
+        width: 'fit-content',
+        backgroundColor: 'transparent'
+      },
+      hoverBackgroundColor: '#EAEAEB',
+      icon: {
+        position: 'left',
+        faIcon: {
+          name: 'trash',
+          color: '#515161',
+          size: 16
+        }
+      },
+      onClick: () => console.log('click')
+    },
+    selected: false,
+    checkable: true
+  }
+};
