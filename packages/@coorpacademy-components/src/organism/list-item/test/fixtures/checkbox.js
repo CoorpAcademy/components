70b3601ad7ac2d6c5476c1c837ce03784dcb9816
Emeline Leduc <emeline.leduc@Mac.lan>
2025-03-10 17:54:44 +0100

    clean code

--- packages/@coorpacademy-components/src/organism/list-item/test/fixtures/checkbox.js
+++ packages/@coorpacademy-components/src/organism/list-item/test/fixtures/checkbox.js
@@ -7,7 +7,7 @@ export default {
       'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/partner-wedemain/fr/medias/img/cover/shutterstock_248741149-1470302136299.jpg&h=500&w=500&q=90',
     tags: [
       {
-        label: 'course',
+        label: 'Course',
         type: 'default',
         icon: {
           iconName: 'book-open',
