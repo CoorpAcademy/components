4054bad96dd8aff9ef6e589684975ca21d77b6dc
adamska27 <adams.roy27@gmail.com>
2025-03-14 17:27:14 +0100

    cm-popin: update component to support new design

---
+++ packages/@coorpacademy-components/src/molecule/cm-popin/test/fixtures/dangerous-csutom-skill-delete.js
@@ -0,0 +1,37 @@
+export default {
+  props: {
+    content: 'Delete skill?',
+    descriptionText:
+      'Once deleted, this skill cannot be recovered. Are you sure you want to proceed?',
+    icon: {
+      iconName: 'triangle-exclamation',
+      iconColor: '#A30000',
+      backgroundColor: '#FFD1D1',
+      preset: 'xl'
+    },
+    firstButton: {
+      label: 'Cancel',
+      type: 'secondary',
+      'aria-label': 'Cancel this operation',
+      handleOnClick: () => {}
+    },
+    secondButton: {
+      label: 'Delete skill',
+      type: 'dangerous',
+      'aria-label': 'Undo this operation',
+      handleOnClick: () => {},
+      icon: {
+        position: 'left',
+        faIcon: {
+          name: 'trash',
+          color: 'white',
+          size: 14,
+          customStyle: {
+            padding: 0,
+            alignItems: 'baseline'
+          }
+        }
+      }
+    }
+  }
+};
