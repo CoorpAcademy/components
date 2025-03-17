4fbad52fe1c4ca7e03a88c8318fd2e6b6e7fc702
adamska27 <adams.roy27@gmail.com>
2025-03-17 16:43:39 +0100

    add fixtures

---
+++ packages/@coorpacademy-components/src/molecule/cm-popin/test/fixtures/custom-skill-archive.js
@@ -0,0 +1,37 @@
+export default {
+  props: {
+    content: 'Archive skill?',
+    descriptionText:
+      'The skill will be hidden from the catalog.You can always re-publish it afterwards.',
+    icon: {
+      iconName: 'circle-exclamation',
+      iconColor: '#A38300',
+      backgroundColor: '#FFF9D1',
+      preset: 'xl'
+    },
+    firstButton: {
+      label: 'Cancel',
+      type: 'secondary',
+      'aria-label': 'Cancel this operation',
+      handleOnClick: () => {}
+    },
+    secondButton: {
+      label: 'Confirm',
+      type: 'primary',
+      'aria-label': 'Undo this operation',
+      handleOnClick: () => {},
+      icon: {
+        position: 'left',
+        faIcon: {
+          name: 'circle-check',
+          color: 'white',
+          size: 14,
+          customStyle: {
+            padding: 0,
+            alignItems: 'baseline'
+          }
+        }
+      }
+    }
+  }
+};
