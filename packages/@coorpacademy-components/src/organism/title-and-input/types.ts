c8e99de7b4d56ab1ace27ab7cd35caf6f70df21d
EmelineLeduc <leduc.emeline44@gmail.com>
2025-03-28 09:34:29 +0100

    add IconEditor in TitleAndInput component

--- packages/@coorpacademy-components/src/organism/title-and-input/types.ts
+++ packages/@coorpacademy-components/src/organism/title-and-input/types.ts
@@ -7,7 +7,7 @@ import {SelectOpponentsProps} from '../select-opponents/types';
 import InputText from '../../atom/input-text';
 import InputTextarea from '../../atom/input-textarea';
 import Select from '../../atom/select';
-import {TitleProps} from '../../atom/title/types';
+import IconEditor from '../icon-editor';
 
 const validInputSize = ['default', 'medium', 'large'];
 
@@ -15,6 +15,7 @@ const propTypes = {
   title: PropTypes.shape(Title.propTypes).isRequired,
   field: PropTypes.oneOfType([
     PropTypes.shape(Autocomplete.propTypes),
+    PropTypes.shape(IconEditor.propTypes),
     PropTypes.shape({
       ...InputText.propTypes,
       size: PropTypes.oneOf(validInputSize)
@@ -29,6 +30,7 @@ const propTypes = {
   ]),
   childType: PropTypes.oneOf([
     'autoComplete',
+    'iconEditor',
     'inputText',
     'inputTextArea',
     'select',
@@ -40,15 +42,15 @@ const propTypes = {
 export type ValidInputSize = 'default' | 'medium' | 'large';
 
 export type TitleAndInputProps = {
-  title: TitleProps;
+  title: React.ComponentProps<typeof Title>;
   field:
     | React.ComponentProps<typeof Autocomplete>
+    | React.ComponentProps<typeof IconEditor>
     | (React.ComponentProps<typeof InputText> & {size?: ValidInputSize})
     | (React.ComponentProps<typeof InputTextarea> & {size?: ValidInputSize})
     | (React.ComponentProps<typeof Select> & {size?: ValidInputSize})
     | React.ComponentProps<typeof SelectMultiple>
     | SelectOpponentsProps;
-
   childType: string;
 };
 
