426cb71e640ac12e473da3367b5a1ba77cdc2a1c
EmelineLeduc <leduc.emeline44@gmail.com>
2025-04-24 13:57:56 +0200

    update HeaderWithActions component

--- packages/@coorpacademy-components/src/organism/header-with-actions/test/fixtures/default.ts
+++ packages/@coorpacademy-components/src/organism/header-with-actions/test/fixtures/default.ts
@@ -1,12 +1,9 @@
-import {HeaderWithActionsPropsFixture} from '../../types';
+import {ButtonActionProps, HeaderWithActionsPropsFixture} from '../../types';
 import {COLORS} from '../../../../variables/colors';
 
 const defaultFixture: HeaderWithActionsPropsFixture = {
   props: {
     closeButton: {
-      size: 'default',
-      icon: 'close',
-      'data-name': 'close-button',
       'aria-label': 'Close',
       onClick: () => {
         console.log('Close');
@@ -25,45 +22,29 @@ const defaultFixture: HeaderWithActionsPropsFixture = {
     bulletPointMenuButton: {
       buttons: [
         {
-          'data-name': 'discard-button',
+          dataName: 'discard-button',
           label: 'Discard changes',
-          buttonLinkType: 'tertiary',
-          icon: {
-            position: 'left',
-            faIcon: {
-              name: 'circle-xmark',
-              color: '#515161',
-              size: 14,
-              customStyle: {padding: 0}
-            }
-          },
-          onClick: () => console.log('click on Discard changes'),
-          type: 'defaultLeft'
+          iconName: 'circle-xmark',
+          iconColor: '#515161',
+          onClick: () => console.log('click on Discard changes')
         },
         {
-          'data-name': 'archive-button',
+          dataName: 'archive-button',
           label: 'Archive',
-          buttonLinkType: 'tertiary',
-          icon: {
-            position: 'left',
-            faIcon: {name: 'folder-open', color: '#515161', size: 14, customStyle: {padding: 0}}
-          },
-          onClick: () => console.log('click on Archive'),
-          type: 'defaultLeft'
+          iconName: 'folder-open',
+          iconColor: '#515161',
+          onClick: () => console.log('click on Archive')
         },
         {
-          'data-name': 'delete-button',
+          dataName: 'delete-button',
           label: 'Delete',
-          buttonLinkType: 'tertiary',
-          icon: {
-            position: 'left',
-            faIcon: {name: 'trash', color: '#B81400', size: 14, customStyle: {padding: 0}}
-          },
-          onClick: () => console.log('click on Delete'),
-          type: 'dangerousLeft'
+          iconName: 'trash',
+          iconColor: '#B81400',
+          onClick: () => console.log('click on Delete')
         }
       ],
-      onClick: () => console.log('click on bullet point menu')
+      onClick: () => console.log('click on bullet point menu'),
+      buttonAriaLabel: 'More actions'
     },
     actionButtons: [
       {
@@ -71,39 +52,17 @@ const defaultFixture: HeaderWithActionsPropsFixture = {
         type: 'secondary',
         onClick: () => console.log('click Save changes'),
         disabled: true,
-        icon: {
-          position: 'left',
-          faIcon: {
-            name: 'floppy-disk',
-            color: COLORS.cm_grey_700,
-            size: 14,
-            customStyle: {padding: 0}
-          }
-        },
-        customStyle: {
-          fontWeight: '600',
-          borderRadius: '12px'
-        }
+        iconName: 'floppy-disk',
+        iconColor: COLORS.cm_grey_700
       },
       {
         label: 'Publish changes',
         type: 'primary',
         onClick: () => console.log('click Publish changes'),
-        icon: {
-          position: 'left',
-          faIcon: {
-            name: 'circle-check',
-            color: COLORS.white,
-            size: 14,
-            customStyle: {padding: 0}
-          }
-        },
-        customStyle: {
-          fontWeight: '600',
-          borderRadius: '12px'
-        }
+        iconName: 'paper-plane',
+        iconColor: COLORS.white
       }
-    ]
+    ] as ButtonActionProps[]
   }
 };
 
