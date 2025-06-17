f65fd169083639ba2471441114a05064ba93de43
Djamel <djamel.soualmi@go1.com>
2025-06-17 15:46:21 +0100

    clean up

--- packages/@coorpacademy-components/src/util/test/check-is-mobile.js
+++ packages/@coorpacademy-components/src/util/test/check-is-mobile.js
@@ -66,8 +66,3 @@ test('should return true for devices with msMaxTouchPoints and mobile user agent
     'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1';
   t.truthy(isMobile(navigator.userAgent));
 });
-
-test('should return false for touch devices without mobile user agent', t => {
-  global.window.ontouchstart = true;
-  t.falsy(isMobile(navigator.userAgent));
-});
