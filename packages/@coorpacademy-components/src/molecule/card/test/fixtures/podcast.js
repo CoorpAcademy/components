7b64d626fb853641eace2295f13ec61e33e2c777
antonia.balluais <antonia.balluais@coorpacademy.com>
2025-06-19 18:26:16 +0200

    CLXP-290 update fixtures

--- packages/@coorpacademy-components/src/molecule/card/test/fixtures/podcast.js
+++ packages/@coorpacademy-components/src/molecule/card/test/fixtures/podcast.js
@@ -9,6 +9,10 @@ export default {
     type: 'podcast',
     image:
       'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/partner-wedemain/fr/medias/img/cover/shutterstock_248741149-1470302136299.jpg&h=280&w=280&q=80&m=crop',
-    progress: 0.1
+    progress: 0.1,
+    notification: {
+      message: 'Removed from my list',
+      icon: 'removeFavorite'
+    }
   })
 };
