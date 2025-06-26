4f1e4879370c42be64667cb1ce26cadfc4bb707e
antonia.balluais <antonia.balluais@coorpacademy.com>
2025-06-26 15:45:26 +0200

    CLXP-287 default style

--- packages/@coorpacademy-components/src/molecule/quick-filters/index.tsx
+++ packages/@coorpacademy-components/src/molecule/quick-filters/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import FaIcon from '../../atom/icon';
 import style from './style.css';
+import {QuickFiltersProps} from './types';
 
-const QuickFilters = () => {
-  return <div className={style.filtersContainer}>QuickFilters</div>;
+const QuickFilters = ({filterOptions}: QuickFiltersProps) => {
+  // eslint-disable-next-line no-console
+  console.log('QuickFilters component rendered with options:', filterOptions);
+  return (
+    <div className={style.filtersContainer}>
+      {filterOptions.map((filterOption, idx) => {
+        const {iconName, label} = filterOption;
+        return (
+          <div key={idx} className={style.filterOption}>
+            <FaIcon
+              {...{
+                iconName
+              }}
+            />
+            <span>{label}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default QuickFilters;
