33c1226e5c538016744ce369240a544014693193
antonia.balluais <antonia.balluais@coorpacademy.com>
2025-06-26 13:57:47 +0200

    CLXP-290 boilerplate

---
+++ packages/@coorpacademy-components/src/molecule/quick-filters/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './style.css';
+
+const QuickFilters = () => {
+  return <div className={style.filtersContainer}>QuickFilters</div>;
+};
+
+export default QuickFilters;
