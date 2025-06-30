7e045e088ab0edd48d1fb70e52f0746ab7fc2b9a
antonia.balluais <antonia.balluais@coorpacademy.com>
2025-06-30 12:44:21 +0200

    CLXP-287 selected style

--- packages/@coorpacademy-components/src/molecule/quick-filters/index.tsx
+++ packages/@coorpacademy-components/src/molecule/quick-filters/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import classNames from 'classnames';
 import FaIcon from '../../atom/icon';
+import {COLORS} from '../../variables/colors';
 import style from './style.css';
 import {QuickFiltersProps} from './types';
 
@@ -7,26 +9,31 @@ const QuickFilters = ({primaryOption, filterOptions}: QuickFiltersProps) => {
   const {defaultLabel, defaultIconName, defaultSelected, onDefaultClick} = primaryOption;
   return (
     <div className={style.filtersMainContainer}>
-      <div data-name="all-content" className={style.onDefaultOption}>
+      <div
+        data-name="all-content"
+        className={classNames(style.defaultOption, defaultSelected && style.filterSelected)}
+      >
         <FaIcon
           {...{
             iconName: defaultIconName,
             label: defaultLabel,
             selected: defaultSelected,
-            onClick: onDefaultClick
+            onClick: onDefaultClick,
+            iconColor: defaultSelected ? COLORS.cm_grey_700 : COLORS.cm_grey_500
           }}
         />
-        <span>{defaultLabel}</span>
+        <span className={style.filterLabel}>{defaultLabel}</span>
       </div>
       <div className={style.filterSeparator} />
       <div className={style.filtersContainer}>
         {filterOptions.map((filterOption, idx) => {
-          const {iconName, label} = filterOption;
+          const {iconName, label, selected} = filterOption;
           return (
             <div key={idx} className={style.filterOption}>
               <FaIcon
                 {...{
-                  iconName
+                  iconName,
+                  iconColor: selected ? COLORS.cm_grey_700 : COLORS.cm_grey_500
                 }}
               />
               <span>{label}</span>
