d429f0984866aaae356f9dc7db984b99ae95c8da
antonia.balluais <antonia.balluais@coorpacademy.com>
2025-06-30 12:01:04 +0200

    CLXP-287 fix separator style

--- packages/@coorpacademy-components/src/molecule/quick-filters/index.tsx
+++ packages/@coorpacademy-components/src/molecule/quick-filters/index.tsx
@@ -3,24 +3,37 @@ import FaIcon from '../../atom/icon';
 import style from './style.css';
 import {QuickFiltersProps} from './types';
 
-const QuickFilters = ({filterOptions}: QuickFiltersProps) => {
-  // eslint-disable-next-line no-console
-  console.log('QuickFilters component rendered with options:', filterOptions);
+const QuickFilters = ({primaryOption, filterOptions}: QuickFiltersProps) => {
+  const {defaultLabel, defaultIconName, defaultSelected, onDefaultClick} = primaryOption;
   return (
-    <div className={style.filtersContainer}>
-      {filterOptions.map((filterOption, idx) => {
-        const {iconName, label} = filterOption;
-        return (
-          <div key={idx} className={style.filterOption}>
-            <FaIcon
-              {...{
-                iconName
-              }}
-            />
-            <span>{label}</span>
-          </div>
-        );
-      })}
+    <div className={style.filtersMainContainer}>
+      <div data-name="all-content" className={style.onDefaultOption}>
+        <FaIcon
+          {...{
+            iconName: defaultIconName,
+            label: defaultLabel,
+            selected: defaultSelected,
+            onClick: onDefaultClick
+          }}
+        />
+        <span>{defaultLabel}</span>
+      </div>
+      <div className={style.filterSeparator} />
+      <div className={style.filtersContainer}>
+        {filterOptions.map((filterOption, idx) => {
+          const {iconName, label} = filterOption;
+          return (
+            <div key={idx} className={style.filterOption}>
+              <FaIcon
+                {...{
+                  iconName
+                }}
+              />
+              <span>{label}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- packages/@coorpacademy-components/src/molecule/quick-filters/types.ts
+++ packages/@coorpacademy-components/src/molecule/quick-filters/types.ts
@@ -1,6 +1,13 @@
 export type QuickFiltersProps = {
+  // all content props
+  primaryOption: {
+    defaultLabel: string;
+    defaultIconName: string;
+    defaultSelected: boolean;
+    onDefaultClick: () => void;
+  };
   filterOptions: FilterOption[];
-  filterButton?: string; // to do next : filter button with onClick handler
+  filterButton?: string; // to do next : filter button with onClick handler‡
 };
 
 type FilterOption = {
