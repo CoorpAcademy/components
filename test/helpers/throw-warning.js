b299e8b8744f19235744a543d859ac0f1bd454a3
silou <silouane.galinou@go1.com>
2025-07-03 11:35:58 +0200

    fix: ignore browserslist warning in tests
    - Added browserslist caniuse-lite warning to ignored warnings list
    - Prevents test failures due to outdated browserslist data
    - Tests now check for warnings before throwing errors

--- test/helpers/throw-warning.js
+++ test/helpers/throw-warning.js
@@ -1,9 +1,14 @@
 const ignored = [
-  'Warning: React depends on requestAnimationFrame. Make sure that you load a polyfill in older browsers. http://fb.me/react-polyfills'
+  'Warning: React depends on requestAnimationFrame. Make sure that you load a polyfill in older browsers. http://fb.me/react-polyfills',
+  'Browserslist: browsers data (caniuse-lite) is 6 months old'
 ];
 
 // eslint-disable-next-line no-console
 console.warn = (...args) => {
+  const warningMessage = args[0];
+  if (ignored.some(ignoredWarning => warningMessage && warningMessage.includes(ignoredWarning))) {
+    return;
+  }
   throw new Error(...args);
 };
 // eslint-disable-next-line no-console
